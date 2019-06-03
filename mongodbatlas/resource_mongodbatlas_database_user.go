e0d317bc63d748e69007e64677ab58c0fd534f8d
Marin Salinas <marinssalinas@gmail.com>
2019-06-03 15:34:02 -0500

    feat: add database_user resource create function

--- mongodbatlas/resource_mongodbatlas_database_user.go
+++ mongodbatlas/resource_mongodbatlas_database_user.go
@@ -1,7 +1,12 @@
 package mongodbatlas
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/helper/validation"
+	matlas "github.com/mongodb-partners/go-client-mongodbatlas/mongodbatlas"
 )
 
 func resourceMongoDBAtlasDatabaseUser() *schema.Resource {
@@ -31,9 +36,10 @@ func resourceMongoDBAtlasDatabaseUser() *schema.Resource {
 				Sensitive: true,
 			},
 			"delete_after_date": {
-				Type:     schema.TypeString,
-				Optional: true,
-				Computed: true,
+				Type:         schema.TypeString,
+				Optional:     true,
+				Computed:     true,
+				ValidateFunc: validation.ValidateRFC3339TimeString,
 			},
 			"roles": {
 				Type:     schema.TypeList,
@@ -68,7 +74,34 @@ func resourceMongoDBAtlasDatabaseUserRead(d *schema.ResourceData, meta interface
 }
 
 func resourceMongoDBAtlasDatabaseUserCreate(d *schema.ResourceData, meta interface{}) error {
-	return nil
+	//Get client connection.
+	conn := meta.(*matlas.Client)
+	groupID := d.Get("group_id").(string)
+
+	dbUserReq := &matlas.DatabaseUser{
+		Roles:        expandRoles(d),
+		GroupID:      groupID,
+		Username:     d.Get("username").(string),
+		DatabaseName: d.Get("database_name").(string),
+	}
+
+	if v, ok := d.GetOk("password"); ok {
+		dbUserReq.Password = v.(string)
+	}
+
+	if v, ok := d.GetOk("delete_after_date"); ok {
+		dbUserReq.DeleteAfterDate = v.(string)
+	}
+
+	dbUserRes, _, err := conn.DatabaseUsers.Create(context.Background(), groupID, dbUserReq)
+
+	if err != nil {
+		return fmt.Errorf("error creating database user: %s", err)
+	}
+
+	d.SetId(dbUserRes.Username)
+
+	return resourceMongoDBAtlasDatabaseUserRead(d, meta)
 }
 
 func resourceMongoDBAtlasDatabaseUserUpdate(d *schema.ResourceData, meta interface{}) error {
@@ -77,4 +110,22 @@ func resourceMongoDBAtlasDatabaseUserUpdate(d *schema.ResourceData, meta interfa
 
 func resourceMongoDBAtlasDatabaseUserDelete(d *schema.ResourceData, meta interface{}) error {
 	return nil
+}
+
+func expandRoles(d *schema.ResourceData) []matlas.Role {
+	var roles []matlas.Role
+	if v, ok := d.GetOk("roles"); ok {
+		if rs := v.([]interface{}); len(rs) > 0 {
+			roles = make([]matlas.Role, len(rs))
+			for k, r := range rs {
+				roleMap := r.(map[string]interface{})
+				roles[k] = matlas.Role{
+					RoleName:       roleMap["role_name"].(string),
+					DatabaseName:   roleMap["database_name"].(string),
+					CollectionName: roleMap["collection_name"].(string),
+				}
+			}
+		}
+	}
+	return roles
 }
