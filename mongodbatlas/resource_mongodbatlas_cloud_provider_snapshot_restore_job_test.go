dd3a854c2c34dc817588ba500303e5a272b36493
Paco Preciado Mendoza <francisco.preciado@digitalonus.com>
2019-07-09 12:27:02 -0500

    fix: change mongo path

--- mongodbatlas/resource_mongodbatlas_cloud_provider_snapshot_restore_job_test.go
+++ mongodbatlas/resource_mongodbatlas_cloud_provider_snapshot_restore_job_test.go
@@ -8,7 +8,7 @@ import (
 
 	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/terraform"
-	matlas "github.com/mongodb-partners/go-client-mongodb-atlas/mongodbatlas"
+	matlas "github.com/mongodb/go-client-mongodb-atlas/mongodbatlas"
 )
 
 func TestAccResourceMongoDBAtlasCloudProviderSnapshotRestoreJob_basic(t *testing.T) {
