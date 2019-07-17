7d97eace79eb37505185944fff6d01c17024c255
Paco Preciado Mendoza <francisco.preciado@digitalonus.com>
2019-07-17 12:15:08 -0500

    chore: add env variables to the datasources and the clouster resource wass added to the test

--- mongodbatlas/data_source_mongodbatlas_cloud_provider_snapshot_restore_job_test.go
+++ mongodbatlas/data_source_mongodbatlas_cloud_provider_snapshot_restore_job_test.go
@@ -2,24 +2,21 @@ package mongodbatlas
 
 import (
 	"fmt"
+	"os"
 	"testing"
 
+	"github.com/hashicorp/terraform/helper/acctest"
 	"github.com/hashicorp/terraform/helper/resource"
 	matlas "github.com/mongodb/go-client-mongodb-atlas/mongodbatlas"
 )
 
 func TestAccDataSourceMongoDBAtlasCloudProviderSnapshotRestoreJob_basic(t *testing.T) {
-	var cloudProviderSnapshotRestoreJob matlas.CloudProviderSnapshotRestoreJob
 	var cloudProviderSnapshot matlas.CloudProviderSnapshot
 
-	resourceName := "data.mongodbatlas_cloud_provider_snapshot_restore_job.test"
-
-	projectID := "5cf5a45a9ccf6400e60981b6"
-	clusterName := "MyCluster"
-	description := "myDescription"
+	projectID := os.Getenv("MONGODB_ATLAS_PROJECT_ID")
+	clusterName := fmt.Sprintf("test-acc-%s", acctest.RandString(10))
+	description := fmt.Sprintf("My description in %s", clusterName)
 	retentionInDays := "1"
-	// targetClusterName := "MyCluster"
-	// targetGroupID := "5cf5a45a9ccf6400e60981b6"
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:  func() { testAccPreCheck(t) },
@@ -35,48 +32,47 @@ func TestAccDataSourceMongoDBAtlasCloudProviderSnapshotRestoreJob_basic(t *testi
 					resource.TestCheckResourceAttrSet("mongodbatlas_cloud_provider_snapshot.test", "retention_in_days"),
 				),
 			},
-			{
-				Config: testAccMongoDBAtlasCloudProviderSnapshotRestoreJobConfigWithDS(projectID, clusterName, description, retentionInDays),
-				Check: resource.ComposeTestCheckFunc(
-					testAccCheckMongoDBAtlasCloudProviderSnapshotRestoreJobExists(resourceName, &cloudProviderSnapshotRestoreJob),
-					resource.TestCheckResourceAttrSet(resourceName, "snapshot_id"),
-					resource.TestCheckResourceAttrSet(resourceName, "project_id"),
-					resource.TestCheckResourceAttrSet(resourceName, "cluster_name"),
-				),
-			},
 		},
 	})
 }
 
 func testAccMongoDBAtlasDataSourceCloudProviderSnapshotRestoreJobConfig(projectID, clusterName, description, retentionInDays string) string {
 	return fmt.Sprintf(`
+		resource "mongodbatlas_cluster" "my_cluster" {
+			project_id   = "%s"
+			name         = "%s"
+			disk_size_gb = 5
+			
+			//Provider Settings "block"
+			provider_name               = "AWS"
+			provider_region_name        = "US_EAST_1"
+			provider_instance_size_name = "M10"
+			provider_backup_enabled     = true //enable cloud provider snapshots
+			provider_disk_iops          = 100
+			provider_encrypt_ebs_volume = false
+		}
+
 		resource "mongodbatlas_cloud_provider_snapshot" "test" {
-			project_id          = "%s"
-			cluster_name      = "%s"
+			project_id        = mongodbatlas_cluster.my_cluster.project_id
+			cluster_name      = mongodbatlas_cluster.my_cluster.name
 			description       = "%s"
 			retention_in_days = %s
 		}
 		
 		resource "mongodbatlas_cloud_provider_snapshot_restore_job" "test" {
-			project_id      = "${mongodbatlas_cloud_provider_snapshot.test.project_id}"
-			cluster_name  = "${mongodbatlas_cloud_provider_snapshot.test.cluster_name}"
-			snapshot_id   = "${mongodbatlas_cloud_provider_snapshot.test.id}"
+			project_id    = mongodbatlas_cloud_provider_snapshot.test.project_id
+			cluster_name  = mongodbatlas_cloud_provider_snapshot.test.cluster_name
+			snapshot_id   = mongodbatlas_cloud_provider_snapshot.test.id
 			delivery_type = {
 				download = true
 			}
 			depends_on = ["mongodbatlas_cloud_provider_snapshot.test"]
 		}
-	`, projectID, clusterName, description, retentionInDays)
-}
 
-func testAccMongoDBAtlasCloudProviderSnapshotRestoreJobConfigWithDS(projectID, clusterName, description, retentionInDays string) string {
-	return fmt.Sprintf(`
-		%s		
-		
 		data "mongodbatlas_cloud_provider_snapshot_restore_job" "test" {
-			project_id     = "${mongodbatlas_cloud_provider_snapshot_restore_job.test.project_id}"
-			cluster_name = "${mongodbatlas_cloud_provider_snapshot_restore_job.test.cluster_name}"
-			job_id       = "${mongodbatlas_cloud_provider_snapshot_restore_job.test.id}"
+			project_id   = mongodbatlas_cloud_provider_snapshot_restore_job.test.project_id
+			cluster_name = mongodbatlas_cloud_provider_snapshot_restore_job.test.cluster_name
+			job_id       = mongodbatlas_cloud_provider_snapshot_restore_job.test.id
 		}
-		`, testAccMongoDBAtlasDataSourceCloudProviderSnapshotRestoreJobConfig(projectID, clusterName, description, retentionInDays))
+	`, projectID, clusterName, description, retentionInDays)
 }
