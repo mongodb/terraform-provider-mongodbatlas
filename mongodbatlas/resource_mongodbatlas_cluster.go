49123dde38d94da0905c97b8f393b48b21b95c1c
PacoDw <__pm@outlook.com>
2019-11-25 10:25:12 -0600

    chore: Added default Disk Size when it doesn't set up

--- mongodbatlas/resource_mongodbatlas_cluster.go
+++ mongodbatlas/resource_mongodbatlas_cluster.go
@@ -294,6 +294,7 @@ func resourceMongoDBAtlasClusterCreate(d *schema.ResourceData, meta interface{})
 	//Get client connection.
 	conn := meta.(*matlas.Client)
 	projectID := d.Get("project_id").(string)
+	providerName := d.Get("provider_name").(string)
 
 	//validate cluster_type conditional
 	if _, ok := d.GetOk("replication_specs"); ok {
@@ -306,7 +307,7 @@ func resourceMongoDBAtlasClusterCreate(d *schema.ResourceData, meta interface{})
 		}
 	}
 
-	if d.Get("provider_name") != "AWS" {
+	if providerName != "AWS" {
 		if _, ok := d.GetOk("provider_disk_iops"); ok {
 			return fmt.Errorf("`provider_disk_iops` shouldn't be set when provider name is `GCP` or `AZURE`")
 		}
@@ -318,13 +319,13 @@ func resourceMongoDBAtlasClusterCreate(d *schema.ResourceData, meta interface{})
 		}
 	}
 
-	if d.Get("provider_name") != "AZURE" {
+	if providerName != "AZURE" {
 		if _, ok := d.GetOk("provider_disk_type_name"); ok {
 			return fmt.Errorf("`provider_disk_type_name` shouldn't be set when provider name is `GCP` or `AWS`")
 		}
 	}
 
-	if d.Get("provider_name") == "AZURE" {
+	if providerName == "AZURE" {
 		if _, ok := d.GetOk("disk_size_gb"); ok {
 			return fmt.Errorf("`disk_size_gb` cannot be used with Azure clusters")
 		}
@@ -355,12 +356,19 @@ func resourceMongoDBAtlasClusterCreate(d *schema.ResourceData, meta interface{})
 		DiskGBEnabled: pointy.Bool(d.Get("auto_scaling_disk_gb_enabled").(bool)),
 	}
 
+	// set the default disk size gb for the provider and tier
+	diskSizeGB := matlas.DefaultDiskSizeGB[providerName][d.Get("provider_instance_size_name").(string)]
+	// But if it's specify the disk size will set up
+	if v, ok := d.GetOk("disk_size_gb"); ok {
+		diskSizeGB = v.(float64)
+	}
+
 	clusterRequest := &matlas.Cluster{
 		Name:                     d.Get("name").(string),
 		EncryptionAtRestProvider: d.Get("encryption_at_rest_provider").(string),
 		ClusterType:              cast.ToString(d.Get("cluster_type")),
 		BackupEnabled:            pointy.Bool(d.Get("backup_enabled").(bool)),
-		DiskSizeGB:               pointy.Float64(d.Get("disk_size_gb").(float64)),
+		DiskSizeGB:               &diskSizeGB,
 		ProviderBackupEnabled:    pointy.Bool(d.Get("provider_backup_enabled").(bool)),
 		AutoScaling:              autoScaling,
 		BiConnector:              biConnector,
@@ -722,7 +730,10 @@ func expandProviderSetting(d *schema.ResourceData) matlas.ProviderSettings {
 	providerSettings := matlas.ProviderSettings{}
 
 	if d.Get("provider_name") == "AWS" {
-		providerSettings.DiskIOPS = pointy.Int64(cast.ToInt64(d.Get("provider_disk_iops")))
+		// Asks if the Provider Disk IOS was set if it didn't the server will set it up automatically
+		if v, ok := d.GetOk("provider_disk_iops"); ok {
+			providerSettings.DiskIOPS = pointy.Int64(cast.ToInt64(v))
+		}
 		providerSettings.EncryptEBSVolume = pointy.Bool(true)
 		if encryptEBSVolume, ok := d.GetOkExists("provider_encrypt_ebs_volume"); ok {
 			providerSettings.EncryptEBSVolume = pointy.Bool(cast.ToBool(encryptEBSVolume))
