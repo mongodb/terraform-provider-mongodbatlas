0f1a00b90757c24ebd18af4b7a0f0019c3ff00cf
PacoDw <__pm@outlook.com>
2019-12-31 09:40:24 -0600

    feat: added alert configuration resource and datasource inside of the provider

--- mongodbatlas/provider.go
+++ mongodbatlas/provider.go
@@ -55,6 +55,7 @@ func Provider() terraform.ResourceProvider {
 			"mongodbatlas_team":                                 dataSourceMongoDBAtlasTeam(),
 			"mongodbatlas_teams":                                dataSourceMongoDBAtlasTeam(),
 			"mongodbatlas_global_cluster_config":                dataSourceMongoDBAtlasGlobalCluster(),
+			"mongodbatlas_alert_configuration":                  dataSourceMongoDBAtlasAlertConfiguration(),
 		},
 
 		ResourcesMap: map[string]*schema.Resource{
@@ -74,6 +75,7 @@ func Provider() terraform.ResourceProvider {
 			"mongodbatlas_team":                                resourceMongoDBAtlasTeam(),
 			"mongodbatlas_teams":                               resourceMongoDBAtlasTeam(),
 			"mongodbatlas_global_cluster_config":               resourceMongoDBAtlasGlobalCluster(),
+			"mongodbatlas_alert_configuration":                 resourceMongoDBAtlasAlertConfiguration(),
 		},
 
 		ConfigureFunc: providerConfigure,
