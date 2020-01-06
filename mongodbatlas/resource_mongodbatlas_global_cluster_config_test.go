252c302403da1fbae49b789d6c29d2ce7303d0f5
PacoDw <__pm@outlook.com>
2020-01-06 13:08:55 -0600

    chore: added other test case with AWS cluster

--- mongodbatlas/resource_mongodbatlas_global_cluster_config_test.go
+++ mongodbatlas/resource_mongodbatlas_global_cluster_config_test.go
@@ -39,16 +39,63 @@ func TestAccResourceMongoDBAtlasGlobalCluster_basic(t *testing.T) {
 					testAccCheckMongoDBAtlasGlobalClusterAttributes(&globalConfig, 1),
 				),
 			},
+		},
+	})
+}
+
+func TestAccResourceMongoDBAtlasGlobalCluster_WithAWSCluster(t *testing.T) {
+	var globalConfig matlas.GlobalCluster
+
+	resourceName := "mongodbatlas_global_cluster_config.config"
+
+	projectID := os.Getenv("MONGODB_ATLAS_PROJECT_ID")
+	name := fmt.Sprintf("test-acc-global-%s", acctest.RandString(10))
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckMongoDBAtlasGlobalClusterDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccMongoDBAtlasGlobalClusterWithAWSClusterConfig(projectID, name, "true"),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckMongoDBAtlasGlobalClusterExists(resourceName, &globalConfig),
+					resource.TestCheckResourceAttrSet(resourceName, "managed_namespaces.#"),
+					resource.TestCheckResourceAttrSet(resourceName, "custom_zone_mappings.#"),
+					resource.TestCheckResourceAttrSet(resourceName, "custom_zone_mapping.%"),
+					resource.TestCheckResourceAttrSet(resourceName, "custom_zone_mapping.CA"),
+					resource.TestCheckResourceAttr(resourceName, "cluster_name", name),
+					resource.TestCheckResourceAttr(resourceName, "project_id", projectID),
+					testAccCheckMongoDBAtlasGlobalClusterAttributes(&globalConfig, 1),
+				),
+			},
+		},
+	})
+}
+
+func TestAccResourceMongoDBAtlasGlobalCluster_importBasic(t *testing.T) {
+	resourceName := "mongodbatlas_global_cluster_config.config"
+
+	projectID := os.Getenv("MONGODB_ATLAS_PROJECT_ID")
+	name := fmt.Sprintf("test-acc-global-%s", acctest.RandString(10))
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckMongoDBAtlasProjectIPWhitelistDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccMongoDBAtlasGlobalClusterConfig(projectID, name, "false"),
+			},
 			{
 				ResourceName:            resourceName,
+				ImportStateIdFunc:       testAccCheckMongoDBAtlasGlobalClusterImportStateIDFunc(resourceName),
 				ImportState:             true,
 				ImportStateVerify:       true,
-				ImportStateIdFunc:       testAccCheckMongoDBAtlasGlobalClusterImportStateIDFunc(resourceName),
 				ImportStateVerifyIgnore: []string{"custom_zone_mappings"},
 			},
 		},
 	})
-
 }
 
 func testAccCheckMongoDBAtlasGlobalClusterExists(resourceName string, globalConfig *matlas.GlobalCluster) resource.TestCheckFunc {
@@ -62,7 +109,11 @@ func testAccCheckMongoDBAtlasGlobalClusterExists(resourceName string, globalConf
 		if rs.Primary.ID == "" {
 			return fmt.Errorf("no ID is set")
 		}
-		if globalConfigResp, _, err := conn.GlobalClusters.Get(context.Background(), rs.Primary.Attributes["project_id"], rs.Primary.Attributes["cluster_name"]); err == nil {
+
+		ids := decodeStateID(rs.Primary.ID)
+
+		globalConfigResp, _, err := conn.GlobalClusters.Get(context.Background(), ids["project_id"], ids["cluster_name"])
+		if err == nil {
 			*globalConfig = *globalConfigResp
 
 			if len(globalConfig.CustomZoneMapping) > 0 || len(globalConfig.ManagedNamespaces) > 0 {
@@ -70,7 +121,7 @@ func testAccCheckMongoDBAtlasGlobalClusterExists(resourceName string, globalConf
 			}
 		}
 
-		return fmt.Errorf("global config for cluster(%s) does not exist", rs.Primary.Attributes["cluster_name"])
+		return fmt.Errorf("global config for cluster(%s) does not exist", ids["cluster_name"])
 	}
 }
 
@@ -80,7 +131,10 @@ func testAccCheckMongoDBAtlasGlobalClusterImportStateIDFunc(resourceName string)
 		if !ok {
 			return "", fmt.Errorf("Not found: %s", resourceName)
 		}
-		return fmt.Sprintf("%s-%s", rs.Primary.Attributes["project_id"], rs.Primary.Attributes["cluster_name"]), nil
+
+		ids := decodeStateID(rs.Primary.ID)
+
+		return fmt.Sprintf("%s-%s", ids["project_id"], ids["cluster_name"]), nil
 	}
 }
 
@@ -101,8 +155,10 @@ func testAccCheckMongoDBAtlasGlobalClusterDestroy(s *terraform.State) error {
 			continue
 		}
 
+		ids := decodeStateID(rs.Primary.ID)
+
 		// Try to find the cluster
-		globalConfig, _, err := conn.GlobalClusters.Get(context.Background(), rs.Primary.Attributes["project_id"], rs.Primary.Attributes["cluster_name"])
+		globalConfig, _, err := conn.GlobalClusters.Get(context.Background(), ids["project_id"], ids["cluster_name"])
 		if err != nil {
 			if strings.Contains(err.Error(), fmt.Sprintf("No cluster named %s exists in group %s", rs.Primary.Attributes["cluster_name"], rs.Primary.Attributes["project_id"])) {
 				return nil
@@ -120,57 +176,95 @@ func testAccCheckMongoDBAtlasGlobalClusterDestroy(s *terraform.State) error {
 
 func testAccMongoDBAtlasGlobalClusterConfig(projectID, name, backupEnabled string) string {
 	return fmt.Sprintf(`
-	resource "mongodbatlas_cluster" "test" {
-		project_id              = "%s"
-		name                    = "%s"
-		disk_size_gb            = 80
-		num_shards              = 1
-		backup_enabled          = %s
-		provider_backup_enabled = true
-		cluster_type            = "GEOSHARDED"
+		resource "mongodbatlas_cluster" "test" {
+			project_id              = "%s"
+			name                    = "%s"
+			disk_size_gb            = 80
+			backup_enabled          = "%s"
+			provider_backup_enabled = true
+			cluster_type            = "GEOSHARDED"
 		
-		//Provider Settings "block"
-		provider_name               = "AWS"
-		provider_disk_iops          = 240
-		provider_instance_size_name = "M30"
+			//Provider Settings "block"
+			provider_name               = "AWS"
+			provider_disk_iops          = 240
+			provider_instance_size_name = "M30"
 		
-		replication_specs {
-			zone_name  = "Zone 1"
-			num_shards = 2
-			regions_config {
-			region_name     = "EU_CENTRAL_1"
-			electable_nodes = 3
-			priority        = 7
-			read_only_nodes = 0
+			replication_specs {
+				zone_name  = "Zone 1"
+				num_shards = 1
+				regions_config {
+					region_name     = "EU_CENTRAL_1"
+					electable_nodes = 3
+					priority        = 7
+					read_only_nodes = 0
+				}
+			}
+		
+			replication_specs {
+				zone_name  = "Zone 2"
+				num_shards = 1
+				regions_config {
+					region_name     = "US_EAST_2"
+					electable_nodes = 3
+					priority        = 7
+					read_only_nodes = 0
+				}
 			}
 		}
 		
-		replication_specs { 
-			zone_name  = "Zone 2"
-			num_shards = 2
-			regions_config {
-			region_name     = "US_EAST_2"
-			electable_nodes = 3
-			priority        = 7
-			read_only_nodes = 0
+		resource "mongodbatlas_global_cluster_config" "config" {
+			project_id   = mongodbatlas_cluster.test.project_id
+			cluster_name = mongodbatlas_cluster.test.name
+		
+			managed_namespaces {
+				db               = "mydata"
+				collection       = "publishers"
+				custom_shard_key = "city"
+			}
+		
+			custom_zone_mappings {
+				location = "CA"
+				zone     = "Zone 1"
 			}
 		}
-	}
-	
-	resource "mongodbatlas_global_cluster_config" "config" {
-		project_id = mongodbatlas_cluster.test.project_id
-		cluster_name = mongodbatlas_cluster.test.name
-	
-		managed_namespaces {
-			db 				 = "mydata"
-			collection 		 = "publishers"
-			custom_shard_key = "city"
+	`, projectID, name, backupEnabled)
+}
+
+func testAccMongoDBAtlasGlobalClusterWithAWSClusterConfig(projectID, name, backupEnabled string) string {
+	return fmt.Sprintf(`
+		resource "mongodbatlas_cluster" "test" {
+			project_id   = "%s"
+			name         = "%s"
+			disk_size_gb = 100
+			num_shards   = 1
+
+			replication_factor           = 3
+			backup_enabled               = %s
+			auto_scaling_disk_gb_enabled = true
+			mongo_db_major_version       = "4.0"
+		
+			//Provider Settings "block"
+			provider_name               = "AWS"
+			provider_disk_iops 			    = 300
+			provider_encrypt_ebs_volume = false
+			provider_instance_size_name = "M30"
+			provider_region_name        = "EU_CENTRAL_1"
 		}
-	
-		custom_zone_mappings {
-			location ="CA"
-			zone =  "Zone 1"
+
+		resource "mongodbatlas_global_cluster_config" "config" {
+			project_id   = mongodbatlas_cluster.test.project_id
+			cluster_name = mongodbatlas_cluster.test.name
+		
+			managed_namespaces {
+				db               = "mydata"
+				collection       = "publishers"
+				custom_shard_key = "city"
+			}
+		
+			custom_zone_mappings {
+				location = "CA"
+				zone     = "Zone 1"
+			}
 		}
-	}
 	`, projectID, name, backupEnabled)
 }
