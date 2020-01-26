6071f7cbbef0805afd88d3a4e5651f701b5819a1
PacoDw <__pm@outlook.com>
2020-01-26 12:28:12 -0600

    chore: updated data source due to resource changes

--- mongodbatlas/data_source_mongodbatlas_team.go
+++ mongodbatlas/data_source_mongodbatlas_team.go
@@ -17,10 +17,6 @@ func dataSourceMongoDBAtlasTeam() *schema.Resource {
 				Type:     schema.TypeString,
 				Required: true,
 			},
-			"project_id": {
-				Type:     schema.TypeString,
-				Required: true,
-			},
 			"team_id": {
 				Type:     schema.TypeString,
 				Required: true,
@@ -36,13 +32,6 @@ func dataSourceMongoDBAtlasTeam() *schema.Resource {
 					Type: schema.TypeString,
 				},
 			},
-			"team_roles": {
-				Type:     schema.TypeSet,
-				Computed: true,
-				Elem: &schema.Schema{
-					Type: schema.TypeString,
-				},
-			},
 		},
 	}
 }
@@ -52,7 +41,6 @@ func dataSourceMongoDBAtlasTeamRead(d *schema.ResourceData, meta interface{}) er
 	conn := meta.(*matlas.Client)
 	orgID := d.Get("org_id").(string)
 	teamID := d.Get("team_id").(string)
-	projectID := d.Get("project_id").(string)
 
 	team, _, err := conn.Teams.Get(context.Background(), orgID, teamID)
 	if err != nil {
@@ -78,30 +66,9 @@ func dataSourceMongoDBAtlasTeamRead(d *schema.ResourceData, meta interface{}) er
 		return fmt.Errorf(errorTeamSetting, "usernames", d.Id(), err)
 	}
 
-	// Get all the teams assiged to a Project
-	teams, _, err := conn.Projects.GetProjectTeamsAssigned(context.Background(), projectID)
-	if err != nil {
-		return fmt.Errorf(errorTeamRead, err)
-	}
-
-	var teamRoles []string
-	for _, team := range teams.Results { // looking for our current team
-		// If the team exists then save the roles
-		if team.TeamID == teamID {
-			teamRoles = team.RoleNames
-			break
-		}
-	}
-
-	// Set the roles
-	if err := d.Set("team_roles", teamRoles); err != nil {
-		return fmt.Errorf(errorTeamSetting, "team_roles", teamID, err)
-	}
-
 	d.SetId(encodeStateID(map[string]string{
-		"org_id":     orgID,
-		"id":         team.ID,
-		"project_id": projectID,
+		"org_id": orgID,
+		"id":     team.ID,
 	}))
 
 	return nil
