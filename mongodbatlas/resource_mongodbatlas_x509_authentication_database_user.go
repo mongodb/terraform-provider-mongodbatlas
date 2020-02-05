99f7bb8737c0d3d42bfc695fc0896ccd7a234ee0
PacoDw <__pm@outlook.com>
2020-02-05 15:35:26 -0600

    feat: added resource/test/website, of x509 authentication

---
+++ mongodbatlas/resource_mongodbatlas_x509_authentication_database_user.go
@@ -0,0 +1,231 @@
+package mongodbatlas
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"strings"
+
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/spf13/cast"
+
+	matlas "github.com/mongodb/go-client-mongodb-atlas/mongodbatlas"
+)
+
+const (
+	errorX509AuthDBUsersCreate         = "error creating MongoDB X509 Authentication for DB User(%s) in the project(%s): %s"
+	errorX509AuthDBUsersRead           = "error reading MongoDB X509 Authentication for DB Users(%s) in the project(%s): %s"
+	errorX509AuthDBUsersSetting        = "error setting `%s` for MongoDB X509 Authentication DB User(%s): %s"
+	errorCustomerX509AuthDBUsersCreate = "error creating Customer X509 Authentication in the project(%s): %s"
+	errorCustomerX509AuthDBUsersRead   = "error reading Customer X509 Authentication in the project(%s): %s"
+	errorCustomerX509AuthDBUsersDelete = "error deleting Customer X509 Authentication in the project(%s): %s"
+)
+
+func resourceMongoDBAtlasX509AuthDBUser() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceMongoDBAtlasX509AuthDBUserCreate,
+		Read:   resourceMongoDBAtlasX509AuthDBUserRead,
+		Delete: resourceMongoDBAtlasX509AuthDBUserDelete,
+		Importer: &schema.ResourceImporter{
+			State: resourceMongoDBAtlasX509AuthDBUserImportState,
+		},
+		Schema: map[string]*schema.Schema{
+			"project_id": {
+				Type:     schema.TypeString,
+				ForceNew: true,
+				Required: true,
+			},
+			"username": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+			"months_until_expiration": {
+				Type:     schema.TypeInt,
+				Optional: true,
+				ForceNew: true,
+				ValidateFunc: func(val interface{}, key string) (warns []string, errs []error) {
+					v := val.(int)
+					if v < 1 || v > 24 {
+						errs = append(errs, fmt.Errorf("%q value should be between 1 and 24, got: %d", key, v))
+					}
+					return
+				},
+			},
+			"current_certificate": {
+				Type:      schema.TypeString,
+				Computed:  true,
+				Sensitive: true,
+			},
+			"customer_x509_cas": {
+				Type:          schema.TypeString,
+				Optional:      true,
+				ForceNew:      true,
+				Sensitive:     true,
+				ConflictsWith: []string{"months_until_expiration", "username"},
+			},
+			"certificates": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"id": {
+							Type:     schema.TypeInt,
+							Computed: true,
+						},
+						"created_at": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"group_id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"not_after": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"subject": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func resourceMongoDBAtlasX509AuthDBUserCreate(d *schema.ResourceData, meta interface{}) error {
+	conn := meta.(*matlas.Client)
+
+	projectID := d.Get("project_id").(string)
+	username := d.Get("username").(string)
+
+	var currentCertificate string
+	if expirationMonths, ok := d.GetOk("months_until_expiration"); ok {
+		res, _, err := conn.X509AuthDBUsers.CreateUserCertificate(context.Background(), projectID, username, expirationMonths.(int))
+		if err != nil {
+			return fmt.Errorf(errorX509AuthDBUsersCreate, username, projectID, err)
+		}
+		currentCertificate = res.Certificate
+	} else {
+		customerX509Cas := d.Get("customer_x509_cas").(string)
+		_, _, err := conn.X509AuthDBUsers.SaveConfiguration(context.Background(), projectID, &matlas.CustomerX509{Cas: customerX509Cas})
+		if err != nil {
+			return fmt.Errorf(errorCustomerX509AuthDBUsersCreate, projectID, err)
+		}
+	}
+
+	d.SetId(encodeStateID(map[string]string{
+		"project_id":          projectID,
+		"username":            username,
+		"current_certificate": currentCertificate,
+	}))
+
+	return resourceMongoDBAtlasX509AuthDBUserRead(d, meta)
+}
+
+func resourceMongoDBAtlasX509AuthDBUserRead(d *schema.ResourceData, meta interface{}) error {
+	conn := meta.(*matlas.Client)
+
+	ids := decodeStateID(d.Id())
+	projectID := ids["project_id"]
+	username := ids["username"]
+	currentCertificate := ids["current_certificate"]
+
+	var certificates []matlas.UserCertificate
+	var err error
+	if username != "" {
+		certificates, _, err = conn.X509AuthDBUsers.GetUserCertificates(context.Background(), projectID, username)
+		if err != nil {
+			return fmt.Errorf(errorX509AuthDBUsersRead, username, projectID, err)
+		}
+	}
+
+	if err := d.Set("current_certificate", cast.ToString(currentCertificate)); err != nil {
+		return fmt.Errorf(errorX509AuthDBUsersSetting, "current_certificate", username, err)
+	}
+	if err := d.Set("certificates", flattenCertificates(certificates)); err != nil {
+		return fmt.Errorf(errorX509AuthDBUsersSetting, "certificates", username, err)
+	}
+
+	return nil
+}
+
+func resourceMongoDBAtlasX509AuthDBUserDelete(d *schema.ResourceData, meta interface{}) error {
+	conn := meta.(*matlas.Client)
+
+	ids := decodeStateID(d.Id())
+	currentCertificate := ids["current_certificate"]
+	projectID := ids["project_id"]
+
+	if currentCertificate == "" {
+		_, err := conn.X509AuthDBUsers.DisableCustomerX509(context.Background(), projectID)
+		if err != nil {
+			return fmt.Errorf(errorCustomerX509AuthDBUsersDelete, projectID, err)
+		}
+	}
+
+	d.SetId("")
+	return nil
+}
+
+func resourceMongoDBAtlasX509AuthDBUserImportState(d *schema.ResourceData, meta interface{}) ([]*schema.ResourceData, error) {
+	conn := meta.(*matlas.Client)
+
+	parts := strings.SplitN(d.Id(), "-", 2)
+	if len(parts) < 1 && len(parts) > 2 {
+		return nil, errors.New("import format error: to import a X509 Authentication, use the formats {project_id} or {project_id}-{username}")
+	}
+
+	var username string
+	if len(parts) == 2 {
+		username = parts[1]
+	}
+	projectID := parts[0]
+
+	if username != "" {
+		_, _, err := conn.X509AuthDBUsers.GetUserCertificates(context.Background(), projectID, username)
+		if err != nil {
+			return nil, fmt.Errorf(errorX509AuthDBUsersRead, username, projectID, err)
+		}
+		if err := d.Set("username", username); err != nil {
+			return nil, fmt.Errorf(errorX509AuthDBUsersSetting, "username", username, err)
+		}
+	}
+
+	customerX509, _, err := conn.X509AuthDBUsers.GetCurrentX509Conf(context.Background(), projectID)
+	if err != nil {
+		return nil, fmt.Errorf(errorCustomerX509AuthDBUsersRead, projectID, err)
+	}
+
+	if err := d.Set("customer_x509_cas", customerX509.Cas); err != nil {
+		return nil, fmt.Errorf(errorX509AuthDBUsersSetting, "certificates", username, err)
+	}
+	if err := d.Set("project_id", projectID); err != nil {
+		return nil, fmt.Errorf(errorX509AuthDBUsersSetting, "project_id", username, err)
+	}
+
+	d.SetId(encodeStateID(map[string]string{
+		"project_id":          projectID,
+		"username":            username,
+		"current_certificate": "",
+	}))
+
+	return []*schema.ResourceData{d}, nil
+}
+
+func flattenCertificates(userCertificates []matlas.UserCertificate) []map[string]interface{} {
+	certificates := make([]map[string]interface{}, len(userCertificates))
+	for i, v := range userCertificates {
+		certificates[i] = map[string]interface{}{
+			"id":         v.ID,
+			"created_at": v.CreatedAt,
+			"group_id":   v.GroupID,
+			"not_after":  v.NotAfter,
+			"subject":    v.Subject,
+		}
+	}
+	return certificates
+}
