1a94a87f9d111b0073f09a8db6c9100161c3df9a
gmlp <gmlp.24a@gmail.com>
2020-05-18 12:45:50 -0500

    fix: fixes #210 Issues with advanced_configuration section on mongodbatlas_cluster

--- mongodbatlas/resource_mongodbatlas_cluster.go
+++ mongodbatlas/resource_mongodbatlas_cluster.go
@@ -44,6 +44,14 @@ func resourceMongoDBAtlasCluster() *schema.Resource {
 		Importer: &schema.ResourceImporter{
 			State: resourceMongoDBAtlasClusterImportState,
 		},
+		SchemaVersion: 1,
+		StateUpgraders: []schema.StateUpgrader{
+			{
+				Type:    resourceMongoDBAtlasClusterResourceV0().CoreConfigSchema().ImpliedType(),
+				Upgrade: resourceMongoDBAtlasClusterStateUpgradeV0,
+				Version: 0,
+			},
+		},
 		Schema: map[string]*schema.Schema{
 			"project_id": {
 				Type:     schema.TypeString,
@@ -287,9 +295,10 @@ func resourceMongoDBAtlasCluster() *schema.Resource {
 				Computed: true,
 			},
 			"advanced_configuration": {
-				Type:     schema.TypeMap,
+				Type:     schema.TypeList,
 				Optional: true,
 				Computed: true,
+				MaxItems: 1,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
 						"fail_index_key_too_long": {
@@ -427,7 +436,7 @@ func resourceMongoDBAtlasClusterCreate(d *schema.ResourceData, meta interface{})
 
 	// We need to validate the oplog_size_mb attr of the advanced configuration option to show the error
 	// before that the cluster is created
-	if oplogSizeMB, ok := d.GetOk("advanced_configuration.oplog_size_mb"); ok {
+	if oplogSizeMB, ok := d.GetOk("advanced_configuration.0.oplog_size_mb"); ok {
 		if cast.ToInt64(oplogSizeMB) <= 0 {
 			return fmt.Errorf("`advanced_configuration.oplog_size_mb` cannot be <= 0")
 		}
@@ -505,12 +514,15 @@ func resourceMongoDBAtlasClusterCreate(d *schema.ResourceData, meta interface{})
 		the advanced configuration option to attach it
 	*/
 	ac, ok := d.GetOk("advanced_configuration")
-	advancedConfReq := expandProcessArgs(ac.(map[string]interface{}))
-	if ok {
-		_, _, err := conn.Clusters.UpdateProcessArgs(context.Background(), projectID, cluster.Name, advancedConfReq)
-		if err != nil {
-			return fmt.Errorf(errorAdvancedConfUpdate, cluster.Name, err)
+	if aclist, ok1 := ac.([]interface{}); ok1 && len(aclist) > 0 {
+		advancedConfReq := expandProcessArgs(d, aclist[0].(map[string]interface{}))
+		if ok {
+			_, _, err := conn.Clusters.UpdateProcessArgs(context.Background(), projectID, cluster.Name, advancedConfReq)
+			if err != nil {
+				return fmt.Errorf(errorAdvancedConfUpdate, cluster.Name, err)
+			}
 		}
+
 	}
 
 	d.SetId(encodeStateID(map[string]string{
@@ -756,12 +768,14 @@ func resourceMongoDBAtlasClusterUpdate(d *schema.ResourceData, meta interface{})
 		Check if advaced configuration option has a changes to update it
 	*/
 	if d.HasChange("advanced_configuration") {
-		advancedConfReq := expandProcessArgs(d.Get("advanced_configuration").(map[string]interface{}))
-
-		if !reflect.DeepEqual(advancedConfReq, matlas.ProcessArgs{}) {
-			_, _, err := conn.Clusters.UpdateProcessArgs(context.Background(), projectID, clusterName, advancedConfReq)
-			if err != nil {
-				return fmt.Errorf(errorAdvancedConfUpdate, clusterName, err)
+		ac := d.Get("advanced_configuration")
+		if aclist, ok1 := ac.([]interface{}); ok1 && len(aclist) > 0 {
+			advancedConfReq := expandProcessArgs(d, aclist[0].(map[string]interface{}))
+			if !reflect.DeepEqual(advancedConfReq, matlas.ProcessArgs{}) {
+				_, _, err := conn.Clusters.UpdateProcessArgs(context.Background(), projectID, clusterName, advancedConfReq)
+				if err != nil {
+					return fmt.Errorf(errorAdvancedConfUpdate, clusterName, err)
+				}
 			}
 		}
 	}
@@ -1004,32 +1018,47 @@ func flattenRegionsConfig(regionsConfig map[string]matlas.RegionsConfig) []map[s
 	return regions
 }
 
-func expandProcessArgs(p map[string]interface{}) *matlas.ProcessArgs {
-	res := &matlas.ProcessArgs{
-		FailIndexKeyTooLong:              pointy.Bool(cast.ToBool(p["fail_index_key_too_long"])),
-		JavascriptEnabled:                pointy.Bool(cast.ToBool(p["javascript_enabled"])),
-		MinimumEnabledTLSProtocol:        cast.ToString(p["minimum_enabled_tls_protocol"]),
-		NoTableScan:                      pointy.Bool(cast.ToBool(p["no_table_scan"])),
-		SampleSizeBIConnector:            pointy.Int64(cast.ToInt64(p["sample_size_bi_connector"])),
-		SampleRefreshIntervalBIConnector: pointy.Int64(cast.ToInt64(p["sample_refresh_interval_bi_connector"])),
+func expandProcessArgs(d *schema.ResourceData, p map[string]interface{}) *matlas.ProcessArgs {
+	res := &matlas.ProcessArgs{}
+	if _, ok := d.GetOk("advanced_configuration.0.fail_index_key_too_long"); ok {
+		res.FailIndexKeyTooLong = pointy.Bool(cast.ToBool(p["fail_index_key_too_long"]))
 	}
-	if sizeMB := cast.ToInt64(p["oplog_size_mb"]); sizeMB != 0 {
-		res.OplogSizeMB = pointy.Int64(cast.ToInt64(p["oplog_size_mb"]))
-	} else {
-		log.Printf(errorClusterSetting, `oplog_size_mb`, "", cast.ToString(sizeMB))
+	if _, ok := d.GetOk("advanced_configuration.0.javascript_enabled"); ok {
+		res.JavascriptEnabled = pointy.Bool(cast.ToBool(p["javascript_enabled"]))
+	}
+	if _, ok := d.GetOk("advanced_configuration.0.minimum_enabled_tls_protocol"); ok {
+		res.MinimumEnabledTLSProtocol = cast.ToString(p["minimum_enabled_tls_protocol"])
+	}
+	if _, ok := d.GetOk("advanced_configuration.0.no_table_scan"); ok {
+		res.NoTableScan = pointy.Bool(cast.ToBool(p["no_table_scan"]))
+	}
+	if _, ok := d.GetOk("advanced_configuration.0.sample_size_bi_connector"); ok {
+		res.SampleSizeBIConnector = pointy.Int64(cast.ToInt64(p["sample_size_bi_connector"]))
+	}
+	if _, ok := d.GetOk("advanced_configuration.0.sample_refresh_interval_bi_connector"); ok {
+		res.SampleRefreshIntervalBIConnector = pointy.Int64(cast.ToInt64(p["sample_refresh_interval_bi_connector"]))
+	}
+	if _, ok := d.GetOk("advanced_configuration.0.oplog_size_mb"); ok {
+		if sizeMB := cast.ToInt64(p["oplog_size_mb"]); sizeMB != 0 {
+			res.OplogSizeMB = pointy.Int64(cast.ToInt64(p["oplog_size_mb"]))
+		} else {
+			log.Printf(errorClusterSetting, `oplog_size_mb`, "", cast.ToString(sizeMB))
+		}
 	}
 	return res
 }
 
-func flattenProcessArgs(p *matlas.ProcessArgs) map[string]interface{} {
-	return map[string]interface{}{
-		"fail_index_key_too_long":              cast.ToString(*p.FailIndexKeyTooLong),
-		"javascript_enabled":                   cast.ToString(*p.JavascriptEnabled),
-		"minimum_enabled_tls_protocol":         cast.ToString(p.MinimumEnabledTLSProtocol),
-		"no_table_scan":                        cast.ToString(*p.NoTableScan),
-		"oplog_size_mb":                        cast.ToString(p.OplogSizeMB),
-		"sample_size_bi_connector":             cast.ToString(p.SampleSizeBIConnector),
-		"sample_refresh_interval_bi_connector": cast.ToString(p.SampleRefreshIntervalBIConnector),
+func flattenProcessArgs(p *matlas.ProcessArgs) []interface{} {
+	return []interface{}{
+		map[string]interface{}{
+			"fail_index_key_too_long":              *p.FailIndexKeyTooLong,
+			"javascript_enabled":                   *p.JavascriptEnabled,
+			"minimum_enabled_tls_protocol":         p.MinimumEnabledTLSProtocol,
+			"no_table_scan":                        *p.NoTableScan,
+			"oplog_size_mb":                        p.OplogSizeMB,
+			"sample_size_bi_connector":             p.SampleSizeBIConnector,
+			"sample_refresh_interval_bi_connector": p.SampleRefreshIntervalBIConnector,
+		},
 	}
 }
 
---
+++ mongodbatlas/resource_mongodbatlas_cluster_migrate.go
@@ -0,0 +1,339 @@
+package mongodbatlas
+
+import (
+	"bytes"
+	"log"
+
+	"github.com/hashicorp/terraform/helper/hashcode"
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+func resourceMongoDBAtlasClusterResourceV0() *schema.Resource {
+	return &schema.Resource{
+		Schema: map[string]*schema.Schema{
+			"project_id": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"cluster_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"auto_scaling_disk_gb_enabled": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Computed: true,
+			},
+			"backup_enabled": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  false,
+			},
+			"bi_connector": {
+				Type:     schema.TypeMap,
+				Optional: true,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"enabled": {
+							Type:     schema.TypeString,
+							Optional: true,
+							Computed: true,
+						},
+						"read_preference": {
+							Type:     schema.TypeString,
+							Optional: true,
+							Computed: true,
+						},
+					},
+				},
+			},
+			"cluster_type": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"connection_strings": {
+				Type:     schema.TypeList,
+				MinItems: 1,
+				MaxItems: 1,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"standard": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"standard_srv": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"aws_private_link": {
+							Type:     schema.TypeMap,
+							Computed: true,
+						},
+						"aws_private_link_srv": {
+							Type:     schema.TypeMap,
+							Computed: true,
+						},
+						"private": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"private_srv": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+					},
+				},
+			},
+			"disk_size_gb": {
+				Type:     schema.TypeFloat,
+				Optional: true,
+				Computed: true,
+			},
+			"encryption_at_rest_provider": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"name": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"mongo_db_major_version": {
+				Type:      schema.TypeString,
+				Optional:  true,
+				Computed:  true,
+				StateFunc: formatMongoDBMajorVersion,
+			},
+			"num_shards": {
+				Type:     schema.TypeInt,
+				Optional: true,
+				Default:  1,
+			},
+			"provider_backup_enabled": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  false,
+			},
+			"provider_instance_size_name": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"provider_name": {
+				Type:     schema.TypeString,
+				ForceNew: true,
+				Required: true,
+			},
+			"pit_enabled": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Computed: true,
+			},
+			"backing_provider_name": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"provider_disk_iops": {
+				Type:     schema.TypeInt,
+				Optional: true,
+				Computed: true,
+			},
+			"provider_disk_type_name": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"provider_encrypt_ebs_volume": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Computed: true,
+			},
+			"provider_region_name": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"provider_volume_type": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"replication_factor": {
+				Type:     schema.TypeInt,
+				Optional: true,
+				Computed: true,
+			},
+			"replication_specs": {
+				Type:     schema.TypeList,
+				Optional: true,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"id": {
+							Type:     schema.TypeString,
+							Optional: true,
+							Computed: true,
+						},
+						"num_shards": {
+							Type:     schema.TypeInt,
+							Required: true,
+						},
+						"regions_config": {
+							Type:     schema.TypeSet,
+							Optional: true,
+							Computed: true,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"region_name": {
+										Type:     schema.TypeString,
+										Optional: true,
+										Computed: true,
+									},
+									"electable_nodes": {
+										Type:     schema.TypeInt,
+										Optional: true,
+										Computed: true,
+									},
+									"priority": {
+										Type:     schema.TypeInt,
+										Optional: true,
+										Computed: true,
+									},
+									"read_only_nodes": {
+										Type:     schema.TypeInt,
+										Optional: true,
+										Default:  0,
+									},
+									"analytics_nodes": {
+										Type:     schema.TypeInt,
+										Optional: true,
+										Default:  0,
+									},
+								},
+							},
+						},
+						"zone_name": {
+							Type:     schema.TypeString,
+							Optional: true,
+							Default:  "ZoneName managed by Terraform",
+						},
+					},
+				},
+			},
+			"mongo_db_version": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"mongo_uri": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"mongo_uri_updated": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"mongo_uri_with_options": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"paused": {
+				Type:     schema.TypeBool,
+				Computed: true,
+			},
+			"srv_address": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"state_name": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"advanced_configuration": {
+				Type:     schema.TypeMap,
+				Optional: true,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"fail_index_key_too_long": {
+							Type:     schema.TypeBool,
+							Optional: true,
+							Computed: true,
+						},
+						"javascript_enabled": {
+							Type:     schema.TypeBool,
+							Optional: true,
+							Computed: true,
+						},
+						"minimum_enabled_tls_protocol": {
+							Type:     schema.TypeString,
+							Optional: true,
+							Computed: true,
+						},
+						"no_table_scan": {
+							Type:     schema.TypeBool,
+							Optional: true,
+							Computed: true,
+						},
+						"oplog_size_mb": {
+							Type:     schema.TypeInt,
+							Optional: true,
+							Computed: true,
+						},
+						"sample_size_bi_connector": {
+							Type:     schema.TypeInt,
+							Optional: true,
+							Computed: true,
+						},
+						"sample_refresh_interval_bi_connector": {
+							Type:     schema.TypeInt,
+							Optional: true,
+							Computed: true,
+						},
+					},
+				},
+			},
+			"labels": {
+				Type:     schema.TypeSet,
+				Optional: true,
+				Set: func(v interface{}) int {
+					var buf bytes.Buffer
+					m := v.(map[string]interface{})
+					buf.WriteString(m["key"].(string))
+					buf.WriteString(m["value"].(string))
+					return hashcode.String(buf.String())
+				},
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"key": {
+							Type:     schema.TypeString,
+							Optional: true,
+							Computed: true,
+						},
+						"value": {
+							Type:     schema.TypeString,
+							Optional: true,
+							Computed: true,
+						},
+					},
+				},
+			},
+			"snapshot_backup_policy": computedCloudProviderSnapshotBackupPolicySchema(),
+		},
+	}
+}
+
+func resourceMongoDBAtlasClusterStateUpgradeV0(rawState map[string]interface{}, meta interface{}) (map[string]interface{}, error) {
+	log.Println("[INFO] Found MongoDB Cluser state v0; migrating to v1")
+	if v, ok := rawState["advanced_configuration"]; ok || v != nil {
+		rawState["advanced_configuration"] = []interface{}{v.(map[string]interface{})}
+	} else {
+		rawState["advanced_configuration"] = []interface{}{}
+	}
+
+	return rawState, nil
+}
