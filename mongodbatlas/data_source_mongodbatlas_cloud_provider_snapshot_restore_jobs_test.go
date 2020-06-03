597118ab81ff808dfb9ccc1811f40c8f02b24ca6
Edgar López <edgarlopez@pop-os.localdomain>
2020-06-03 10:25:55 -0500

    test: added testacc for pagination for restore jobs, snapshots and projects

--- mongodbatlas/data_source_mongodbatlas_cloud_provider_snapshot_restore_jobs_test.go
+++ mongodbatlas/data_source_mongodbatlas_cloud_provider_snapshot_restore_jobs_test.go
@@ -23,7 +23,7 @@ func TestAccDataSourceMongoDBAtlasCloudProviderSnapshotRestoreJobs_basic(t *test
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccMongoDBAtlasCloudProviderSnapshotRestoreJobsConfig(projectID, clusterName, description, retentionInDays, 1, 5),
+				Config: testAccMongoDBAtlasCloudProviderSnapshotRestoreJobsConfig(projectID, clusterName, description, retentionInDays),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckMongoDBAtlasCloudProviderSnapshotExists("mongodbatlas_cloud_provider_snapshot.test", &cloudProviderSnapshot),
 					resource.TestCheckResourceAttrSet("mongodbatlas_cloud_provider_snapshot.test", "project_id"),
@@ -36,7 +36,72 @@ func TestAccDataSourceMongoDBAtlasCloudProviderSnapshotRestoreJobs_basic(t *test
 	})
 }
 
-func testAccMongoDBAtlasCloudProviderSnapshotRestoreJobsConfig(projectID, clusterName, description, retentionInDays string, pageNum, itemPage int) string {
+func TestAccDataSourceMongoDBAtlasCloudProviderSnapshotRestoreJobs_withPagination(t *testing.T) {
+	var cloudProviderSnapshot matlas.CloudProviderSnapshot
+
+	projectID := os.Getenv("MONGODB_ATLAS_PROJECT_ID")
+	clusterName := fmt.Sprintf("test-acc-%s", acctest.RandString(10))
+	description := fmt.Sprintf("My description in %s", clusterName)
+	retentionInDays := "1"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccMongoDBAtlasCloudProviderSnapshotRestoreJobsConfigWithPagination(projectID, clusterName, description, retentionInDays, 1, 5),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckMongoDBAtlasCloudProviderSnapshotExists("mongodbatlas_cloud_provider_snapshot.test", &cloudProviderSnapshot),
+					resource.TestCheckResourceAttrSet("mongodbatlas_cloud_provider_snapshot.test", "project_id"),
+					resource.TestCheckResourceAttrSet("mongodbatlas_cloud_provider_snapshot.test", "cluster_name"),
+					resource.TestCheckResourceAttrSet("mongodbatlas_cloud_provider_snapshot.test", "description"),
+					resource.TestCheckResourceAttrSet("mongodbatlas_cloud_provider_snapshot.test", "retention_in_days"),
+				),
+			},
+		},
+	})
+}
+
+func testAccMongoDBAtlasCloudProviderSnapshotRestoreJobsConfig(projectID, clusterName, description, retentionInDays string) string {
+	return fmt.Sprintf(`
+		resource "mongodbatlas_cluster" "my_cluster" {
+			project_id   = "%s"
+			name         = "%s"
+			disk_size_gb = 5
+			
+			//Provider Settings "block"
+			provider_name               = "AWS"
+			provider_region_name        = "EU_CENTRAL_1"
+			provider_instance_size_name = "M10"
+			provider_backup_enabled     = true //enable cloud provider snapshots
+			provider_disk_iops          = 100
+			provider_encrypt_ebs_volume = false
+		}
+		
+		resource "mongodbatlas_cloud_provider_snapshot" "test" {
+			project_id        = mongodbatlas_cluster.my_cluster.project_id
+			cluster_name      = mongodbatlas_cluster.my_cluster.name
+			description       = "%s"
+			retention_in_days = %s
+		}
+
+		resource "mongodbatlas_cloud_provider_snapshot_restore_job" "test" {
+			project_id    = mongodbatlas_cloud_provider_snapshot.test.project_id
+			cluster_name  = mongodbatlas_cloud_provider_snapshot.test.cluster_name
+			snapshot_id   = mongodbatlas_cloud_provider_snapshot.test.id
+			delivery_type = {
+				download = true
+			}
+		}
+	
+		data "mongodbatlas_cloud_provider_snapshot_restore_jobs" "test" {
+			project_id   = mongodbatlas_cloud_provider_snapshot_restore_job.test.project_id
+			cluster_name = mongodbatlas_cloud_provider_snapshot_restore_job.test.cluster_name
+		}
+	`, projectID, clusterName, description, retentionInDays)
+}
+
+func testAccMongoDBAtlasCloudProviderSnapshotRestoreJobsConfigWithPagination(projectID, clusterName, description, retentionInDays string, pageNum, itemPage int) string {
 	return fmt.Sprintf(`
 		resource "mongodbatlas_cluster" "my_cluster" {
 			project_id   = "%s"
--- mongodbatlas/data_source_mongodbatlas_projects_test.go
+++ mongodbatlas/data_source_mongodbatlas_projects_test.go
@@ -30,6 +30,36 @@ func TestAccDataSourceMongoDBAtlasProjects_basic(t *testing.T) {
 							RoleNames: []string{"GROUP_DATA_ACCESS_ADMIN", "GROUP_OWNER"},
 						},
 					},
+				),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("mongodbatlas_project.test", "name"),
+					resource.TestCheckResourceAttrSet("mongodbatlas_project.test", "org_id"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccDataSourceMongoDBAtlasProjects_withPagination(t *testing.T) {
+	projectName := fmt.Sprintf("test-datasource-project-%s", acctest.RandString(10))
+	orgID := os.Getenv("MONGODB_ATLAS_ORG_ID")
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccMongoDBAtlasProjectsConfigWithPagination(projectName, orgID,
+					[]*matlas.ProjectTeam{
+						{
+							TeamID:    "5e0fa8c99ccf641c722fe683",
+							RoleNames: []string{"GROUP_READ_ONLY", "GROUP_DATA_ACCESS_ADMIN"},
+						},
+						{
+							TeamID:    "5e1dd7b4f2a30ba80a70cd3a",
+							RoleNames: []string{"GROUP_DATA_ACCESS_ADMIN", "GROUP_OWNER"},
+						},
+					},
 					2, 5,
 				),
 				Check: resource.ComposeTestCheckFunc(
@@ -41,7 +71,14 @@ func TestAccDataSourceMongoDBAtlasProjects_basic(t *testing.T) {
 	})
 }
 
-func testAccMongoDBAtlasProjectsConfigWithDS(projectName, orgID string, teams []*matlas.ProjectTeam, pageNum, itemPage int) string {
+func testAccMongoDBAtlasProjectsConfigWithDS(projectName, orgID string, teams []*matlas.ProjectTeam) string {
+	return fmt.Sprintf(`
+		%s
+		data "mongodbatlas_projects" "test" {}
+	`, testAccMongoDBAtlasPropjectConfig(projectName, orgID, teams))
+}
+
+func testAccMongoDBAtlasProjectsConfigWithPagination(projectName, orgID string, teams []*matlas.ProjectTeam, pageNum, itemPage int) string {
 	return fmt.Sprintf(`
 		%s
 		data "mongodbatlas_projects" "test" {
