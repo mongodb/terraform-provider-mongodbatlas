32754cdacfab976c84c5da088955449c9e43826e
Paco Preciado Mendoza <__pm@outlook.com>
2020-08-20 16:42:04 -0500

    Fix #285: Unable to import $external auth users (#297)
    * chore: improved regex to allow to use a $ character
    
    * chore: added a new test case to verify the  dabase name when it's imported

--- mongodbatlas/resource_mongodbatlas_database_user_test.go
+++ mongodbatlas/resource_mongodbatlas_database_user_test.go
@@ -571,6 +571,7 @@ func testAccMongoDBAtlasDatabaseUserWithAWSIAMTypeConfig(projectName, orgID, rol
 
 		resource "mongodbatlas_database_user" "test" {
 <<<<<<< HEAD
+<<<<<<< HEAD
 			username           = "%[3]s"
 			aws_iam_type       = "USER"
 			project_id         = "%[1]s"
@@ -579,6 +580,16 @@ func testAccMongoDBAtlasDatabaseUserWithAWSIAMTypeConfig(projectName, orgID, rol
 			aws_iam_type       = "USER"
 			project_id         = "${mongodbatlas_project.test.id}"
 >>>>>>> test: changed database user to create project and get it, changed import to rs primare attributes instead of decode state id
+=======
+			username           = "%[4]s"
+			aws_iam_type       = "USER"
+			project_id         = "${mongodbatlas_project.test.id}"
+=======
+			username           = "%[3]s"
+			aws_iam_type       = "USER"
+			project_id         = "%[1]s"
+>>>>>>> Fix #285: Unable to import $external auth users (#297)
+>>>>>>> Fix #285: Unable to import $external auth users (#297)
 			auth_database_name = "$external"
 
 			roles {
