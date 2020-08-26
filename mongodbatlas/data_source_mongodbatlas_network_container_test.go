c0c03229bed16b0b9ba47985458e6cbe2663277a
Edgar López <edgarlopez@pop-os.localdomain>
2020-08-26 00:48:45 -0500

    test: fixes some failed testacc

--- mongodbatlas/data_source_mongodbatlas_network_container_test.go
+++ mongodbatlas/data_source_mongodbatlas_network_container_test.go
@@ -16,9 +16,10 @@ func TestAccDataSourceMongoDBAtlasNetworkContainer_basic(t *testing.T) {
 	randInt := acctest.RandIntRange(0, 255)
 
 	resourceName := "mongodbatlas_network_container.test_ds"
-	projectID := os.Getenv("MONGODB_ATLAS_PROJECT_ID")
 	cidrBlock := fmt.Sprintf("10.8.%d.0/24", randInt)
 	dataSourceName := "data.mongodbatlas_network_container.test_ds"
+	orgID := os.Getenv("MONGODB_ATLAS_ORG_ID")
+	projectName := acctest.RandomWithPrefix("test-acc")
 
 	providerName := "AWS"
 
@@ -28,7 +29,7 @@ func TestAccDataSourceMongoDBAtlasNetworkContainer_basic(t *testing.T) {
 		CheckDestroy: testAccCheckMongoDBAtlasNetworkContainerDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccMongoDBAtlasNetworkContainerDSConfig(projectID, cidrBlock),
+				Config: testAccMongoDBAtlasNetworkContainerDSConfig(projectName, orgID, cidrBlock),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckMongoDBAtlasNetworkContainerExists(resourceName, &container),
 					testAccCheckMongoDBAtlasNetworkContainerAttributes(&container, providerName),
@@ -44,10 +45,15 @@ func TestAccDataSourceMongoDBAtlasNetworkContainer_basic(t *testing.T) {
 	})
 }
 
-func testAccMongoDBAtlasNetworkContainerDSConfig(projectID, cidrBlock string) string {
+func testAccMongoDBAtlasNetworkContainerDSConfig(projectName, orgID, cidrBlock string) string {
 	return fmt.Sprintf(`
+		resource "mongodbatlas_project" "test" {
+			name   = "%s"
+			org_id = "%s"
+		}
+
 		resource "mongodbatlas_network_container" "test_ds" {
-			project_id   		 = "%s"
+			project_id   		 = "${mongodbatlas_project.test.id}"
 			atlas_cidr_block = "%s"
 			provider_name		 = "AWS"
 			region_name			 = "US_EAST_1"
@@ -57,5 +63,5 @@ func testAccMongoDBAtlasNetworkContainerDSConfig(projectID, cidrBlock string) st
 			project_id   		= mongodbatlas_network_container.test_ds.project_id
 			container_id		= mongodbatlas_network_container.test_ds.container_id
 		}
-	`, projectID, cidrBlock)
+	`, projectName, orgID, cidrBlock)
 }
