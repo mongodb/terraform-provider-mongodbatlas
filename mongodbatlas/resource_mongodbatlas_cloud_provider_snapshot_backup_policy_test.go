bd18b59e14829e15c2983262ee4d3525f1d6a616
Paco Preciado Mendoza <__pm@outlook.com>
2020-08-27 15:42:32 -0500

    fix: added a validation for restore_window_days (#300)

--- mongodbatlas/resource_mongodbatlas_cloud_provider_snapshot_backup_policy_test.go
+++ mongodbatlas/resource_mongodbatlas_cloud_provider_snapshot_backup_policy_test.go
@@ -59,6 +59,49 @@ func TestAccResourceMongoDBAtlasCloudProviderSnapshotBackupPolicy_basic(t *testi
 	})
 }
 
+func TestAccResourceMongoDBAtlasCloudProviderSnapshotBackupPolicy_withoutRestoreWindowDays(t *testing.T) {
+	var (
+		resourceName = "mongodbatlas_cloud_provider_snapshot_backup_policy.test"
+		projectID    = os.Getenv("MONGODB_ATLAS_PROJECT_ID")
+		clusterName  = fmt.Sprintf("test-acc-%s", acctest.RandString(10))
+	)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckMongoDBAtlasCloudProviderSnapshotBackupPolicyDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccMongoDBAtlasCloudProviderSnapshotBackupPolicyConfigWithoutRestoreDays(projectID, clusterName, &matlas.CloudProviderSnapshotBackupPolicy{
+					ReferenceHourOfDay:    pointy.Int64(3),
+					ReferenceMinuteOfHour: pointy.Int64(45),
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckMongoDBAtlasCloudProviderSnapshotBackupPolicyExists(resourceName),
+					resource.TestCheckResourceAttr(resourceName, "project_id", projectID),
+					resource.TestCheckResourceAttr(resourceName, "cluster_name", clusterName),
+					resource.TestCheckResourceAttr(resourceName, "reference_hour_of_day", "3"),
+					resource.TestCheckResourceAttr(resourceName, "reference_minute_of_hour", "45"),
+				),
+			},
+			{
+				Config: testAccMongoDBAtlasCloudProviderSnapshotBackupPolicyConfigWithoutRestoreDays(projectID, clusterName, &matlas.CloudProviderSnapshotBackupPolicy{
+					ReferenceHourOfDay:    pointy.Int64(0),
+					ReferenceMinuteOfHour: pointy.Int64(0),
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckMongoDBAtlasCloudProviderSnapshotBackupPolicyExists(resourceName),
+					resource.TestCheckResourceAttr(resourceName, "project_id", projectID),
+					resource.TestCheckResourceAttr(resourceName, "cluster_name", clusterName),
+					resource.TestCheckResourceAttr(resourceName, "reference_minute_of_hour", "0"),
+					resource.TestCheckResourceAttr(resourceName, "reference_hour_of_day", "0"),
+					resource.TestCheckResourceAttr(resourceName, "restore_window_days", "7"),
+				),
+			},
+		},
+	})
+}
+
 func TestAccResourceMongoDBAtlasCloudProviderSnapshotBackupPolicy_importBasic(t *testing.T) {
 	SkipTestImport(t)
 	var (
@@ -213,4 +256,63 @@ func testAccMongoDBAtlasCloudProviderSnapshotBackupPolicyConfig(projectID, clust
 			}
 		}
 	`, projectID, clusterName, *p.ReferenceHourOfDay, *p.ReferenceMinuteOfHour, *p.RestoreWindowDays)
+}
+
+func testAccMongoDBAtlasCloudProviderSnapshotBackupPolicyConfigWithoutRestoreDays(projectID, clusterName string, p *matlas.CloudProviderSnapshotBackupPolicy) string {
+	return fmt.Sprintf(`
+		resource "mongodbatlas_cluster" "my_cluster" {
+			project_id   = "%s"
+			name         = "%s"
+			disk_size_gb = 5
+
+			// Provider Settings "block"
+			provider_name               = "AWS"
+			provider_region_name        = "EU_CENTRAL_1"
+			provider_instance_size_name = "M10"
+			provider_backup_enabled     = true //enable cloud provider snapshots
+			provider_disk_iops          = 100
+			provider_encrypt_ebs_volume = false
+		}
+
+		resource "mongodbatlas_cloud_provider_snapshot_backup_policy" "test" {
+			project_id   = mongodbatlas_cluster.my_cluster.project_id
+			cluster_name = mongodbatlas_cluster.my_cluster.name
+
+			reference_hour_of_day    = %d
+			reference_minute_of_hour = %d
+
+			policies {
+				id = mongodbatlas_cluster.my_cluster.snapshot_backup_policy.0.policies.0.id
+
+				policy_item {
+					id                 = mongodbatlas_cluster.my_cluster.snapshot_backup_policy.0.policies.0.policy_item.0.id
+					frequency_interval = 1
+					frequency_type     = "hourly"
+					retention_unit     = "days"
+					retention_value    = 1
+				}
+				policy_item {
+					id                 = mongodbatlas_cluster.my_cluster.snapshot_backup_policy.0.policies.0.policy_item.1.id
+					frequency_interval = 1
+					frequency_type     = "daily"
+					retention_unit     = "days"
+					retention_value    = 2
+				}
+				policy_item {
+					id                 = mongodbatlas_cluster.my_cluster.snapshot_backup_policy.0.policies.0.policy_item.2.id
+					frequency_interval = 4
+					frequency_type     = "weekly"
+					retention_unit     = "weeks"
+					retention_value    = 3
+				}
+				policy_item {
+					id                 = mongodbatlas_cluster.my_cluster.snapshot_backup_policy.0.policies.0.policy_item.3.id
+					frequency_interval = 5
+					frequency_type     = "monthly"
+					retention_unit     = "months"
+					retention_value    = 4
+				}
+			}
+		}
+	`, projectID, clusterName, *p.ReferenceHourOfDay, *p.ReferenceMinuteOfHour)
 }
