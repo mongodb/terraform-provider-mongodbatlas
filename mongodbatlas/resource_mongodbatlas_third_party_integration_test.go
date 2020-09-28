7f4bd2a52d2b80de3dc385346920e5377b848d1c
leofigy <angel.figueroa@digitalonus.com>
2020-09-28 17:35:39 -0500

    import testing

--- mongodbatlas/resource_mongodbatlas_third_party_integration_test.go
+++ mongodbatlas/resource_mongodbatlas_third_party_integration_test.go
@@ -47,6 +47,48 @@ func TestAccResourceMongoDBAtlasThirdPartyIntegration_basic(t *testing.T) {
 	)
 }
 
+func TestAccResourceMongoDBAtlasThirdPartyIntegration_importBasic(t *testing.T) {
+	var (
+		targetIntegration = matlas.ThirdPartyIntegration{}
+		projectID         = os.Getenv("MONGODB_ATLAS_PROJECT_ID")
+		config            = testAccCreateThirdPartyIntegrationConfig()
+		testExecutionName = "test_3rd_party_" + config.AccountID
+		resourceName      = "mongodbatlas_third_party_integration." + testExecutionName
+	)
+
+	config.Type = "OPS_GENIE"
+
+	seedConfig := thirdPartyConfig{
+		Name:        testExecutionName,
+		ProjectID:   projectID,
+		Integration: *config,
+	}
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckMongoDBAtlasThirdPartyIntegrationDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccMongoDBAtlasThirdPartyIntegrationResourceConfig(&seedConfig),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckThirdPartyIntegrationExists(resourceName, &targetIntegration),
+					resource.TestCheckResourceAttr(resourceName, "type", config.Type),
+					resource.TestCheckResourceAttr(resourceName, "api_key", config.APIKey),
+					resource.TestCheckResourceAttr(resourceName, "region", config.Region),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportStateIdFunc: testAccCheckMongoDBAtlasThirdPartyIntegrationImportStateIDFunc(resourceName),
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	},
+	)
+}
+
 func testAccCheckMongoDBAtlasThirdPartyIntegrationDestroy(s *terraform.State) error {
 	conn := testAccProvider.Meta().(*matlas.Client)
 	for _, rs := range s.RootModule().Resources {
@@ -63,4 +105,17 @@ func testAccCheckMongoDBAtlasThirdPartyIntegrationDestroy(s *terraform.State) er
 	}
 
 	return nil
+}
+
+func testAccCheckMongoDBAtlasThirdPartyIntegrationImportStateIDFunc(resourceName string) resource.ImportStateIdFunc {
+	return func(s *terraform.State) (string, error) {
+		rs, ok := s.RootModule().Resources[resourceName]
+		if !ok {
+			return "", fmt.Errorf("not found: %s", resourceName)
+		}
+
+		ids := decodeStateID(rs.Primary.ID)
+
+		return fmt.Sprintf("%s-%s", ids["project_id"], ids["type"]), nil
+	}
 }
