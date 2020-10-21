90925465368e12f5e77de6c2069f46589fc3312b
Nikhil Singh <nikhil.singh@mongodb.com>
2020-10-22 04:17:11 +0530

    documentation fix #335 and examples added for the Azure VNET peering and AWS Private Link (#340)
    * added examples for the Azure VNET peering and AWS Private Link
    
    * typo fix as mentioned in #335
    
    * fix Readme.md with Terraform version

---
+++ examples/mongodbatlas-azure-vnet-peering/azure.tf
@@ -0,0 +1,51 @@
+provider "azurerm" {
+  # The "feature" block is required for AzureRM provider 2.x. 
+  # If you're using version 1.x, the "features" block is not allowed.
+  version = "~>2.0"
+  features {}
+}
+data "azurerm_client_config" "current" {
+}
+
+output "subscription_id" {
+  value = data.azurerm_client_config.current.subscription_id
+}
+output "tenant_id" {
+  value = data.azurerm_client_config.current.tenant_id
+}
+resource "azurerm_resource_group" "resourcegroup" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet_name
+  resource_group_name = azurerm_resource_group.resourcegroup.name
+  address_space       = [var.address_space]
+  location            = var.location
+}
+resource "azuread_service_principal" "sp" {
+  application_id               = var.application_id
+  app_role_assignment_required = true
+}
+resource "azurerm_role_definition" "rd" {
+  name  = "my-custom-role-definition"
+  scope = "/subscriptions/${var.application_id}/resourceGroups/${var.resource_group_name}/providers/Microsoft.Network/virtualNetworks/${var.vnet_name}"
+
+  permissions {
+    actions = ["Microsoft.Network/virtualNetworks/virtualNetworkPeerings/read",
+      "Microsoft.Network/virtualNetworks/virtualNetworkPeerings/write",
+      "Microsoft.Network/virtualNetworks/virtualNetworkPeerings/delete",
+    "Microsoft.Network/virtualNetworks/peer/action"]
+    not_actions = []
+  }
+
+  assignable_scopes = [
+    "/subscriptions/${var.application_id}/resourceGroups/${var.resource_group_name}/providers/Microsoft.Network/virtualNetworks/${var.vnet_name}",
+  ]
+}
+resource "azurerm_role_assignment" "ra" {
+  scope              = "/subscriptions/${var.application_id}/resourceGroups/${var.resource_group_name}/providers/Microsoft.Network/virtualNetworks/${var.vnet_name}"
+  role_definition_id = azurerm_role_definition.rd.role_definition_resource_id
+  principal_id       = azuread_service_principal.sp.id
+}
---
+++ examples/mongodbatlas-azure-vnet-peering/variables.tf
@@ -0,0 +1,32 @@
+variable "public_key" {
+}
+variable "private_key" {
+}
+variable "project_id" {
+}
+variable "provider_instance_size_name" {
+}
+variable "provider_disk_type_name" {
+}
+variable "resource_group_name" {
+}
+variable "vnet_name" {
+}
+variable "atlas_cidr_block" {
+  default = "192.168.248.0/21"
+}
+variable "location" {
+  description = "The Azure region"
+}
+variable "provider_region_name" {
+  description = "The Atlas region name"
+}
+variable "name" {
+  description = "Atlas cluster name"
+}
+variable "address_space" {
+  description = "Azure VNET CIDR"
+}
+variable "application_id" {
+  default = "e90a1407-55c3-432d-9cb1-3638900a9d22"
+}
