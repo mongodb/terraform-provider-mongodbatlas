65f98ece174a78e029bfc9e51d1decd5fbeb4d11
Nikhil Singh <nikhil.singh@mongodb.com>
2020-10-30 17:06:12 +0530

    update the ipaccesslist

--- examples/starter/ip_access_list.tf
+++ examples/starter/ip_access_list.tf
@@ -1,8 +1,8 @@
-resource "mongodbatlas_project_ip_access_list" "ip_whitelist" {
+resource "mongodbatlas_project_ip_access_list" "ip" {
   project_id = mongodbatlas_project.project.id
   ip_address = "77.107.233.162"
   comment    = "cidr block for accessing the cluster"
 }
 output "ipwhitelist" {
-  value = mongodbatlas_project_ip_whitelist.ip_whitelist.cidr_block
+  value = mongodbatlas_project_ip_access_list.ip.ip_address
 }
