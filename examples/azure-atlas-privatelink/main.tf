549f9637c1d0c394931351c8b09504109d16b1c8
Edgar López <edgarlopez@pop-os.localdomain>
2020-11-16 21:11:09 -0700

    test: added a folder example with config tf for azure endpoint

---
+++ examples/azure-atlas-privatelink/main.tf
@@ -0,0 +1,54 @@
+ provider "azurerm" {    
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id = var.tenant_id
+  features {
+  }
+}
+
+data "azurerm_resource_group" "test" {
+  name = var.resource_group_name
+}
+
+resource "azurerm_virtual_network" "test" {
+  name                = "acceptanceTestVirtualNetwork1"
+  address_space       = ["10.0.0.0/16"]
+  location            = data.azurerm_resource_group.test.location
+  resource_group_name = var.resource_group_name
+}
+
+resource "azurerm_subnet" "test" {
+  name                 = "testsubnet"
+  resource_group_name  = "edgar-lopez-2-rg"
+  virtual_network_name = azurerm_virtual_network.test.name
+  address_prefixes     = ["10.0.1.0/24"]
+  enforce_private_link_service_network_policies = true
+  enforce_private_link_endpoint_network_policies = true
+}
+
+resource "mongodbatlas_private_endpoint" "test" {
+  project_id    = "5cf5a45a9ccf6400e60981b6"
+  provider_name = "AZURE"
+  region        = "eastus2"
+}
+
+resource "azurerm_private_endpoint" "test" {
+  name                = "endpoint-test"
+  location            = data.azurerm_resource_group.test.location
+  resource_group_name = var.resource_group_name
+  subnet_id           = azurerm_subnet.test.id
+  private_service_connection {
+    name                           = mongodbatlas_private_endpoint.test.private_link_service_name
+    private_connection_resource_id = mongodbatlas_private_endpoint.test.private_link_service_resource_id
+    is_manual_connection           = false
+  }
+        
+}
+
+resource "mongodbatlas_private_endpoint_service_link" "test" {
+  project_id            = mongodbatlas_private_endpoint.test.project_id
+  private_link_id       =  azurerm_private_endpoint.test.id
+  endpoint_service_id = mongodbatlas_private_endpoint.test.private_link_id
+  provider_name = "AZURE"
+}
---
+++ examples/azure-atlas-privatelink/versions.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+    }
+    mongodbatlas = {
+      source = "mongodb/mongodbatlas"
+      version = "0.7-dev"
+    }
+  }
+  required_version = ">= 0.13"
+}
