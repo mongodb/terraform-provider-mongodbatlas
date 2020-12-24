5791689eb71e6e91cd146860f5f8e0e2b1726c13
Edgar López <edgarlopez@pop-os.localdomain>
2020-12-23 21:27:56 -0700

    refactor: changed schema ldap to root instead of typelist

--- mongodbatlas/data_source_mongodbatlas_ldap_configuration.go
+++ mongodbatlas/data_source_mongodbatlas_ldap_configuration.go
@@ -17,64 +17,54 @@ func dataSourceMongoDBAtlasLDAPConfiguration() *schema.Resource {
 				Type:     schema.TypeString,
 				Required: true,
 			},
-			"ldap": {
+			"authentication_enabled": {
+				Type:     schema.TypeBool,
+				Computed: true,
+			},
+			"authorization_enabled": {
+				Type:     schema.TypeBool,
+				Computed: true,
+			},
+			"hostname": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"port": {
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"bind_username": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"bind_password": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"ca_certificate": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"authz_query_template": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"user_to_dn_mapping": {
 				Type:     schema.TypeList,
-				MinItems: 1,
-				MaxItems: 1,
 				Computed: true,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
-						"authentication_enabled": {
-							Type:     schema.TypeBool,
-							Computed: true,
-						},
-						"authorization_enabled": {
-							Type:     schema.TypeBool,
-							Computed: true,
-						},
-						"hostname": {
+						"match": {
 							Type:     schema.TypeString,
 							Computed: true,
 						},
-						"port": {
-							Type:     schema.TypeInt,
-							Computed: true,
-						},
-						"bind_username": {
+						"substitution": {
 							Type:     schema.TypeString,
 							Computed: true,
 						},
-						"bind_password": {
+						"ldap_query": {
 							Type:     schema.TypeString,
 							Computed: true,
-						},
-						"ca_certificate": {
-							Type:     schema.TypeString,
-							Computed: true,
-						},
-						"authz_query_template": {
-							Type:     schema.TypeString,
-							Computed: true,
-						},
-						"user_to_dn_mapping": {
-							Type:     schema.TypeList,
-							Computed: true,
-							Elem: &schema.Resource{
-								Schema: map[string]*schema.Schema{
-									"match": {
-										Type:     schema.TypeString,
-										Computed: true,
-									},
-									"substitution": {
-										Type:     schema.TypeString,
-										Computed: true,
-									},
-									"ldap_query": {
-										Type:     schema.TypeString,
-										Computed: true,
-									},
-								},
-							},
 						},
 					},
 				},
@@ -92,8 +82,32 @@ func dataSourceMongoDBAtlasLDAPConfigurationRead(d *schema.ResourceData, meta in
 		return fmt.Errorf(errorLDAPConfigurationRead, projectID, err)
 	}
 
-	if err := d.Set("ldap", flattenLDAP(ldap.LDAP)); err != nil {
-		return fmt.Errorf(errorLDAPConfigurationSetting, "ldap", projectID, err)
+	if err = d.Set("authentication_enabled", ldap.LDAP.AuthenticationEnabled); err != nil {
+		return fmt.Errorf(errorLDAPConfigurationSetting, "authentication_enabled", d.Id(), err)
+	}
+	if err = d.Set("authorization_enabled", ldap.LDAP.AuthorizationEnabled); err != nil {
+		return fmt.Errorf(errorLDAPConfigurationSetting, "authorization_enabled", d.Id(), err)
+	}
+	if err = d.Set("hostname", ldap.LDAP.Hostname); err != nil {
+		return fmt.Errorf(errorLDAPConfigurationSetting, "hostname", d.Id(), err)
+	}
+	if err = d.Set("port", ldap.LDAP.Port); err != nil {
+		return fmt.Errorf(errorLDAPConfigurationSetting, "port", d.Id(), err)
+	}
+	if err = d.Set("bind_username", ldap.LDAP.BindUsername); err != nil {
+		return fmt.Errorf(errorLDAPConfigurationSetting, "bind_username", d.Id(), err)
+	}
+	if err = d.Set("bind_password", ldap.LDAP.BindPassword); err != nil {
+		return fmt.Errorf(errorLDAPConfigurationSetting, "bind_password", d.Id(), err)
+	}
+	if err = d.Set("ca_certificate", ldap.LDAP.CaCertificate); err != nil {
+		return fmt.Errorf(errorLDAPConfigurationSetting, "ca_certificate", d.Id(), err)
+	}
+	if err = d.Set("authz_query_template", ldap.LDAP.AuthzQueryTemplate); err != nil {
+		return fmt.Errorf(errorLDAPConfigurationSetting, "authz_query_template", d.Id(), err)
+	}
+	if err = d.Set("user_to_dn_mapping", flattenDNMapping(ldap.LDAP.UserToDNMapping)); err != nil {
+		return fmt.Errorf(errorLDAPConfigurationSetting, "user_to_dn_mapping", d.Id(), err)
 	}
 
 	d.SetId(projectID)
--- mongodbatlas/resource_mongodbatlas_ldap_configuration.go
+++ mongodbatlas/resource_mongodbatlas_ldap_configuration.go
@@ -30,71 +30,61 @@ func resourceMongoDBAtlasLDAPConfiguration() *schema.Resource {
 				Type:     schema.TypeString,
 				Required: true,
 			},
-			"ldap": {
-				Type:     schema.TypeList,
-				MinItems: 1,
-				MaxItems: 1,
+			"authentication_enabled": {
+				Type:     schema.TypeBool,
 				Required: true,
+			},
+			"authorization_enabled": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Computed: true,
+			},
+			"hostname": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"port": {
+				Type:     schema.TypeInt,
+				Required: true,
+			},
+			"bind_username": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"bind_password": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"ca_certificate": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"authz_query_template": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"user_to_dn_mapping": {
+				Type:     schema.TypeList,
+				Optional: true,
+				Computed: true,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
-						"authentication_enabled": {
-							Type:     schema.TypeBool,
-							Required: true,
-						},
-						"authorization_enabled": {
-							Type:     schema.TypeBool,
-							Optional: true,
-							Computed: true,
-						},
-						"hostname": {
-							Type:     schema.TypeString,
-							Required: true,
-						},
-						"port": {
-							Type:     schema.TypeInt,
-							Required: true,
-						},
-						"bind_username": {
-							Type:     schema.TypeString,
-							Required: true,
-						},
-						"bind_password": {
-							Type:     schema.TypeString,
-							Required: true,
-						},
-						"ca_certificate": {
+						"match": {
 							Type:     schema.TypeString,
 							Optional: true,
 							Computed: true,
 						},
-						"authz_query_template": {
+						"substitution": {
 							Type:     schema.TypeString,
 							Optional: true,
 							Computed: true,
 						},
-						"user_to_dn_mapping": {
-							Type:     schema.TypeList,
+						"ldap_query": {
+							Type:     schema.TypeString,
 							Optional: true,
 							Computed: true,
-							Elem: &schema.Resource{
-								Schema: map[string]*schema.Schema{
-									"match": {
-										Type:     schema.TypeString,
-										Optional: true,
-										Computed: true,
-									},
-									"substitution": {
-										Type:     schema.TypeString,
-										Optional: true,
-										Computed: true,
-									},
-									"ldap_query": {
-										Type:     schema.TypeString,
-										Optional: true,
-										Computed: true,
-									},
-								},
-							},
 						},
 					},
 				},
@@ -107,8 +97,46 @@ func resourceMongoDBAtlasLDAPConfigurationCreate(d *schema.ResourceData, meta in
 	conn := meta.(*matlas.Client)
 
 	projectID := d.Get("project_id").(string)
+	ldap := &matlas.LDAP{}
+
+	if v, ok := d.GetOk("authentication_enabled"); ok {
+		ldap.AuthenticationEnabled = v.(bool)
+	}
+
+	if v, ok := d.GetOk("authorization_enabled"); ok {
+		ldap.AuthorizationEnabled = v.(bool)
+	}
+
+	if v, ok := d.GetOk("hostname"); ok {
+		ldap.Hostname = v.(string)
+	}
+
+	if v, ok := d.GetOk("port"); ok {
+		ldap.Port = v.(int)
+	}
+
+	if v, ok := d.GetOk("bind_username"); ok {
+		ldap.BindUsername = v.(string)
+	}
+
+	if v, ok := d.GetOk("bind_password"); ok {
+		ldap.BindPassword = v.(string)
+	}
+
+	if v, ok := d.GetOk("ca_certificate"); ok {
+		ldap.CaCertificate = v.(string)
+	}
+
+	if v, ok := d.GetOk("authz_query_template"); ok {
+		ldap.AuthzQueryTemplate = v.(string)
+	}
+
+	if v, ok := d.GetOk("user_to_dn_mapping"); ok {
+		ldap.UserToDNMapping = expandDNMapping(v.([]interface{}))
+	}
+
 	ladpReq := &matlas.LDAPConfiguration{
-		LDAP: expandLADP(d),
+		LDAP: ldap,
 	}
 
 	_, _, err := conn.LDAPConfigurations.Save(context.Background(), projectID, ladpReq)
@@ -129,8 +157,32 @@ func resourceMongoDBAtlasLDAPConfigurationRead(d *schema.ResourceData, meta inte
 		return fmt.Errorf(errorLDAPConfigurationRead, d.Id(), err)
 	}
 
-	if err := d.Set("ldap", flattenLDAP(ldapResp.LDAP)); err != nil {
-		return fmt.Errorf(errorLDAPConfigurationSetting, "ldap", d.Id(), err)
+	if err = d.Set("authentication_enabled", ldapResp.LDAP.AuthenticationEnabled); err != nil {
+		return fmt.Errorf(errorLDAPConfigurationSetting, "authentication_enabled", d.Id(), err)
+	}
+	if err = d.Set("authorization_enabled", ldapResp.LDAP.AuthorizationEnabled); err != nil {
+		return fmt.Errorf(errorLDAPConfigurationSetting, "authorization_enabled", d.Id(), err)
+	}
+	if err = d.Set("hostname", ldapResp.LDAP.Hostname); err != nil {
+		return fmt.Errorf(errorLDAPConfigurationSetting, "hostname", d.Id(), err)
+	}
+	if err = d.Set("port", ldapResp.LDAP.Port); err != nil {
+		return fmt.Errorf(errorLDAPConfigurationSetting, "port", d.Id(), err)
+	}
+	if err = d.Set("bind_username", ldapResp.LDAP.BindUsername); err != nil {
+		return fmt.Errorf(errorLDAPConfigurationSetting, "bind_username", d.Id(), err)
+	}
+	if err = d.Set("bind_password", ldapResp.LDAP.BindPassword); err != nil {
+		return fmt.Errorf(errorLDAPConfigurationSetting, "bind_password", d.Id(), err)
+	}
+	if err = d.Set("ca_certificate", ldapResp.LDAP.CaCertificate); err != nil {
+		return fmt.Errorf(errorLDAPConfigurationSetting, "ca_certificate", d.Id(), err)
+	}
+	if err = d.Set("authz_query_template", ldapResp.LDAP.AuthzQueryTemplate); err != nil {
+		return fmt.Errorf(errorLDAPConfigurationSetting, "authz_query_template", d.Id(), err)
+	}
+	if err = d.Set("user_to_dn_mapping", flattenDNMapping(ldapResp.LDAP.UserToDNMapping)); err != nil {
+		return fmt.Errorf(errorLDAPConfigurationSetting, "user_to_dn_mapping", d.Id(), err)
 	}
 
 	return nil
@@ -140,10 +192,46 @@ func resourceMongoDBAtlasLDAPConfigurationUpdate(d *schema.ResourceData, meta in
 	// Get the client connection.
 	conn := meta.(*matlas.Client)
 
-	ldapReq := &matlas.LDAPConfiguration{}
+	ldap := &matlas.LDAP{}
 
-	if d.HasChange("ldap") {
-		ldapReq.LDAP = expandLADP(d)
+	if d.HasChange("authentication_enabled") {
+		ldap.AuthenticationEnabled = d.Get("").(bool)
+	}
+
+	if d.HasChange("authorization_enabled") {
+		ldap.AuthorizationEnabled = d.Get("authorization_enabled").(bool)
+	}
+
+	if d.HasChange("hostname") {
+		ldap.Hostname = d.Get("hostname").(string)
+	}
+
+	if d.HasChange("port") {
+		ldap.Port = d.Get("port").(int)
+	}
+
+	if d.HasChange("bind_username") {
+		ldap.BindUsername = d.Get("bind_username").(string)
+	}
+
+	if d.HasChange("bind_password") {
+		ldap.BindPassword = d.Get("bind_password").(string)
+	}
+
+	if d.HasChange("ca_certificate") {
+		ldap.CaCertificate = d.Get("ca_certificate").(string)
+	}
+
+	if d.HasChange("authz_query_template") {
+		ldap.AuthzQueryTemplate = d.Get("authz_query_template").(string)
+	}
+
+	if d.HasChange("user_to_dn_mapping") {
+		ldap.UserToDNMapping = expandDNMapping(d.Get("user_to_dn_mapping").([]interface{}))
+	}
+
+	ldapReq := &matlas.LDAPConfiguration{
+		LDAP: ldap,
 	}
 
 	_, _, err := conn.LDAPConfigurations.Save(context.Background(), d.Id(), ldapReq)
@@ -165,48 +253,6 @@ func resourceMongoDBAtlasLDAPConfigurationDelete(d *schema.ResourceData, meta in
 	return nil
 }
 
-func expandLADP(d *schema.ResourceData) *matlas.LDAP {
-	ldap := &matlas.LDAP{}
-
-	if v, ok := d.GetOk("ldap.0.authentication_enabled"); ok {
-		ldap.AuthenticationEnabled = v.(bool)
-	}
-
-	if v, ok := d.GetOk("ldap.0.authorization_enabled"); ok {
-		ldap.AuthorizationEnabled = v.(bool)
-	}
-
-	if v, ok := d.GetOk("ldap.0.hostname"); ok {
-		ldap.Hostname = v.(string)
-	}
-
-	if v, ok := d.GetOk("ldap.0.port"); ok {
-		ldap.Port = v.(int)
-	}
-
-	if v, ok := d.GetOk("ldap.0.bind_username"); ok {
-		ldap.BindUsername = v.(string)
-	}
-
-	if v, ok := d.GetOk("ldap.0.bind_password"); ok {
-		ldap.BindPassword = v.(string)
-	}
-
-	if v, ok := d.GetOk("ldap.0.ca_certificate"); ok {
-		ldap.CaCertificate = v.(string)
-	}
-
-	if v, ok := d.GetOk("ldap.0.authz_query_template"); ok {
-		ldap.AuthzQueryTemplate = v.(string)
-	}
-
-	if v, ok := d.GetOk("ldap.0.user_to_dn_mapping"); ok {
-		ldap.UserToDNMapping = expandDNMapping(v.([]interface{}))
-	}
-
-	return ldap
-}
-
 func expandDNMapping(p []interface{}) []*matlas.UserToDNMapping {
 	mappings := make([]*matlas.UserToDNMapping, len(p))
 
@@ -220,22 +266,6 @@ func expandDNMapping(p []interface{}) []*matlas.UserToDNMapping {
 	}
 
 	return mappings
-}
-
-func flattenLDAP(ldap *matlas.LDAP) []map[string]interface{} {
-	policyItems := make([]map[string]interface{}, 1)
-	policyItems = append(policyItems, map[string]interface{}{
-		"authentication_enabled": ldap.AuthenticationEnabled,
-		"authorization_enabled":  ldap.AuthorizationEnabled,
-		"hostname":               ldap.Hostname,
-		"port":                   ldap.Port,
-		"bind_username":          ldap.BindUsername,
-		"bind_password":          ldap.BindUsername,
-		"ca_certificate":         ldap.CaCertificate,
-		"user_to_dn_mapping":     flattenDNMapping(ldap.UserToDNMapping),
-	})
-
-	return policyItems
 }
 
 func flattenDNMapping(usersDNMappings []*matlas.UserToDNMapping) []map[string]interface{} {
