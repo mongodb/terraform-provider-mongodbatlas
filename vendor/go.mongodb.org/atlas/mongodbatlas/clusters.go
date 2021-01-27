f8e0fa5627527dc6b3265f3b461ab74d39be1a09
Edgar López <edgarlopez@pop-os.localdomain>
2021-01-27 10:34:56 -0700

    chore: updated client mongodb with recent release

--- vendor/go.mongodb.org/atlas/mongodbatlas/clusters.go
+++ vendor/go.mongodb.org/atlas/mongodbatlas/clusters.go
@@ -1,3 +1,17 @@
+// Copyright 2021 MongoDB Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package mongodbatlas
 
 import (
@@ -86,6 +100,7 @@ type PrivateEndpoint struct {
 	ConnectionString    string     `json:"connectionString,omitempty"`
 	Endpoints           []Endpoint `json:"endpoints,omitempty"`
 	SRVConnectionString string     `json:"srvConnectionString,omitempty"`
+	Type                string     `json:"type,omitempty"`
 }
 
 // Endpoint through which you connect to Atlas
@@ -93,7 +108,6 @@ type Endpoint struct {
 	EndpointID   string `json:"endpointId,omitempty"`
 	ProviderName string `json:"providerName,omitempty"`
 	Region       string `json:"region,omitempty"`
-	Type         string `json:"type,omitempty"`
 }
 
 // ConnectionStrings configuration for applications use to connect to this cluster
--- vendor/go.mongodb.org/atlas/mongodbatlas/mongodbatlas.go
+++ vendor/go.mongodb.org/atlas/mongodbatlas/mongodbatlas.go
@@ -1,3 +1,17 @@
+// Copyright 2021 MongoDB Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package mongodbatlas // import "go.mongodb.org/atlas/mongodbatlas"
 
 import (
@@ -27,8 +41,8 @@ const (
 	plainMediaType  = "text/plain"
 	gzipMediaType   = "application/gzip"
 	libraryName     = "go-mongodbatlas"
-	// Version the version of the current API client
-	Version = "0.7.0" // Should be set to the next version planned to be released
+	// Version the version of the current API client. Should be set to the next version planned to be released
+	Version = "0.7.1"
 )
 
 var (
