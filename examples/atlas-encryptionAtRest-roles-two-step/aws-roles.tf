2d34fd52f683fbd85c02ba5d26e12a8bccd91c38
Zohar Meir <zoharmeir@Zohars-MacBook-Pro.local>
2021-02-22 17:56:24 +0200

    two options for aws encryption at rest with iam roles

---
+++ examples/atlas-encryptionAtRest-roles-two-step/aws-roles.tf
@@ -0,0 +1,54 @@
+
+resource "mongodbatlas_cloud_provider_access" "test" {
+        project_id = mongodbatlas_project.my_project.id
+        provider_name = "AWS"
+        #after first apply, add the following line:
+        #iam_assumed_role_arn = aws_iam_role.test_role.arn
+    }
+
+resource "aws_iam_role_policy" "test_policy" {
+  name = "test_policy"
+  role = aws_iam_role.test_role.id
+
+  policy = <<-EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+		"Action": "*",
+		"Resource": "*"
+      }
+    ]
+  }
+  EOF
+}
+
+resource "aws_iam_role" "test_role" {
+  name = "test_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "${mongodbatlas_cloud_provider_access.test.atlas_aws_account_arn}"
+      },
+      "Action": "sts:AssumeRole",
+      "Condition": {
+        "StringEquals": {
+          "sts:ExternalId": "${mongodbatlas_cloud_provider_access.test.atlas_assumed_role_external_id}"
+        }
+      }
+    }
+  ]
+}
+EOF
+}
+
+
+output "cpa_role_id" {
+  value = mongodbatlas_cloud_provider_access.test.role_id
+}
