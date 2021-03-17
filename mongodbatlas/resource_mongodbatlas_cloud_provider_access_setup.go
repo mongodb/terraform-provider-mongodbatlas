8b208a05f126a9c5ca9f0e296a853b937cf10df5
leofigy <angel.fig@yandex.com>
2021-03-17 07:50:00 -0600

    added read for second resource

--- mongodbatlas/resource_mongodbatlas_cloud_provider_access_setup.go
+++ mongodbatlas/resource_mongodbatlas_cloud_provider_access_setup.go
@@ -1,12 +1,17 @@
 package mongodbatlas
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+	matlas "go.mongodb.org/atlas/mongodbatlas"
 )
 
 func resourceMongoDBAtlasCloudProviderAccessSetup() *schema.Resource {
 	return &schema.Resource{
+		Read: resourceMongoDBAtlasCloudProviderAccessSetupRead,
 		Schema: map[string]*schema.Schema{
 			"project_id": {
 				Type:     schema.TypeString,
@@ -45,34 +50,62 @@ func resourceMongoDBAtlasCloudProviderAccessSetup() *schema.Resource {
 	}
 }
 
-func resourceMongoDBAtlasCloudProviderAccessAuthorization() *schema.Resource {
-	return &schema.Resource{
-		Schema: map[string]*schema.Schema{
-			"project_id": {
-				Type:     schema.TypeString,
-				Required: true,
-			},
-			"role_id": {
-				Type:     schema.TypeString,
-				Required: true,
-			},
-			"aws": {
-				Type:     schema.TypeMap,
-				Optional: true,
-				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						"iam_assumed_role_arn": {
-							Type:     schema.TypeString,
-							Required: true,
-						},
-					},
-				},
-			},
-			"feature_usages": {
-				Type:     schema.TypeList,
-				Elem:     featureUsagesSchema(),
-				Computed: true,
-			},
-		},
+func resourceMongoDBAtlasCloudProviderAccessSetupRead(d *schema.ResourceData, meta interface{}) error {
+	// sadly there is no just get API
+	conn := meta.(*matlas.Client)
+	ids := decodeStateID(d.Id())
+	projectID := ids["project_id"]
+	providerName := ids["provider_name"]
+
+	roles, _, err := conn.CloudProviderAccess.ListRoles(context.Background(), projectID)
+
+	if err != nil {
+		return fmt.Errorf(errorGetRead, err)
 	}
+
+	// aws specific
+	if providerName == "AWS" {
+		var targetRole matlas.AWSIAMRole
+		// searching in roles
+		for i := range roles.AWSIAMRoles {
+			role := &(roles.AWSIAMRoles[i])
+			if role.RoleID == ids["id"] && role.ProviderName == ids["provider_name"] {
+				targetRole = *role
+			}
+		}
+		// Not Found
+		if targetRole.RoleID == "" && !d.IsNewResource() {
+			d.SetId("")
+			return nil
+		}
+
+		roleSchema := roleToSchemaSetup(&targetRole)
+
+		for key, val := range roleSchema {
+			if err := d.Set(key, val); err != nil {
+				return fmt.Errorf(errorGetRead, err)
+			}
+		}
+
+	} else {
+		// planning for the future multiple providers
+		return fmt.Errorf(errorGetRead,
+			fmt.Sprintf("unsopported provider type %s", providerName))
+	}
+
+	return nil
+}
+
+func roleToSchemaSetup(role *matlas.AWSIAMRole) map[string]interface{} {
+	out := map[string]interface{}{
+		"provider_name": role.ProviderName,
+		"aws": map[string]interface{}{
+			"atlas_aws_account_arn":          role.AtlasAWSAccountARN,
+			"atlas_assumed_role_external_id": role.AtlasAssumedRoleExternalID,
+		},
+		"created_date": role.CreatedDate,
+		"role_id":      role.RoleID,
+	}
+
+	return out
 }
