529a7cae2117d092eb489b7d1e20e229f2d40b4c
Edgar López <edgarlopez@pop-os.localdomain>
2021-06-07 17:00:57 -0600

    tests: added terratest for upgrading/import for whitelist and access list

---
+++ examples/test-upgrade/v100/ip-whitelist-accestList/v100/main.tf
@@ -0,0 +1,11 @@
+resource "mongodbatlas_project" "test" {
+  name   = var.project_name
+  org_id = var.org_id
+}
+
+
+resource "mongodbatlas_project_ip_access_list" "test" {
+  project_id = mongodbatlas_project.test.id
+  ip_address = var.ip_address
+  comment    = var.comment
+}
