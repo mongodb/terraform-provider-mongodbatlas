49940dbcd566c0d1dc184bea4569b5bee90a611b
Edgar López <edgarlopez@pop-os.localdomain>
2021-06-24 21:54:37 -0600

    changed name of resource

--- examples/aws-atlas-privatelink/atlas-pl.tf
+++ examples/aws-atlas-privatelink/atlas-pl.tf
@@ -1,4 +1,4 @@
-resource "mongodbatlas_private_endpoint" "atlaspl" {
+resource "mongodbatlas_privatelink_endpoint" "atlaspl" {
   project_id    = var.atlasprojectid
   provider_name = "AWS"
   region        = var.aws_region
@@ -6,14 +6,14 @@ resource "mongodbatlas_private_endpoint" "atlaspl" {
 
 resource "aws_vpc_endpoint" "ptfe_service" {
   vpc_id             = aws_vpc.primary.id
-  service_name       = mongodbatlas_private_endpoint.atlaspl.endpoint_service_name
+  service_name       = mongodbatlas_privatelink_endpoint.atlaspl.endpoint_service_name
   vpc_endpoint_type  = "Interface"
   subnet_ids         = [aws_subnet.primary-az1.id, aws_subnet.primary-az2.id]
   security_group_ids = [aws_security_group.primary_default.id]
 }
 
-resource "mongodbatlas_private_endpoint_interface_link" "atlaseplink" {
-  project_id            = mongodbatlas_private_endpoint.atlaspl.project_id
-  private_link_id       = mongodbatlas_private_endpoint.atlaspl.private_link_id
+resource "mongodbatlas_privatelink_endpoint_service" "atlaseplink" {
+  project_id            = mongodbatlas_privatelink_endpoint.atlaspl.project_id
+  private_link_id       = mongodbatlas_privatelink_endpoint.atlaspl.private_link_id
   interface_endpoint_id = aws_vpc_endpoint.ptfe_service.id
 }
