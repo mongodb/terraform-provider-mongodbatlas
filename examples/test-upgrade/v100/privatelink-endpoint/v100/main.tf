c1b1e83e55fe57b6c6a5354398a28eb880905537
Edgar López <edgarlopez@pop-os.localdomain>
2021-06-24 21:55:16 -0600

    added terratest for deprecation private endpoint and import with privatelink endpoint

---
+++ examples/test-upgrade/v100/privatelink-endpoint/v100/main.tf
@@ -0,0 +1,35 @@
+// This example is the same with the recent name of resource but will do nothing
+// This is only for import stuff because it needs the resource names and set to
+// avoid changes when terraform plan
+
+resource "mongodbatlas_project" "test" {
+  name   = var.project_name
+  org_id = var.org_id
+}
+
+provider "aws" {
+  region     = "us-east-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+resource "mongodbatlas_privatelink_endpoint" "test" {
+  project_id    = mongodbatlas_project.test.id
+  provider_name = "AWS"
+  region        = "us-east-1"
+}
+
+resource "aws_vpc_endpoint" "ptfe_service" {
+  vpc_id             = var.aws_vpc_id
+  service_name       = mongodbatlas_privatelink_endpoint.test.endpoint_service_name
+  vpc_endpoint_type  = "Interface"
+  subnet_ids         = [var.aws_subnet_ids]
+  security_group_ids = [var.aws_sg_ids]
+}
+
+resource "mongodbatlas_privatelink_endpoint_service" "test" {
+  project_id            = mongodbatlas_privatelink_endpoint.test.project_id
+  private_link_id       = mongodbatlas_privatelink_endpoint.test.private_link_id
+  endpoint_service_id = aws_vpc_endpoint.ptfe_service.id
+  provider_name = "AWS"
+}
