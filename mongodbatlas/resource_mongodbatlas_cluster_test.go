e2c8f7f74d13b3078c830b7feaf0fb0999b51765
Edgar Lopez <edgarlopez@pop-os.localdomain>
2021-07-23 10:58:35 -0600

    changed name of test

--- mongodbatlas/resource_mongodbatlas_cluster_test.go
+++ mongodbatlas/resource_mongodbatlas_cluster_test.go
@@ -939,7 +939,7 @@ func TestAccResourceMongoDBAtlasCluster_basicGCPRegionName(t *testing.T) {
 	})
 }
 
-func TestAccResourceMongoDBAtlasCluster_basicRegionsConfig(t *testing.T) {
+func TestAccResourceMongoDBAtlasCluster_RegionsConfig(t *testing.T) {
 	var (
 		resourceName = "mongodbatlas_cluster.test"
 		projectID    = os.Getenv("MONGODB_ATLAS_PROJECT_ID")
