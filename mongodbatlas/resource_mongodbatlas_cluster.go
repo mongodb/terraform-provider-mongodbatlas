3ac8f0a209798cb8e31ff231dc64328c59e654db
Abner Garcia <84406577+abner-dou@users.noreply.github.com>
2021-08-18 12:12:55 -0600

    INTMDB-219: Fixed cluster scaling issue (#515)
    * fixed cluster scaling issue

--- mongodbatlas/resource_mongodbatlas_cluster.go
+++ mongodbatlas/resource_mongodbatlas_cluster.go
@@ -9,18 +9,15 @@ import (
 	"net/http"
 	"reflect"
 	"regexp"
-	"time"
-
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-
 	"github.com/mwielbut/pointy"
 	"github.com/spf13/cast"
-
 	matlas "go.mongodb.org/atlas/mongodbatlas"
 )
 
@@ -207,14 +204,16 @@ func resourceMongoDBAtlasCluster() *schema.Resource {
 				Computed: true,
 			},
 			"provider_auto_scaling_compute_min_instance_size": {
-				Type:     schema.TypeString,
-				Optional: true,
-				Computed: true,
+				Type:             schema.TypeString,
+				Optional:         true,
+				Computed:         true,
+				DiffSuppressFunc: isEqualProviderAutoScalingMinInstanceSize,
 			},
 			"provider_auto_scaling_compute_max_instance_size": {
-				Type:     schema.TypeString,
-				Optional: true,
-				Computed: true,
+				Type:             schema.TypeString,
+				Optional:         true,
+				Computed:         true,
+				DiffSuppressFunc: isEqualProviderAutoScalingMaxInstanceSize,
 			},
 			"replication_factor": {
 				Type:     schema.TypeInt,
@@ -1524,4 +1523,30 @@ func clusterConnectionStringsSchema() *schema.Schema {
 			},
 		},
 	}
+}
+
+func isEqualProviderAutoScalingMinInstanceSize(k, old, newStr string, d *schema.ResourceData) bool {
+	canScaleDown, scaleDownOK := d.GetOk("auto_scaling_compute_scale_down_enabled")
+	canScaleUp, scaleUpOk := d.GetOk("auto_scaling_compute_enabled")
+
+	if !scaleDownOK || !scaleUpOk {
+		return true // if the return is true, it means that both values are the same and there's nothing to do
+	}
+
+	if canScaleUp.(bool) && canScaleDown.(bool) {
+		if old != newStr {
+			return false
+		}
+	}
+	return true
+}
+
+func isEqualProviderAutoScalingMaxInstanceSize(k, old, newStr string, d *schema.ResourceData) bool {
+	canScaleUp, _ := d.GetOk("auto_scaling_compute_enabled")
+	if canScaleUp != nil && canScaleUp.(bool) {
+		if old != newStr {
+			return false
+		}
+	}
+	return true
 }
