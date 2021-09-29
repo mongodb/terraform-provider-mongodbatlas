195ec6fcb319d8a468d0b78466ed2a339a8ab198
Abner Garcia <abner.garcia@digitalonus.com>
2021-09-29 13:40:04 -0600

    fix

--- mongodbatlas/resource_mongodbatlas_cluster_test.go
+++ mongodbatlas/resource_mongodbatlas_cluster_test.go
@@ -61,7 +61,6 @@ func TestAccResourceMongoDBAtlasCluster_basicAWS_simple(t *testing.T) {
 }
 
 func TestAccResourceMongoDBAtlasCluster_basicAWS_instanceScale(t *testing.T) {
-	//SkipTest(t) // Skipped for now because of paramater provider_disk_iops breaks the terraform flow
 	var (
 		cluster      matlas.Cluster
 		resourceName = "mongodbatlas_cluster.test"
