08b6e785bae1aee831c9d6d510a06f3c06e28700
Edgar Lopez <edgarlopez@pop-os.localdomain>
2021-10-08 09:59:59 -0600

    modified version

--- examples/test-upgrade/v110/cloud-backup-snapshot/v110/versions.tf
+++ examples/test-upgrade/v110/cloud-backup-snapshot/v110/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     mongodbatlas = {
       source  = "mongodb/mongodbatlas"
-      version = "0.1.0-dev"
+      version = "1.1.0"
     }
   }
   required_version = ">= 0.13"
