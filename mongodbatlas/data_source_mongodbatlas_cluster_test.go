6001072fbd79c5be298c9496f1588161f7283d01
Abner Garcia <84406577+abner-dou@users.noreply.github.com>
2021-12-14 10:38:44 -0700

    INTMDB-280: Fix cluster datasource scaling issue (#627)
    * fix cluster datasource scaling issue
    
    * deleted unused comment
    
    * fix documentation

--- mongodbatlas/data_source_mongodbatlas_cluster_test.go
+++ mongodbatlas/data_source_mongodbatlas_cluster_test.go
@@ -12,11 +12,13 @@ import (
 
 func TestAccDataSourceMongoDBAtlasCluster_basic(t *testing.T) {
 	var (
-		cluster        matlas.Cluster
-		resourceName   = "mongodbatlas_cluster.basic_ds"
-		dataSourceName = "data.mongodbatlas_cluster.basic_ds"
-		projectID      = os.Getenv("MONGODB_ATLAS_PROJECT_ID")
-		name           = fmt.Sprintf("test-acc-%s", acctest.RandString(10))
+		cluster         matlas.Cluster
+		resourceName    = "mongodbatlas_cluster.basic_ds"
+		dataSourceName  = "data.mongodbatlas_cluster.basic_ds"
+		projectID       = os.Getenv("MONGODB_ATLAS_PROJECT_ID")
+		name            = fmt.Sprintf("test-acc-%s", acctest.RandString(10))
+		minSizeInstance = "M20"
+		maxSizeInstance = "M80"
 	)
 
 	resource.ParallelTest(t, resource.TestCase{
@@ -25,7 +27,7 @@ func TestAccDataSourceMongoDBAtlasCluster_basic(t *testing.T) {
 		CheckDestroy:      testAccCheckMongoDBAtlasClusterDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccDataSourceMongoDBAtlasClusterConfig(projectID, name, "true"),
+				Config: testAccDataSourceMongoDBAtlasClusterConfig(projectID, name, "true", "true", "true", minSizeInstance, maxSizeInstance),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckMongoDBAtlasClusterExists(resourceName, &cluster),
 					testAccCheckMongoDBAtlasClusterAttributes(&cluster, name),
@@ -42,15 +44,19 @@ func TestAccDataSourceMongoDBAtlasCluster_basic(t *testing.T) {
 					resource.TestCheckResourceAttrSet(dataSourceName, "mongo_uri"),
 					resource.TestCheckResourceAttrSet(dataSourceName, "replication_specs.#"),
 					resource.TestCheckResourceAttrSet(dataSourceName, "replication_specs.0.regions_config.#"),
-					resource.TestCheckResourceAttr(resourceName, "labels.#", "2"),
-					resource.TestCheckResourceAttr(resourceName, "auto_scaling_disk_gb_enabled", "true"),
+					resource.TestCheckResourceAttr(dataSourceName, "labels.#", "3"),
+					resource.TestCheckResourceAttr(dataSourceName, "auto_scaling_disk_gb_enabled", "true"),
+					resource.TestCheckResourceAttr(dataSourceName, "auto_scaling_compute_enabled", "true"),
+					resource.TestCheckResourceAttr(dataSourceName, "auto_scaling_compute_scale_down_enabled", "true"),
+					resource.TestCheckResourceAttr(dataSourceName, "provider_auto_scaling_compute_min_instance_size", minSizeInstance),
+					resource.TestCheckResourceAttr(dataSourceName, "provider_auto_scaling_compute_max_instance_size", maxSizeInstance),
 				),
 			},
 		},
 	})
 }
 
-func testAccDataSourceMongoDBAtlasClusterConfig(projectID, name, backupEnabled string) string {
+func testAccDataSourceMongoDBAtlasClusterConfig(projectID, name, backupEnabled, autoScalingEnabled, scaleDownEnabled, minSizeName, maxSizeName string) string {
 	return fmt.Sprintf(`
 		resource "mongodbatlas_cluster" "basic_ds" {
 			project_id   = "%s"
@@ -82,11 +88,16 @@ func testAccDataSourceMongoDBAtlasClusterConfig(projectID, name, backupEnabled s
 				key   = "key 2"
 				value = "value 2"
 			}
+
+		auto_scaling_compute_enabled            = %s
+		auto_scaling_compute_scale_down_enabled = %s
+		provider_auto_scaling_compute_min_instance_size = "%s"
+		provider_auto_scaling_compute_max_instance_size = "%s"
 		}
 
 		data "mongodbatlas_cluster" "basic_ds" {
 			project_id = mongodbatlas_cluster.basic_ds.project_id
 			name 	     = mongodbatlas_cluster.basic_ds.name
 		}
-	`, projectID, name, backupEnabled)
+	`, projectID, name, backupEnabled, autoScalingEnabled, scaleDownEnabled, minSizeName, maxSizeName)
 }
