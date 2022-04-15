1983c815bc723bb8256950974ece4b7a6b0a65a8
admin <33664051+martinstibbe@users.noreply.github.com>
2022-04-15 08:43:07 -0500

    Update third party integration resource enabled to boolean

--- mongodbatlas/data_source_mongodbatlas_third_party_integration.go
+++ mongodbatlas/data_source_mongodbatlas_third_party_integration.go
@@ -125,7 +125,7 @@ func thirdPartyIntegrationSchema() *schema.Resource {
 				Optional: true,
 			},
 			"enabled": {
-				Type:     schema.TypeString,
+				Type:     schema.TypeBool,
 				Optional: true,
 			},
 		},
