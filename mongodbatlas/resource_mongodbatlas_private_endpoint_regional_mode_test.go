7e50f5da4f01d0dadfe81ee6453bce673f4920f5
Dosty <dev.dosty@gmail.com>
2022-04-19 16:09:48 -0500

    Removed unused testing infrastructure

--- mongodbatlas/resource_mongodbatlas_private_endpoint_regional_mode_test.go
+++ mongodbatlas/resource_mongodbatlas_private_endpoint_regional_mode_test.go
@@ -12,11 +12,8 @@ import (
 
 func TestAccResourceMongoDBAtlasPrivateEndpointRegionalMode_basic(t *testing.T) {
 	var (
-		resourceName  = "mongodbatlas_private_endpoint_regional_mode.test"
-		projectID     = os.Getenv("MONGODB_ATLAS_PROJECT_ID")
-		endpointID    = "vpce-jjg5e24qp93513h03"
-		commentOrigin = "this is a comment for adl private link endpoint"
-		commentUpdate = "this is a comment for adl private link endpoint [UPDATED]"
+		resourceName = "mongodbatlas_private_endpoint_regional_mode.test"
+		projectID    = os.Getenv("MONGODB_ATLAS_PROJECT_ID")
 	)
 
 	resource.ParallelTest(t, resource.TestCase{
@@ -40,57 +37,6 @@ func TestAccResourceMongoDBAtlasPrivateEndpointRegionalMode_basic(t *testing.T) 
 			},
 		},
 	})
-}
-
-func TestAccResourceMongoDBAtlasPrivateLinkEndpointServiceADL_importBasic(t *testing.T) {
-	var (
-		resourceName  = "mongodbatlas_privatelink_endpoint_service_adl.test"
-		projectID     = os.Getenv("MONGODB_ATLAS_PROJECT_ID")
-		endpointID    = "vpce-jjg5e24qp93513h03"
-		commentOrigin = "this is a comment for adl private link endpoint"
-	)
-	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:          func() { testAccPreCheck(t) },
-		ProviderFactories: testAccProviderFactories,
-		CheckDestroy:      testAccCheckMongoDBAtlasSearchIndexDestroy,
-		Steps: []resource.TestStep{
-			{
-				Config: testAccMongoDBAtlasPrivateLinkEndpointServiceADLConfig(projectID, endpointID, commentOrigin),
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr(resourceName, "endpoint_id", endpointID),
-					resource.TestCheckResourceAttr(resourceName, "type", "DATA_LAKE"),
-					resource.TestCheckResourceAttr(resourceName, "provider_name", "AWS"),
-					resource.TestCheckResourceAttr(resourceName, "comment", commentOrigin),
-				),
-			},
-			{
-				Config:            testAccMongoDBAtlasPrivateLinkEndpointServiceADLConfig(projectID, endpointID, commentOrigin),
-				ResourceName:      resourceName,
-				ImportStateIdFunc: testAccCheckMongoDBAtlasPrivateLinkEndpointServiceADLImportStateIDFunc(resourceName),
-				ImportState:       true,
-				ImportStateVerify: true,
-			},
-		},
-	})
-}
-
-func testAccCheckMongoDBAtlasPrivateLinkEndpointServiceADLDestroy(state *terraform.State) error {
-	conn := testAccProvider.Meta().(*MongoDBClient).Atlas
-
-	for _, rs := range state.RootModule().Resources {
-		if rs.Type != "mongodbatlas_privatelink_endpoint_service_adl" {
-			continue
-		}
-
-		ids := decodeStateID(rs.Primary.ID)
-
-		privateLink, _, err := conn.DataLakes.GetPrivateLinkEndpoint(context.Background(), ids["project_id"], ids["endpoint_id"])
-		if err == nil && privateLink != nil {
-			return fmt.Errorf("endpoint_id (%s) still exists", ids["endpoint_id"])
-		}
-	}
-
-	return nil
 }
 
 func testAccMongoDBAtlasPrivateEndpointRegionalModeConfig(projectID string, enabled bool) string {
@@ -124,18 +70,5 @@ func testAccCheckMongoDBAtlasPrivateEndpointRegionalModeExists(resourceName stri
 		}
 
 		return fmt.Errorf("regional mode for project_id (%s) does not exist", ids["project_id"])
-	}
-}
-
-func testAccCheckMongoDBAtlasPrivateLinkEndpointServiceADLImportStateIDFunc(resourceName string) resource.ImportStateIdFunc {
-	return func(s *terraform.State) (string, error) {
-		rs, ok := s.RootModule().Resources[resourceName]
-		if !ok {
-			return "", fmt.Errorf("not found: %s", resourceName)
-		}
-
-		ids := decodeStateID(rs.Primary.ID)
-
-		return fmt.Sprintf("%s--%s", ids["project_id"], ids["endpoint_id"]), nil
 	}
 }
