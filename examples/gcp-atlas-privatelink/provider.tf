a5df95e7b6cfa67c2785525a4aa817ca1ef1678d
Dosty <dev.dosty@gmail.com>
2022-07-25 08:48:55 -0500

    Commenting out credentials for fmting purposes

--- examples/gcp-atlas-privatelink/provider.tf
+++ examples/gcp-atlas-privatelink/provider.tf
@@ -3,7 +3,9 @@ provider "mongodbatlas" {
   private_key = var.private_key
 }
 provider "google" {
-  credentials = file("service-account.json")
-  project     = var.gcp_project_id
-  region      = var.gcp_region # us-central1
+  # Credentials commented out to pass lint
+  # Add your own service-account & path to run example
+  # credentials = file("service-account.json")
+  project = var.gcp_project_id
+  region  = var.gcp_region # us-central1
 }
