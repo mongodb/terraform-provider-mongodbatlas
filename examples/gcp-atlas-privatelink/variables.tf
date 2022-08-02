bfc365e3d63f0b5cca38c29bd14babf6ffd06421
Dosty Everts <dev.dosty@gmail.com>
2022-08-01 23:43:57 -0500

    INTMDB-331 - Update GCP documentation (#793)
    * Updated gcp compute address & forwarding rule examples
    
    * Fixed formatting
    
    * Commenting out credentials for fmting purposes

--- examples/gcp-atlas-privatelink/variables.tf
+++ examples/gcp-atlas-privatelink/variables.tf
@@ -1,10 +1,15 @@
-
-variable "project_id" {
-  default = "PROJECT-ID"
-}
-variable "gcp_project" {
+variable "gcp_project_id" {
   default = "GCP-PROJECT"
 }
 variable "gcp_region" {
   default = "us-central1"
+}
+variable "project_id" {
+  default = "PROJECT-ID"
+}
+variable "public_key" {
+  description = "Public API key to authenticate to Atlas"
+}
+variable "private_key" {
+  description = "Private API key to authenticate to Atlas"
 }
