72b7dd8e49952395223012542e1176f6cb1abc0d
Dosty Everts <dev.dosty@gmail.com>
2022-10-28 19:27:39 -0500

    INTMDB-370 -- PIT Restore Example (#870)
    * INTMDB-369: Updated validation logic for cloud_backup_snapshot_restore_job (#813)
    
    * Updated validation logic for cloud_backup_snapshot_restore_job
    
    * Fixed lint errors
    
    * Updated test-upgrade v110 to use pit restore
    
    * Fixed var name in example for pit ts
    
    * Updated v110 example to use cloud backup snapshot resource with pit and valid instructions
    
    * Removed changes to v110 example and created a v146 example
    
    * Fixed the v110 example to work with disk_size requirements and added link to new examples on the tf docs.
    
    * Fixed documentation and moved example
    
    * Moved example again, and fixed links in documentation
    
    * Fixed formatting on example
    
    * Fixed formatting to account for removal of disk size

--- examples/atlas-backup-snapshot-restore-job/point-in-time/main.tf
+++ examples/atlas-backup-snapshot-restore-job/point-in-time/main.tf
@@ -14,24 +14,27 @@ resource "mongodbatlas_cluster" "cluster_test" {
   provider_region_name        = "US_EAST_1"
   provider_instance_size_name = "M10"
   cloud_backup                = true # enable cloud provider snapshots
+  pit_enabled                 = true
 }
 
 
-resource "mongodbatlas_cloud_provider_snapshot" "test" {
+resource "mongodbatlas_cloud_backup_snapshot" "test" {
   project_id        = mongodbatlas_cluster.cluster_test.project_id
   cluster_name      = mongodbatlas_cluster.cluster_test.name
   description       = "My description"
   retention_in_days = "1"
 }
 
-resource "mongodbatlas_cloud_provider_snapshot_restore_job" "test" {
-  project_id   = mongodbatlas_cloud_provider_snapshot.test.project_id
-  cluster_name = mongodbatlas_cloud_provider_snapshot.test.cluster_name
-  snapshot_id  = mongodbatlas_cloud_provider_snapshot.test.id
+resource "mongodbatlas_cloud_backup_snapshot_restore_job" "test" {
+  count        = (var.point_in_time_utc_seconds == 0 ? 0 : 1)
+  project_id   = mongodbatlas_cloud_backup_snapshot.test.project_id
+  cluster_name = mongodbatlas_cloud_backup_snapshot.test.cluster_name
+  snapshot_id  = mongodbatlas_cloud_backup_snapshot.test.id
 
   delivery_type_config {
-    automated           = true
-    target_cluster_name = mongodbatlas_cluster.cluster_test.name
-    target_project_id   = mongodbatlas_cluster.cluster_test.project_id
+    point_in_time             = true
+    target_cluster_name       = mongodbatlas_cluster.cluster_test.name
+    target_project_id         = mongodbatlas_cluster.cluster_test.project_id
+    point_in_time_utc_seconds = var.point_in_time_utc_seconds
   }
 }
