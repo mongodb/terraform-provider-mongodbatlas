4f869839f3b68279e16acb1d05750f0ac2332cd7
Dosty <dev.dosty@gmail.com>
2022-10-31 13:26:23 -0500

    Using different endpointID values for the endpoint service adl tests

--- mongodbatlas/resource_mongodbatlas_privatelink_endpoint_service_adl_test.go
+++ mongodbatlas/resource_mongodbatlas_privatelink_endpoint_service_adl_test.go
@@ -14,7 +14,7 @@ func TestAccResourceMongoDBAtlasPrivateLinkEndpointServiceADL_basic(t *testing.T
 	var (
 		resourceName  = "mongodbatlas_privatelink_endpoint_service_adl.test"
 		projectID     = os.Getenv("MONGODB_ATLAS_PROJECT_ID")
-		endpointID    = "vpce-jjg5e24qp93513h03"
+		endpointID    = "vpce-jjg5e24qp93513h04"
 		commentOrigin = "this is a comment for adl private link endpoint"
 		commentUpdate = "this is a comment for adl private link endpoint UPDATED"
 	)
@@ -102,11 +102,11 @@ func testAccCheckMongoDBAtlasPrivateLinkEndpointServiceADLDestroy(state *terrafo
 func testAccMongoDBAtlasPrivateLinkEndpointServiceADLConfig(projectID, endpointID, comment string) string {
 	return fmt.Sprintf(`
 		resource "mongodbatlas_privatelink_endpoint_service_adl" "test" {
-			project_id   = "%[1]s"
-			endpoint_id  = "%[2]s"
-			comment      = "%[3]s"
-			type		 = "DATA_LAKE"
-			provider_name	 = "AWS"
+			project_id    = "%[1]s"
+			endpoint_id   = "%[2]s"
+			comment       = "%[3]s"
+			type          = "DATA_LAKE"
+			provider_name = "AWS"
 		}
 	`, projectID, endpointID, comment)
 }
