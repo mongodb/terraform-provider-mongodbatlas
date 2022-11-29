99c06b0d47c4adb9584adb2a4f8b6af85f4ef20b
martinstibbe <33664051+martinstibbe@users.noreply.github.com>
2022-11-29 10:02:22 -0600

    INTMDB-498: Updated Prometheus Example (#942)
    * Serverless Endpoint Service Doc Bug (#930)
    
    * Update CHANGELOG.md
    
    * Chore(deps): Bump github.com/gruntwork-io/terratest (#936)
    
    Bumps [github.com/gruntwork-io/terratest](https://github.com/gruntwork-io/terratest) from 0.41.0 to 0.41.3.
    - [Release notes](https://github.com/gruntwork-io/terratest/releases)
    - [Commits](https://github.com/gruntwork-io/terratest/compare/v0.41.0...v0.41.3)
    
    ---
    updated-dependencies:
    - dependency-name: github.com/gruntwork-io/terratest
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Add additional context for prometheus example
    
    * Update Readme.md
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: Zuhair Ahmed <zuhair.ahmed@mongodb.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- examples/MongoDB-Atlas-Third-Party-Integration/versions.tf
+++ examples/MongoDB-Atlas-Third-Party-Integration/versions.tf
@@ -1,7 +1,8 @@
 terraform {
   required_providers {
     mongodbatlas = {
-      source = "mongodb/mongodbatlas"
+      source  = "mongodb/mongodbatlas"
+      version = "1.6.0"
     }
   }
   required_version = ">= 0.13"
