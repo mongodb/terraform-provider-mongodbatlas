4461cbfa0fc34f9b3b39a83da562203039bbec52
Dosty <dev.dosty@gmail.com>
2022-11-30 10:30:52 -0600

    Added INITIATING back to list of Pending statuses on delete

--- mongodbatlas/resource_mongodbatlas_privatelink_endpoint_service.go
+++ mongodbatlas/resource_mongodbatlas_privatelink_endpoint_service.go
@@ -296,7 +296,7 @@ func resourceMongoDBAtlasPrivateEndpointServiceLinkDelete(ctx context.Context, d
 		}
 
 		stateConf := &resource.StateChangeConf{
-			Pending:    []string{"NONE", "PENDING_ACCEPTANCE", "PENDING", "DELETING"},
+			Pending:    []string{"NONE", "PENDING_ACCEPTANCE", "PENDING", "DELETING", "INITIATING"},
 			Target:     []string{"REJECTED", "DELETED", "FAILED"},
 			Refresh:    resourceServiceEndpointRefreshFunc(ctx, conn, projectID, providerName, privateLinkID, endpointServiceID),
 			Timeout:    d.Timeout(schema.TimeoutDelete),
