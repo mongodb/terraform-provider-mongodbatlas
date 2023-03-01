8d176ad09c166d912e6f84e6a1ac6191fdb359dd
robbertkauffman <robbertkauffman@gmail.com>
2023-03-01 12:29:40 -0500

    Improved encryption at rest example (#1060)
    * create key in KMS and restrict IAM policy to key
    
    * added readme
    
    * removed other encryption examples as they're redundant
    
    * fixed formatting

--- examples/atlas-encryption-at-rest/aws/providers.tf
+++ examples/atlas-encryption-at-rest/aws/providers.tf
@@ -1,9 +1,6 @@
 provider "mongodbatlas" {
-  public_key  = var.public_key
-  private_key = var.private_key
 }
+
 provider "aws" {
-  access_key = var.access_key
-  secret_key = var.secret_key
-  region     = var.aws_region
+  region = var.aws_region
 }
