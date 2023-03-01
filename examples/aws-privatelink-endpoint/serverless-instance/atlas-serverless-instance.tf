8ab7c917013e6f3202c8a07820f8c0c03557c3eb
Dosty <dev.dosty@gmail.com>
2023-02-28 21:11:00 -0600

    fixed tflint failure

--- examples/aws-privatelink-endpoint/serverless-instance/atlas-serverless-instance.tf
+++ examples/aws-privatelink-endpoint/serverless-instance/atlas-serverless-instance.tf
@@ -1,6 +1,6 @@
 resource "mongodbatlas_serverless_instance" "aws_private_connection" {
   project_id                              = var.project_id
-  name                                    = "aws-private-connection"
+  name                                    = var.instance_name
   provider_settings_backing_provider_name = "AWS"
   provider_settings_provider_name         = "SERVERLESS"
   provider_settings_region_name           = "US_EAST_1"
