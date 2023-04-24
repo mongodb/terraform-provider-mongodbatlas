adaf966192126cdad1fd07fa80375d92ad94eff1
Zuhair Ahmed <zuhair.ahmed@mongodb.com>
2023-04-23 21:42:26 -0400

    Update atlas-cluster.tf

--- examples/aws-privatelink-endpoint/cluster/atlas-cluster.tf
+++ examples/aws-privatelink-endpoint/cluster/atlas-cluster.tf
@@ -20,4 +20,4 @@ resource "mongodbatlas_cluster" "aws_private_connection" {
   provider_instance_size_name = "M10"
 
   depends_on = [mongodbatlas_privatelink_endpoint_service.pe_east_service]
-}
+} 
