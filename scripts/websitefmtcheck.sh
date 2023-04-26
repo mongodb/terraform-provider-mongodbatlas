2aed0775150079e4a3eaaa49b02a59f710743db6
andreaangiolillo <andrea.angiolillo@mongodb.com>
2023-04-26 10:49:20 +0100

    Update websitefmtcheck.sh

--- scripts/websitefmtcheck.sh
+++ scripts/websitefmtcheck.sh
@@ -5,14 +5,14 @@ set -eou pipefail
 npm list codedown > /dev/null 2>&1 || npm install --no-save codedown > /dev/null 2>&1
 
 problems=false
-find website -name '*.markdown' -print0 | while IFS= read -r -d '' f; do
+while IFS= read -r -d '' f; do
     if [ "${1-}" = "diff" ]; then
         echo "$f"
         < "$f" node_modules/.bin/codedown hcl | terraform fmt -diff=true -
     else
-        < "$f" node_modules/.bin/codedown hcl | terraform fmt -check=true - || problems=true && echo "Formatting errors in $f"
+        < "$f" node_modules/.bin/codedown hcl | terraform fmt -check=true - || { problems=true && echo "Formatting errors in $f"; }
     fi
-done
+done < <(find website -name '*.markdown' -print0)
 
 if [ "$problems" = true ] ; then
     exit 1
