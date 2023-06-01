fde9b07c69db66b14bd67aff9a67bf44e1c0f686
Aastha Mahendru <aastha.mahendru@mongodb.com>
2023-06-01 10:21:43 +0100

    lint fix

--- examples/cluster-outage-simulation/main.tf
+++ examples/cluster-outage-simulation/main.tf
@@ -5,30 +5,30 @@ resource "mongodbatlas_advanced_cluster" "atlas_cluster" {
 
   replication_specs {
     region_configs {
-      provider_name         = var.provider_name
+      provider_name = var.provider_name
       electable_specs {
         instance_size = var.provider_instance_size_name
       }
-      region_name           = "US_EAST_1"
-      priority              = 7
-    }
-  
-    region_configs {
-      provider_name         = var.provider_name
-      electable_specs {
-        instance_size = var.provider_instance_size_name
-      }
-      region_name           = "US_EAST_2"
-      priority              = 6
+      region_name = "US_EAST_1"
+      priority    = 7
     }
 
     region_configs {
-      provider_name         = var.provider_name
+      provider_name = var.provider_name
       electable_specs {
         instance_size = var.provider_instance_size_name
       }
-      region_name           = "US_WEST_1"
-      priority              = 5
+      region_name = "US_EAST_2"
+      priority    = 6
+    }
+
+    region_configs {
+      provider_name = var.provider_name
+      electable_specs {
+        instance_size = var.provider_instance_size_name
+      }
+      region_name = "US_WEST_1"
+      priority    = 5
     }
   }
 }
