81d5cca6ec76d4caeed8c5c143c2cf21139e2a73
maastha <122359335+maastha@users.noreply.github.com>
2023-06-01 07:41:07 +0100

    Update main.tf

--- examples/cluster-outage-simulation/main.tf
+++ examples/cluster-outage-simulation/main.tf
@@ -1,33 +1,36 @@
-resource "mongodbatlas_cluster" "atlas_cluster" {
+resource "mongodbatlas_advanced_cluster" "atlas_cluster" {
   project_id   = var.project_id
   name         = var.atlas_cluster_name
   cluster_type = var.atlas_cluster_type
 
-  provider_name               = var.provider_name
-  provider_instance_size_name = var.provider_instance_size_name
-
   replication_specs {
-    num_shards = 1
-    regions_config {
-      region_name     = "US_EAST_1"
-      electable_nodes = 3
-      priority        = 7
-      read_only_nodes = 0
+    region_configs {
+      provider_name         = var.provider_name
+      electable_specs {
+        instance_size = var.provider_instance_size_name
+      }
+      region_name           = "US_EAST_1"
+      priority              = 7
     }
-    regions_config {
-      region_name     = "US_EAST_2"
-      electable_nodes = 2
-      priority        = 6
-      read_only_nodes = 0
+  
+    region_configs {
+      provider_name         = var.provider_name
+      electable_specs {
+        instance_size = var.provider_instance_size_name
+      }
+      region_name           = "US_EAST_2"
+      priority              = 6
     }
-    regions_config {
-      region_name     = "US_WEST_1"
-      electable_nodes = 2
-      priority        = 5
-      read_only_nodes = 2
+
+    region_configs {
+      provider_name         = var.provider_name
+      electable_specs {
+        instance_size = var.provider_instance_size_name
+      }
+      region_name           = "US_WEST_1"
+      priority              = 5
     }
   }
-}
 
 resource "mongodbatlas_cluster_outage_simulation" "outage_simulation" {
   cluster_name = var.atlas_cluster_name
