a6bb1ea5bb7a5fa6b611040982ea8f426413078c
Andrea Angiolillo <andrea.angiolillo@mongodb.com>
2023-06-02 09:51:49 +0100

    Update mongodbatlas/provider.go
    Co-authored-by: zach-carr <54542042+zach-carr@users.noreply.github.com>

--- mongodbatlas/provider.go
+++ mongodbatlas/provider.go
@@ -39,7 +39,7 @@ var (
 
 const (
 	endPointSTSDefault = "https://sts.amazonaws.com"
-	DeprecationMessage = "this resource is deprecated, and will be removed in %s release, please transition as soon as possible to %s"
+	DeprecationMessage = "this resource is deprecated and will be removed in %s, please transition to %s"
 )
 
 type SecretData struct {
