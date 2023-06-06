f2a683e2a88d95c54742d9823ec13cd347f9f6f8
andreaangiolillo <andrea.angiolillo@mongodb.com>
2023-06-06 18:30:26 +0100

    Added example

---
+++ examples/atlas-backup-snapshot-restore-job/point-in-time-advance-cluster/main.tf
@@ -0,0 +1,65 @@
+# This will Create a Project,  Cluster, cloud backup snapshot and restore job
+
+resource "mongodbatlas_project" "project_test" {
+  name   = var.project_name
+  org_id = var.org_id
+}
+
+resource "mongodbatlas_cluster" "cluster_test" {
+  project_id = mongodbatlas_project.project_test.id
+  name       = var.cluster_name
+
+  # Provider Settings "block"
+  provider_name               = "AWS"
+  provider_region_name        = "US_EAST_1"
+  provider_instance_size_name = "M10"
+  cloud_backup                = true # enable cloud provider snapshots
+  pit_enabled                 = true
+  retain_backups_enabled      = true # keep the backup snapshopts once the cluster is deleted
+}
+
+resource "mongodbatlas_advanced_cluster" "cluster_test" {
+  project_id   = mongodbatlas_project.project_test.id
+  name         = var.cluster_name
+  cluster_type = "REPLICASET"
+
+  replication_specs {
+    num_shards = 1
+
+    region_configs {
+      electable_specs {
+        instance_size = "M10"
+      }
+
+      provider_name = "AWS"
+      region_name   = "US_EAST_1"
+      priority      = 7
+    }
+  }
+
+  backup_enabled         = true                       # enable cloud backup snapshots
+  pit_enabled            = true                       # Flag that indicates whether the cluster uses continuous cloud backups
+  retain_backups_enabled = var.retain_backups_enabled # keep the backup snapshopts once the cluster is deleted
+}
+
+
+resource "mongodbatlas_cloud_backup_snapshot" "test" {
+  project_id        = mongodbatlas_cluster.cluster_test.project_id
+  cluster_name      = mongodbatlas_cluster.cluster_test.name
+  description       = "My description"
+  retention_in_days = "1"
+}
+
+resource "mongodbatlas_cloud_backup_snapshot_restore_job" "test" {
+  count        = (var.point_in_time_utc_seconds == 0 ? 0 : 1)
+  project_id   = mongodbatlas_cloud_backup_snapshot.test.project_id
+  cluster_name = mongodbatlas_cloud_backup_snapshot.test.cluster_name
+  snapshot_id  = mongodbatlas_cloud_backup_snapshot.test.id
+
+  delivery_type_config {
+    point_in_time             = true
+    target_cluster_name       = mongodbatlas_cluster.cluster_test.name
+    target_project_id         = mongodbatlas_cluster.cluster_test.project_id
+    point_in_time_utc_seconds = var.point_in_time_utc_seconds
+  }
+}
