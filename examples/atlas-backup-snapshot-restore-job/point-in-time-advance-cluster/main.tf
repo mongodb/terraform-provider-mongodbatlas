2ee19175a225e73f6fb799d2391e2e43d547a4a2
andreaangiolillo <andrea.angiolillo@mongodb.com>
2023-06-06 18:47:46 +0100

    Update main.tf

--- examples/atlas-backup-snapshot-restore-job/point-in-time-advance-cluster/main.tf
+++ examples/atlas-backup-snapshot-restore-job/point-in-time-advance-cluster/main.tf
@@ -5,20 +5,7 @@ resource "mongodbatlas_project" "project_test" {
   org_id = var.org_id
 }
 
-resource "mongodbatlas_cluster" "cluster_test" {
-  project_id = mongodbatlas_project.project_test.id
-  name       = var.cluster_name
-
-  # Provider Settings "block"
-  provider_name               = "AWS"
-  provider_region_name        = "US_EAST_1"
-  provider_instance_size_name = "M10"
-  cloud_backup                = true # enable cloud provider snapshots
-  pit_enabled                 = true
-  retain_backups_enabled      = true # keep the backup snapshopts once the cluster is deleted
-}
-
-resource "mongodbatlas_advanced_cluster" "cluster_test" {
+resource "mongodbatlas_advanced_cluster" "advanced_cluster_test" {
   project_id   = mongodbatlas_project.project_test.id
   name         = var.cluster_name
   cluster_type = "REPLICASET"
@@ -29,6 +16,7 @@ resource "mongodbatlas_advanced_cluster" "cluster_test" {
     region_configs {
       electable_specs {
         instance_size = "M10"
+        node_count    = 3
       }
 
       provider_name = "AWS"
@@ -42,10 +30,9 @@ resource "mongodbatlas_advanced_cluster" "cluster_test" {
   retain_backups_enabled = var.retain_backups_enabled # keep the backup snapshopts once the cluster is deleted
 }
 
-
 resource "mongodbatlas_cloud_backup_snapshot" "test" {
-  project_id        = mongodbatlas_cluster.cluster_test.project_id
-  cluster_name      = mongodbatlas_cluster.cluster_test.name
+  project_id        = mongodbatlas_advanced_cluster.advanced_cluster_test.project_id
+  cluster_name      = mongodbatlas_advanced_cluster.advanced_cluster_test.name
   description       = "My description"
   retention_in_days = "1"
 }
@@ -58,8 +45,8 @@ resource "mongodbatlas_cloud_backup_snapshot_restore_job" "test" {
 
   delivery_type_config {
     point_in_time             = true
-    target_cluster_name       = mongodbatlas_cluster.cluster_test.name
-    target_project_id         = mongodbatlas_cluster.cluster_test.project_id
+    target_cluster_name       = mongodbatlas_advanced_cluster.advanced_cluster_test.name
+    target_project_id         = mongodbatlas_advanced_cluster.advanced_cluster_test.project_id
     point_in_time_utc_seconds = var.point_in_time_utc_seconds
   }
 }
