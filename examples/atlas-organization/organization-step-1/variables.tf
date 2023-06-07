b40661dab52a85adf6c80c57ade41161c0753904
admin <33664051+martinstibbe@users.noreply.github.com>
2023-06-06 20:32:27 -0500

    Update variables.tf

--- examples/atlas-organization/organization-step-1/variables.tf
+++ examples/atlas-organization/organization-step-1/variables.tf
@@ -11,42 +11,6 @@ variable "org_owner_id" {
   description = "MongoDB Organization Owner ID"
 }
 
-variable "project_name" {
-  type        = string
-  description = "The MongoDB Atlas Project Name"
-}
-variable "cluster_name" {
-  type        = string
-  description = "The MongoDB Atlas Cluster Name"
-}
-variable "cloud_provider" {
-  type        = string
-  description = "The cloud provider to use, must be AWS, GCP or AZURE"
-}
-variable "region" {
-  type        = string
-  description = "MongoDB Atlas Cluster Region, must be a region for the provider given"
-}
-variable "mongodbversion" {
-  type        = string
-  description = "The Major MongoDB Version"
-}
-variable "dbuser" {
-  type        = string
-  description = "MongoDB Atlas Database User Name"
-}
-variable "dbuser_password" {
-  type        = string
-  description = "MongoDB Atlas Database User Password"
-}
-variable "database_name" {
-  type        = string
-  description = "The database in the cluster to limit the database user to, the database does not have to exist yet"
-}
-variable "ip_address" {
-  type        = string
-  description = "The IP address that the cluster will be accessed from, can also be a CIDR range or AWS security group"
-}
 
 
 
