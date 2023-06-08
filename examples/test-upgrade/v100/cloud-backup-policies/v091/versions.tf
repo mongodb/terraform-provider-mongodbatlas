04308819309777599692833596230e6c5fafc015
andreaangiolillo <andrea.angiolillo@mongodb.com>
2023-06-08 18:11:03 +0100

    updated tests

--- examples/test-upgrade/v100/cloud-backup-policies/v091/versions.tf
+++ examples/test-upgrade/v100/cloud-backup-policies/v091/versions.tf
@@ -2,7 +2,6 @@ terraform {
   required_providers {
     mongodbatlas = {
       source  = "mongodb/mongodbatlas"
-      version = "0.9.1"
     }
   }
   required_version = ">= 0.13"
--- examples/test-upgrade/v110/cloud-backup-snapshot/v110/versions.tf
+++ examples/test-upgrade/v110/cloud-backup-snapshot/v110/versions.tf
@@ -2,7 +2,6 @@ terraform {
   required_providers {
     mongodbatlas = {
       source  = "mongodb/mongodbatlas"
-      version = "~> 1.0.2"
     }
   }
   required_version = ">= 0.13"
