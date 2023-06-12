e5c29bf66ec2d61766e71fed51b3882f031737b6
andreaangiolillo <andrea.angiolillo@mongodb.com>
2023-06-12 17:52:31 +0100

    Update versions.tf

--- examples/test-upgrade/encryption-at-rest/aws/v091/versions.tf
+++ examples/test-upgrade/encryption-at-rest/aws/v091/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     mongodbatlas = {
       source  = "mongodb/mongodbatlas"
-      version = "0.9.1"
+      version = "~> 1.0"
     }
     aws = {
       source  = "hashicorp/aws"
