8a3ebd8b3c4912ccfbaf1102bdd9aaac6e127125
andreaangiolillo <andrea.angiolillo@mongodb.com>
2023-06-12 17:30:42 +0100

    fixes

--- examples/atlas-api-key/create-and-assign-pak-together/variables.tf
+++ examples/atlas-api-key/create-and-assign-pak-together/variables.tf
@@ -1,14 +1,16 @@
 variable "public_key" {
   description = "Public API key to authenticate to Atlas"
+  type        = string
 }
 variable "private_key" {
   description = "Private API key to authenticate to Atlas"
+  type        = string
 }
 variable "project_id" {
   description = "Atlas project name"
-  default     = ""
+  type        = string
 }
 variable "org_id" {
   description = "Atlas organization id"
-  default     = ""
+  type        = string
 }
