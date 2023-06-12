85d74d4f413170d084c67faffb9d3a1c89e0c36d
andreaangiolillo <andrea.angiolillo@mongodb.com>
2023-06-12 20:40:48 +0100

    Update data_source_mongodbatlas_search_indexes_test.go

--- mongodbatlas/data_source_mongodbatlas_search_indexes_test.go
+++ mongodbatlas/data_source_mongodbatlas_search_indexes_test.go
@@ -32,7 +32,6 @@ func TestAccConfigDSSearchIndexes_basic(t *testing.T) {
 					resource.TestCheckResourceAttrSet(datasourceName, "results.#"),
 					resource.TestCheckResourceAttrSet(datasourceName, "results.0.index_id"),
 					resource.TestCheckResourceAttrSet(datasourceName, "results.0.name"),
-					resource.TestCheckResourceAttrSet(datasourceName, "results.0.analyzer"),
 				),
 			},
 		},
@@ -84,7 +83,6 @@ func testAccMongoDBAtlasSearchIndexesDSConfig(projectID, clusterName, databaseNa
 			collection_name = "%s"
 			page_num = 1
 			items_per_page = 100
-			analyzer = "lucene.simple"
 			
 		}
 	`, testAccMongoDBAtlasSearchIndexConfig(projectID, clusterName), databaseName, collectionName)
