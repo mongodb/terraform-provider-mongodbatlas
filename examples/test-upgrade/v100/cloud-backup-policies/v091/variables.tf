499f05ceeccce80accc4189db705020d238f2a9c
andreaangiolillo <andrea.angiolillo@mongodb.com>
2023-06-12 18:57:43 +0100

    Update variables.tf

--- examples/test-upgrade/v100/cloud-backup-policies/v091/variables.tf
+++ examples/test-upgrade/v100/cloud-backup-policies/v091/variables.tf
@@ -1,12 +1,12 @@
 variable "project_name" {
   description = "Atlas project name"
-  type = string
+  type        = string
 }
 variable "org_id" {
   description = "The organization ID"
-  type = string
+  type        = string
 }
 variable "cluster_name" {
   description = "Cluster name"
-  type = string
+  type        = string
 }
