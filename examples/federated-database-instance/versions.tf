a8146da2223455c616195680c3358dded78066cb
andreaangiolillo <andrea.angiolillo@mongodb.com>
2023-06-12 17:29:45 +0100

    fixes

--- examples/federated-database-instance/versions.tf
+++ examples/federated-database-instance/versions.tf
@@ -1,10 +1,12 @@
 terraform {
   required_providers {
     mongodbatlas = {
-      source = "mongodb/mongodbatlas"
+      source  = "mongodb/mongodbatlas"
+      version = "~> 1.0"
     }
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
   required_version = ">= 0.13"
