3e468b2b63a623de8b33094f12cab032e9cd5b9e
andreaangiolillo <andrea.angiolillo@mongodb.com>
2023-06-22 15:02:57 +0200

    Update resource_mongodbatlas_team_test.go

--- mongodbatlas/resource_mongodbatlas_team_test.go
+++ mongodbatlas/resource_mongodbatlas_team_test.go
@@ -46,8 +46,7 @@ func TestAccConfigRSTeam_basic(t *testing.T) {
 			{
 				Config: testAccMongoDBAtlasTeamConfig(orgID, updatedName,
 					[]string{
-						"edgar.lopez@digitalonus.com",
-						"antonio.cabrera@digitalonus.com",
+						username,
 					},
 				),
 				Check: resource.ComposeTestCheckFunc(
@@ -55,7 +54,7 @@ func TestAccConfigRSTeam_basic(t *testing.T) {
 					testAccCheckMongoDBAtlasTeamAttributes(&team, updatedName),
 					resource.TestCheckResourceAttrSet(resourceName, "org_id"),
 					resource.TestCheckResourceAttr(resourceName, "name", updatedName),
-					resource.TestCheckResourceAttr(resourceName, "usernames.#", "2"),
+					resource.TestCheckResourceAttr(resourceName, "usernames.#", "1"),
 				),
 			},
 			{
