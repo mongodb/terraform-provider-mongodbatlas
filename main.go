7a31aa68a0a765cd91932e51346f5671cd593340
Aastha Mahendru <aastha.mahendru@mongodb.com>
2023-07-03 11:04:15 +0100

    main upgrade

--- main.go
+++ main.go
@@ -5,27 +5,70 @@ import (
 	"flag"
 	"log"
 
-	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
+	framework "github.com/cloudflare/terraform-provider-cloudflare/internal/framework/provider"
+	"github.com/hashicorp/terraform-plugin-framework/providerserver"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov6/tf6server"
+	"github.com/hashicorp/terraform-plugin-mux/tf5to6server"
+	"github.com/hashicorp/terraform-plugin-mux/tf6muxserver"
 	"github.com/mongodb/terraform-provider-mongodbatlas/mongodbatlas"
 )
 
 func main() {
+	// var debugMode bool
+
+	// flag.BoolVar(&debugMode, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	// flag.Parse()
+
+	// opts := &plugin.ServeOpts{ProviderFunc: mongodbatlas.Provider}
+
+	// if debugMode {
+	// 	err := plugin.Debug(context.Background(), "registry.terraform.io/mongodb/mongodbatlas", opts)
+
+	// 	if err != nil {
+	// 		log.Fatal(err.Error())
+	// 	}
+
+	// 	return
+	// }
+
+	// plugin.Serve(opts)
+
 	var debugMode bool
 
 	flag.BoolVar(&debugMode, "debug", false, "set to true to run the provider with support for debuggers like delve")
 	flag.Parse()
 
-	opts := &plugin.ServeOpts{ProviderFunc: mongodbatlas.Provider}
-
-	if debugMode {
-		err := plugin.Debug(context.Background(), "registry.terraform.io/mongodb/mongodbatlas", opts)
-
-		if err != nil {
-			log.Fatal(err.Error())
-		}
-
-		return
+	ctx := context.Background()
+	upgradedSdkProvider, err := tf5to6server.UpgradeServer(ctx, mongodbatlas.Provider().GRPCProvider)
+	if err != nil {
+		log.Fatal(err)
+	}
+	providers := []func() tfprotov6.ProviderServer{
+		func() tfprotov6.ProviderServer {
+			return upgradedSdkProvider
+		},
+		providerserver.NewProtocol6(framework.New(version)()),
 	}
 
-	plugin.Serve(opts)
+	muxServer, err := tf6muxserver.NewMuxServer(ctx, providers...)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var serveOpts []tf6server.ServeOpt
+	if debugMode {
+		serveOpts = append(serveOpts, tf6server.WithManagedDebug())
+	}
+
+	err = tf6server.Serve(
+		"registry.terraform.io/mongodb/mongodbatlas",
+		muxServer.ProviderServer,
+		serveOpts...,
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
