d22f4c0a21e33ffe8ae67d27574126aa3f99f3bd
andreaangiolillo <andrea.angiolillo@mongodb.com>
2023-07-12 11:12:59 +0200

    introduced again removed tests

--- mongodbatlas/resource_mongodbatlas_advanced_cluster_migration_test.go
+++ mongodbatlas/resource_mongodbatlas_advanced_cluster_migration_test.go
@@ -1,135 +1,132 @@
 package mongodbatlas
 
-// import (
-// 	"fmt"
-// 	"testing"
+import (
+	"fmt"
+	"testing"
 
-// 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
-// )
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+)
 
-// This resource cannot migrate to "github.com/hashicorp/terraform-plugin-sdk/terraform"
-// until the related TF resource is migrated to the new framework
+func TestAccClusterRSAdvancedClusterMigrateState_empty_advancedConfig(t *testing.T) {
+	v0State := map[string]interface{}{
+		"project_id":   "test-id",
+		"name":         "test-cluster",
+		"cluster_type": "REPLICASET",
+		"replication_specs": []interface{}{
+			map[string]interface{}{
+				"region_configs": []interface{}{
+					map[string]interface{}{
+						"electable_specs": []interface{}{
+							map[string]interface{}{
+								"instance_size": "M30",
+								"node_count":    3,
+							},
+						},
+						"provider_name": "AWS",
+						"region_name":   "US_EAST_1",
+						"priority":      7,
+					},
+				},
+			},
+		},
+		"bi_connector": []interface{}{
+			map[string]interface{}{
+				"enabled":         1,
+				"read_preference": "secondary",
+			},
+		},
+	}
 
-// func TestAccClusterRSAdvancedClusterMigrateState_empty_advancedConfig(t *testing.T) {
-// 	v0State := map[string]interface{}{
-// 		"project_id":   "test-id",
-// 		"name":         "test-cluster",
-// 		"cluster_type": "REPLICASET",
-// 		"replication_specs": []interface{}{
-// 			map[string]interface{}{
-// 				"region_configs": []interface{}{
-// 					map[string]interface{}{
-// 						"electable_specs": []interface{}{
-// 							map[string]interface{}{
-// 								"instance_size": "M30",
-// 								"node_count":    3,
-// 							},
-// 						},
-// 						"provider_name": "AWS",
-// 						"region_name":   "US_EAST_1",
-// 						"priority":      7,
-// 					},
-// 				},
-// 			},
-// 		},
-// 		"bi_connector": []interface{}{
-// 			map[string]interface{}{
-// 				"enabled":         1,
-// 				"read_preference": "secondary",
-// 			},
-// 		},
-// 	}
+	v0Config := terraform.NewResourceConfigRaw(v0State)
+	diags := resourceMongoDBAtlasAdvancedClusterResourceV0().Validate(v0Config)
 
-// 	v0Config := terraform.NewResourceConfigRaw(v0State)
-// 	diags := resourceMongoDBAtlasAdvancedClusterResourceV0().Validate(v0Config)
+	if len(diags) > 0 {
+		t.Error("test precondition failed - invalid mongodb cluster v0 config")
 
-// 	if len(diags) > 0 {
-// 		t.Error("test precondition failed - invalid mongodb cluster v0 config")
+		return
+	}
 
-// 		return
-// 	}
+	// test migrate function
+	v1State := migrateBIConnectorConfig(v0State)
 
-// 	// test migrate function
-// 	v1State := migrateBIConnectorConfig(v0State)
+	v1Config := terraform.NewResourceConfigRaw(v1State)
+	diags = resourceMongoDBAtlasAdvancedCluster().Validate(v1Config)
+	if len(diags) > 0 {
+		fmt.Println(diags)
+		t.Error("migrated cluster advanced config is invalid")
 
-// 	v1Config := terraform.NewResourceConfigRaw(v1State)
-// 	diags = resourceMongoDBAtlasAdvancedCluster().Validate(v1Config)
-// 	if len(diags) > 0 {
-// 		fmt.Println(diags)
-// 		t.Error("migrated cluster advanced config is invalid")
+		return
+	}
+}
 
-// 		return
-// 	}
-// }
+func TestAccClusterRSAdvancedClusterV0StateUpgrade_ReplicationSpecs(t *testing.T) {
+	v0State := map[string]interface{}{
+		"project_id":     "test-id",
+		"name":           "test-cluster",
+		"cluster_type":   "REPLICASET",
+		"backup_enabled": true,
+		"disk_size_gb":   256,
+		"replication_specs": []interface{}{
+			map[string]interface{}{
+				"zone_name": "Test Zone",
+				"region_configs": []interface{}{
+					map[string]interface{}{
+						"priority":      7,
+						"provider_name": "AWS",
+						"region_name":   "US_EAST_1",
+						"electable_specs": []interface{}{
+							map[string]interface{}{
+								"instance_size": "M30",
+								"node_count":    3,
+							},
+						},
+						"read_only_specs": []interface{}{
+							map[string]interface{}{
+								"disk_iops":     0,
+								"instance_size": "M30",
+								"node_count":    0,
+							},
+						},
+						"auto_scaling": []interface{}{
+							map[string]interface{}{
+								"compute_enabled":            true,
+								"compute_max_instance_size":  "M60",
+								"compute_min_instance_size":  "M30",
+								"compute_scale_down_enabled": true,
+								"disk_gb_enabled":            false,
+							},
+						},
+					},
+				},
+			},
+		},
+	}
 
-// func TestAccClusterRSAdvancedClusterV0StateUpgrade_ReplicationSpecs(t *testing.T) {
-// 	v0State := map[string]interface{}{
-// 		"project_id":     "test-id",
-// 		"name":           "test-cluster",
-// 		"cluster_type":   "REPLICASET",
-// 		"backup_enabled": true,
-// 		"disk_size_gb":   256,
-// 		"replication_specs": []interface{}{
-// 			map[string]interface{}{
-// 				"zone_name": "Test Zone",
-// 				"region_configs": []interface{}{
-// 					map[string]interface{}{
-// 						"priority":      7,
-// 						"provider_name": "AWS",
-// 						"region_name":   "US_EAST_1",
-// 						"electable_specs": []interface{}{
-// 							map[string]interface{}{
-// 								"instance_size": "M30",
-// 								"node_count":    3,
-// 							},
-// 						},
-// 						"read_only_specs": []interface{}{
-// 							map[string]interface{}{
-// 								"disk_iops":     0,
-// 								"instance_size": "M30",
-// 								"node_count":    0,
-// 							},
-// 						},
-// 						"auto_scaling": []interface{}{
-// 							map[string]interface{}{
-// 								"compute_enabled":            true,
-// 								"compute_max_instance_size":  "M60",
-// 								"compute_min_instance_size":  "M30",
-// 								"compute_scale_down_enabled": true,
-// 								"disk_gb_enabled":            false,
-// 							},
-// 						},
-// 					},
-// 				},
-// 			},
-// 		},
-// 	}
+	v0Config := terraform.NewResourceConfigRaw(v0State)
+	diags := resourceMongoDBAtlasAdvancedClusterResourceV0().Validate(v0Config)
 
-// 	v0Config := terraform.NewResourceConfigRaw(v0State)
-// 	diags := resourceMongoDBAtlasAdvancedClusterResourceV0().Validate(v0Config)
+	if len(diags) > 0 {
+		fmt.Println(diags)
+		t.Error("test precondition failed - invalid mongodb cluster v0 config")
 
-// 	if len(diags) > 0 {
-// 		fmt.Println(diags)
-// 		t.Error("test precondition failed - invalid mongodb cluster v0 config")
+		return
+	}
 
-// 		return
-// 	}
+	// test migrate function
+	v1State := migrateBIConnectorConfig(v0State)
 
-// 	// test migrate function
-// 	v1State := migrateBIConnectorConfig(v0State)
+	v1Config := terraform.NewResourceConfigRaw(v1State)
+	diags = resourceMongoDBAtlasAdvancedCluster().Validate(v1Config)
+	if len(diags) > 0 {
+		fmt.Println(diags)
+		t.Error("migrated advanced cluster replication_specs invalid")
 
-// 	v1Config := terraform.NewResourceConfigRaw(v1State)
-// 	diags = resourceMongoDBAtlasAdvancedCluster().Validate(v1Config)
-// 	if len(diags) > 0 {
-// 		fmt.Println(diags)
-// 		t.Error("migrated advanced cluster replication_specs invalid")
+		return
+	}
 
-// 		return
-// 	}
+	if len(v1State["replication_specs"].([]interface{})) != len(v0State["replication_specs"].([]interface{})) {
+		t.Error("migrated replication specs did not contain the same number of elements")
 
-// 	if len(v1State["replication_specs"].([]interface{})) != len(v0State["replication_specs"].([]interface{})) {
-// 		t.Error("migrated replication specs did not contain the same number of elements")
-
-// 		return
-// 	}
-// }
+		return
+	}
+}
--- mongodbatlas/resource_mongodbatlas_cluster_migration_test.go
+++ mongodbatlas/resource_mongodbatlas_cluster_migration_test.go
@@ -1,133 +1,130 @@
 package mongodbatlas
 
-// import (
-// 	"fmt"
-// 	"testing"
+import (
+	"fmt"
+	"testing"
 
-// 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
-// )
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+)
 
-// This resource cannot migrate to "github.com/hashicorp/terraform-plugin-sdk/terraform"
-// until the related TF resource is migrated to the new framework
+func TestAccClusterRSClusterMigrateState_empty_advancedConfig(t *testing.T) {
+	v0State := map[string]interface{}{
+		"project_id":                  "test-id",
+		"name":                        "test-cluster",
+		"provider_instance_size_name": "M10",
+		"provider_name":               "AWS",
+		"replication_specs": []interface{}{
+			map[string]interface{}{
+				"num_shards": 1,
+			},
+		},
+		"advanced_configuration": map[string]interface{}{},
+	}
 
-// func TestAccClusterRSClusterMigrateState_empty_advancedConfig(t *testing.T) {
-// 	v0State := map[string]interface{}{
-// 		"project_id":                  "test-id",
-// 		"name":                        "test-cluster",
-// 		"provider_instance_size_name": "M10",
-// 		"provider_name":               "AWS",
-// 		"replication_specs": []interface{}{
-// 			map[string]interface{}{
-// 				"num_shards": 1,
-// 			},
-// 		},
-// 		"advanced_configuration": map[string]interface{}{},
-// 	}
+	v0Config := terraform.NewResourceConfigRaw(v0State)
+	diags := resourceMongoDBAtlasClusterResourceV0().Validate(v0Config)
 
-// 	v0Config := terraform.NewResourceConfigRaw(v0State)
-// 	diags := resourceMongoDBAtlasClusterResourceV0().Validate(v0Config)
+	if len(diags) > 0 {
+		t.Error("test precondition failed - invalid mongodb cluster v0 config")
 
-// 	if len(diags) > 0 {
-// 		t.Error("test precondition failed - invalid mongodb cluster v0 config")
+		return
+	}
 
-// 		return
-// 	}
+	// test migrate function
+	v1State := migrateAdvancedConfiguration(v0State)
 
-// 	// test migrate function
-// 	v1State := migrateAdvancedConfiguration(v0State)
+	v1Config := terraform.NewResourceConfigRaw(v1State)
+	diags = resourceMongoDBAtlasCluster().Validate(v1Config)
+	if len(diags) > 0 {
+		fmt.Println(diags)
+		t.Error("migrated cluster advanced config is invalid")
 
-// 	v1Config := terraform.NewResourceConfigRaw(v1State)
-// 	diags = resourceMongoDBAtlasCluster().Validate(v1Config)
-// 	if len(diags) > 0 {
-// 		fmt.Println(diags)
-// 		t.Error("migrated cluster advanced config is invalid")
+		return
+	}
+}
 
-// 		return
-// 	}
-// }
+func TestAccClusterRSClusterMigrateState_with_advancedConfig(t *testing.T) {
+	v0State := map[string]interface{}{
+		"project_id":                  "test-id",
+		"name":                        "test-cluster",
+		"provider_instance_size_name": "M10",
+		"provider_name":               "AWS",
+		"replication_specs": []interface{}{
+			map[string]interface{}{
+				"num_shards": 1,
+			},
+		},
+		"advanced_configuration": map[string]interface{}{
+			"fail_index_key_too_long":              "true",
+			"javascript_enabled":                   "true",
+			"minimum_enabled_tls_protocol":         "TLS1_2",
+			"no_table_scan":                        "false",
+			"oplog_size_mb":                        "1000",
+			"sample_refresh_interval_bi_connector": "310",
+			"sample_size_bi_connector":             "110",
+		},
+	}
 
-// func TestAccClusterRSClusterMigrateState_with_advancedConfig(t *testing.T) {
-// 	v0State := map[string]interface{}{
-// 		"project_id":                  "test-id",
-// 		"name":                        "test-cluster",
-// 		"provider_instance_size_name": "M10",
-// 		"provider_name":               "AWS",
-// 		"replication_specs": []interface{}{
-// 			map[string]interface{}{
-// 				"num_shards": 1,
-// 			},
-// 		},
-// 		"advanced_configuration": map[string]interface{}{
-// 			"fail_index_key_too_long":              "true",
-// 			"javascript_enabled":                   "true",
-// 			"minimum_enabled_tls_protocol":         "TLS1_2",
-// 			"no_table_scan":                        "false",
-// 			"oplog_size_mb":                        "1000",
-// 			"sample_refresh_interval_bi_connector": "310",
-// 			"sample_size_bi_connector":             "110",
-// 		},
-// 	}
+	v0Config := terraform.NewResourceConfigRaw(v0State)
+	diags := resourceMongoDBAtlasClusterResourceV0().Validate(v0Config)
+	if len(diags) > 0 {
+		t.Error("test precondition failed - invalid mongodb cluster v0 config")
 
-// 	v0Config := terraform.NewResourceConfigRaw(v0State)
-// 	diags := resourceMongoDBAtlasClusterResourceV0().Validate(v0Config)
-// 	if len(diags) > 0 {
-// 		t.Error("test precondition failed - invalid mongodb cluster v0 config")
+		return
+	}
 
-// 		return
-// 	}
+	// test migrate function
+	v1State := migrateAdvancedConfiguration(v0State)
 
-// 	// test migrate function
-// 	v1State := migrateAdvancedConfiguration(v0State)
+	v1Config := terraform.NewResourceConfigRaw(v1State)
+	diags = resourceMongoDBAtlasCluster().Validate(v1Config)
+	if len(diags) > 0 {
+		fmt.Println(diags)
+		t.Error("migrated cluster advanced config is invalid")
 
-// 	v1Config := terraform.NewResourceConfigRaw(v1State)
-// 	diags = resourceMongoDBAtlasCluster().Validate(v1Config)
-// 	if len(diags) > 0 {
-// 		fmt.Println(diags)
-// 		t.Error("migrated cluster advanced config is invalid")
+		return
+	}
+}
 
-// 		return
-// 	}
-// }
+func TestAccClusterRSClusterMigrateState_with_defaultAdvancedConfig_v0_5_1(t *testing.T) {
+	v0State := map[string]interface{}{
+		"project_id":                  "test-id",
+		"name":                        "test-cluster",
+		"provider_instance_size_name": "M10",
+		"provider_name":               "AWS",
+		"replication_specs": []interface{}{
+			map[string]interface{}{
+				"num_shards": 1,
+			},
+		},
+		"advanced_configuration": map[string]interface{}{
+			"fail_index_key_too_long":              "true",
+			"javascript_enabled":                   "true",
+			"minimum_enabled_tls_protocol":         "TLS1_2",
+			"no_table_scan":                        "false",
+			"oplog_size_mb":                        "",
+			"sample_refresh_interval_bi_connector": "",
+			"sample_size_bi_connector":             "",
+		},
+	}
 
-// func TestAccClusterRSClusterMigrateState_with_defaultAdvancedConfig_v0_5_1(t *testing.T) {
-// 	v0State := map[string]interface{}{
-// 		"project_id":                  "test-id",
-// 		"name":                        "test-cluster",
-// 		"provider_instance_size_name": "M10",
-// 		"provider_name":               "AWS",
-// 		"replication_specs": []interface{}{
-// 			map[string]interface{}{
-// 				"num_shards": 1,
-// 			},
-// 		},
-// 		"advanced_configuration": map[string]interface{}{
-// 			"fail_index_key_too_long":              "true",
-// 			"javascript_enabled":                   "true",
-// 			"minimum_enabled_tls_protocol":         "TLS1_2",
-// 			"no_table_scan":                        "false",
-// 			"oplog_size_mb":                        "",
-// 			"sample_refresh_interval_bi_connector": "",
-// 			"sample_size_bi_connector":             "",
-// 		},
-// 	}
+	v0Config := terraform.NewResourceConfigRaw(v0State)
+	diags := resourceMongoDBAtlasClusterResourceV0().Validate(v0Config)
+	if len(diags) > 0 {
+		t.Error("test precondition failed - invalid mongodb cluster v0 config")
 
-// 	v0Config := terraform.NewResourceConfigRaw(v0State)
-// 	diags := resourceMongoDBAtlasClusterResourceV0().Validate(v0Config)
-// 	if len(diags) > 0 {
-// 		t.Error("test precondition failed - invalid mongodb cluster v0 config")
+		return
+	}
 
-// 		return
-// 	}
+	// test migrate function
+	v1State := migrateAdvancedConfiguration(v0State)
 
-// 	// test migrate function
-// 	v1State := migrateAdvancedConfiguration(v0State)
+	v1Config := terraform.NewResourceConfigRaw(v1State)
+	diags = resourceMongoDBAtlasCluster().Validate(v1Config)
+	if len(diags) > 0 {
+		fmt.Println(diags)
+		t.Error("migrated cluster advanced config is invalid")
 
-// 	v1Config := terraform.NewResourceConfigRaw(v1State)
-// 	diags = resourceMongoDBAtlasCluster().Validate(v1Config)
-// 	if len(diags) > 0 {
-// 		fmt.Println(diags)
-// 		t.Error("migrated cluster advanced config is invalid")
-
-// 		return
-// 	}
-// }
+		return
+	}
+}
