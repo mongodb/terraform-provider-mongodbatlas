0d62211574bfc27d6f543ce0e93489a11f26308c
andreaangiolillo <andrea.angiolillo@mongodb.com>
2023-07-17 15:22:25 +0200

    INTMDB-546: shared tier restore jobs data source

---
+++ mongodbatlas/data_source_mongodbatlas_cloud_shared_tier_restore_job.go
@@ -0,0 +1,129 @@
+package mongodbatlas
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+// This datasource does not have a resource: we tested it manually
+func dataSourceMongoDBAtlasCloudSharedTierRestoreJobs() *schema.Resource {
+	return &schema.Resource{
+		ReadContext: dataSourceMongoDBAtlasCloudSharedTierRestoreJobsRead,
+		Schema: map[string]*schema.Schema{
+			"project_id": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"cluster_name": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"job_id": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"status": {
+				Type:     schema.TypeBool,
+				Computed: true,
+			},
+			"target_project_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"target_deployment_item_name": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"snapshot_url": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"snapshot_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"snapshot_finished_date": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"restore_scheduled_date": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"restore_finished_date": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"delivery_type": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"expiration_date": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func dataSourceMongoDBAtlasCloudSharedTierRestoreJobsRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	conn := meta.(*MongoDBClient).AtlasV2
+
+	jobID := d.Get("job_id").(string)
+	projectID := d.Get("project_id").(string)
+	clusterName := d.Get("cluster_name").(string)
+
+	job, resp, err := conn.SharedTierRestoreJobsApi.GetSharedClusterBackupRestoreJob(ctx, clusterName, projectID, jobID).Execute()
+	defer resp.Body.Close()
+
+	if err != nil {
+		return diag.FromErr(fmt.Errorf("error getting shared tier restore job '%s': %w", jobID, err))
+	}
+
+	if err = d.Set("status", job.Status); err != nil {
+		return diag.FromErr(fmt.Errorf("error setting `status` for shared tier restore job '%s': %w", jobID, err))
+	}
+
+	if err = d.Set("target_project_id", job.TargetProjectId); err != nil {
+		return diag.FromErr(fmt.Errorf("error setting `target_project_id` for shared tier restore job '%s': %w", jobID, err))
+	}
+
+	if err = d.Set("target_deployment_item_name", job.TargetDeploymentItemName); err != nil {
+		return diag.FromErr(fmt.Errorf("error setting `target_deployment_item_name` for shared tier restore job '%s': %w", jobID, err))
+	}
+
+	if err = d.Set("snapshot_url", job.SnapshotUrl); err != nil {
+		return diag.FromErr(fmt.Errorf("error setting `snapshot_url` for shared tier restore job '%s': %w", jobID, err))
+	}
+
+	if err = d.Set("snapshot_id", job.SnapshotId); err != nil {
+		return diag.FromErr(fmt.Errorf("error setting `snapshot_id` for shared tier restore job '%s': %w", jobID, err))
+	}
+
+	if err = d.Set("snapshot_finished_date", job.SnapshotFinishedDate); err != nil {
+		return diag.FromErr(fmt.Errorf("error setting `snapshot_finished_date` for shared tier restore job '%s': %w", jobID, err))
+	}
+
+	if err = d.Set("restore_scheduled_date", job.RestoreScheduledDate); err != nil {
+		return diag.FromErr(fmt.Errorf("error setting `restore_scheduled_date` for shared tier restore job '%s': %w", jobID, err))
+	}
+
+	if err = d.Set("restore_finished_date", job.RestoreFinishedDate); err != nil {
+		return diag.FromErr(fmt.Errorf("error setting `restore_finished_date` for shared tier restore job '%s': %w", jobID, err))
+	}
+
+	if err = d.Set("delivery_type", job.DeliveryType); err != nil {
+		return diag.FromErr(fmt.Errorf("error setting `delivery_type` for shared tier restore job '%s': %w", jobID, err))
+	}
+
+	if err = d.Set("expiration_date", job.ExpirationDate); err != nil {
+		return diag.FromErr(fmt.Errorf("error setting `expiration_date` for shared tier restore job '%s': %w", jobID, err))
+	}
+
+	d.SetId(*job.Id)
+
+	return nil
+}
