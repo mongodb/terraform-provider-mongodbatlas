266971b9e1c543534893dc4b74248a825c51c79e
andreaangiolillo <andrea.angiolillo@mongodb.com>
2023-07-19 13:05:25 +0200

    Update data_source_mongodbatlas_cloud_shared_tier_restore_job.go

--- mongodbatlas/data_source_mongodbatlas_cloud_shared_tier_restore_job.go
+++ mongodbatlas/data_source_mongodbatlas_cloud_shared_tier_restore_job.go
@@ -103,15 +103,15 @@ func dataSourceMongoDBAtlasCloudSharedTierRestoreJobsRead(ctx context.Context, d
 		return diag.FromErr(fmt.Errorf("error setting `snapshot_id` for shared tier restore job '%s': %w", jobID, err))
 	}
 
-	if err = d.Set("snapshot_finished_date", job.SnapshotFinishedDate.String()); err != nil {
+	if err = d.Set("snapshot_finished_date", job.GetSnapshotFinishedDate().String()); err != nil {
 		return diag.FromErr(fmt.Errorf("error setting `snapshot_finished_date` for shared tier restore job '%s': %w", jobID, err))
 	}
 
-	if err = d.Set("restore_scheduled_date", job.RestoreScheduledDate.String()); err != nil {
+	if err = d.Set("restore_scheduled_date", job.GetRestoreScheduledDate().String()); err != nil {
 		return diag.FromErr(fmt.Errorf("error setting `restore_scheduled_date` for shared tier restore job '%s': %w", jobID, err))
 	}
 
-	if err = d.Set("restore_finished_date", job.RestoreFinishedDate.String()); err != nil {
+	if err = d.Set("restore_finished_date", job.GetRestoreFinishedDate().String()); err != nil {
 		return diag.FromErr(fmt.Errorf("error setting `restore_finished_date` for shared tier restore job '%s': %w", jobID, err))
 	}
 
@@ -119,7 +119,7 @@ func dataSourceMongoDBAtlasCloudSharedTierRestoreJobsRead(ctx context.Context, d
 		return diag.FromErr(fmt.Errorf("error setting `delivery_type` for shared tier restore job '%s': %w", jobID, err))
 	}
 
-	if err = d.Set("expiration_date", job.ExpirationDate.String()); err != nil {
+	if err = d.Set("expiration_date", job.GetExpirationDate().String()); err != nil {
 		return diag.FromErr(fmt.Errorf("error setting `expiration_date` for shared tier restore job '%s': %w", jobID, err))
 	}
 
