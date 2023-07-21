7f06cacf727dfae9fd8cebb6f8698fbd9b1e145e
Andrea Angiolillo <andrea.angiolillo@mongodb.com>
2023-07-21 08:51:54 +0200

    INTMDB-546: [Terraform] Feature Add: Shared-Tier Restore Job (#1324)

--- mongodbatlas/provider.go
+++ mongodbatlas/provider.go
@@ -233,6 +233,8 @@ func getDataSourcesMap() map[string]*schema.Resource {
 		"mongodbatlas_serverless_instance":                                          dataSourceMongoDBAtlasServerlessInstance(),
 		"mongodbatlas_serverless_instances":                                         dataSourceMongoDBAtlasServerlessInstances(),
 		"mongodbatlas_cluster_outage_simulation":                                    dataSourceMongoDBAtlasClusterOutageSimulation(),
+		"mongodbatlas_shared_tier_restore_job":                                      dataSourceMongoDBAtlasCloudSharedTierRestoreJob(),
+		"mongodbatlas_shared_tier_restore_jobs":                                     dataSourceMongoDBAtlasCloudSharedTierRestoreJobs(),
 	}
 	return dataSourcesMap
 }
