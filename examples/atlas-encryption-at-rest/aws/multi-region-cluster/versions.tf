844f421b555125478fa8d31be138f77532537a94
andreaangiolillo <andrea.angiolillo@mongodb.com>
2023-07-26 17:37:09 +0200

    Update versions.tf

--- examples/atlas-encryption-at-rest/aws/multi-region-cluster/versions.tf
+++ examples/atlas-encryption-at-rest/aws/multi-region-cluster/versions.tf
@@ -1,7 +1,8 @@
 terraform {
   required_providers {
     mongodbatlas = {
-      source = "mongodb/mongodbatlas"
+      source  = "mongodb/mongodbatlas"
+      version = "~> 1.0"
     }
     aws = {
       source  = "hashicorp/aws"
