3e211cfb5cd26e41c4eea528940c2aa2d0e27373
Andrea Angiolillo <andrea.angiolillo@mongodb.com>
2023-07-28 10:43:40 +0200

    doc: [Terraform] Create new example for BYOK and multi-region cluster (#1349)

--- examples/atlas-encryption-at-rest/aws/multi-region-cluster/main.tf
+++ examples/atlas-encryption-at-rest/aws/multi-region-cluster/main.tf
@@ -1,20 +1,33 @@
+module "multi-region-cluster" {
+  atlas_project_id   = mongodbatlas_encryption_at_rest.test.project_id
+  provider_name      = "AWS"
+  aws_region_shard_1 = var.aws_region_shard_1
+  aws_region_shard_2 = var.aws_region_shard_2
+  cluster_name       = var.cluster_name
+  instance_size      = "M10"
+  source             = "./modules/multi-region-cluster"
+}
+
+resource "mongodbatlas_project" "project" {
+  name   = var.atlas_project_name
+  org_id = var.atlas_org_id
+}
+
 resource "mongodbatlas_cloud_provider_access_setup" "setup_only" {
-  project_id    = var.atlas_project_id
+  project_id    = mongodbatlas_project.project.id
   provider_name = "AWS"
 }
 
 resource "mongodbatlas_cloud_provider_access_authorization" "auth_role" {
-  project_id = var.atlas_project_id
+  project_id = mongodbatlas_project.project.id
   role_id    = mongodbatlas_cloud_provider_access_setup.setup_only.role_id
-
   aws {
     iam_assumed_role_arn = aws_iam_role.test_role.arn
   }
 }
 
 resource "mongodbatlas_encryption_at_rest" "test" {
-  project_id = var.atlas_project_id
-
+  project_id = mongodbatlas_project.project.id
   aws_kms_config {
     enabled                = true
     customer_master_key_id = aws_kms_key.kms_key.id
@@ -23,11 +36,4 @@ resource "mongodbatlas_encryption_at_rest" "test" {
   }
 }
 
-# uncomment below when importing an existing cluster
-# resource "mongodbatlas_cluster" "my_cluster" {
-#   project_id                  = var.atlas_project_id
-#   provider_name               = "AWS"
-#   encryption_at_rest_provider = "AWS"
-#   name                        = "MyCluster"
-#   provider_instance_size_name = "M10"
-# }
+
