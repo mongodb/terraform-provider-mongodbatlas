559009aa43a7fdd7015f8305f0ca596ce858271a
AgustinBettati <bettatiagustin@gmail.com>
2023-08-17 11:21:09 +0200

    add migration tests for all scenarios of list attributes

--- mongodbatlas/fw_resource_mongodbatlas_alert_configuration_migration_test.go
+++ mongodbatlas/fw_resource_mongodbatlas_alert_configuration_migration_test.go
@@ -1,6 +1,7 @@
 package mongodbatlas
 
 import (
+	"fmt"
 	"os"
 	"testing"
 
@@ -10,7 +11,7 @@ import (
 	matlas "go.mongodb.org/atlas/mongodbatlas"
 )
 
-func TestAccConfigRSAlertConfiguration_Migration_Notifications(t *testing.T) {
+func TestAccConfigRSAlertConfiguration_Migration_NotificationsWithMetricThreshold(t *testing.T) {
 	var (
 		resourceName = "mongodbatlas_alert_configuration.test"
 		orgID        = os.Getenv("MONGODB_ATLAS_ORG_ID")
@@ -51,7 +52,7 @@ func TestAccConfigRSAlertConfiguration_Migration_Notifications(t *testing.T) {
 	})
 }
 
-func TestAccConfigRSAlertConfiguration_Migration_With_Threshold(t *testing.T) {
+func TestAccConfigRSAlertConfiguration_Migration_WithThreshold(t *testing.T) {
 	var (
 		resourceName = "mongodbatlas_alert_configuration.test"
 		orgID        = os.Getenv("MONGODB_ATLAS_ORG_ID")
@@ -89,4 +90,117 @@ func TestAccConfigRSAlertConfiguration_Migration_With_Threshold(t *testing.T) {
 			},
 		},
 	})
+}
+
+func TestAccConfigRSAlertConfiguration_Migration_EmptyOptionalBlocks(t *testing.T) {
+	var (
+		resourceName = "mongodbatlas_alert_configuration.test"
+		orgID        = os.Getenv("MONGODB_ATLAS_ORG_ID")
+		projectName  = acctest.RandomWithPrefix("test-acc")
+		alert        = &matlas.AlertConfiguration{}
+		config       = testAccMongoDBAtlasAlertConfigurationConfigEmptyOptionalBlocks(orgID, projectName)
+	)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheckBasic(t) },
+		CheckDestroy: testAccCheckMongoDBAtlasAlertConfigurationDestroy,
+		Steps: []resource.TestStep{
+			{
+				ExternalProviders: map[string]resource.ExternalProvider{
+					"mongodbatlas": {
+						VersionConstraint: "1.11.0",
+						Source:            "mongodb/mongodbatlas",
+					},
+				},
+				Config: config,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckMongoDBAtlasAlertConfigurationExists(resourceName, alert),
+					resource.TestCheckResourceAttrSet(resourceName, "project_id"),
+				),
+			},
+			{
+				ProtoV6ProviderFactories: testAccProviderV6Factories,
+				Config:                   config,
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PostApplyPreRefresh: []plancheck.PlanCheck{
+						DebugPlan(),
+					},
+				},
+				PlanOnly: true,
+			},
+		},
+	})
+}
+
+func TestAccConfigRSAlertConfiguration_Migration_MultipleMatchers(t *testing.T) {
+	var (
+		resourceName = "mongodbatlas_alert_configuration.test"
+		orgID        = os.Getenv("MONGODB_ATLAS_ORG_ID")
+		projectName  = acctest.RandomWithPrefix("test-acc")
+		alert        = &matlas.AlertConfiguration{}
+		config       = testAccMongoDBAtlasAlertConfigurationConfigWithMatchers(orgID, projectName, true, false, true,
+			matlas.Matcher{
+				FieldName: "TYPE_NAME",
+				Operator:  "EQUALS",
+				Value:     "SECONDARY",
+			},
+			matlas.Matcher{
+				FieldName: "TYPE_NAME",
+				Operator:  "CONTAINS",
+				Value:     "MONGOS",
+			})
+	)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheckBasic(t) },
+		CheckDestroy: testAccCheckMongoDBAtlasAlertConfigurationDestroy,
+		Steps: []resource.TestStep{
+			{
+				ExternalProviders: map[string]resource.ExternalProvider{
+					"mongodbatlas": {
+						VersionConstraint: "1.11.0",
+						Source:            "mongodb/mongodbatlas",
+					},
+				},
+				Config: config,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckMongoDBAtlasAlertConfigurationExists(resourceName, alert),
+					resource.TestCheckResourceAttrSet(resourceName, "project_id"),
+				),
+			},
+			{
+				ProtoV6ProviderFactories: testAccProviderV6Factories,
+				Config:                   config,
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PostApplyPreRefresh: []plancheck.PlanCheck{
+						DebugPlan(),
+					},
+				},
+				PlanOnly: true,
+			},
+		},
+	})
+}
+
+func testAccMongoDBAtlasAlertConfigurationConfigEmptyOptionalBlocks(orgID, projectName string) string {
+	return fmt.Sprintf(`
+		resource "mongodbatlas_project" "test" {
+			name   = %[2]q
+			org_id = %[1]q
+		}
+		resource "mongodbatlas_alert_configuration" "test" {
+			project_id = mongodbatlas_project.test.id
+			event_type = "NO_PRIMARY"
+			enabled    = true
+
+			notification {
+				type_name     = "GROUP"
+				interval_min  = 5
+				delay_min     = 0
+				sms_enabled   = true
+				email_enabled = false
+				roles = ["GROUP_DATA_ACCESS_READ_ONLY"]
+			}
+		}
+	`, orgID, projectName)
 }
