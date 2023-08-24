43b9a9f407e206d21f50bf427c2a00c50dad4c0f
andreaangiolillo <andrea.angiolillo@mongodb.com>
2023-08-24 15:36:15 +0100

    added additional migration tests

--- mongodbatlas/fw_resource_mongodbatlas_project_ip_access_list_test.go
+++ mongodbatlas/fw_resource_mongodbatlas_project_ip_access_list_test.go
@@ -96,13 +96,13 @@ func TestAccProjectRSProjectIPAccessList_SettingCIDRBlock(t *testing.T) {
 func TestAccProjectRSProjectIPAccessList_SettingAWSSecurityGroup(t *testing.T) {
 	SkipTestExtCred(t)
 	resourceName := "mongodbatlas_project_ip_access_list.test"
-	vpcID := os.Getenv("AWS_VPC_ID")
-	vpcCIDRBlock := os.Getenv("AWS_VPC_CIDR_BLOCK")
-	awsAccountID := os.Getenv("AWS_ACCOUNT_ID")
-	awsRegion := os.Getenv("AWS_REGION")
+	vpcID := "vpc-0355c13f3d8af46c1"
+	vpcCIDRBlock := "10.2.10.0/24"
+	awsAccountID := "358363220050"
+	awsRegion := "EU_WEST_1"
 	providerName := "AWS"
 
-	projectID := os.Getenv("MONGODB_ATLAS_PROJECT_ID")
+	projectID := "64b8d3219c4cdc3f9108e318"
 	awsSGroup := "sg-0026348ec11780bd1"
 	comment := fmt.Sprintf("TestAcc for awsSecurityGroup (%s)", awsSGroup)
 
@@ -170,7 +170,6 @@ func TestAccProjectRSProjectIPAccessList_SettingMultiple(t *testing.T) {
 		accessList = append(accessList, entry)
 	}
 
-	//TODO: make testAccCheckMongoDBAtlasProjectIPAccessListExists dynamic
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:                 func() { testAccPreCheckBasic(t) },
 		ProtoV6ProviderFactories: testAccProviderV6Factories,
