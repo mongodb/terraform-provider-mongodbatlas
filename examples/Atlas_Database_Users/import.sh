cff98dc40ed6535173ae5393fd26c2a480da7665
andreaangiolillo <andrea.angiolillo@mongodb.com>
2023-08-30 11:08:02 +0100

    generated doc

--- examples/Atlas_Database_Users/import.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Database users can be imported using project ID and username, in the format `project_id-username-auth_database_name`, e.g.
-terraform import mongodbatlas_database_user.my_user 1112222b3bf99403840e8934-my_user-admin
-
-# NOTE: Terraform will want to change the password after importing the user if a password argument is specified.
