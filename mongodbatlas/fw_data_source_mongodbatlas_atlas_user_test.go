84939bf65cdef2b31b598e3902f50f28edad74fd
AgustinBettati <bettatiagustin@gmail.com>
2023-08-30 16:09:02 +0200

    remove fetching user before execution of tf acceptance test to avoid error when running ==> Checking that code complies with gofmt requirements... go test $(go list ./... | grep -v /integrationtesting) -timeout=30s -parallel=4 ?   	github.com/mongodb/terraform-provider-mongodbatlas	[no test files] ?   	github.com/mongodb/terraform-provider-mongodbatlas/mongodbatlas/framework/conversion	[no test files] ?   	github.com/mongodb/terraform-provider-mongodbatlas/mongodbatlas/testutils	[no test files] ?   	github.com/mongodb/terraform-provider-mongodbatlas/version	[no test files] ok  	github.com/mongodb/terraform-provider-mongodbatlas/mongodbatlas	1.120s ok  	github.com/mongodb/terraform-provider-mongodbatlas/mongodbatlas/framework/validator	(cached)

--- mongodbatlas/fw_data_source_mongodbatlas_atlas_user_test.go
+++ mongodbatlas/fw_data_source_mongodbatlas_atlas_user_test.go
@@ -1,21 +1,18 @@
 package mongodbatlas
 
 import (
-	"context"
 	"fmt"
 	"os"
 	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
-	"go.mongodb.org/atlas-sdk/v20230201002/admin"
 )
 
 func TestAccConfigDSAtlasUser_ByUserID(t *testing.T) {
 	var (
 		dataSourceName = "data.mongodbatlas_atlas_user.test"
 		userID         = os.Getenv("MONGODB_ATLAS_PROJECT_OWNER_ID")
-		user           = fetchUser(userID, t)
 	)
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:                 func() { testAccPreCheckBasic(t); testAccPreCheckBasicOwnerID(t) },
@@ -24,7 +21,12 @@ func TestAccConfigDSAtlasUser_ByUserID(t *testing.T) {
 			{
 				Config: testAccDSMongoDBAtlasUserByUserID(userID),
 				Check: resource.ComposeTestCheckFunc(
-					dataSourceChecksForUser(dataSourceName, user)...,
+					resource.TestCheckResourceAttrSet(dataSourceName, "username"),
+					resource.TestCheckResourceAttr(dataSourceName, "user_id", userID),
+					resource.TestCheckResourceAttrSet(dataSourceName, "email_address"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "first_name"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "last_name"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "country"),
 				),
 			},
 		},
@@ -35,7 +37,6 @@ func TestAccConfigDSAtlasUser_ByUsername(t *testing.T) {
 	var (
 		dataSourceName = "data.mongodbatlas_atlas_user.test"
 		username       = os.Getenv("MONGODB_ATLAS_USERNAME_CLOUD_DEV")
-		user           = fetchUserByUsername(username, t)
 	)
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:                 func() { testAccPreCheckBasic(t); testAccPreCheckAtlasUsername(t) },
@@ -44,27 +45,16 @@ func TestAccConfigDSAtlasUser_ByUsername(t *testing.T) {
 			{
 				Config: testAccDSMongoDBAtlasUserByUsername(username),
 				Check: resource.ComposeTestCheckFunc(
-					dataSourceChecksForUser(dataSourceName, user)...,
+					resource.TestCheckResourceAttr(dataSourceName, "username", username),
+					resource.TestCheckResourceAttrSet(dataSourceName, "user_id"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "email_address"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "first_name"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "last_name"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "country"),
 				),
 			},
 		},
 	})
-}
-
-func dataSourceChecksForUser(dataSourceName string, user *admin.CloudAppUser) []resource.TestCheckFunc {
-	return []resource.TestCheckFunc{
-		resource.TestCheckResourceAttr(dataSourceName, "username", user.Username),
-		resource.TestCheckResourceAttr(dataSourceName, "user_id", *user.Id),
-		resource.TestCheckResourceAttr(dataSourceName, "email_address", user.EmailAddress),
-		resource.TestCheckResourceAttr(dataSourceName, "first_name", user.FirstName),
-		resource.TestCheckResourceAttr(dataSourceName, "last_name", user.LastName),
-		resource.TestCheckResourceAttr(dataSourceName, "mobile_number", user.MobileNumber),
-		resource.TestCheckResourceAttr(dataSourceName, "country", user.Country),
-		resource.TestCheckResourceAttr(dataSourceName, "created_at", *TimePtrToStringPtr(user.CreatedAt)),
-		resource.TestCheckResourceAttr(dataSourceName, "roles.#", fmt.Sprintf("%d", len(user.Roles))),
-		resource.TestCheckResourceAttr(dataSourceName, "team_ids.#", fmt.Sprintf("%d", len(user.TeamIds))),
-		resource.TestCheckResourceAttr(dataSourceName, "links.#", fmt.Sprintf("%d", len(user.Links))),
-	}
 }
 
 func TestAccConfigDSAtlasUser_InvalidAttrCombination(t *testing.T) {
@@ -78,25 +68,6 @@ func TestAccConfigDSAtlasUser_InvalidAttrCombination(t *testing.T) {
 			},
 		},
 	})
-}
-
-func fetchUser(userID string, t *testing.T) *admin.CloudAppUser {
-	connV2 := testMongoDBClient.(*MongoDBClient).AtlasV2
-	userResp, _, err := connV2.MongoDBCloudUsersApi.GetUser(context.Background(), userID).Execute()
-	if err != nil {
-		t.Fatalf("the Atlas User (%s) could not be fetched: %v", userID, err)
-	}
-	return userResp
-}
-
-func fetchUserByUsername(username string, t *testing.T) *admin.CloudAppUser {
-	connV2 := testMongoDBClient.(*MongoDBClient).AtlasV2
-
-	userResp, _, err := connV2.MongoDBCloudUsersApi.GetUserByUsername(context.Background(), username).Execute()
-	if err != nil {
-		t.Fatalf("the Atlas User (%s) could not be fetched: %v", username, err)
-	}
-	return userResp
 }
 
 func testAccDSMongoDBAtlasUserByUsername(username string) string {
