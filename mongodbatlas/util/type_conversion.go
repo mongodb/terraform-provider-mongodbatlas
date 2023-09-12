4fbb5d3bd1fced5771dd70b64e0f5db0666c95c2
Agustin Bettati <bettatiagustin@gmail.com>
2023-09-12 12:59:42 +0200

    fix format in date-time attributes defined in atlas user data sources

--- mongodbatlas/util/type_conversion.go
+++ mongodbatlas/util/type_conversion.go
@@ -7,8 +7,15 @@ func TimePtrToStringPtr(t *time.Time) *string {
 	if t == nil {
 		return nil
 	}
-	res := t.String()
+	res := TimeToString(*t)
 	return &res
+}
+
+// TimeToString returns a RFC3339 date time string format.
+// The resulting format is identical to the format returned by Atlas API, documented as ISO 8601 timestamp format in UTC.
+// Example format: "2023-07-18T16:12:23Z"
+func TimeToString(t time.Time) string {
+	return t.UTC().Format(time.RFC3339)
 }
 
 func Int64PtrToIntPtr(i64 *int64) *int {
