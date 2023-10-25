0ea6a6ad7f288569fbceb4957ba620fba45de461
Aastha Mahendru <aastha.mahendru@mongodb.com>
2023-10-25 13:16:34 +0100

    fix online archive

--- mongodbatlas/resource_mongodbatlas_online_archive_migration_test.go
+++ mongodbatlas/resource_mongodbatlas_online_archive_migration_test.go
@@ -1,5 +1,19 @@
 package mongodbatlas
 
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	matlas "go.mongodb.org/atlas/mongodbatlas"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/plancheck"
+
+	"github.com/mongodb/terraform-provider-mongodbatlas/mongodbatlas/testutils"
+)
+
 func TestAccBackupRSOnlineArchiveWithNoChangeBetweenVersions(t *testing.T) {
 	var (
 		cluster                   matlas.Cluster
@@ -34,14 +48,14 @@ func TestAccBackupRSOnlineArchiveWithNoChangeBetweenVersions(t *testing.T) {
 						Source:            "mongodb/mongodbatlas",
 					},
 				},
-				Config: testAccBackupRSOnlineArchiveConfigWithDailySchedule(orgID, projectName, name, 1),
+				Config: testAccBackupRSOnlineArchiveConfigWithDailySchedule(orgID, projectName, name, 1, 7),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(onlineArchiveResourceName, "partition_fields.0.field_name", "last_review"),
 				),
 			},
 			{
 				ProtoV6ProviderFactories: testAccProviderV6Factories,
-				Config:                   testAccBackupRSOnlineArchiveConfigWithDailySchedule(orgID, projectName, name, 1),
+				Config:                   testAccBackupRSOnlineArchiveConfigWithDailySchedule(orgID, projectName, name, 1, 7),
 				ConfigPlanChecks: resource.ConfigPlanChecks{
 					PostApplyPreRefresh: []plancheck.PlanCheck{
 						testutils.DebugPlan(),
