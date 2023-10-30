6df6ad6701c6d0661ca2e7926c424dba3bf864c2
Marco Suma <marco.suma@mongodb.com>
2023-10-30 07:32:58 +0100

    feat: set format for AWS region value in the provider definition (#1549)
    * fixed region value when creating configuration for aws role authentication.
    
    * restore go.mod and go.sum.
    
    * separates logic into util func.
    
    * feat: adds more environment variables to be used when authenticating with an AWS Role (#1551)
    
    * set all possible default values taken from environment varialbes (if exist).
    
    * addresses comments.
    
    * feat: adds test coverage for AWS Role Authentication (#1552)
    
    * adds test coverage.
    
    * pre-check that regular credentials env variables are not set.
    
    * feat: adds test infrastructure to run acceptance tests on PRs (#1553)
    
    * adds test infra.
    
    * fix.
    
    * Update mongodbatlas/provider.go
    
    Co-authored-by: Leo Antoli <430982+lantoli@users.noreply.github.com>
    
    * Update mongodbatlas/provider.go
    
    Co-authored-by: Leo Antoli <430982+lantoli@users.noreply.github.com>
    
    * addresses comments.
    
    ---------
    
    Co-authored-by: Leo Antoli <430982+lantoli@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Leo Antoli <430982+lantoli@users.noreply.github.com>

---
+++ scripts/generate-credentials-with-sts-assume-role.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -Eeou pipefail
+
+# This script uses aws sts assume-role to generate temporary credentials
+# and outputs them in $GITHUB_OUTPUT so those can be used in other workflow jobs.
+# role-arn = arn:aws:iam::358363220050:role/terraform-provider-mongodbatlas-acceptancetests
+
+# Define a function to convert a string to lowercase
+function to_lowercase() {
+  echo "$1" | tr '[:upper:]' '[:lower:]'
+}
+# Convert the input string to lowercase
+aws_region=$(to_lowercase "$AWS_REGION")
+# Replace all underscores with hyphens
+aws_region=${aws_region//_/-}
+# e.g. from US_EAST_1 to us-east-1
+
+# Get the STS credentials
+export AWS_REGION="$aws_region"
+CREDENTIALS=$(aws sts assume-role --role-arn "$ASSUME_ROLE_ARN" --role-session-name newSession --output text --query 'Credentials.[AccessKeyId, SecretAccessKey, SessionToken]')
+
+# Extract the AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, and AWS_SESSION_TOKEN from the STS credentials
+AWS_ACCESS_KEY_ID=$(echo "$CREDENTIALS" | awk '{print $1}')
+AWS_SECRET_ACCESS_KEY=$(echo "$CREDENTIALS" | awk '{print $2}')
+AWS_SESSION_TOKEN=$(echo "$CREDENTIALS" | awk '{print $3}')
+
+{
+  echo "aws_access_key_id=${AWS_ACCESS_KEY_ID}"
+  echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY"
+  echo "aws_session_token=$AWS_SESSION_TOKEN"
+} >> "$GITHUB_OUTPUT"
