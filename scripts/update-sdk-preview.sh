0988bc150c71ebe07a2fecd46da6628a57336807
Leo Antoli <430982+lantoli@users.noreply.github.com>
2023-11-05 21:00:01 +0100

    fix sdk preview script

--- scripts/update-sdk-preview.sh
+++ scripts/update-sdk-preview.sh
@@ -5,12 +5,8 @@ set -euo pipefail
 LATEST_SDK_RELEASE=$(curl -sSfL -X GET  https://api.github.com/repos/mongodb/atlas-sdk-go/releases/latest | jq -r '.tag_name' | cut -d '.' -f 1)
 echo  "==> Updating SDK to latest PREVIEW major version $LATEST_SDK_RELEASE"
 
-gomajor get "go.mongodb.org/atlas-sdk/$LATEST_SDK_RELEASE@dev-latest"
-ret=$?
-if [ $ret -ne 0 ]; then
-	go mod tidy
-	echo "Finished update - This is SDK Preview, ***** DONT MERGE *******"
-else
-	echo "Failed to update - You may need to update to the latest SDK first using the 'Update SDK' GitHub Action"
-	exit 1
-fi	
+go get "go.mongodb.org/atlas-sdk/$LATEST_SDK_RELEASE@dev-latest"
+go mod tidy
+echo
+echo "Finished update - This is SDK Preview, ***** DONT MERGE THESE CHANGES *******"
+echo "If no files were changed you may need first to update to the latest SDK using the 'Update SDK' GitHub Action"
