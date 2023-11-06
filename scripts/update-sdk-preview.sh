63aead15cdec93a8431b59491ccc9ba235341acd
Leo Antoli <430982+lantoli@users.noreply.github.com>
2023-11-06 08:23:30 +0100

    change doc

--- scripts/update-sdk-preview.sh
+++ scripts/update-sdk-preview.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-echo "Script will update to dev preview. Note: you need to have latest major version in order to be able to use it"
+echo "Script will update to SDL Dev Preview. Note: you need to have the latest major version in order to be able to use it."
 
 LATEST_SDK_RELEASE=$(curl -sSfL -X GET  https://api.github.com/repos/mongodb/atlas-sdk-go/releases/latest | jq -r '.tag_name' | cut -d '.' -f 1)
 echo  "==> Updating SDK to latest PREVIEW major version $LATEST_SDK_RELEASE"
