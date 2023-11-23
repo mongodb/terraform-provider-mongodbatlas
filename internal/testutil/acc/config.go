aec0bdbb839f73c69755303489ea409081ce06ad
Leo Antoli <430982+lantoli@users.noreply.github.com>
2023-11-23 01:15:50 +0100

    fix TF test dependencies

--- internal/testutil/acc/config.go
+++ internal/testutil/acc/config.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
 	matlas "go.mongodb.org/atlas/mongodbatlas"
 )
 
