f9bb42890786ce208e0709436e5259d1e36ef6d1
Oriol Arbusi <oriol.abadal@mongodb.com>
2023-11-28 16:46:51 +0100

    fix import

--- internal/service/alertconfiguration/model_alert_configuration.go
+++ internal/service/alertconfiguration/model_alert_configuration.go
@@ -7,7 +7,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/mongodb/terraform-provider-mongodbatlas/internal/common/conversion"
 	"github.com/mwielbut/pointy"
-	"go.mongodb.org/atlas-sdk/v20231115001/admin"
+	"go.mongodb.org/atlas-sdk/v20231115002/admin"
 )
 
 func NewNotificationList(tfNotificationSlice []TfNotificationModel) ([]admin.AlertsNotificationRootForGroup, error) {
--- internal/service/alertconfiguration/model_alert_configuration_test.go
+++ internal/service/alertconfiguration/model_alert_configuration_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/mongodb/terraform-provider-mongodbatlas/internal/common/conversion"
 	"github.com/mongodb/terraform-provider-mongodbatlas/internal/service/alertconfiguration"
-	"go.mongodb.org/atlas-sdk/v20231115001/admin"
+	"go.mongodb.org/atlas-sdk/v20231115002/admin"
 )
 
 type sdkToTFNotificationModelTestCase struct {
