ebe95d4415ff2e7adabb32495a127de2128342b4
Agustin Bettati <bettatiagustin@gmail.com>
2023-12-05 11:48:37 +0100

    feat: new `mongodbatlas_stream_instance` resource (#1685)
    * feat: new mongodbatlas_stream_instance resource implementation
    
    * unit testing for sdk to TF model
    
    * unit testing for tf to sdk models
    
    * include acceptance tests
    
    * add test case for potential empty fields
    
    * resource documentation
    
    * include example section
    
    * configure acceptance tests in CI
    
    * include missing step for terraform setup
    
    * Update website/docs/r/stream_instance.html.markdown
    
    Co-authored-by: Andrea Angiolillo <andrea.angiolillo@mongodb.com>
    
    * remove redundant return statement in import function
    
    * addressing doc comments
    
    ---------
    
    Co-authored-by: Andrea Angiolillo <andrea.angiolillo@mongodb.com>

---
+++ internal/service/streaminstance/model_stream_instance.go
@@ -0,0 +1,61 @@
+package streaminstance
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
+	"go.mongodb.org/atlas-sdk/v20231115002/admin"
+)
+
+func NewStreamInstanceCreateReq(ctx context.Context, plan *TFStreamInstanceRSModel) (*admin.StreamsTenant, diag.Diagnostics) {
+	dataProcessRegion := &TFInstanceProcessRegionSpecModel{}
+	if diags := plan.DataProcessRegion.As(ctx, dataProcessRegion, basetypes.ObjectAsOptions{}); diags.HasError() {
+		return nil, diags
+	}
+	return &admin.StreamsTenant{
+		GroupId: plan.ProjectID.ValueStringPointer(),
+		Name:    plan.InstanceName.ValueStringPointer(),
+		DataProcessRegion: &admin.StreamsDataProcessRegion{
+			CloudProvider: dataProcessRegion.CloudProvider.ValueString(),
+			Region:        dataProcessRegion.Region.ValueString(),
+		},
+	}, nil
+}
+
+func NewStreamInstanceUpdateReq(ctx context.Context, plan *TFStreamInstanceRSModel) (*admin.StreamsDataProcessRegion, diag.Diagnostics) {
+	dataProcessRegion := &TFInstanceProcessRegionSpecModel{}
+	if diags := plan.DataProcessRegion.As(ctx, dataProcessRegion, basetypes.ObjectAsOptions{}); diags.HasError() {
+		return nil, diags
+	}
+	return &admin.StreamsDataProcessRegion{
+		CloudProvider: dataProcessRegion.CloudProvider.ValueString(),
+		Region:        dataProcessRegion.Region.ValueString(),
+	}, nil
+}
+
+func NewTFStreamInstance(ctx context.Context, apiResp *admin.StreamsTenant) (*TFStreamInstanceRSModel, diag.Diagnostics) {
+	hostnames, diags := types.ListValueFrom(ctx, types.StringType, apiResp.Hostnames)
+
+	var dataProcessRegion = types.ObjectNull(ProcessRegionObjectType.AttrTypes)
+	if apiResp.DataProcessRegion != nil {
+		returnedProcessRegion, diagsProcessRegion := types.ObjectValueFrom(ctx, ProcessRegionObjectType.AttrTypes, TFInstanceProcessRegionSpecModel{
+			CloudProvider: types.StringValue(apiResp.DataProcessRegion.CloudProvider),
+			Region:        types.StringValue(apiResp.DataProcessRegion.Region),
+		})
+		dataProcessRegion = returnedProcessRegion
+		diags.Append(diagsProcessRegion...)
+	}
+	if diags.HasError() {
+		return nil, diags
+	}
+
+	return &TFStreamInstanceRSModel{
+		ID:                types.StringPointerValue(apiResp.Id),
+		InstanceName:      types.StringPointerValue(apiResp.Name),
+		ProjectID:         types.StringPointerValue(apiResp.GroupId),
+		DataProcessRegion: dataProcessRegion,
+		Hostnames:         hostnames,
+	}, nil
+}
---
+++ internal/service/streaminstance/model_stream_instance_test.go
@@ -0,0 +1,174 @@
+package streaminstance_test
+
+import (
+	"context"
+	"reflect"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/mongodb/terraform-provider-mongodbatlas/internal/service/streaminstance"
+	"go.mongodb.org/atlas-sdk/v20231115002/admin"
+)
+
+type sdkToTFModelTestCase struct {
+	SDKResp         *admin.StreamsTenant
+	expectedTFModel *streaminstance.TFStreamInstanceRSModel
+	name            string
+}
+
+const (
+	dummyProjectID        = "111111111111111111111111"
+	dummyStreamInstanceID = "222222222222222222222222"
+	cloudProvider         = "AWS"
+	region                = "VIRGINIA_USA"
+	instanceName          = "InstanceName"
+)
+
+var hostnames = []string{"atlas-stream.virginia-usa.a.query.mongodb-dev.net"}
+
+func TestStreamInstanceSDKToTFModel(t *testing.T) {
+	testCases := []sdkToTFModelTestCase{
+		{
+			name: "Complete SDK response",
+			SDKResp: &admin.StreamsTenant{
+				Id: admin.PtrString(dummyStreamInstanceID),
+				DataProcessRegion: &admin.StreamsDataProcessRegion{
+					CloudProvider: cloudProvider,
+					Region:        region,
+				},
+				GroupId:   admin.PtrString(dummyProjectID),
+				Hostnames: hostnames,
+				Name:      admin.PtrString(instanceName),
+			},
+			expectedTFModel: &streaminstance.TFStreamInstanceRSModel{
+				ID:                types.StringValue(dummyStreamInstanceID),
+				DataProcessRegion: tfRegionObject(t, cloudProvider, region),
+				ProjectID:         types.StringValue(dummyProjectID),
+				Hostnames:         tfHostnamesList(t, hostnames),
+				InstanceName:      types.StringValue(instanceName),
+			},
+		},
+		{
+			name: "Empty hostnames and dataProcessRegion in response", // should never happen, but verifying it is handled gracefully
+			SDKResp: &admin.StreamsTenant{
+				Id:      admin.PtrString(dummyStreamInstanceID),
+				GroupId: admin.PtrString(dummyProjectID),
+				Name:    admin.PtrString(instanceName),
+			},
+			expectedTFModel: &streaminstance.TFStreamInstanceRSModel{
+				ID:                types.StringValue(dummyStreamInstanceID),
+				DataProcessRegion: types.ObjectNull(streaminstance.ProcessRegionObjectType.AttrTypes),
+				ProjectID:         types.StringValue(dummyProjectID),
+				Hostnames:         types.ListNull(types.StringType),
+				InstanceName:      types.StringValue(instanceName),
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			resultModel, diags := streaminstance.NewTFStreamInstance(context.Background(), tc.SDKResp)
+			if diags.HasError() {
+				t.Errorf("unexpected errors found: %s", diags.Errors()[0].Summary())
+			}
+			if !reflect.DeepEqual(resultModel, tc.expectedTFModel) {
+				t.Errorf("created terraform model did not match expected output")
+			}
+		})
+	}
+}
+
+type tfToSDKCreateModelTestCase struct {
+	tfModel        *streaminstance.TFStreamInstanceRSModel
+	expectedSDKReq *admin.StreamsTenant
+	name           string
+}
+
+func TestStreamInstanceTFToSDKCreateModel(t *testing.T) {
+	testCases := []tfToSDKCreateModelTestCase{
+		{
+			name: "Complete TF state",
+			tfModel: &streaminstance.TFStreamInstanceRSModel{
+				DataProcessRegion: tfRegionObject(t, cloudProvider, region),
+				ProjectID:         types.StringValue(dummyProjectID),
+				InstanceName:      types.StringValue(instanceName),
+			},
+			expectedSDKReq: &admin.StreamsTenant{
+				DataProcessRegion: &admin.StreamsDataProcessRegion{
+					CloudProvider: cloudProvider,
+					Region:        region,
+				},
+				GroupId: admin.PtrString(dummyProjectID),
+				Name:    admin.PtrString(instanceName),
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			apiReqResult, diags := streaminstance.NewStreamInstanceCreateReq(context.Background(), tc.tfModel)
+			if diags.HasError() {
+				t.Errorf("unexpected errors found: %s", diags.Errors()[0].Summary())
+			}
+			if !reflect.DeepEqual(apiReqResult, tc.expectedSDKReq) {
+				t.Errorf("created sdk model did not match expected output")
+			}
+		})
+	}
+}
+
+type tfToSDKUpdateModelTestCase struct {
+	tfModel        *streaminstance.TFStreamInstanceRSModel
+	expectedSDKReq *admin.StreamsDataProcessRegion
+	name           string
+}
+
+func TestStreamInstanceTFToSDKUpdateModel(t *testing.T) {
+	testCases := []tfToSDKUpdateModelTestCase{
+		{
+			name: "Complete TF state",
+			tfModel: &streaminstance.TFStreamInstanceRSModel{
+				ID:                types.StringValue(dummyStreamInstanceID),
+				DataProcessRegion: tfRegionObject(t, cloudProvider, region),
+				ProjectID:         types.StringValue(dummyProjectID),
+				Hostnames:         tfHostnamesList(t, hostnames),
+				InstanceName:      types.StringValue(instanceName),
+			},
+			expectedSDKReq: &admin.StreamsDataProcessRegion{
+				CloudProvider: cloudProvider,
+				Region:        region,
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			apiReqResult, diags := streaminstance.NewStreamInstanceUpdateReq(context.Background(), tc.tfModel)
+			if diags.HasError() {
+				t.Errorf("unexpected errors found: %s", diags.Errors()[0].Summary())
+			}
+			if !reflect.DeepEqual(apiReqResult, tc.expectedSDKReq) {
+				t.Errorf("created sdk model did not match expected output")
+			}
+		})
+	}
+}
+
+func tfRegionObject(t *testing.T, cloudProvider, region string) types.Object {
+	dataProcessRegion, diags := types.ObjectValueFrom(context.Background(), streaminstance.ProcessRegionObjectType.AttrTypes, streaminstance.TFInstanceProcessRegionSpecModel{
+		CloudProvider: types.StringValue(cloudProvider),
+		Region:        types.StringValue(region),
+	})
+	if diags.HasError() {
+		t.Errorf("failed to create terraform data process region model: %s", diags.Errors()[0].Summary())
+	}
+	return dataProcessRegion
+}
+
+func tfHostnamesList(t *testing.T, hostnames []string) types.List {
+	resultList, diags := types.ListValueFrom(context.Background(), types.StringType, hostnames)
+	if diags.HasError() {
+		t.Errorf("failed to create terraform hostnames list: %s", diags.Errors()[0].Summary())
+	}
+	return resultList
+}
