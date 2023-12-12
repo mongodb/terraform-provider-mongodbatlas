f102c48cab822348f8164a2a3213f013c2cc05be
Oriol Arbusi <oriol.abadal@mongodb.com>
2023-12-12 11:08:00 +0100

    add planModifier

--- internal/service/databaseuser/resource_database_user.go
+++ internal/service/databaseuser/resource_database_user.go
@@ -14,6 +14,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/setplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringdefault"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
@@ -191,6 +192,9 @@ func (r *databaseUserRS) Schema(ctx context.Context, req resource.SchemaRequest,
 				},
 			},
 			"scopes": schema.SetNestedBlock{
+				PlanModifiers: []planmodifier.Set{
+					setplanmodifier.RequiresReplace(),
+				},
 				NestedObject: schema.NestedBlockObject{
 					Attributes: map[string]schema.Attribute{
 						"name": schema.StringAttribute{
