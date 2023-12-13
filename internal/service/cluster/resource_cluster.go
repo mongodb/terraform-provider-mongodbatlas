980680e35add8d0b68b41dd0d689408e84bc7611
Leo Antoli <430982+lantoli@users.noreply.github.com>
2023-12-13 07:37:22 +0100

    change default 0 to computed

--- internal/service/cluster/resource_cluster.go
+++ internal/service/cluster/resource_cluster.go
@@ -252,12 +252,12 @@ func Resource() *schema.Resource {
 									"read_only_nodes": {
 										Type:     schema.TypeInt,
 										Optional: true,
-										Default:  0,
+										Computed: true,
 									},
 									"analytics_nodes": {
 										Type:     schema.TypeInt,
 										Optional: true,
-										Default:  0,
+										Computed: true,
 									},
 								},
 							},
