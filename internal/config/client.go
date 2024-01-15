5bdf6d04982e10d87bd4a3af02f005d999fee87c
Leo Antoli <430982+lantoli@users.noreply.github.com>
2024-01-15 16:18:15 +0100

    add online_archive to old sdk comment

--- internal/config/client.go
+++ internal/config/client.go
@@ -31,7 +31,7 @@ var (
 type MongoDBClient struct {
 	Atlas      *matlasClient.Client
 	AtlasV2    *atlasSDK.APIClient
-	OldAtlasV2 *oldAtlasSDK.APIClient // Needed to avoid sudden breaking changes in federated_settings_identity_provider resource. Will be removed in terraform-provider-1.16.0
+	OldAtlasV2 *oldAtlasSDK.APIClient // Needed to avoid sudden breaking changes in federated_settings_identity_provider and online_archive resources. Will be removed in terraform-provider-1.16.0
 	Config     *Config
 }
 
