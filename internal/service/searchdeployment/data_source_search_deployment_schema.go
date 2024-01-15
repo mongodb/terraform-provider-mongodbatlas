94d46093f2d16604f334904f9a5962badf6fd98e
andreaangiolillo <andrea.angiolillo@mongodb.com>
2024-01-15 10:20:16 +0000

    chore: address folloup comments in 1833

--- internal/service/searchdeployment/data_source_search_deployment_schema.go
+++ internal/service/searchdeployment/data_source_search_deployment_schema.go
@@ -31,8 +31,8 @@ func DataSourceSchema(ctx context.Context) schema.Schema {
 					Attributes: map[string]schema.Attribute{
 						"instance_size": schema.StringAttribute{
 							Computed:            true,
-							Description:         "Hardware specification for the search node instance sizes.",
-							MarkdownDescription: "Hardware specification for the search node instance sizes.",
+							Description:         "Hardware specification for the search node instance sizes. The [MongoDB Atlas API](https://www.mongodb.com/docs/atlas/reference/api-resources-spec/#tag/Atlas-Search/operation/createAtlasSearchDeployment) describes the valid values. More details can also be found in the [Search Node Documentation](https://www.mongodb.com/docs/atlas/cluster-config/multi-cloud-distribution/#search-tier).",
+							MarkdownDescription: "Hardware specification for the search node instance sizes. The [MongoDB Atlas API](https://www.mongodb.com/docs/atlas/reference/api-resources-spec/#tag/Atlas-Search/operation/createAtlasSearchDeployment) describes the valid values. More details can also be found in the [Search Node Documentation](https://www.mongodb.com/docs/atlas/cluster-config/multi-cloud-distribution/#search-tier).",
 						},
 						"node_count": schema.Int64Attribute{
 							Computed:            true,
--- internal/service/searchdeployment/resource_search_deployment_schema.go
+++ internal/service/searchdeployment/resource_search_deployment_schema.go
@@ -47,8 +47,8 @@ func ResourceSchema(ctx context.Context) schema.Schema {
 					Attributes: map[string]schema.Attribute{
 						"instance_size": schema.StringAttribute{
 							Required:            true,
-							Description:         "Hardware specification for the search node instance sizes.",
-							MarkdownDescription: "Hardware specification for the search node instance sizes.",
+							Description:         "Hardware specification for the search node instance sizes. The [MongoDB Atlas API](https://www.mongodb.com/docs/atlas/reference/api-resources-spec/#tag/Atlas-Search/operation/createAtlasSearchDeployment) describes the valid values. More details can also be found in the [Search Node Documentation](https://www.mongodb.com/docs/atlas/cluster-config/multi-cloud-distribution/#search-tier).",
+							MarkdownDescription: "Hardware specification for the search node instance sizes. The [MongoDB Atlas API](https://www.mongodb.com/docs/atlas/reference/api-resources-spec/#tag/Atlas-Search/operation/createAtlasSearchDeployment) describes the valid values. More details can also be found in the [Search Node Documentation](https://www.mongodb.com/docs/atlas/cluster-config/multi-cloud-distribution/#search-tier).",
 						},
 						"node_count": schema.Int64Attribute{
 							Required:            true,
