582f0181c2ccccab8130dc5c4ccd8e9950fdd977
Oriol Arbusi <oriol.abadal@mongodb.com>
2024-01-17 12:27:23 +0100

    fix

--- internal/service/federatedsettingsidentityprovider/resource_federated_settings_identity_provider.go
+++ internal/service/federatedsettingsidentityprovider/resource_federated_settings_identity_provider.go
@@ -216,7 +216,7 @@ func resourceMongoDBAtlasFederatedSettingsIdentityProviderUpdate(ctx context.Con
 		return append(oldSDKUpdate(ctx, federationSettingsID, oktaIdpID, d, meta), getGracePeriodWarning())
 	}
 
-	var updateRequest *admin.FederationIdentityProviderUpdate
+	var updateRequest *admin.IdentityProviderUpdate
 	_, _, err := connV2.FederatedAuthenticationApi.GetIdentityProvider(context.Background(), federationSettingsID, oktaIdpID).Execute()
 
 	if err != nil {
