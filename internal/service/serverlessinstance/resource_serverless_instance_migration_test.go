7f9f2d146a236d0feb2535083d9d6731ccbb4d14
Leo Antoli <430982+lantoli@users.noreply.github.com>
2024-02-07 20:32:20 +0100

    mig TestStep

--- internal/service/serverlessinstance/resource_serverless_instance_migration_test.go
+++ internal/service/serverlessinstance/resource_serverless_instance_migration_test.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
-	"github.com/hashicorp/terraform-plugin-testing/plancheck"
 	"github.com/mongodb/terraform-provider-mongodbatlas/internal/testutil/acc"
 	"github.com/mongodb/terraform-provider-mongodbatlas/internal/testutil/mig"
 )
@@ -16,6 +15,7 @@ func TestAccMigrationServerlessInstance_basic(t *testing.T) {
 		instanceName = acctest.RandomWithPrefix("test-acc-serverless")
 		orgID        = os.Getenv("MONGODB_ATLAS_ORG_ID")
 		projectName  = acctest.RandomWithPrefix("test-acc-serverless")
+		config       = acc.ConfigServerlessInstanceBasic(orgID, projectName, instanceName, true)
 	)
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:     func() { mig.PreCheckBasic(t) },
@@ -23,7 +23,7 @@ func TestAccMigrationServerlessInstance_basic(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				ExternalProviders: mig.ExternalProviders(),
-				Config:            acc.ConfigServerlessInstanceBasic(orgID, projectName, instanceName, true),
+				Config:            config,
 				Check: resource.ComposeTestCheckFunc(
 					checkConnectionStringPrivateEndpointIsPresentWithNoElement(resourceName),
 					checkExists(resourceName),
@@ -31,16 +31,7 @@ func TestAccMigrationServerlessInstance_basic(t *testing.T) {
 					resource.TestCheckResourceAttr(resourceName, "termination_protection_enabled", "false"),
 				),
 			},
-			{
-				ProtoV6ProviderFactories: acc.TestAccProviderV6Factories,
-				Config:                   acc.ConfigServerlessInstanceBasic(orgID, projectName, instanceName, true),
-				ConfigPlanChecks: resource.ConfigPlanChecks{
-					PreApply: []plancheck.PlanCheck{
-						acc.DebugPlan(),
-						plancheck.ExpectEmptyPlan(),
-					},
-				},
-			},
+			mig.TestStep(config),
 		},
 	})
 }
---
+++ internal/testutil/mig/test_step.go
@@ -0,0 +1,20 @@
+package mig
+
+import (
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/plancheck"
+	"github.com/mongodb/terraform-provider-mongodbatlas/internal/testutil/acc"
+)
+
+func TestStep(config string) resource.TestStep {
+	return resource.TestStep{
+		ProtoV6ProviderFactories: acc.TestAccProviderV6Factories,
+		Config:                   config,
+		ConfigPlanChecks: resource.ConfigPlanChecks{
+			PreApply: []plancheck.PlanCheck{
+				acc.DebugPlan(),
+				plancheck.ExpectEmptyPlan(),
+			},
+		},
+	}
+}
