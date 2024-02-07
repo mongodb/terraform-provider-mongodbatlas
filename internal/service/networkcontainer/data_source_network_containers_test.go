8300e6d4e3218c6c3ce29fa61359a17bf671a421
Oriol Arbusi <oriol.abadal@mongodb.com>
2024-02-07 14:31:48 +0100

    plural data source tests

---
+++ internal/service/networkcontainer/data_source_network_containers_test.go
@@ -0,0 +1,158 @@
+package networkcontainer_test
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/mongodb/terraform-provider-mongodbatlas/internal/testutil/acc"
+	matlas "go.mongodb.org/atlas/mongodbatlas"
+)
+
+var (
+	container                matlas.Container
+	randInt                  = acctest.RandIntRange(0, 255)
+	resourceName             = "mongodbatlas_network_container.test"
+	dataSourceContainersName = "data.mongodbatlas_network_containers.test"
+	cidrBlock                = fmt.Sprintf("10.8.%d.0/24", randInt)
+	gcpCidrBlock             = fmt.Sprintf("10.%d.0.0/18", randInt)
+	providerNameAws          = "AWS"
+	providerNameAzure        = "AZURE"
+	providerNameGCP          = "GCP"
+	orgID                    = os.Getenv("MONGODB_ATLAS_ORG_ID")
+	projectName              = acctest.RandomWithPrefix("test-acc")
+)
+
+func TestAccNetworkContainerDSPlural_basicAWS(t *testing.T) {
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:                 func() { acc.PreCheckBasic(t) },
+		ProtoV6ProviderFactories: acc.TestAccProviderV6Factories,
+		Steps: []resource.TestStep{
+			{
+				Config: dataSourceConfigBasicAWS(projectName, orgID, cidrBlock, providerNameAws, "US_EAST_1"),
+				Check: resource.ComposeTestCheckFunc(
+					checkExists(resourceName, &container),
+					resource.TestCheckResourceAttrSet(resourceName, "project_id"),
+					resource.TestCheckResourceAttr(resourceName, "provider_name", providerNameAws),
+					resource.TestCheckResourceAttrSet(resourceName, "provisioned"),
+					resource.TestCheckResourceAttrSet(dataSourceContainersName, "results.#"),
+					resource.TestCheckResourceAttrSet(dataSourceContainersName, "results.0.id"),
+					resource.TestCheckResourceAttrSet(dataSourceContainersName, "results.0.atlas_cidr_block"),
+					resource.TestCheckResourceAttrSet(dataSourceContainersName, "results.0.provider_name"),
+					resource.TestCheckResourceAttrSet(dataSourceContainersName, "results.0.provisioned"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccNetworkContainerDSPlural_basicAzure(t *testing.T) {
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:                 func() { acc.PreCheckBasic(t) },
+		ProtoV6ProviderFactories: acc.TestAccProviderV6Factories,
+		Steps: []resource.TestStep{
+			{
+				Config: dataSourceConfigBasicAzure(projectName, orgID, cidrBlock, providerNameAzure, "US_EAST_2"),
+				Check: resource.ComposeTestCheckFunc(
+					checkExists(resourceName, &container),
+					resource.TestCheckResourceAttrSet(resourceName, "project_id"),
+					resource.TestCheckResourceAttr(resourceName, "provider_name", providerNameAzure),
+					resource.TestCheckResourceAttrSet(resourceName, "provisioned"),
+					resource.TestCheckResourceAttrSet(dataSourceContainersName, "results.#"),
+					resource.TestCheckResourceAttrSet(dataSourceContainersName, "results.0.id"),
+					resource.TestCheckResourceAttrSet(dataSourceContainersName, "results.0.atlas_cidr_block"),
+					resource.TestCheckResourceAttrSet(dataSourceContainersName, "results.0.provider_name"),
+					resource.TestCheckResourceAttrSet(dataSourceContainersName, "results.0.provisioned"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccNetworkContainerDSPlural_basicGCP(t *testing.T) {
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:                 func() { acc.PreCheckBasic(t) },
+		ProtoV6ProviderFactories: acc.TestAccProviderV6Factories,
+		Steps: []resource.TestStep{
+			{
+				Config: dataSourceConfigBasicGCP(projectName, orgID, gcpCidrBlock, providerNameGCP),
+				Check: resource.ComposeTestCheckFunc(
+					checkExists(resourceName, &container),
+					resource.TestCheckResourceAttrSet(resourceName, "project_id"),
+					resource.TestCheckResourceAttr(resourceName, "provider_name", providerNameGCP),
+					resource.TestCheckResourceAttrSet(resourceName, "provisioned"),
+					resource.TestCheckResourceAttrSet(dataSourceContainersName, "results.#"),
+					resource.TestCheckResourceAttrSet(dataSourceContainersName, "results.0.id"),
+					resource.TestCheckResourceAttrSet(dataSourceContainersName, "results.0.atlas_cidr_block"),
+					resource.TestCheckResourceAttrSet(dataSourceContainersName, "results.0.provider_name"),
+					resource.TestCheckResourceAttrSet(dataSourceContainersName, "results.0.provisioned"),
+				),
+			},
+		},
+	})
+}
+
+func dataSourceConfigBasicAWS(projectName, orgID, cidrBlock, providerName, region string) string {
+	return fmt.Sprintf(`
+		resource "mongodbatlas_project" "test" {
+			name   = "%s"
+			org_id = "%s"
+		}
+
+		resource "mongodbatlas_network_container" "test" {
+			project_id   		 = mongodbatlas_project.test.id
+			atlas_cidr_block 	 = "%s"
+			provider_name		 = "%s"
+			region_name			 = "%s"
+		}
+
+		data "mongodbatlas_network_containers" "test" {
+			project_id = "${mongodbatlas_network_container.test.project_id}"
+			provider_name = "AWS"
+		}
+
+	`, projectName, orgID, cidrBlock, providerName, region)
+}
+
+func dataSourceConfigBasicAzure(projectName, orgID, cidrBlock, providerName, region string) string {
+	return fmt.Sprintf(`
+		resource "mongodbatlas_project" "test" {
+			name   = "%s"
+			org_id = "%s"
+		}
+
+		resource "mongodbatlas_network_container" "test" {
+			project_id   		 = "${mongodbatlas_project.test.id}"
+			atlas_cidr_block     = "%s"
+			provider_name		 = "%s"
+			region			     = "%s"
+		}
+
+		data "mongodbatlas_network_containers" "test" {
+			project_id = "${mongodbatlas_network_container.test.project_id}"
+			provider_name = "AZURE"
+		}
+	`, projectName, orgID, cidrBlock, providerName, region)
+}
+
+func dataSourceConfigBasicGCP(projectName, orgID, cidrBlock, providerName string) string {
+	return fmt.Sprintf(`
+		resource "mongodbatlas_project" "test" {
+			name   = "%s"
+			org_id = "%s"
+		}
+
+		resource "mongodbatlas_network_container" "test" {
+			project_id   		 = "${mongodbatlas_project.test.id}"
+			atlas_cidr_block     = "%s"
+			provider_name		 = "%s"
+		}
+
+		data "mongodbatlas_network_containers" "test" {
+			project_id = "${mongodbatlas_network_container.test.project_id}"
+			provider_name = "GCP"
+		}
+	`, projectName, orgID, cidrBlock, providerName)
+}
