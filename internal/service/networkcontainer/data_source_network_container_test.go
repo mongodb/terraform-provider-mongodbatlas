a8c75718a2a23de0a38aa7f62be626462819d677
Oriol Arbusi <oriol.abadal@mongodb.com>
2024-02-08 16:24:50 +0100

    fix test

--- internal/service/networkcontainer/data_source_network_container_test.go
+++ internal/service/networkcontainer/data_source_network_container_test.go
@@ -25,7 +25,6 @@ func TestAccNetworkContainerDS_basicAWS(t *testing.T) {
 			{
 				Config: dataSourceConfigBasicAWS(projectName, orgID, cidrBlock, providerNameAws, "US_EAST_1"),
 				Check: resource.ComposeTestCheckFunc(
-					checkExists(dataSourceName),
 					resource.TestCheckResourceAttrSet(dataSourceName, "project_id"),
 					resource.TestCheckResourceAttr(dataSourceName, "provider_name", providerNameAws),
 					resource.TestCheckResourceAttrSet(dataSourceName, "provisioned"),
@@ -47,7 +46,6 @@ func TestAccNetworkContainerDS_basicAzure(t *testing.T) {
 			{
 				Config: dataSourceConfigBasicAzure(projectName, orgID, cidrBlock, providerNameAzure, "US_EAST_2"),
 				Check: resource.ComposeTestCheckFunc(
-					checkExists(dataSourceName),
 					resource.TestCheckResourceAttrSet(dataSourceName, "project_id"),
 					resource.TestCheckResourceAttr(dataSourceName, "provider_name", providerNameAzure),
 					resource.TestCheckResourceAttrSet(dataSourceName, "provisioned"),
@@ -69,7 +67,6 @@ func TestAccNetworkContainerDS_basicGCP(t *testing.T) {
 			{
 				Config: dataSourceConfigBasicGCP(projectName, orgID, gcpCidrBlock, providerNameGCP),
 				Check: resource.ComposeTestCheckFunc(
-					checkExists(dataSourceName),
 					resource.TestCheckResourceAttrSet(dataSourceName, "project_id"),
 					resource.TestCheckResourceAttr(dataSourceName, "provider_name", providerNameGCP),
 					resource.TestCheckResourceAttrSet(dataSourceName, "provisioned"),
