f8fb7297b25eafe2042e6aed233e74bff2948c27
Leo Antoli <430982+lantoli@users.noreply.github.com>
2024-02-08 11:15:45 +0100

    chore: Upgrades maintenance_window resource to auto-generated SDK (#1886)
    * rename
    
    * extract resource/ds names
    
    * migration test
    
    * connV2 in tests
    
    * refactor checkExists
    
    * day_of_week is mandatory, number_of_deferrals is computed
    
    * mig.TestStep
    
    * remove unneeded checks
    
    * data source
    
    * read
    
    * delete
    
    * create and update
    
    * use projecID variable
    
    * Update website/docs/d/maintenance_window.html.markdown
    
    Co-authored-by: Oriol <oriol.abadal@mongodb.com>
    
    * Update website/docs/r/maintenance_window.html.markdown
    
    Co-authored-by: Oriol <oriol.abadal@mongodb.com>
    
    * make Sunday and Saturday more explicit
    
    ---------
    
    Co-authored-by: Oriol <oriol.abadal@mongodb.com>

---
+++ internal/service/maintenancewindow/resource_maintenance_window_migration_test.go
@@ -0,0 +1,39 @@
+package maintenancewindow_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/mongodb/terraform-provider-mongodbatlas/internal/testutil/mig"
+	"github.com/spf13/cast"
+)
+
+func TestAccMigrationConfigMaintenanceWindow_basic(t *testing.T) {
+	var (
+		orgID       = os.Getenv("MONGODB_ATLAS_ORG_ID")
+		projectName = acctest.RandomWithPrefix("test-acc")
+		dayOfWeek   = 7
+		hourOfDay   = 3
+		config      = configBasic(orgID, projectName, dayOfWeek, hourOfDay)
+	)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() { mig.PreCheckBasic(t) },
+		Steps: []resource.TestStep{
+			{
+				ExternalProviders: mig.ExternalProviders(),
+				Config:            config,
+				Check: resource.ComposeTestCheckFunc(
+					checkExists(resourceName),
+					resource.TestCheckResourceAttrSet(resourceName, "project_id"),
+					resource.TestCheckResourceAttr(resourceName, "day_of_week", cast.ToString(dayOfWeek)),
+					resource.TestCheckResourceAttr(resourceName, "hour_of_day", cast.ToString(hourOfDay)),
+					resource.TestCheckResourceAttr(resourceName, "number_of_deferrals", "0"),
+				),
+			},
+			mig.TestStep(config),
+		},
+	})
+}
