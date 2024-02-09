f48e0774ded8023af5c7fb8c604b04b391e659d5
lantoli <lantoli@users.noreply.github.com>
2024-02-09 15:16:09 +0000

    build(deps): bump go.mongodb.org/atlas-sdk

--- internal/service/alertconfiguration/resource_alert_configuration.go
+++ internal/service/alertconfiguration/resource_alert_configuration.go
@@ -21,7 +21,7 @@ import (
 	"github.com/mongodb/terraform-provider-mongodbatlas/internal/common/conversion"
 	"github.com/mongodb/terraform-provider-mongodbatlas/internal/config"
 	"github.com/mwielbut/pointy"
-	"go.mongodb.org/atlas-sdk/v20231115005/admin"
+	"go.mongodb.org/atlas-sdk/v20231115006/admin"
 )
 
 const (
