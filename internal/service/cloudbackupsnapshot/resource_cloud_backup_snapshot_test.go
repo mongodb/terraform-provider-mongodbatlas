6d912b591f8a5f024e9983f63f8fa4c8b6cc6f4d
Leo Antoli <430982+lantoli@users.noreply.github.com>
2024-02-13 13:45:34 +0100

    sharded tests

--- internal/service/cloudbackupsnapshot/resource_cloud_backup_snapshot_test.go
+++ internal/service/cloudbackupsnapshot/resource_cloud_backup_snapshot_test.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"testing"
 
+	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
 	"github.com/mongodb/terraform-provider-mongodbatlas/internal/common/conversion"
@@ -13,12 +14,12 @@ import (
 )
 
 const (
-	resourceName = "mongodbatlas_cloud_backup_snapshot.test"
+	resourceName   = "mongodbatlas_cloud_backup_snapshot.test"
+	dataSourceName = "data.mongodbatlas_cloud_backup_snapshot.test"
 )
 
 func TestAccBackupRSCloudBackupSnapshot_basic(t *testing.T) {
 	var (
-		dataSourceName                 = "data.mongodbatlas_cloud_backup_snapshot.test"
 		dataSourcePluralSimpleName     = "data.mongodbatlas_cloud_backup_snapshots.test"
 		dataSourcePluralPaginationName = "data.mongodbatlas_cloud_backup_snapshots.pagination"
 		orgID                          = os.Getenv("MONGODB_ATLAS_ORG_ID")
@@ -37,11 +38,19 @@ func TestAccBackupRSCloudBackupSnapshot_basic(t *testing.T) {
 				Check: resource.ComposeTestCheckFunc(
 					checkExists(resourceName),
 					resource.TestCheckResourceAttrSet(resourceName, "project_id"),
+					resource.TestCheckResourceAttr(resourceName, "type", "replicaSet"),
+					resource.TestCheckResourceAttr(resourceName, "members.#", "0"),
+					resource.TestCheckResourceAttr(resourceName, "snapshot_ids.#", "0"),
 					resource.TestCheckResourceAttr(resourceName, "cluster_name", clusterInfo.ClusterName),
+					resource.TestCheckResourceAttr(resourceName, "replica_set_name", clusterInfo.ClusterName),
+					resource.TestCheckResourceAttr(resourceName, "cloud_provider", "AWS"),
 					resource.TestCheckResourceAttr(resourceName, "description", description),
 					resource.TestCheckResourceAttr(resourceName, "retention_in_days", retentionInDays),
 					resource.TestCheckResourceAttrSet(dataSourceName, "project_id"),
+					resource.TestCheckResourceAttr(dataSourceName, "type", "replicaSet"),
 					resource.TestCheckResourceAttr(dataSourceName, "cluster_name", clusterInfo.ClusterName),
+					resource.TestCheckResourceAttr(dataSourceName, "replica_set_name", clusterInfo.ClusterName),
+					resource.TestCheckResourceAttr(dataSourceName, "cloud_provider", "AWS"),
 					resource.TestCheckResourceAttr(dataSourceName, "description", description),
 					resource.TestCheckResourceAttrSet(dataSourcePluralSimpleName, "results.#"),
 					resource.TestCheckResourceAttrSet(dataSourcePluralPaginationName, "results.#"),
@@ -53,6 +62,37 @@ func TestAccBackupRSCloudBackupSnapshot_basic(t *testing.T) {
 				ImportState:             true,
 				ImportStateVerify:       true,
 				ImportStateVerifyIgnore: []string{"retention_in_days"},
+			},
+		},
+	})
+}
+
+func TestAccBackupRSCloudBackupSnapshot_sharded(t *testing.T) {
+	var (
+		orgID           = os.Getenv("MONGODB_ATLAS_ORG_ID")
+		projectName     = acctest.RandomWithPrefix("test-acc")
+		description     = "My description in my cluster"
+		retentionInDays = "4"
+	)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:                 func() { acc.PreCheckBasic(t) },
+		ProtoV6ProviderFactories: acc.TestAccProviderV6Factories,
+		CheckDestroy:             checkDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: configSharded(orgID, projectName, description, retentionInDays),
+				Check: resource.ComposeTestCheckFunc(
+					checkExists(resourceName),
+					resource.TestCheckResourceAttrSet(resourceName, "project_id"),
+					resource.TestCheckResourceAttr(resourceName, "type", "shardedCluster"),
+					resource.TestCheckResourceAttrWith(resourceName, "members.#", acc.IntGreatThan(0)),
+					resource.TestCheckResourceAttrWith(resourceName, "snapshot_ids.#", acc.IntGreatThan(0)),
+					resource.TestCheckResourceAttr(resourceName, "description", description),
+					resource.TestCheckResourceAttr(resourceName, "retention_in_days", retentionInDays),
+					resource.TestCheckResourceAttrSet(dataSourceName, "project_id"),
+					resource.TestCheckResourceAttr(dataSourceName, "type", "shardedCluster"),
+					resource.TestCheckResourceAttr(dataSourceName, "description", description)),
 			},
 		},
 	})
@@ -78,6 +118,9 @@ func checkExists(resourceName string) resource.TestCheckFunc {
 }
 
 func checkDestroy(s *terraform.State) error {
+	if os.Getenv("MONGODB_ATLAS_CLUSTER_NAME") != "" {
+		return nil
+	}
 	for _, rs := range s.RootModule().Resources {
 		if rs.Type != "mongodbatlas_cloud_backup_snapshot" {
 			continue
@@ -129,4 +172,47 @@ func configBasic(info *acc.ClusterInfo, description, retentionInDays string) str
 			items_per_page = 5
 		}
 	`, info.ClusterNameStr, info.ProjectIDStr, description, retentionInDays)
+}
+
+func configSharded(orgID, projectName, description, retentionInDays string) string {
+	return fmt.Sprintf(`
+		resource "mongodbatlas_project" "backup_project" {
+			org_id = %[1]q
+			name   = %[2]q
+		}
+
+		resource "mongodbatlas_advanced_cluster" "my_cluster" {
+			project_id   = mongodbatlas_project.backup_project.id
+			name           = "ClusterSharded"
+			cluster_type   = "SHARDED"
+			backup_enabled = true
+		
+			replication_specs {
+				num_shards = 3
+		
+				region_configs {
+					electable_specs {
+						instance_size = "M10"
+						node_count    = 3
+					}
+					analytics_specs {
+						instance_size = "M10"
+						node_count    = 1
+					}
+					provider_name = "AWS"
+					priority      = 7
+					region_name   = "US_EAST_1"
+				}
+		
+			}
+		}
+
+		resource "mongodbatlas_cloud_backup_snapshot" "test" {
+			project_id        = mongodbatlas_cluster.my_cluster.project_id
+			cluster_name      = mongodbatlas_cluster.my_cluster.name
+			description       = %[3]q
+			retention_in_days = %[4]q
+		}
+
+	`, orgID, projectName, description, retentionInDays)
 }
