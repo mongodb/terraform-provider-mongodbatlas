5f09abf73ee2005b52116e54d99b7e810000e5e5
Leo Antoli <430982+lantoli@users.noreply.github.com>
2024-02-15 08:21:52 +0100

    acc.SkipInUnitTest in atlas_user ds

--- internal/service/atlasuser/data_source_atlas_users_test.go
+++ internal/service/atlasuser/data_source_atlas_users_test.go
@@ -16,9 +16,7 @@ import (
 )
 
 func TestAccConfigDSAtlasUsers_ByOrgID(t *testing.T) {
-	if os.Getenv("TF_ACC") == "" { // needed while fetchOrgUsers is called from the test
-		t.Skip()
-	}
+	acc.SkipInUnitTest(t) // needed while fetchOrgUsers is called from the test
 	var (
 		dataSourceName = "data.mongodbatlas_atlas_users.test"
 		orgID          = os.Getenv("MONGODB_ATLAS_ORG_ID")
