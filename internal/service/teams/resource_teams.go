3377823dc4219ddfb3298ce5d9d6e8d97d9b6a93
Leo Antoli <430982+lantoli@users.noreply.github.com>
2024-02-21 09:32:08 +0100

    fix teams

--- internal/service/teams/resource_teams.go
+++ internal/service/teams/resource_teams.go
@@ -133,7 +133,7 @@ func resourceUpdate(ctx context.Context, d *schema.ResourceData, meta any) diag.
 
 	if d.HasChange("name") {
 		_, _, err := connV2.TeamsApi.RenameTeam(ctx, orgID, teamID,
-			&admin.Team{Name: d.Get("name").(string)},
+			&admin.TeamUpdate{Name: d.Get("name").(string)},
 		).Execute()
 		if err != nil {
 			return diag.FromErr(fmt.Errorf(errorTeamUpdate, err))
