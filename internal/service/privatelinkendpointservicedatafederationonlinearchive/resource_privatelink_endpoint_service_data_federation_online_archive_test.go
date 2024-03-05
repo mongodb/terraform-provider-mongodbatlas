210395699f266824cbb0762cca2b453c53b80a20
EspenAlbert <espen.albert@mongodb.com>
2024-03-05 11:05:37 +0000

    test: add a test for region and customer_endpoint_dns_name

--- internal/service/privatelinkendpointservicedatafederationonlinearchive/resource_privatelink_endpoint_service_data_federation_online_archive_test.go
+++ internal/service/privatelinkendpointservicedatafederationonlinearchive/resource_privatelink_endpoint_service_data_federation_online_archive_test.go
@@ -89,6 +89,40 @@ func TestAccNetworkPrivatelinkEndpointServiceDataFederationOnlineArchive_updateC
 	})
 }
 
+func TestAccNetworkPrivatelinkEndpointServiceDataFederationOnlineArchive_basicWithRegionDnsName(t *testing.T) {
+	// Skip because private endpoints are deleted daily from dev environment
+	acc.SkipTestForCI(t)
+	// Found in `.AWS.dev.us-east-1`:  https://github.com/10gen/mms/blob/85ec3df92711014b17643c05a61f5c580786556c/server/conf/data-lake-endpoint-services.json
+	serviceName := "vpce-svc-0a7247db33497082e"
+	customerEndpointDNSName := fmt.Sprintf("%s-8charsra.%s.us-east-1.vpce.amazonaws.com", endpointID, serviceName)
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { acc.PreCheck(t); acc.PreCheckPrivateEndpointServiceDataFederationOnlineArchiveRun(t) },
+		ProtoV6ProviderFactories: acc.TestAccProviderV6Factories,
+		CheckDestroy:             checkDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: resourceConfigBasicWithRegionDNSName(projectID, endpointID, customerEndpointDNSName),
+				Check: resource.ComposeTestCheckFunc(
+					checkExists(resourceName),
+					resource.TestCheckResourceAttr(resourceName, "project_id", projectID),
+					resource.TestCheckResourceAttr(resourceName, "endpoint_id", endpointID),
+					resource.TestCheckResourceAttr(resourceName, "comment", "Terraform Acceptance Test"),
+					resource.TestCheckResourceAttr(resourceName, "region", "US_EAST_1"),
+					resource.TestCheckResourceAttr(resourceName, "customer_endpoint_dns_name", customerEndpointDNSName),
+					resource.TestCheckResourceAttrSet(resourceName, "type"),
+					resource.TestCheckResourceAttrSet(resourceName, "provider_name"),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportStateIdFunc: importStateIDFunc(resourceName),
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
 func importStateIDFunc(resourceName string) resource.ImportStateIdFunc {
 	return func(s *terraform.State) (string, error) {
 		rs, ok := s.RootModule().Resources[resourceName]
@@ -154,4 +188,17 @@ func resourceConfigBasicWithComment(projectID, endpointID, comment string) strin
 	  comment					= %[3]q
 	}
 	`, projectID, endpointID, comment)
+}
+
+func resourceConfigBasicWithRegionDNSName(projectID, endpointID, customerEndpointDNSName string) string {
+	return fmt.Sprintf(`
+	resource "mongodbatlas_privatelink_endpoint_service_data_federation_online_archive" "test" {
+	  project_id					= %[1]q
+	  endpoint_id					= %[2]q
+	  provider_name					= "AWS"
+	  comment						= "Terraform Acceptance Test"
+	  region						= "US_EAST_1"
+	  customer_endpoint_dns_name 	= %[3]q
+	}
+	`, projectID, endpointID, customerEndpointDNSName)
 }
