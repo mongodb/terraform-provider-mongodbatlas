d62a3ae6a0a627a2e397a38c19410a2a1222847f
EspenAlbert <espen.albert@mongodb.com>
2024-03-08 08:03:34 +0000

    docs: rename the comment of example

--- examples/mongodbatlas_privatelink_endpoint/aws/data-federation-online-archive/atlas-privatelink.tf
+++ examples/mongodbatlas_privatelink_endpoint/aws/data-federation-online-archive/atlas-privatelink.tf
@@ -8,7 +8,7 @@ resource "mongodbatlas_privatelink_endpoint_service_data_federation_online_archi
   project_id                 = var.project_id
   endpoint_id                = aws_vpc_endpoint.vpce_east.id
   provider_name              = "AWS"
-  comment                    = "Terraform Acceptance Test"
+  comment                    = "Terraform Example Comment"
   region                     = "US_EAST_1"
   customer_endpoint_dns_name = aws_vpc_endpoint.vpce_east.dns_entry[0].dns_name
 }
