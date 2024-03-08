ed6524c91687e3ce7c014bceb9ee90c85a1f6aec
Leo Antoli <430982+lantoli@users.noreply.github.com>
2024-03-08 15:14:07 +0100

    fix prechecks to fail if not met

--- internal/testutil/acc/pre_check.go
+++ internal/testutil/acc/pre_check.go
@@ -97,7 +97,7 @@ func PreCheckGov(tb testing.TB) {
 		os.Getenv("MONGODB_ATLAS_PRIVATE_KEY") == "" ||
 		os.Getenv("MONGODB_ATLAS_PROJECT_ID_GOV") == "" ||
 		os.Getenv("MONGODB_ATLAS_ORG_ID_GOV") == "" {
-		tb.Skip()
+		tb.Fatal("`MONGODB_ATLAS_PUBLIC_KEY`, `MONGODB_ATLAS_PRIVATE_KEY`, `MONGODB_ATLAS_PROJECT_ID_GOV` and `MONGODB_ATLAS_ORG_ID_GOV` must be set for acceptance testing")
 	}
 }
 
@@ -202,7 +202,7 @@ func PreCheckSTSAssumeRole(tb testing.TB) {
 func PreCheckDataLakePipelineRun(tb testing.TB) {
 	tb.Helper()
 	if os.Getenv("MONGODB_ATLAS_DATA_LAKE_PIPELINE_RUN_ID") == "" {
-		tb.Skip("`MONGODB_ATLAS_DATA_LAKE_PIPELINE_RUN_ID` must be set for Projects acceptance testing")
+		tb.Fatal("`MONGODB_ATLAS_DATA_LAKE_PIPELINE_RUN_ID` must be set for Projects acceptance testing")
 	}
 	PreCheckDataLakePipelineRuns(tb)
 }
@@ -210,7 +210,7 @@ func PreCheckDataLakePipelineRun(tb testing.TB) {
 func PreCheckDataLakePipelineRuns(tb testing.TB) {
 	tb.Helper()
 	if os.Getenv("MONGODB_ATLAS_DATA_LAKE_PIPELINE_NAME") == "" {
-		tb.Skip("`MONGODB_ATLAS_DATA_LAKE_PIPELINE_NAME` must be set for Projects acceptance testing")
+		tb.Fatal("`MONGODB_ATLAS_DATA_LAKE_PIPELINE_NAME` must be set for Projects acceptance testing")
 	}
 	PreCheck(tb)
 }
@@ -245,7 +245,7 @@ func PreCheckFederatedSettings(tb testing.TB) {
 func PreCheckPrivateEndpointServiceDataFederationOnlineArchiveRun(tb testing.TB) {
 	tb.Helper()
 	if os.Getenv("MONGODB_ATLAS_PRIVATE_ENDPOINT_ID") == "" {
-		tb.Skip("`MONGODB_ATLAS_PRIVATE_ENDPOINT_ID` must be set for Private Endpoint Service Data Federation and Online Archive acceptance testing")
+		tb.Fatal("`MONGODB_ATLAS_PRIVATE_ENDPOINT_ID` must be set for Private Endpoint Service Data Federation and Online Archive acceptance testing")
 	}
 }
 
