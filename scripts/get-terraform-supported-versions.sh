13b0c12a11a6417ab97d5ee2c6c8afef413de802
Oriol <oriol.abadal@mongodb.com>
2024-03-14 17:58:13 +0100

    Update scripts/get-terraform-supported-versions.sh
    Co-authored-by: Andrea Angiolillo <andrea.angiolillo@mongodb.com>

--- scripts/get-terraform-supported-versions.sh
+++ scripts/get-terraform-supported-versions.sh
@@ -13,7 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+set -euo pipefail
 get_last_day_of_month() {
 	last_day_of_month=0
 	case $1 in
