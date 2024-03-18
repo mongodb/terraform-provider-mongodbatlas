4b57f813bb964de7ab863bb0ed3908b38905fbce
Leo Antoli <430982+lantoli@users.noreply.github.com>
2024-03-18 18:19:09 +0100

    fix tests

--- internal/service/networkcontainer/resource_network_container_test.go
+++ internal/service/networkcontainer/resource_network_container_test.go
@@ -161,7 +161,7 @@ func TestAccNetworkContainerRS_withRegionsGCP(t *testing.T) {
 	var (
 		projectID               = acc.ProjectIDExecution(t)
 		randInt                 = acctest.RandIntRange(0, 255)
-		gcpWithRegionsCidrBlock = fmt.Sprintf("10.%d.0.0/18", randInt)
+		gcpWithRegionsCidrBlock = fmt.Sprintf("10.%d.0.0/21", randInt)
 		regions                 = "[\"US_EAST_4\", \"US_WEST_3\"]"
 	)
 	// No ParallelTest because only one GCP network container is allowed per project
@@ -197,7 +197,7 @@ func TestAccNetworkContainerRS_importBasic(t *testing.T) {
 	var (
 		projectID = acc.ProjectIDExecution(t)
 		randInt   = acctest.RandIntRange(0, 255)
-		cidrBlock = fmt.Sprintf("11.8.%d.0/24", randInt)
+		cidrBlock = fmt.Sprintf("10.8.%d.0/24", randInt)
 	)
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:                 func() { acc.PreCheckBasic(t) },
