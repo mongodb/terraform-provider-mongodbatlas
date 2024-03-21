32180ecb5f1955c0f502f9e5cf239130851bce69
Oriol Arbusi <oriol.abadal@mongodb.com>
2024-03-21 16:54:36 +0100

    send slack

---
+++ scripts/generate-slack-notification-content.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+# Copyright 2024 MongoDB Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+set -euo pipefail
+
+# Check if the parameter is provided
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <success|failure>"
+    exit 1
+fi
+
+# Check if the parameter is either "success" or "failure"
+if [ "$1" != "success" ] && [ "$1" != "failure" ]; then
+    echo "Invalid parameter. Please provide either 'success' or 'failure'."
+    exit 1
+fi
+
+# Set the value of the "text" attribute based on the parameter
+if [ "$1" = "success" ]; then
+    text_value="HashiCorp Terraform Compatibility Matrix succeeded!"
+else
+    text_value="HashiCorp Terraform Compatibility Matrix failed!"
+fi
+
+# Construct the JSON and output it
+cat <<EOF
+{
+  "text": "$text_value",
+  "blocks": [
+    {
+      "type": "section",
+      "text": {
+        "type": "mrkdwn",
+        "text": "$text_value"
+      }
+    },
+	{
+	  "type": "actions",
+	  "elements": [
+		{
+			"type": "button",
+			"text": {
+			"type": "plain_text",
+			"text": ":github: Failed action"
+			},
+			"url": "${{ github.server_url }}/${{ github.repository }}/actions/runs/${{ github.run_id }}"
+		},
+  	  ]
+	}
+  ]
+}
+EOF
