c670c3a318be140115ddde0608d6fb566f83d4cc
EspenAlbert <espen.albert@mongodb.com>
2024-03-26 08:49:39 +0000

    test: support external providers

--- internal/testutil/mig/test_case.go
+++ internal/testutil/mig/test_case.go
@@ -4,21 +4,33 @@ import (
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/stretchr/testify/require"
 )
 
-func ConvertToMigrationTest(t *testing.T, test *resource.TestCase) resource.TestCase {
+func ConvertToMigrationTest(t *testing.T, test *resource.TestCase, externalProviders ...map[string]resource.ExternalProvider) resource.TestCase {
 	t.Helper()
 	checkLastVersion(t)
-
+	require.GreaterOrEqual(t, len(test.Steps), 1, "Must have at least 1 test step.")
 	firstStep := test.Steps[0]
-	firstStep.ExternalProviders = ExternalProviders()
 
+	if len(externalProviders) == 0 {
+		externalProviders = append(externalProviders, ExternalProviders())
+	}
+	steps := []resource.TestStep{}
+
+	for _, provider := range externalProviders {
+		steps = append(steps, *useExternalProvider(firstStep, provider))
+	}
+	steps = append(steps, TestStepCheckEmptyPlan(firstStep.Config))
 	return resource.TestCase{
 		PreCheck:     test.PreCheck,
 		CheckDestroy: test.CheckDestroy,
-		Steps: []resource.TestStep{
-			firstStep,
-			TestStepCheckEmptyPlan(firstStep.Config),
-		},
+		Steps:        steps,
 	}
+}
+
+//nolint:gocritic
+func useExternalProvider(step resource.TestStep, provider map[string]resource.ExternalProvider) *resource.TestStep {
+	step.ExternalProviders = provider
+	return &step
 }
--- internal/testutil/mig/test_case_test.go
+++ internal/testutil/mig/test_case_test.go
@@ -3,9 +3,107 @@ package mig_test
 import (
 	"testing"
 
-	"github.com/stretchr/testify/require"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
+	"github.com/mongodb/terraform-provider-mongodbatlas/internal/testutil/acc"
+	"github.com/mongodb/terraform-provider-mongodbatlas/internal/testutil/mig"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestConvertToMigration(t *testing.T) {
-	require.True(t, true, "always true")
+	var (
+		preCheckCalled     = false
+		checkDestroyCalled = false
+		config             = "someTerraformConfig"
+	)
+	preCheck := func() {
+		preCheckCalled = true
+	}
+	firstStep := resource.TestStep{
+		Config: config,
+		Check:  resource.TestCheckResourceAttrSet("someTarget", "someAttribute"),
+	}
+
+	asserter := assert.New(t)
+
+	convertAndCall := func(test resource.TestCase) resource.TestCase {
+		newTest := mig.ConvertToMigrationTest(t, &test)
+		newTest.PreCheck()
+		if newTest.CheckDestroy != nil {
+			asserter.NoError(newTest.CheckDestroy(nil))
+		}
+		return newTest
+	}
+	defaultAssertions := func(test resource.TestCase) {
+		asserter.Len(test.Steps, 2, "Expected 2 steps (one extra test step)")
+		newFirstStep := test.Steps[0]
+		asserter.Equal(config, newFirstStep.Config)
+
+		planStep := test.Steps[1]
+		asserter.Equal(mig.TestStepCheckEmptyPlan(config), planStep)
+	}
+
+	t.Run("normal call with check and destroy", func(t *testing.T) {
+		checkDestroy := func(*terraform.State) error {
+			checkDestroyCalled = true
+			return nil
+		}
+		test := convertAndCall(resource.TestCase{
+			PreCheck:     preCheck,
+			CheckDestroy: checkDestroy,
+			Steps: []resource.TestStep{
+				firstStep,
+			},
+		})
+		asserter.True(preCheckCalled)
+		asserter.True(checkDestroyCalled)
+		defaultAssertions(test)
+	})
+
+	t.Run("check destroy is nil has no panic", func(t *testing.T) {
+		test := convertAndCall(resource.TestCase{
+			PreCheck: preCheck,
+			Steps: []resource.TestStep{
+				firstStep,
+			},
+		})
+		defaultAssertions(test)
+	})
+
+	t.Run("more than 1 step uses only 1 step", func(t *testing.T) {
+		test := convertAndCall(resource.TestCase{
+			PreCheck: preCheck,
+			Steps: []resource.TestStep{
+				firstStep,
+				{
+					Config: "differentConfig",
+					Check:  resource.TestCheckResourceAttrSet("target", "attribute"),
+				},
+			},
+		})
+		defaultAssertions(test)
+	})
+
+	t.Run("explicit ExternalProvider version", func(t *testing.T) {
+		test := mig.ConvertToMigrationTest(t, &resource.TestCase{
+			PreCheck: preCheck,
+			Steps:    []resource.TestStep{firstStep},
+		}, acc.ExternalProviders("1.2.3"))
+		defaultAssertions(test)
+		asserter.Equal("1.2.3", test.Steps[0].ExternalProviders["mongodbatlas"].VersionConstraint)
+	})
+
+	t.Run("multiple ExternalProviders", func(t *testing.T) {
+		test := mig.ConvertToMigrationTest(t, &resource.TestCase{
+			PreCheck: preCheck,
+			Steps:    []resource.TestStep{firstStep},
+		}, acc.ExternalProviders("1.2.3"), acc.ExternalProvidersWithAWS("4.5.6"))
+		asserter.Len(test.Steps, 3)
+		asserter.Equal("1.2.3", test.Steps[0].ExternalProviders["mongodbatlas"].VersionConstraint)
+		asserter.Equal("4.5.6", test.Steps[1].ExternalProviders["mongodbatlas"].VersionConstraint)
+		// must be upgraded when the aws provider version is changed
+		asserter.Equal("5.1.0", test.Steps[1].ExternalProviders["aws"].VersionConstraint)
+		planStep := test.Steps[2]
+		asserter.Equal(mig.TestStepCheckEmptyPlan(config), planStep)
+	})
 }
