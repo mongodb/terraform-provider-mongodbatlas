6c33853d5280da29dcb13bdc07f56393bd3e3646
Leo Antoli <430982+lantoli@users.noreply.github.com>
2024-03-26 11:19:30 +0100

    join import test

--- internal/service/auditing/resource_auditing_test.go
+++ internal/service/auditing/resource_auditing_test.go
@@ -64,39 +64,6 @@ func TestAccGenericAuditing_basic(t *testing.T) {
 					resource.TestCheckResourceAttr(resourceName, "configuration_type", "FILTER_JSON"),
 				),
 			},
-		},
-	})
-}
-
-func TestAccGenericAuditing_importBasic(t *testing.T) {
-	var (
-		resourceName = "mongodbatlas_auditing.test"
-		orgID        = os.Getenv("MONGODB_ATLAS_ORG_ID")
-		projectName  = acc.RandomProjectName()
-		auditAuth    = true
-		auditFilter  = "{ 'atype': 'authenticate', 'param': {   'user': 'auditAdmin',   'db': 'admin',   'mechanism': 'SCRAM-SHA-1' }}"
-		enabled      = true
-	)
-
-	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:                 func() { acc.PreCheckBasic(t) },
-		ProtoV6ProviderFactories: acc.TestAccProviderV6Factories,
-		CheckDestroy:             checkDestroy,
-		Steps: []resource.TestStep{
-			{
-				Config: configBasic(orgID, projectName, auditFilter, auditAuth, enabled),
-				Check: resource.ComposeTestCheckFunc(
-					checkExists(resourceName),
-					resource.TestCheckResourceAttrSet(resourceName, "project_id"),
-					resource.TestCheckResourceAttrSet(resourceName, "audit_filter"),
-					resource.TestCheckResourceAttrSet(resourceName, "audit_authorization_success"),
-					resource.TestCheckResourceAttrSet(resourceName, "enabled"),
-					resource.TestCheckResourceAttr(resourceName, "audit_filter", auditFilter),
-					resource.TestCheckResourceAttr(resourceName, "audit_authorization_success", "true"),
-					resource.TestCheckResourceAttr(resourceName, "enabled", "true"),
-					resource.TestCheckResourceAttr(resourceName, "configuration_type", "FILTER_JSON"),
-				),
-			},
 			{
 				ResourceName:            resourceName,
 				ImportStateIdFunc:       importStateIDFunc(resourceName),
