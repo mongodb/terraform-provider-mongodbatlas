14117a56460a152f8d5b8556869ea1ba6cb763cd
Leo Antoli <430982+lantoli@users.noreply.github.com>
2024-04-04 09:55:59 +0200

    cloud_backup_snapshot_export_bucket

--- internal/service/cloudbackupsnapshotexportbucket/resource_cloud_backup_snapshot_export_bucket_test.go
+++ internal/service/cloudbackupsnapshotexportbucket/resource_cloud_backup_snapshot_export_bucket_test.go
@@ -10,43 +10,65 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
 	"github.com/mongodb/terraform-provider-mongodbatlas/internal/common/conversion"
 	"github.com/mongodb/terraform-provider-mongodbatlas/internal/testutil/acc"
-	matlas "go.mongodb.org/atlas/mongodbatlas"
 )
 
-func TestAccBackupRSBackupSnapshotExportBucket_basic(t *testing.T) {
-	acc.SkipTestForCI(t)
+var (
+	resourceName         = "mongodbatlas_cloud_backup_snapshot_export_bucket.test"
+	dataSourceName       = "data.mongodbatlas_cloud_backup_snapshot_export_bucket.test"
+	dataSourcePluralName = "data.mongodbatlas_cloud_backup_snapshot_export_buckets.test"
+)
+
+func TestAccBackupSnapshotExportBucket_basic(t *testing.T) {
+	resource.ParallelTest(t, *basicTestCase(t))
+}
+
+func basicTestCase(tb testing.TB) *resource.TestCase {
+	tb.Helper()
+	acc.SkipTestForCI(tb) // needs AWS IAM role and S3 bucket
+
 	var (
-		snapshotExportBucket matlas.CloudProviderSnapshotExportBucket
-		resourceName         = "mongodbatlas_cloud_backup_snapshot_export_bucket.test"
-		projectID            = os.Getenv("MONGODB_ATLAS_PROJECT_ID")
-		bucketName           = os.Getenv("AWS_S3_BUCKET")
-		iamRoleID            = os.Getenv("IAM_ROLE_ID")
+		projectID  = os.Getenv("MONGODB_ATLAS_PROJECT_ID")
+		bucketName = os.Getenv("AWS_S3_BUCKET")
+		iamRoleID  = os.Getenv("IAM_ROLE_ID")
 	)
-	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:                 func() { acc.PreCheck(t); acc.PreCheckS3Bucket(t) },
+
+	return &resource.TestCase{
+		PreCheck:                 func() { acc.PreCheck(tb); acc.PreCheckS3Bucket(tb) },
 		ProtoV6ProviderFactories: acc.TestAccProviderV6Factories,
-		CheckDestroy:             testAccCheckMongoDBAtlasBackupSnapshotExportBucketDestroy,
+		CheckDestroy:             checkDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccMongoDBAtlasBackupSnapshotExportBucketConfig(projectID, bucketName, iamRoleID),
+				Config: configBasic(projectID, bucketName, iamRoleID),
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckMongoDBAtlasBackupSnapshotExportBucketExists(resourceName, &snapshotExportBucket),
+					checkExists(resourceName),
 					resource.TestCheckResourceAttr(resourceName, "project_id", projectID),
 					resource.TestCheckResourceAttr(resourceName, "bucket_name", "example-bucket"),
 					resource.TestCheckResourceAttr(resourceName, "cloud_provider", "AWS"),
+					resource.TestCheckResourceAttrSet(resourceName, "iam_role_id"),
+
+					resource.TestCheckResourceAttr(dataSourceName, "project_id", projectID),
+					resource.TestCheckResourceAttr(dataSourceName, "bucket_name", "example-bucket"),
+					resource.TestCheckResourceAttr(dataSourceName, "cloud_provider", "AWS"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "iam_role_id"),
+
+					resource.TestCheckResourceAttrSet(dataSourcePluralName, "project_id"),
+					resource.TestCheckResourceAttr(dataSourcePluralName, "bucket_name", "example-bucket"),
+					resource.TestCheckResourceAttr(dataSourcePluralName, "cloud_provider", "AWS"),
+					resource.TestCheckResourceAttrSet(dataSourcePluralName, "iam_role_id"),
+					resource.TestCheckResourceAttr(dataSourcePluralName, "results.#", "1"),
 				),
 			},
 			{
 				ResourceName:      resourceName,
-				ImportStateIdFunc: testAccCheckMongoDBAtlasBackupSnapshotExportBucketImportStateIDFunc(resourceName),
+				ImportStateIdFunc: importStateIDFunc(resourceName),
 				ImportState:       true,
 				ImportStateVerify: true,
 			},
 		},
-	})
+	}
 }
 
-func testAccCheckMongoDBAtlasBackupSnapshotExportBucketExists(resourceName string, snapshotExportBucket *matlas.CloudProviderSnapshotExportBucket) resource.TestCheckFunc {
+func checkExists(resourceName string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		rs, ok := s.RootModule().Resources[resourceName]
 		if !ok {
@@ -56,16 +78,15 @@ func testAccCheckMongoDBAtlasBackupSnapshotExportBucketExists(resourceName strin
 			return fmt.Errorf("no ID is set")
 		}
 		ids := conversion.DecodeStateID(rs.Primary.ID)
-		response, _, err := acc.Conn().CloudProviderSnapshotExportBuckets.Get(context.Background(), ids["project_id"], ids["id"])
+		_, _, err := acc.Conn().CloudProviderSnapshotExportBuckets.Get(context.Background(), ids["project_id"], ids["id"])
 		if err == nil {
-			*snapshotExportBucket = *response
 			return nil
 		}
 		return fmt.Errorf("snapshot export bucket (%s) does not exist", ids["id"])
 	}
 }
 
-func testAccCheckMongoDBAtlasBackupSnapshotExportBucketDestroy(state *terraform.State) error {
+func checkDestroy(state *terraform.State) error {
 	for _, rs := range state.RootModule().Resources {
 		if rs.Type != "mongodbatlas_cloud_backup_snapshot_export_bucket" {
 			continue
@@ -79,7 +100,7 @@ func testAccCheckMongoDBAtlasBackupSnapshotExportBucketDestroy(state *terraform.
 	return nil
 }
 
-func testAccCheckMongoDBAtlasBackupSnapshotExportBucketImportStateIDFunc(resourceName string) resource.ImportStateIdFunc {
+func importStateIDFunc(resourceName string) resource.ImportStateIdFunc {
 	return func(s *terraform.State) (string, error) {
 		rs, ok := s.RootModule().Resources[resourceName]
 		if !ok {
@@ -90,7 +111,7 @@ func testAccCheckMongoDBAtlasBackupSnapshotExportBucketImportStateIDFunc(resourc
 	}
 }
 
-func testAccMongoDBAtlasBackupSnapshotExportBucketConfig(projectID, bucketName, iamRoleID string) string {
+func configBasic(projectID, bucketName, iamRoleID string) string {
 	return fmt.Sprintf(`
 	resource "mongodbatlas_cloud_backup_snapshot_export_bucket" "test" {
 			project_id     = "%[1]s"
@@ -98,5 +119,14 @@ func testAccMongoDBAtlasBackupSnapshotExportBucketConfig(projectID, bucketName, 
        		bucket_name    = "%[2]s"
        		cloud_provider = "AWS"
     }
+
+		data "mongodbatlas_cloud_backup_snapshot_export_bucket" "test" {
+			project_id   = mongodbatlas_cloud_backup_snapshot_export_bucket.test.project_id
+			id = mongodbatlas_cloud_backup_snapshot_export_bucket.test.id
+		}		
+
+		data "mongodbatlas_cloud_backup_snapshot_export_buckets" "test" {
+			project_id   = mongodbatlas_cloud_backup_snapshot_export_bucket.test.project_id
+		}
 	`, projectID, bucketName, iamRoleID)
 }
