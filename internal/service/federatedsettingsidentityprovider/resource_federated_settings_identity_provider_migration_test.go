7c0ece4305e407ef2be4d7f5ee408a5e3621215e
Leo Antoli <430982+lantoli@users.noreply.github.com>
2024-04-04 13:32:09 +0200

    skip TestMigFederatedSettingsIdentityProviderRS_basic

--- internal/service/federatedsettingsidentityprovider/resource_federated_settings_identity_provider_migration_test.go
+++ internal/service/federatedsettingsidentityprovider/resource_federated_settings_identity_provider_migration_test.go
@@ -3,9 +3,11 @@ package federatedsettingsidentityprovider_test
 import (
 	"testing"
 
+	"github.com/mongodb/terraform-provider-mongodbatlas/internal/testutil/acc"
 	"github.com/mongodb/terraform-provider-mongodbatlas/internal/testutil/mig"
 )
 
 func TestMigFederatedSettingsIdentityProviderRS_basic(t *testing.T) {
+	acc.SkipTestForCI(t) // this resource can only be imported
 	mig.CreateAndRunTest(t, basicTestCase(t))
 }
