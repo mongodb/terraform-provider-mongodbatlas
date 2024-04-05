53b309db3fa8c3c544a61898393b554268671959
Agustin Bettati <bettatiagustin@gmail.com>
2024-04-05 10:31:22 +0200

    shell check fixes

--- scripts/generate-changelog.sh
+++ scripts/generate-changelog.sh
@@ -14,20 +14,20 @@ CHANGELOG_TMP_FILE_NAME="CHANGELOG.tmp"
 TARGET_SHA=$(git rev-parse HEAD)
 
 PREVIOUS_RELEASE_TAG=$(git describe --abbrev=0 --match='v*.*.*' --tags)
-PREVIOUS_RELEASE_SHA=$(git rev-list -n 1 $PREVIOUS_RELEASE_TAG)
+PREVIOUS_RELEASE_SHA=$(git rev-list -n 1 "$PREVIOUS_RELEASE_TAG")
 
-if [ $TARGET_SHA == $PREVIOUS_RELEASE_SHA ]; then
+if [ "$TARGET_SHA" == "$PREVIOUS_RELEASE_SHA" ]; then
   echo "Nothing to do"
   exit 0
 fi
 
 # contains all content of CHANGELOG.md starting from the last release (excludes Unreleased section)
-PREVIOUS_CHANGELOG=$(sed -n -e "/## $(echo ${PREVIOUS_RELEASE_TAG} | tr -d v)/,\$p" $__parent/$CHANGELOG_FILE_NAME)
+PREVIOUS_CHANGELOG=$(sed -n -e "/## $(echo "${PREVIOUS_RELEASE_TAG}" | tr -d v)/,\$p" "$__parent"/$CHANGELOG_FILE_NAME)
 
 # this if then clause is only defined to handle legacy format, can be removed after coming release (succeeding 1.15.3)
 if [ -z "$PREVIOUS_CHANGELOG" ]
 then
-    PREVIOUS_CHANGELOG=$(sed -n -e "/## \[${PREVIOUS_RELEASE_TAG}\]/,\$p" $__parent/$CHANGELOG_FILE_NAME)
+    PREVIOUS_CHANGELOG=$(sed -n -e "/## \[${PREVIOUS_RELEASE_TAG}\]/,\$p" "$__parent"/$CHANGELOG_FILE_NAME)
 fi
 
 if [ -z "$PREVIOUS_CHANGELOG" ]
@@ -36,12 +36,12 @@ then
     exit 1
 fi
 
-CHANGELOG=$($(go env GOPATH)/bin/changelog-build -this-release $TARGET_SHA \
-                      -last-release $PREVIOUS_RELEASE_SHA \
-                      -git-dir $__parent \
+CHANGELOG=$($(go env GOPATH)/bin/changelog-build -this-release "$TARGET_SHA" \
+                      -last-release "$PREVIOUS_RELEASE_SHA" \
+                      -git-dir "$__parent" \
                       -entries-dir .changelog \
-                      -changelog-template $__dir/changelog/changelog.tmpl \
-                      -note-template $__dir/changelog/release-note.tmpl)
+                      -changelog-template "$__dir/changelog/changelog.tmpl" \
+                      -note-template "$__dir/changelog/release-note.tmpl")
 
 if [ -z "$CHANGELOG" ]
 then
@@ -52,9 +52,12 @@ fi
 rm -f $CHANGELOG_TMP_FILE_NAME
 
 sed -n -e "1{/## /p;}" $__parent/$CHANGELOG_FILE_NAME > $CHANGELOG_TMP_FILE_NAME # places header (first line) of current changelog into new one
-echo "$CHANGELOG" >> $CHANGELOG_TMP_FILE_NAME
-echo >> $CHANGELOG_TMP_FILE_NAME
-echo "$PREVIOUS_CHANGELOG" >> $CHANGELOG_TMP_FILE_NAME
+
+{
+    echo "$CHANGELOG" 
+    echo 
+    echo "$PREVIOUS_CHANGELOG"
+} >> $CHANGELOG_TMP_FILE_NAME
 
 cp $CHANGELOG_TMP_FILE_NAME $CHANGELOG_FILE_NAME
 rm $CHANGELOG_TMP_FILE_NAME
