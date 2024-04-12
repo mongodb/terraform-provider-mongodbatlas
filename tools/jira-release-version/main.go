1fd155daf540a0174fccb123a29de7c3e82812c9
Leo Antoli <430982+lantoli@users.noreply.github.com>
2024-04-12 06:11:52 +0200

    version id

--- tools/jira-release-version/main.go
+++ tools/jira-release-version/main.go
@@ -5,49 +5,63 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	jira "github.com/andygrunwald/go-jira/v2/onpremise"
-	"github.com/mongodb/terraform-provider-mongodbatlas/internal/common/conversion"
 )
 
 const (
-	projectID = "CLOUDP"
-	jiraURL   = "https://jira.mongodb.org"
+	envJira       = "JIRA_API_TOKEN"
+	envVersion    = "VERSION_NUMBER"
+	projectID     = "CLOUDP"
+	jiraURL       = "https://jira.mongodb.org"
+	versionPrefix = "terraform-provider-"
 )
 
 func main() {
 	client := getJiraClient()
-	ctx := context.Background()
-
-	_, _, err := client.Project.Get(ctx, projectID)
-	if err != nil {
-		log.Fatalf("Error getting project info: %v", err)
-	}
-
-	versionID := 39020
-	_, _, err = client.Version.Get(ctx, versionID)
-	if err != nil {
-		log.Fatalf("Error getting version: %v", err)
-	}
-
-	updateVersion := &jira.Version{ID: "39020", Released: conversion.Pointer(true), ReleaseDate: "2024-04-09"}
-	update, _, err := client.Version.Update(ctx, updateVersion)
-	fmt.Println(update)
-	if err != nil {
-		log.Fatalf("Error updating version: %v", err)
-	}
+	versionName := versionPrefix + getVersion()
+	versionID := getVersionID(client, versionName)
+	fmt.Println(versionID)
 }
 
 func getJiraClient() *jira.Client {
-	apiToken := os.Getenv("JIRA_API_TOKEN")
+	apiToken := os.Getenv(envJira)
 	if apiToken == "" {
-		log.Fatal("Environment variable JIRA_API_TOKEN is required")
+		log.Fatalf("Environment variable %s is required", envJira)
 	}
-
 	tp := jira.BearerAuthTransport{Token: apiToken}
 	client, err := jira.NewClient(jiraURL, tp.Client())
 	if err != nil {
 		log.Fatalf("Error getting Jira client: %v", err)
 	}
 	return client
+}
+
+func getVersion() string {
+	version := os.Getenv(envVersion)
+	if version == "" {
+		log.Fatalf("Environment variable %s is required", envVersion)
+	}
+	if strings.Contains(version, "pre") {
+		fmt.Printf("Skipping release version for pre-release: %s\n", version)
+		os.Exit(0)
+	}
+	return strings.TrimPrefix(version, "v")
+}
+
+func getVersionID(client *jira.Client, versionName string) string {
+	ctx := context.Background()
+	projects, _, err := client.Project.Get(ctx, projectID)
+	if err != nil {
+		log.Fatalf("Error getting project info: %v", err)
+	}
+	for i := range projects.Versions {
+		v := &projects.Versions[i]
+		if v.Name == versionName {
+			return v.ID
+		}
+	}
+	log.Fatalf("Version not found: %s", versionName)
+	return ""
 }
