b1f0e8d51c3352bce9822359023cf7d4e293fcfd
Marco Suma <marco.suma@mongodb.com>
2024-04-17 17:08:46 +0200

    Update tools/check-changelog-entry-file/main.go

--- tools/check-changelog-entry-file/main.go
+++ tools/check-changelog-entry-file/main.go
@@ -46,7 +46,7 @@ func main() {
 		return
 	}
 
-	log.Fatalf("Changelog file %s not found due to the following reason: %v.\nHave you ran the `make generate-changelog-entry` command?\nIf this PR doesn't need a changelog entry file, consider using label %s.\nRead contributing guides (https://github.com/mongodb/terraform-provider-mongodbatlas/blob/master/contributing/changelog-process.md) for more info.", filePath, errFile, skipLabelName)
+	log.Fatalf("Have you ran the `make generate-changelog-entry` command?\nIf this PR doesn't need a changelog entry file, consider using label %s.\nRead contributing guides (https://github.com/mongodb/terraform-provider-mongodbatlas/blob/master/contributing/changelog-process.md) for more info.\nChangelog entry file %s not found due to the following reason: %v.", skipLabelName, filePath, errFile)
 }
 
 func validateChangelog(filePath, body string) {
