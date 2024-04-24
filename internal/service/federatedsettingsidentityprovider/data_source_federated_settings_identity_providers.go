0022182d3f9263d54b7447788d3ac4a376f70ab2
Agustin Bettati <bettatiagustin@gmail.com>
2024-04-24 11:21:50 +0200

    add deprecation message and comment

--- internal/service/federatedsettingsidentityprovider/data_source_federated_settings_identity_providers.go
+++ internal/service/federatedsettingsidentityprovider/data_source_federated_settings_identity_providers.go
@@ -10,6 +10,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 
+	"github.com/mongodb/terraform-provider-mongodbatlas/internal/common/constant"
 	"github.com/mongodb/terraform-provider-mongodbatlas/internal/config"
 )
 
@@ -22,12 +23,14 @@ func PluralDataSource() *schema.Resource {
 				Required: true,
 			},
 			"page_num": {
-				Type:     schema.TypeInt,
-				Optional: true,
+				Type:       schema.TypeInt,
+				Optional:   true,
+				Deprecated: fmt.Sprintf(constant.DeprecationParamByVersion, "1.18.0"),
 			},
 			"items_per_page": {
-				Type:     schema.TypeInt,
-				Optional: true,
+				Type:       schema.TypeInt,
+				Optional:   true,
+				Deprecated: fmt.Sprintf(constant.DeprecationParamByVersion, "1.18.0"),
 			},
 			"results": {
 				Type:     schema.TypeList,
@@ -257,6 +260,8 @@ func dataSourceMongoDBAtlasFederatedSettingsIdentityProvidersRead(ctx context.Co
 		return diag.FromErr(errors.New("federation_settings_id must be configured"))
 	}
 
+	// once the SDK is upgraded to version v20231115010 we can use pagination parameters to iterate over all results
+	// pagination attribute are deprecated and can be removed as we move towards not exposing these pagination options
 	params := &admin20231115008.ListIdentityProvidersApiParams{
 		FederationSettingsId: federationSettingsID.(string),
 		Protocol:             &[]string{OIDC, SAML},
