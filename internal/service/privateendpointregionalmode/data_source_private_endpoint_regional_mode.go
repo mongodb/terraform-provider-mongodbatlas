6aa4a4dd8202b9be84986dfc81b3709132f69a32
EspenAlbert <espen.albert@mongodb.com>
2024-05-08 06:33:34 +0100

    chore: remove commented legacy code

--- internal/service/privateendpointregionalmode/data_source_private_endpoint_regional_mode.go
+++ internal/service/privateendpointregionalmode/data_source_private_endpoint_regional_mode.go
@@ -47,29 +47,3 @@ func dataSourceRead(ctx context.Context, d *schema.ResourceData, meta any) diag.
 
 	return nil
 }
-
-// func dataSourceReadLegacy(ctx context.Context, d *schema.ResourceData, meta any) diag.Diagnostics {
-// 	// Get client connection.
-// 	conn := meta.(*config.MongoDBClient).Atlas
-// 	projectID := d.Get("project_id").(string)
-
-// 	setting, _, err := conn.PrivateEndpoints.GetRegionalizedPrivateEndpointSetting(ctx, projectID)
-// 	if err != nil {
-// 		// case 404
-// 		// deleted in the backend case
-// 		if strings.Contains(err.Error(), "404") {
-// 			d.SetId("")
-// 			return nil
-// 		}
-
-// 		return diag.Errorf("error getting private endpoint regional mode: %s", err)
-// 	}
-
-// 	if err := d.Set("enabled", setting.Enabled); err != nil {
-// 		return diag.Errorf("error setting `enabled` for enabled (%s): %s", d.Id(), err)
-// 	}
-
-// 	d.SetId(projectID)
-
-// 	return nil
-// }
