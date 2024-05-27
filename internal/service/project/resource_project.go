415c54b9ad46603850148d684b449c4bc22114bc
Espen Albert <EspenAlbert@users.noreply.github.com>
2024-05-27 10:30:09 +0100

    fix: Marks `project.region_usage_restrictions` as OptionalComputed to avoid provider upgrade errors (#2291)

--- internal/service/project/resource_project.go
+++ internal/service/project/resource_project.go
@@ -216,6 +216,7 @@ func (r *projectRS) Schema(ctx context.Context, req resource.SchemaRequest, resp
 				},
 			},
 			"region_usage_restrictions": schema.StringAttribute{
+				Computed: true,
 				Optional: true,
 			},
 			"ip_addresses": schema.SingleNestedAttribute{
@@ -311,7 +312,7 @@ func (r *projectRS) Create(ctx context.Context, req resource.CreateRequest, resp
 		OrgId:                     projectPlan.OrgID.ValueString(),
 		Name:                      projectPlan.Name.ValueString(),
 		WithDefaultAlertsSettings: projectPlan.WithDefaultAlertsSettings.ValueBoolPointer(),
-		RegionUsageRestrictions:   projectPlan.RegionUsageRestrictions.ValueStringPointer(),
+		RegionUsageRestrictions:   conversion.StringNullIfEmpty(projectPlan.RegionUsageRestrictions.ValueString()).ValueStringPointer(),
 		Tags:                      &tags,
 	}
 
