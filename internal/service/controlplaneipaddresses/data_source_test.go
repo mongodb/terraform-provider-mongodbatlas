9f634efeeac9f8a9063c40c89a8ffa03427847d6
Agustin Bettati <bettatiagustin@gmail.com>
2024-06-06 10:30:35 +0200

    adding acceptance test

--- internal/service/controlplaneipaddresses/data_source_test.go
+++ internal/service/controlplaneipaddresses/data_source_test.go
@@ -7,29 +7,23 @@ import (
 	"github.com/mongodb/terraform-provider-mongodbatlas/internal/testutil/acc"
 )
 
-// TODO: if acceptance test will be run in an existing CI group of resources, the name should include the group in the prefix followed by the name of the resource e.i. TestAccStreamRSStreamInstance_basic
-// In addition, if acceptance test contains testing of both resource and data sources, the RS/DS can be omitted.
 func TestAccControlPlaneIpAddressesDS_basic(t *testing.T) {
+	dataSourceName := "data.mongodbatlas_control_plane_ip_addresses.test"
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:                 func() { acc.PreCheckBasic(t) },
 		ProtoV6ProviderFactories: acc.TestAccProviderV6Factories,
-		//		CheckDestroy:             checkDestroyControlPlaneIpAddresses,
-		Steps: []resource.TestStep{ // TODO: verify updates and import in case of resources
-			//			{
-			//				Config: ControlPlaneIpAddressesConfig(),
-			//				Check:  ControlPlaneIpAddressesAttributeChecks(),
-			//			},
-			//          {
-			//				Config: ControlPlaneIpAddressesConfig(),
-			//				Check:  ControlPlaneIpAddressesAttributeChecks(),
-			//			},
-			//			{
-			//				Config:            ControlPlaneIpAddressesConfig(),
-			//				ResourceName:      resourceName,
-			//				ImportStateIdFunc: checkControlPlaneIpAddressesImportStateIDFunc,
-			//				ImportState:       true,
-			//				ImportStateVerify: true,
+		Steps: []resource.TestStep{
+			{
+				Config: configBasic,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(dataSourceName, "outbound.aws.us-east-1.0"),
+				),
+			},
 		},
-	},
-	)
+	})
 }
+
+const configBasic = `
+data "mongodbatlas_control_plane_ip_addresses" "test" {
+}
+`
