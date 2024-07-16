21239d2ffc4119c6228a55c1c1ebefa16b19d32a
maastha <122359335+maastha@users.noreply.github.com>
2024-07-16 09:52:02 +0100

    chore: Groups common code in data source and resource for populating root fields (#2419)

--- internal/service/advancedcluster/data_source_advanced_cluster.go
+++ internal/service/advancedcluster/data_source_advanced_cluster.go
@@ -6,13 +6,11 @@ import (
 	"net/http"
 
 	admin20231115 "go.mongodb.org/atlas-sdk/v20231115014/admin"
-	"go.mongodb.org/atlas-sdk/v20240530002/admin"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 
 	"github.com/mongodb/terraform-provider-mongodbatlas/internal/common/constant"
-	"github.com/mongodb/terraform-provider-mongodbatlas/internal/common/conversion"
 	"github.com/mongodb/terraform-provider-mongodbatlas/internal/config"
 )
 
@@ -288,7 +286,7 @@ func dataSourceRead(ctx context.Context, d *schema.ResourceData, meta any) diag.
 			return diag.FromErr(fmt.Errorf(ErrorClusterAdvancedSetting, "replication_specs", clusterName, err))
 		}
 
-		diags := setRootFields(d, convertClusterDescToLatestExcludeRepSpecs(clusterDescOld))
+		diags := setRootFields(d, convertClusterDescToLatestExcludeRepSpecs(clusterDescOld), false)
 		if diags.HasError() {
 			return diags
 		}
@@ -313,7 +311,7 @@ func dataSourceRead(ctx context.Context, d *schema.ResourceData, meta any) diag.
 			return diag.FromErr(fmt.Errorf(ErrorClusterAdvancedSetting, "replication_specs", clusterName, err))
 		}
 
-		diags := setRootFields(d, clusterDescLatest)
+		diags := setRootFields(d, clusterDescLatest, false)
 		if diags.HasError() {
 			return diags
 		}
@@ -334,80 +332,5 @@ func dataSourceRead(ctx context.Context, d *schema.ResourceData, meta any) diag.
 	}
 
 	d.SetId(clusterID)
-	return nil
-}
-
-func setRootFields(d *schema.ResourceData, cluster *admin.ClusterDescription20250101) diag.Diagnostics {
-	clusterName := *cluster.Name
-
-	if err := d.Set("backup_enabled", cluster.GetBackupEnabled()); err != nil {
-		return diag.FromErr(fmt.Errorf(ErrorClusterAdvancedSetting, "backup_enabled", clusterName, err))
-	}
-
-	if err := d.Set("bi_connector_config", flattenBiConnectorConfig(cluster.BiConnector)); err != nil {
-		return diag.FromErr(fmt.Errorf(ErrorClusterAdvancedSetting, "bi_connector_config", clusterName, err))
-	}
-
-	if err := d.Set("cluster_type", cluster.GetClusterType()); err != nil {
-		return diag.FromErr(fmt.Errorf(ErrorClusterAdvancedSetting, "cluster_type", clusterName, err))
-	}
-
-	if err := d.Set("connection_strings", flattenConnectionStrings(*cluster.ConnectionStrings)); err != nil {
-		return diag.FromErr(fmt.Errorf(ErrorClusterAdvancedSetting, "connection_strings", clusterName, err))
-	}
-
-	if err := d.Set("create_date", conversion.TimePtrToStringPtr(cluster.CreateDate)); err != nil {
-		return diag.FromErr(fmt.Errorf(ErrorClusterAdvancedSetting, "create_date", clusterName, err))
-	}
-
-	if err := d.Set("encryption_at_rest_provider", cluster.GetEncryptionAtRestProvider()); err != nil {
-		return diag.FromErr(fmt.Errorf(ErrorClusterAdvancedSetting, "encryption_at_rest_provider", clusterName, err))
-	}
-
-	if err := d.Set("labels", flattenLabels(*cluster.Labels)); err != nil {
-		return diag.FromErr(fmt.Errorf(ErrorClusterAdvancedSetting, "labels", clusterName, err))
-	}
-
-	if err := d.Set("tags", flattenTags(cluster.Tags)); err != nil {
-		return diag.FromErr(fmt.Errorf(ErrorClusterAdvancedSetting, "tags", clusterName, err))
-	}
-
-	if err := d.Set("mongo_db_major_version", cluster.GetMongoDBMajorVersion()); err != nil {
-		return diag.FromErr(fmt.Errorf(ErrorClusterAdvancedSetting, "mongo_db_major_version", clusterName, err))
-	}
-
-	if err := d.Set("mongo_db_version", cluster.GetMongoDBVersion()); err != nil {
-		return diag.FromErr(fmt.Errorf(ErrorClusterAdvancedSetting, "mongo_db_version", clusterName, err))
-	}
-
-	if err := d.Set("name", cluster.GetName()); err != nil {
-		return diag.FromErr(fmt.Errorf(ErrorClusterAdvancedSetting, "name", clusterName, err))
-	}
-
-	if err := d.Set("paused", cluster.GetPaused()); err != nil {
-		return diag.FromErr(fmt.Errorf(ErrorClusterAdvancedSetting, "paused", clusterName, err))
-	}
-
-	if err := d.Set("pit_enabled", cluster.GetPitEnabled()); err != nil {
-		return diag.FromErr(fmt.Errorf(ErrorClusterAdvancedSetting, "pit_enabled", clusterName, err))
-	}
-
-	if err := d.Set("root_cert_type", cluster.GetRootCertType()); err != nil {
-		return diag.FromErr(fmt.Errorf(ErrorClusterAdvancedSetting, "state_name", clusterName, err))
-	}
-
-	if err := d.Set("state_name", cluster.GetStateName()); err != nil {
-		return diag.FromErr(fmt.Errorf(ErrorClusterAdvancedSetting, "state_name", clusterName, err))
-	}
-	if err := d.Set("termination_protection_enabled", cluster.GetTerminationProtectionEnabled()); err != nil {
-		return diag.FromErr(fmt.Errorf(ErrorClusterAdvancedSetting, "termination_protection_enabled", clusterName, err))
-	}
-	if err := d.Set("version_release_system", cluster.GetVersionReleaseSystem()); err != nil {
-		return diag.FromErr(fmt.Errorf(ErrorClusterAdvancedSetting, "version_release_system", clusterName, err))
-	}
-	if err := d.Set("global_cluster_self_managed_sharding", cluster.GetGlobalClusterSelfManagedSharding()); err != nil {
-		return diag.FromErr(fmt.Errorf(ErrorClusterAdvancedSetting, "global_cluster_self_managed_sharding", clusterName, err))
-	}
-
 	return nil
 }
