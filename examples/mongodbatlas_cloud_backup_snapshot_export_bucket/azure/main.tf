408ac491319d9f507e5e2e180070dbcbd89ce580
Oriol Arbusi <oriol.abadal@mongodb.com>
2024-08-07 17:10:18 +0200

    fix checks

--- examples/mongodbatlas_cloud_backup_snapshot_export_bucket/azure/main.tf
+++ examples/mongodbatlas_cloud_backup_snapshot_export_bucket/azure/main.tf
@@ -2,9 +2,9 @@ resource "mongodbatlas_cloud_provider_access_setup" "setup_only" {
   project_id    = var.project_id
   provider_name = "AZURE"
   azure_config {
-    atlas_azure_app_id = var.azure_atlas_app_id
+    atlas_azure_app_id   = var.azure_atlas_app_id
     service_principal_id = azuread_service_principal.mongo.id
-    tenant_id = var.tenant_id
+    tenant_id            = var.tenant_id
   }
 }
 
@@ -13,9 +13,9 @@ resource "mongodbatlas_cloud_provider_access_authorization" "auth_role" {
   role_id    = mongodbatlas_cloud_provider_access_setup.setup_only.role_id
 
   azure {
-    atlas_azure_app_id = var.azure_atlas_app_id
+    atlas_azure_app_id   = var.azure_atlas_app_id
     service_principal_id = azuread_service_principal.mongo.id
-    tenant_id = var.tenant_id
+    tenant_id            = var.tenant_id
   }
 }
 
@@ -24,7 +24,7 @@ resource "mongodbatlas_cloud_backup_snapshot_export_bucket" "test" {
   project_id     = var.tenant_id
   bucket_name    = azurerm_storage_container.test_storage_container.name
   cloud_provider = "AZURE"
-  service_url	   = azurerm_storage_account.test_storage_account.primary_blob_endpoint
-  role_id		   = mongodbatlas_cloud_provider_access_authorization.auth_role.role_id
-  tenant_id	   = var.tenant_id
+  service_url    = azurerm_storage_account.test_storage_account.primary_blob_endpoint
+  role_id        = mongodbatlas_cloud_provider_access_authorization.auth_role.role_id
+  tenant_id      = var.tenant_id
 }
