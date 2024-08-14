27653a9e8b77c2216c7b2b87fb467b11f67f9623
Oriol Arbusi <oriol.abadal@mongodb.com>
2024-08-14 10:47:10 +0200

    temp: comment out acc tests

--- internal/service/streamprocessor/resource_test.go
+++ internal/service/streamprocessor/resource_test.go
@@ -1,35 +1,35 @@
 package streamprocessor_test
 
-import (
-	"testing"
+// import (
+// 	"testing"
 
-	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
-	"github.com/mongodb/terraform-provider-mongodbatlas/internal/testutil/acc"
-)
+// 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+// 	"github.com/mongodb/terraform-provider-mongodbatlas/internal/testutil/acc"
+// )
 
 // TODO: if acceptance test will be run in an existing CI group of resources, the name should include the group in the prefix followed by the name of the resource e.i. TestAccStreamRSStreamInstance_basic
 // In addition, if acceptance test contains testing of both resource and data sources, the RS/DS can be omitted.
-func TestAccStreamProcessorRS_basic(t *testing.T) {
-	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:                 func() { acc.PreCheckBasic(t) },
-		ProtoV6ProviderFactories: acc.TestAccProviderV6Factories,
-		//		CheckDestroy:             checkDestroyStreamProcessor,
-		Steps: []resource.TestStep{ // TODO: verify updates and import in case of resources
-			//			{
-			//				Config: streamProcessorConfig(),
-			//				Check:  streamProcessorAttributeChecks(),
-			//			},
-			//          {
-			//				Config: streamProcessorConfig(),
-			//				Check:  streamProcessorAttributeChecks(),
-			//			},
-			//			{
-			//				Config:            streamProcessorConfig(),
-			//				ResourceName:      resourceName,
-			//				ImportStateIdFunc: checkStreamProcessorImportStateIDFunc,
-			//				ImportState:       true,
-			//				ImportStateVerify: true,
-		},
-	},
-	)
-}
+// func TestAccStreamProcessorRS_basic(t *testing.T) {
+// 	resource.ParallelTest(t, resource.TestCase{
+// 		PreCheck:                 func() { acc.PreCheckBasic(t) },
+// 		ProtoV6ProviderFactories: acc.TestAccProviderV6Factories,
+// 		//		CheckDestroy:             checkDestroyStreamProcessor,
+// 		Steps: []resource.TestStep{ // TODO: verify updates and import in case of resources
+// 			//			{
+// 			//				Config: streamProcessorConfig(),
+// 			//				Check:  streamProcessorAttributeChecks(),
+// 			//			},
+// 			//          {
+// 			//				Config: streamProcessorConfig(),
+// 			//				Check:  streamProcessorAttributeChecks(),
+// 			//			},
+// 			//			{
+// 			//				Config:            streamProcessorConfig(),
+// 			//				ResourceName:      resourceName,
+// 			//				ImportStateIdFunc: checkStreamProcessorImportStateIDFunc,
+// 			//				ImportState:       true,
+// 			//				ImportStateVerify: true,
+// 		},
+// 	},
+// 	)
+// }
