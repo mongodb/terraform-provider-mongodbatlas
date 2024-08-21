4c0f2fa568537f3e5fbeef63ad18c2785fc88aee
Oriol Arbusi <oriol.abadal@mongodb.com>
2024-08-21 11:19:50 +0200

    add examples and autogen of docs

---
+++ examples/mongodbatlas_stream_processor/README.md
@@ -0,0 +1,11 @@
+# MongoDB Atlas Provider - Atlas Stream Processor defined in a Project
+
+This example shows how to use Atlas Stream Processors in Terraform. It also creates a project, which is a prerequisite.
+
+You must set the following variables:
+
+- `public_key`: Atlas public key
+- `private_key`: Atlas  private key
+- `project_id`: Unique 24-hexadecimal digit string that identifies the project where the stream instance will be created.
+
+To learn more, see the [Stream Processor Documentation](https://www.mongodb.com/docs/atlas/atlas-stream-processing/manage-stream-processor/).
---
+++ examples/mongodbatlas_stream_processor/main.tf
@@ -0,0 +1,43 @@
+resource "mongodbatlas_stream_instance" "example" {
+  project_id    = var.project_id
+  instance_name = "InstanceName"
+  data_process_region = {
+    region         = "VIRGINIA_USA"
+    cloud_provider = "AWS"
+  }
+}
+
+resource "mongodbatlas_stream_connection" "example-sample" {
+  project_id      = var.project_id
+  instance_name   = mongodbatlas_stream_instance.example.instance_name
+  connection_name = "sample_stream_solar"
+  type            = "Sample"
+}
+
+resource "mongodbatlas_stream_processor" "stream-processor-example" {
+  project_id     = var.project_id
+  instance_name  = mongodbatlas_stream_instance.example.instance_name
+  processor_name = "processorName"
+  pipeline       = "[{\"$source\":{\"connectionName\":\"sample_stream_solar\"}},{\"$emit\":{\"connectionName\":\"__testLog\"}}]"
+  state          = "STARTED"
+}
+
+data "mongodbatlas_stream_processors" "example-stream-processors" {
+  project_id    = var.project_id
+  instance_name = mongodbatlas_stream_instance.example.instance_name
+}
+
+data "mongodbatlas_stream_processor" "example-stream-processor" {
+  project_id     = var.project_id
+  instance_name  = mongodbatlas_stream_instance.example.instance_name
+  processor_name = mongodbatlas_stream_processor.stream-processor-example.processor_name
+}
+
+# example making use of data sources
+output "stream_processors_state" {
+  value = data.mongodbatlas_stream_processor.example-stream-processor.state
+}
+
+output "stream_processors_results" {
+  value = data.mongodbatlas_stream_processors.example-stream-processors.results
+}
