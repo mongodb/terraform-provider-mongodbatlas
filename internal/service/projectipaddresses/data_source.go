5bfb79341249e65c29a3546ddd631eb1eb21f735
rubenVB01 <rubenvibax@hotmail.com>
2024-08-21 16:00:27 +0200

    scaffold and initial implementation

--- internal/service/projectipaddresses/data_source.go
+++ internal/service/projectipaddresses/data_source.go
@@ -1,1 +1,52 @@
 package projectipaddresses
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/mongodb/terraform-provider-mongodbatlas/internal/config"
+)
+
+const projectIPAddressesName = "project_ip_addresses"
+
+var _ datasource.DataSource = &projectIPAddressesDS{}
+var _ datasource.DataSourceWithConfigure = &projectIPAddressesDS{}
+
+func DataSource() datasource.DataSource {
+	return &projectIPAddressesDS{
+		DSCommon: config.DSCommon{
+			DataSourceName: projectIPAddressesName,
+		},
+	}
+}
+
+type projectIPAddressesDS struct {
+	config.DSCommon
+}
+
+func (d *projectIPAddressesDS) Schema(ctx context.Context, req datasource.SchemaRequest, resp *datasource.SchemaResponse) {
+	resp.Schema = DataSourceSchema(ctx)
+}
+
+func (d *projectIPAddressesDS) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+	connV2 := d.Client.AtlasV2
+	var err error
+	var databaseDSUserConfig *ProjectIpAddressesModel
+	resp.Diagnostics.Append(req.Config.Get(ctx, &databaseDSUserConfig)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	projectIPAddresses, _, err := connV2.ProjectsApi.ReturnAllIPAddresses(ctx, databaseDSUserConfig.ProjectId.ValueString()).Execute()
+	if err != nil {
+		resp.Diagnostics.AddError("error getting project's IP addresses", err.Error())
+		return
+	}
+
+	newProjectIPAddresses, diags := NewTFProjectIPAddresses(ctx, projectIPAddresses)
+	if diags.HasError() {
+		resp.Diagnostics.Append(diags...)
+		return
+	}
+	resp.Diagnostics.Append(resp.State.Set(ctx, newProjectIPAddresses)...)
+}
--- internal/service/projectipaddresses/model.go
+++ internal/service/projectipaddresses/model.go
@@ -1,1 +1,32 @@
 package projectipaddresses
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"go.mongodb.org/atlas-sdk/v20240805001/admin"
+)
+
+func NewTFProjectIPAddresses(ctx context.Context, ipAddresses *admin.GroupIPAddresses) (types.Object, diag.Diagnostics) {
+	clusterIPs := []ClustersValue{}
+	if ipAddresses != nil && ipAddresses.Services != nil {
+		clusterIPAddresses := ipAddresses.Services.GetClusters()
+		clusterIPs = make([]ClustersValue, len(clusterIPAddresses))
+		for i := range clusterIPAddresses {
+			inbound, _ := types.ListValueFrom(ctx, types.StringType, clusterIPAddresses[i].GetInbound())
+			outbound, _ := types.ListValueFrom(ctx, types.StringType, clusterIPAddresses[i].GetOutbound())
+			clusterIPs[i] = ClustersValue{
+				ClusterName: types.StringPointerValue(clusterIPAddresses[i].ClusterName),
+				Inbound:     inbound,
+				Outbound:    outbound,
+			}
+		}
+	}
+	obj, diags := types.ObjectValueFrom(ctx, IPAddressesObjectType.AttrTypes, ProjectIpAddressesModel{
+		Services: ServicesValue{
+			Clusters: clusterIPs,
+		},
+	})
+	return obj, diags
+}
