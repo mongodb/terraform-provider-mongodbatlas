520cbbe7c982f8378fe7f26f7cda8be0844082c4
EspenAlbert <espen.albert@mongodb.com>
2024-08-21 09:54:22 +0100

    test: Refactors test configs

--- internal/service/streamprocessor/resource_test.go
+++ internal/service/streamprocessor/resource_test.go
@@ -4,16 +4,31 @@ import (
 	"context"
 	"fmt"
 	"regexp"
+	"strings"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
+	"github.com/stretchr/testify/assert"
 
+	"github.com/mongodb/terraform-provider-mongodbatlas/internal/service/streamprocessor"
 	"github.com/mongodb/terraform-provider-mongodbatlas/internal/testutil/acc"
 )
 
+type connectionConfig struct {
+	connectionType       string
+	clusterName          string
+	pipelineStepIsSource bool
+}
+
 var (
-	resourceName = "mongodbatlas_stream_processor.processor"
+	resourceName      = "mongodbatlas_stream_processor.processor"
+	connTypeSample    = "Sample"
+	connTypeCluster   = "Cluster"
+	connTypeKafka     = "Kafka"
+	connTypeTestLog   = "TestLog"
+	sampleSrcConfig   = connectionConfig{connectionType: connTypeSample, pipelineStepIsSource: true}
+	testLogDestConfig = connectionConfig{connectionType: connTypeTestLog, pipelineStepIsSource: false}
 )
 
 func TestAccStreamProcessorRS_basic(t *testing.T) {
@@ -29,7 +44,7 @@ func TestAccStreamProcessorRS_basic(t *testing.T) {
 		CheckDestroy:             checkDestroyStreamProcessor,
 		Steps: []resource.TestStep{
 			{
-				Config: configWithSampleConnection(projectID, instanceName, processorName, ""),
+				Config: config(t, projectID, instanceName, processorName, "", sampleSrcConfig, testLogDestConfig),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					checkExists(resourceName),
 					resource.TestCheckResourceAttrSet(resourceName, "project_id"),
@@ -40,18 +55,17 @@ func TestAccStreamProcessorRS_basic(t *testing.T) {
 				),
 			},
 			{
-				Config: configWithSampleConnection(projectID, instanceName, processorName, "STARTED"),
+				Config: config(t, projectID, instanceName, processorName, streamprocessor.StartedState, sampleSrcConfig, testLogDestConfig),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					checkExists(resourceName),
 					resource.TestCheckResourceAttrSet(resourceName, "project_id"),
 					resource.TestCheckResourceAttrSet(resourceName, "instance_name"),
 					resource.TestCheckResourceAttrSet(resourceName, "processor_name"),
 					resource.TestCheckResourceAttr(resourceName, "processor_name", processorName),
-					resource.TestCheckResourceAttr(resourceName, "state", "STARTED"),
+					resource.TestCheckResourceAttr(resourceName, "state", streamprocessor.StartedState),
 				),
 			},
 			{
-				Config:                  configWithSampleConnection(projectID, instanceName, processorName, ""),
 				ResourceName:            resourceName,
 				ImportStateIdFunc:       importStateIDFunc(resourceName),
 				ImportState:             true,
@@ -66,6 +80,8 @@ func TestAccStreamProcessorRS_withOptions(t *testing.T) {
 		projectID, clusterName = acc.ClusterNameExecution(t)
 		processorName          = "new-processor"
 		instanceName           = acc.RandomName()
+		src                    = connectionConfig{connectionType: connTypeCluster, clusterName: clusterName, pipelineStepIsSource: true}
+		dest                   = connectionConfig{connectionType: connTypeKafka, pipelineStepIsSource: false}
 	)
 
 	resource.ParallelTest(t, resource.TestCase{
@@ -74,12 +90,11 @@ func TestAccStreamProcessorRS_withOptions(t *testing.T) {
 		CheckDestroy:             checkDestroyStreamProcessor,
 		Steps: []resource.TestStep{
 			{
-				Config: configWithKafkaConnection(projectID, clusterName, instanceName, processorName, "CREATED"),
+				Config: config(t, projectID, instanceName, processorName, streamprocessor.CreatedState, src, dest),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					checkExists(resourceName),
 					resource.TestCheckResourceAttrSet(resourceName, "project_id"),
 					resource.TestCheckResourceAttrSet(resourceName, "instance_name"),
-					resource.TestCheckResourceAttrSet(resourceName, "processor_name"),
 					resource.TestCheckResourceAttr(resourceName, "processor_name", processorName),
 					resource.TestCheckResourceAttr(resourceName, "state", "CREATED"),
 				),
@@ -100,7 +115,7 @@ func TestAccStreamProcessorRS_createWithAutoStart(t *testing.T) {
 		CheckDestroy:             checkDestroyStreamProcessor,
 		Steps: []resource.TestStep{
 			{
-				Config: configWithSampleConnection(projectID, instanceName, processorName, "STARTED"),
+				Config: config(t, projectID, instanceName, processorName, streamprocessor.StartedState, sampleSrcConfig, testLogDestConfig),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					checkExists(resourceName),
 					resource.TestCheckResourceAttrSet(resourceName, "project_id"),
@@ -111,7 +126,7 @@ func TestAccStreamProcessorRS_createWithAutoStart(t *testing.T) {
 				),
 			},
 			{
-				Config: configWithSampleConnection(projectID, instanceName, processorName, "STOPPED"),
+				Config: config(t, projectID, instanceName, processorName, streamprocessor.StoppedState, sampleSrcConfig, testLogDestConfig),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					checkExists(resourceName),
 					resource.TestCheckResourceAttrSet(resourceName, "project_id"),
@@ -129,6 +144,7 @@ func TestAccStreamProcessorRS_clusterType(t *testing.T) {
 		projectID, clusterName = acc.ClusterNameExecution(t)
 		processorName          = "new-processor"
 		instanceName           = acc.RandomName()
+		srcConfig              = connectionConfig{connectionType: connTypeCluster, clusterName: clusterName, pipelineStepIsSource: true}
 	)
 
 	resource.ParallelTest(t, resource.TestCase{
@@ -137,7 +153,7 @@ func TestAccStreamProcessorRS_clusterType(t *testing.T) {
 		CheckDestroy:             checkDestroyStreamProcessor,
 		Steps: []resource.TestStep{
 			{
-				Config: configWithClusterConnection(projectID, clusterName, instanceName, processorName, "STARTED"),
+				Config: config(t, projectID, instanceName, processorName, streamprocessor.StartedState, srcConfig, testLogDestConfig),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					checkExists(resourceName),
 					resource.TestCheckResourceAttrSet(resourceName, "project_id"),
@@ -164,145 +180,10 @@ func TestAccStreamProcessorRS_failWithInvalidStateOnCreation(t *testing.T) {
 		CheckDestroy:             checkDestroyStreamProcessor,
 		Steps: []resource.TestStep{
 			{
-				Config:      configWithSampleConnection(projectID, instanceName, processorName, "STOPPED"),
+				Config:      config(t, projectID, instanceName, processorName, streamprocessor.StoppedState, sampleSrcConfig, testLogDestConfig),
 				ExpectError: regexp.MustCompile("When creating a stream processor, the only valid states are CREATED and STARTED"),
 			},
 		}})
-}
-
-func configWithSampleConnection(projectID, instanceName, processorName, state string) string {
-	stateConfig := ""
-	if state != "" {
-		stateConfig = fmt.Sprintf(`state = %[1]q`, state)
-	}
-	return fmt.Sprintf(`
-	resource "mongodbatlas_stream_instance" "instance" {
-		project_id    = %[1]q
-		instance_name = %[2]q
-		data_process_region = {
-			region         = "VIRGINIA_USA"
-			cloud_provider = "AWS"
-		}
-	}
-
-	resource "mongodbatlas_stream_connection" "sample" {
-		project_id      = %[1]q
-		instance_name   = mongodbatlas_stream_instance.instance.instance_name
-		connection_name = "sample_stream_solar"
-		type            = "Sample"
-		depends_on = [mongodbatlas_stream_instance.instance] 
-	}
-
-	resource "mongodbatlas_stream_processor" "processor" {
-		project_id     = %[1]q
-		instance_name  = mongodbatlas_stream_instance.instance.instance_name
-		processor_name = %[3]q
-		pipeline       = "[{\"$source\":{\"connectionName\":\"sample_stream_solar\"}},{\"$emit\":{\"connectionName\":\"__testLog\"}}]"
-		%[4]s
-		depends_on = [mongodbatlas_stream_connection.sample] 
-
-	}
-	`, projectID, instanceName, processorName, stateConfig)
-}
-
-func configWithClusterConnection(projectID, clusterName, instanceName, processorName, state string) string {
-	return fmt.Sprintf(`
-	resource "mongodbatlas_stream_instance" "instance" {
-		project_id    = %[1]q
-		instance_name = %[2]q
-		data_process_region = {
-			region         = "VIRGINIA_USA"
-			cloud_provider = "AWS"
-		}
-	}
-
-	resource "mongodbatlas_stream_connection" "cluster" {
-		project_id      = %[1]q
-		instance_name   = mongodbatlas_stream_instance.instance.instance_name
-		connection_name = "ClusterConnection"
-		type            = "Cluster"
-		cluster_name    = %[3]q
-		db_role_to_execute = {
-			role = "atlasAdmin"
-			type = "BUILT_IN"
-		}
-		depends_on = [mongodbatlas_stream_instance.instance] 
-	}
-
-	resource "mongodbatlas_stream_processor" "processor" {
-		project_id     = %[1]q
-		instance_name  = mongodbatlas_stream_instance.instance.instance_name
-		processor_name = %[4]q
-		pipeline       = "[{\"$source\":{\"connectionName\":\"ClusterConnection\"}},{\"$emit\":{\"connectionName\":\"__testLog\"}}]"
-		state 		   = %[5]q
-		depends_on = [mongodbatlas_stream_connection.cluster] 
-	}
-	`, projectID, instanceName, clusterName, processorName, state)
-}
-
-func configWithKafkaConnection(projectID, clusterName, instanceName, processorName, state string) string {
-	return fmt.Sprintf(`
-	resource "mongodbatlas_stream_instance" "instance" {
-		project_id    = %[1]q
-		instance_name = %[2]q
-		data_process_region = {
-			region         = "VIRGINIA_USA"
-			cloud_provider = "AWS"
-		}
-	}
-
-	resource "mongodbatlas_stream_connection" "cluster" {
-		project_id      = %[1]q
-		instance_name   = mongodbatlas_stream_instance.instance.instance_name
-		connection_name = "ClusterConnection"
-		type            = "Cluster"
-		cluster_name    = %[3]q
-		db_role_to_execute = {
-			role = "atlasAdmin"
-			type = "BUILT_IN"
-		}
-		depends_on = [mongodbatlas_stream_instance.instance] 
-	}
-
-	resource "mongodbatlas_stream_connection" "kafka" {
-		project_id      = %[1]q
-		instance_name   = mongodbatlas_stream_instance.instance.instance_name
-		connection_name = "KafkaPlaintextConnection"
-		type            = "Kafka"
-		authentication = {
-			mechanism = "PLAIN"
-			username  = "user"
-			password  = "rawpassword"
-		}
-		bootstrap_servers = "localhost:9092,localhost:9092"
-		config = {
-			"auto.offset.reset" : "earliest"
-		}
-		security = {
-			protocol = "PLAINTEXT"
-		}
-		depends_on = [mongodbatlas_stream_instance.instance] 
-	}
-
-	resource "mongodbatlas_stream_processor" "processor" {
-		project_id     = %[1]q
-		instance_name  = mongodbatlas_stream_instance.instance.instance_name
-		processor_name = %[4]q
-		pipeline       = "[{\"$source\":{\"connectionName\":\"ClusterConnection\"}},{\"$emit\":{\"connectionName\":\"KafkaPlaintextConnection\",\"topic\":\"random_topic\"}}]"
-		state 		   = %[5]q
-		options = {
-			dlq = {
-				coll = "testcol"
-				connection_name = mongodbatlas_stream_connection.cluster.connection_name
-				db = "test"
-			}
-		}
-		depends_on = [
-			mongodbatlas_stream_connection.kafka,
-			mongodbatlas_stream_connection.cluster
-		] 
-	}
-	`, projectID, instanceName, clusterName, processorName, state)
 }
 
 func checkExists(resourceName string) resource.TestCheckFunc {
@@ -350,4 +231,149 @@ func importStateIDFunc(resourceName string) resource.ImportStateIdFunc {
 
 		return fmt.Sprintf("%s-%s-%s", rs.Primary.Attributes["instance_name"], rs.Primary.Attributes["project_id"], rs.Primary.Attributes["processor_name"]), nil
 	}
+}
+
+func config(t *testing.T, projectID, instanceName, processorName, state string, src, dest connectionConfig) string {
+	t.Helper()
+	stateConfig := ""
+	if state != "" {
+		stateConfig = fmt.Sprintf(`state = %[1]q`, state)
+	}
+
+	connectionConfigSrc, connectionIDSrc, pipelineStepSrc := configConnection(t, projectID, src)
+	connectionConfigDest, connectionIDDest, pipelineStepDest := configConnection(t, projectID, dest)
+	dependsOn := []string{}
+	if connectionIDSrc != "" {
+		dependsOn = append(dependsOn, connectionIDSrc)
+	}
+	if connectionIDDest != "" {
+		dependsOn = append(dependsOn, connectionIDDest)
+	}
+	dependsOnStr := strings.Join(dependsOn, ", ")
+	pipeline := fmt.Sprintf("[{\"$source\":%1s},{\"$emit\":%2s}]", pipelineStepSrc, pipelineStepDest)
+	fmt.Println("\nPIPELINE:")
+	fmt.Println(pipeline)
+
+	configStr := fmt.Sprintf(`
+		resource "mongodbatlas_stream_instance" "instance" {
+			project_id    = %[1]q
+			instance_name = %[2]q
+			data_process_region = {
+				region         = "VIRGINIA_USA"
+				cloud_provider = "AWS"
+			}
+		}
+
+		%[3]s
+		%[4]s
+
+		resource "mongodbatlas_stream_processor" "processor" {
+			project_id     = %[1]q
+			instance_name  = mongodbatlas_stream_instance.instance.instance_name
+			processor_name = %[5]q
+			pipeline       = %[6]q
+			%[7]s
+			depends_on = [%[8]s]
+		}
+	`, projectID, instanceName, connectionConfigSrc, connectionConfigDest, processorName, pipeline, stateConfig, dependsOnStr)
+	fmt.Println("\nCONFIG:")
+	fmt.Println(configStr)
+	return configStr
+}
+
+func configConnection(t *testing.T, projectID string, config connectionConfig) (connectionConfig, resourceID, pipelineStep string) {
+	t.Helper()
+	connectionType := config.connectionType
+	pipelineStepIsSource := config.pipelineStepIsSource
+	switch connectionType {
+	case "Cluster":
+		var connectionName, resourceName string
+		clusterName := config.clusterName
+		assert.NotEqual(t, "", clusterName)
+		if pipelineStepIsSource {
+			connectionName = "ClusterConnectionSrc"
+			resourceName = "cluster_src"
+		} else {
+			connectionName = "ClusterConnectionDest"
+			resourceName = "cluster_dest"
+		}
+		connectionConfig = fmt.Sprintf(`
+            resource "mongodbatlas_stream_connection" %[4]q {
+                project_id      = %[1]q
+                cluster_name    = %[2]q
+                instance_name   = mongodbatlas_stream_instance.instance.instance_name
+                connection_name = %[3]q
+                type            = "Cluster"
+                db_role_to_execute = {
+                    role = "atlasAdmin"
+                    type = "BUILT_IN"
+                }
+                depends_on = [mongodbatlas_stream_instance.instance] 
+            }
+        `, projectID, clusterName, connectionName, resourceName)
+		resourceID = fmt.Sprintf("mongodbatlas_stream_connection.%s", resourceName)
+		pipelineStep = fmt.Sprintf("{\"connectionName\":%q}", connectionName)
+		return connectionConfig, resourceID, pipelineStep
+	case "Kafka":
+		var connectionName, resourceName, pipelineStep string
+		if pipelineStepIsSource {
+			connectionName = "KafkaConnectionSrc"
+			resourceName = "kafka_src"
+			pipelineStep = fmt.Sprintf("{\"connectionName\":%q}", connectionName)
+		} else {
+			connectionName = "KafkaConnectionDest"
+			resourceName = "kafka_dest"
+			pipelineStep = fmt.Sprintf("{\"connectionName\":%q,\"topic\":\"random_topic\"}", connectionName)
+		}
+		connectionConfig = fmt.Sprintf(`
+            resource "mongodbatlas_stream_connection" %[3]q{
+                project_id      = %[1]q
+                instance_name   = mongodbatlas_stream_instance.instance.instance_name
+                connection_name = %[2]q
+                type            = "Kafka"
+                authentication = {
+                    mechanism = "PLAIN"
+                    username  = "user"
+                    password  = "rawpassword"
+                }
+                bootstrap_servers = "localhost:9092,localhost:9092"
+                config = {
+                    "auto.offset.reset" : "earliest"
+                }
+                security = {
+                    protocol = "PLAINTEXT"
+                }
+                depends_on = [mongodbatlas_stream_instance.instance] 
+            }
+        `, projectID, connectionName, resourceName)
+		resourceID = fmt.Sprintf("mongodbatlas_stream_connection.%s", resourceName)
+		return connectionConfig, resourceID, pipelineStep
+	case "Sample":
+		if !pipelineStepIsSource {
+			t.Fatal("Sample connection must be used as a source")
+		}
+		connectionConfig = fmt.Sprintf(`
+            resource "mongodbatlas_stream_connection" "sample" {
+                project_id      = %[1]q
+                instance_name   = mongodbatlas_stream_instance.instance.instance_name
+                connection_name = "sample_stream_solar"
+                type            = "Sample"
+                depends_on = [mongodbatlas_stream_instance.instance] 
+            }
+        `, projectID)
+		resourceID = "mongodbatlas_stream_connection.sample"
+		pipelineStep = "{\"connectionName\":\"sample_stream_solar\"}"
+		return connectionConfig, resourceID, pipelineStep
+
+	case "TestLog":
+		if pipelineStepIsSource {
+			t.Fatal("TestLog connection must be used as a destination")
+		}
+		connectionConfig = ""
+		resourceID = ""
+		pipelineStep = "{\"connectionName\":\"__testLog\"}"
+		return connectionConfig, resourceID, pipelineStep
+	}
+	t.Fatalf("Unknown connection type: %s", connectionType)
+	return connectionConfig, resourceID, pipelineStep
 }
