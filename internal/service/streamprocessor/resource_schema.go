0bddda074de6b922f8cbdec6b5060dcb1aa38f20
EspenAlbert <espen.albert@mongodb.com>
2024-08-22 09:59:56 +0100

    refactor: remove DiffSuppressJSON (didn't work, still showed diff)

--- internal/service/streamprocessor/resource_schema.go
+++ internal/service/streamprocessor/resource_schema.go
@@ -9,7 +9,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 	"github.com/hashicorp/terraform-plugin-framework/types"
-	"github.com/mongodb/terraform-provider-mongodbatlas/internal/common/schemafunc"
 	"github.com/mongodb/terraform-provider-mongodbatlas/internal/common/validate"
 )
 
@@ -32,9 +31,6 @@ func ResourceSchema(ctx context.Context) schema.Schema {
 			"pipeline": schema.StringAttribute{
 				Validators: []validator.String{
 					validate.StringIsJSON(),
-				},
-				PlanModifiers: []planmodifier.String{
-					schemafunc.DiffSuppressJSON(),
 				},
 				Required:            true,
 				Description:         "Stream aggregation pipeline you want to apply to your streaming data.",
