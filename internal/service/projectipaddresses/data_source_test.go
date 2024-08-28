8e7cde0fe95e88d3e825d19f31aed21eef00c226
rubenVB01 <rubenvibax@hotmail.com>
2024-08-28 10:27:00 +0200

    correct file format

--- internal/service/projectipaddresses/data_source_test.go
+++ internal/service/projectipaddresses/data_source_test.go
@@ -1,1 +1,37 @@
 package projectipaddresses_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/mongodb/terraform-provider-mongodbatlas/internal/testutil/acc"
+)
+
+func TestAccProjectIPAddressesDS_basic(t *testing.T) {
+	var (
+		projectID      = acc.ProjectIDExecution(t)
+		dataSourceName = "data.mongodbatlas_project_ip_addresses.test"
+	)
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:                 func() { acc.PreCheckBasic(t) },
+		ProtoV6ProviderFactories: acc.TestAccProviderV6Factories,
+		Steps: []resource.TestStep{
+			{
+				Config: ProjectIPAddressesConfig(projectID),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrWith(dataSourceName, "outbound.aws.us-east-1.0", acc.CIDRBlockExpression()),
+				),
+			},
+		},
+	})
+}
+
+func ProjectIPAddressesConfig(projectID string) string {
+	return fmt.Sprintf(`
+
+	data "mongodbatlas_project_ip_addresses" "test" {
+		project_id = %[1]q
+	}
+`, projectID)
+}
