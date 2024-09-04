1f09ec6a3a2f44a71eb05a87bac562e847968885
EspenAlbert <espen.albert@mongodb.com>
2024-09-04 08:42:27 +0100

    chore: update SDK for stream_processor

--- internal/service/streamprocessor/state_transition_test.go
+++ internal/service/streamprocessor/state_transition_test.go
@@ -6,8 +6,8 @@ import (
 	"net/http"
 	"testing"
 
-	"go.mongodb.org/atlas-sdk/v20240805001/admin"
-	"go.mongodb.org/atlas-sdk/v20240805001/mockadmin"
+	"go.mongodb.org/atlas-sdk/v20240805003/admin"
+	"go.mongodb.org/atlas-sdk/v20240805003/mockadmin"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
