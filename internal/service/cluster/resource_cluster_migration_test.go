18912380183f772a3e95d26d3ea40ee88d48623a
Oriol Arbusi <oriol.abadal@mongodb.com>
2024-09-05 15:29:11 +0200

    skip mig

--- internal/service/cluster/resource_cluster_migration_test.go
+++ internal/service/cluster/resource_cluster_migration_test.go
@@ -11,5 +11,6 @@ func TestMigCluster_basicAWS_simple(t *testing.T) {
 }
 
 func TestMigCluster_partial_advancedConf(t *testing.T) {
+	mig.SkipIfVersionBelow(t, "1.19.0") // version where change_stream_options_pre_and_post_images_expire_after_seconds was introduced
 	mig.CreateAndRunTest(t, partialAdvancedConfTestCase(t))
 }
