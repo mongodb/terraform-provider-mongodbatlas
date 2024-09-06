f7501f8c00ace2e921284682c5587ab6f2f0b848
Leo Antoli <430982+lantoli@users.noreply.github.com>
2024-09-06 10:55:01 +0200

    remove commented-out code, model and tests

--- internal/service/employeeaccessgrant/data_source.go
+++ internal/service/employeeaccessgrant/data_source.go
@@ -23,30 +23,8 @@ type employeeAccessGrantDS struct {
 }
 
 func (d *employeeAccessGrantDS) Schema(ctx context.Context, req datasource.SchemaRequest, resp *datasource.SchemaResponse) {
-	// TODO: Schema and model must be defined in data_source_schema.go. Details on scaffolding this file found in contributing/development-best-practices.md under "Scaffolding Schema and Model Definitions"
 	resp.Schema = DataSourceSchema(ctx)
 }
 
 func (d *employeeAccessGrantDS) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
-	var employeeAccessGrantConfig TFEmployeeAccessGrantModel
-	resp.Diagnostics.Append(req.Config.Get(ctx, &employeeAccessGrantConfig)...)
-	if resp.Diagnostics.HasError() {
-		return
-	}
-
-	// TODO: make get request to resource
-
-	// connV2 := d.Client.AtlasV2
-	// if err != nil {
-	//	resp.Diagnostics.AddError("error fetching resource", err.Error())
-	//	return
-	// }
-
-	// TODO: process response into new terraform state
-	// newEmployeeAccessGrantModel, diags := NewTFEmployeeAccessGrant(ctx, apiResp)
-	// if diags.HasError() {
-	//	resp.Diagnostics.Append(diags...)
-	//	return
-	// }
-	// resp.Diagnostics.Append(resp.State.Set(ctx, newEmployeeAccessGrantModel)...)
 }
--- internal/service/employeeaccessgrant/resource.go
+++ internal/service/employeeaccessgrant/resource.go
@@ -25,124 +25,20 @@ type employeeAccessGrantRS struct {
 }
 
 func (r *employeeAccessGrantRS) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
-	// TODO: Schema and model must be defined in resource_schema.go. Details on scaffolding this file found in contributing/development-best-practices.md under "Scaffolding Schema and Model Definitions"
 	resp.Schema = ResourceSchema(ctx)
 }
 
 func (r *employeeAccessGrantRS) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
-	var employeeAccessGrantPlan TFEmployeeAccessGrantModel
-	resp.Diagnostics.Append(req.Plan.Get(ctx, &employeeAccessGrantPlan)...)
-	if resp.Diagnostics.HasError() {
-		return
-	}
-
-	// employeeAccessGrantReq, diags := NewEmployeeAccessReq(ctx, &employeeAccessGrantPlan)
-	// if diags.HasError() {
-	//		resp.Diagnostics.Append(diags...)
-	//		return
-	//}
-
-	// TODO: make POST request to Atlas API and handle error in response
-
-	// connV2 := r.Client.AtlasV2
-	// if err != nil {
-	//	resp.Diagnostics.AddError("error creating resource", err.Error())
-	//	return
-	// }
-
-	// TODO: process response into new terraform state
-	// newEmployeeAccessModel, diags := NewTFEmployeeAccess(ctx, apiResp)
-	// if diags.HasError() {
-	// 	resp.Diagnostics.Append(diags...)
-	//	return
-	// }
-	// resp.Diagnostics.Append(resp.State.Set(ctx, newEmployeeAccessModel)...)
 }
 
 func (r *employeeAccessGrantRS) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
-	var employeeAccessGrantState TFEmployeeAccessGrantModel
-	resp.Diagnostics.Append(req.State.Get(ctx, &employeeAccessGrantState)...)
-	if resp.Diagnostics.HasError() {
-		return
-	}
-
-	// TODO: make get request to resource
-
-	// connV2 := r.Client.AtlasV2
-	// if err != nil {
-	//	if apiResp != nil && apiResp.StatusCode == http.StatusNotFound {
-	//		resp.State.RemoveResource(ctx)
-	//		return
-	//	}
-	//	resp.Diagnostics.AddError("error fetching resource", err.Error())
-	//	return
-	// }
-
-	// TODO: process response into new terraform state
-	// newEmployeeAccessModel, diags := NewTFEmployeeAccess(ctx, apiResp)
-	// if diags.HasError() {
-	//	resp.Diagnostics.Append(diags...)
-	//	return
-	// }
-	// resp.Diagnostics.Append(resp.State.Set(ctx, newEmployeeAccessModel)...)
 }
 
 func (r *employeeAccessGrantRS) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
-	var employeeAccessGrantPlan TFEmployeeAccessGrantModel
-	resp.Diagnostics.Append(req.Plan.Get(ctx, &employeeAccessGrantPlan)...)
-	if resp.Diagnostics.HasError() {
-		return
-	}
-
-	// employeeAccessGrantReq, diags := NewEmployeeAccessReq(ctx, &employeeAccessGrantPlan)
-	// if diags.HasError() {
-	// 	resp.Diagnostics.Append(diags...)
-	//	return
-	// }
-
-	// TODO: make PATCH request to Atlas API and handle error in response
-	// connV2 := r.Client.AtlasV2
-	// if err != nil {
-	//	resp.Diagnostics.AddError("error updating resource", err.Error())
-	//	return
-	//}
-
-	// TODO: process response into new terraform state
-
-	// newEmployeeAccessModel, diags := NewTFEmployeeAccess(ctx, apiResp)
-	// if diags.HasError() {
-	// 	resp.Diagnostics.Append(diags...)
-	//	return
-	//}
-	// resp.Diagnostics.Append(resp.State.Set(ctx, newEmployeeAccessModel)...)
 }
 
 func (r *employeeAccessGrantRS) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
-	var employeeAccessGrantState *TFEmployeeAccessGrantModel
-	resp.Diagnostics.Append(req.State.Get(ctx, &employeeAccessGrantState)...)
-	if resp.Diagnostics.HasError() {
-		return
-	}
-
-	// TODO: make Delete request to Atlas API
-
-	// connV2 := r.Client.AtlasV2
-	// if _, _, err := connV2.Api.Delete().Execute(); err != nil {
-	// 	 resp.Diagnostics.AddError("error deleting resource", err.Error())
-	// 	 return
-	// }
 }
 
 func (r *employeeAccessGrantRS) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
-	// TODO: parse req.ID string taking into account documented format. Example:
-
-	// projectID, other, err := splitEmployeeAccessGrantImportID(req.ID)
-	// if err != nil {
-	//	resp.Diagnostics.AddError("error splitting import ID", err.Error())
-	//	return
-	//}
-
-	// TODO: define attributes that are required for read operation to work correctly. Example:
-
-	// resp.Diagnostics.Append(resp.State.SetAttribute(ctx, path.Root("project_id"), projectID)...)
 }
