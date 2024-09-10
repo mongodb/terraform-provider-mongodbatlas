7b92a09271b34b2b46eda33051b9baee27e69753
Leo Antoli <430982+lantoli@users.noreply.github.com>
2024-09-10 18:59:18 +0200

    feat: Create / Read / Delete implementation and tests for `mongodbatlas_employee_access_grant` (#2573)
    * main_test file
    
    * CRU operations
    
    * unit tests
    
    * basic acc test
    
    * basic mig test
    
    * more acc tests
    
    * adding expectedErrContains as per PR feedback
    
    * check err in test

--- internal/service/employeeaccessgrant/resource.go
+++ internal/service/employeeaccessgrant/resource.go
@@ -2,12 +2,19 @@ package employeeaccessgrant
 
 import (
 	"context"
+	"net/http"
 
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/mongodb/terraform-provider-mongodbatlas/internal/config"
 )
 
-const employeeAccessGrantName = "employee_access_grant"
+const (
+	resourceName     = "employee_access_grant"
+	fullResourceName = "mongodbatlas_" + resourceName
+	errorCreate      = "Error creating resource " + fullResourceName
+	errorRead        = "Error retrieving info for resource " + fullResourceName
+	errorDelete      = "Error deleting resource " + fullResourceName
+)
 
 var _ resource.ResourceWithConfigure = &employeeAccessGrantRS{}
 var _ resource.ResourceWithImportState = &employeeAccessGrantRS{}
@@ -15,7 +22,7 @@ var _ resource.ResourceWithImportState = &employeeAccessGrantRS{}
 func Resource() resource.Resource {
 	return &employeeAccessGrantRS{
 		RSCommon: config.RSCommon{
-			ResourceName: employeeAccessGrantName,
+			ResourceName: resourceName,
 		},
 	}
 }
@@ -29,19 +36,54 @@ func (r *employeeAccessGrantRS) Schema(ctx context.Context, req resource.SchemaR
 }
 
 func (r *employeeAccessGrantRS) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
-	var tfPlan TFEmployeeAccessGrantModel
-	resp.Diagnostics.Append(req.Plan.Get(ctx, &tfPlan)...)
+	var tfModel TFModel
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &tfModel)...)
 	if resp.Diagnostics.HasError() {
 		return
 	}
-	resp.Diagnostics.Append(resp.State.Set(ctx, tfPlan)...)
+	atlasReq, err := NewAtlasReq(&tfModel)
+	if err != nil {
+		resp.Diagnostics.AddError(errorCreate, err.Error())
+		return
+	}
+	connV2 := r.Client.AtlasV2
+	projectID := tfModel.ProjectID.ValueString()
+	clusterName := tfModel.ClusterName.ValueString()
+	if _, _, err := connV2.ClustersApi.GrantMongoDBEmployeeAccess(ctx, projectID, clusterName, atlasReq).Execute(); err != nil {
+		resp.Diagnostics.AddError(errorCreate, err.Error())
+		return
+	}
+	resp.Diagnostics.Append(resp.State.Set(ctx, tfModel)...)
 }
 
 func (r *employeeAccessGrantRS) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+	var tfModel TFModel
+	resp.Diagnostics.Append(req.State.Get(ctx, &tfModel)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+	connV2 := r.Client.AtlasV2
+	projectID := tfModel.ProjectID.ValueString()
+	clusterName := tfModel.ClusterName.ValueString()
+	cluster, httpResp, err := connV2.ClustersApi.GetCluster(ctx, projectID, clusterName).Execute()
+	if httpResp.StatusCode == http.StatusNotFound {
+		resp.State.RemoveResource(ctx)
+		return
+	}
+	if err != nil {
+		resp.Diagnostics.AddError(errorCreate, err.Error())
+		return
+	}
+	atlasResp, _ := cluster.GetMongoDBEmployeeAccessGrantOk()
+	if atlasResp == nil {
+		resp.State.RemoveResource(ctx)
+		return
+	}
+	resp.Diagnostics.Append(resp.State.Set(ctx, NewTFModel(projectID, clusterName, atlasResp))...)
 }
 
 func (r *employeeAccessGrantRS) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
-	var tfPlan TFEmployeeAccessGrantModel
+	var tfPlan TFModel
 	resp.Diagnostics.Append(req.Plan.Get(ctx, &tfPlan)...)
 	if resp.Diagnostics.HasError() {
 		return
@@ -50,6 +92,19 @@ func (r *employeeAccessGrantRS) Update(ctx context.Context, req resource.UpdateR
 }
 
 func (r *employeeAccessGrantRS) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+	var tfModel TFModel
+	resp.Diagnostics.Append(req.State.Get(ctx, &tfModel)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+	connV2 := r.Client.AtlasV2
+	projectID := tfModel.ProjectID.ValueString()
+	clusterName := tfModel.ClusterName.ValueString()
+	_, httpResp, err := connV2.ClustersApi.RevokeMongoDBEmployeeAccess(ctx, projectID, clusterName).Execute()
+	if err != nil && httpResp.StatusCode != http.StatusNotFound {
+		resp.Diagnostics.AddError(errorDelete, err.Error())
+		return
+	}
 }
 
 func (r *employeeAccessGrantRS) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
