dd4aa4f2ff3c0e7d9a7907d7654a30ab0cdef915
Leo Antoli <430982+lantoli@users.noreply.github.com>
2024-09-11 17:26:56 +0200

    unit tests for common functions

--- internal/common/conversion/import_state_test.go
+++ internal/common/conversion/import_state_test.go
@@ -1,1 +1,62 @@
 package conversion_test
+
+import (
+	"testing"
+
+	"github.com/mongodb/terraform-provider-mongodbatlas/internal/common/conversion"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+const (
+	projectID          = "123456789012345678901234"
+	projectIDInvalid   = "invalid_project_id"
+	clusterName        = "clusterName"
+	clusterNameInvalid = "_invalidClusterName"
+)
+
+func TestIDWithProjectIDClusterName(t *testing.T) {
+	testCases := map[string]struct {
+		projectID           string
+		clusterName         string
+		expectedID          string
+		expectedErrContains string
+	}{
+		"valid": {
+			projectID:   projectID,
+			clusterName: clusterName,
+			expectedID:  projectID + "-" + clusterName,
+		},
+		"invalid project_id": {
+			projectID:           projectIDInvalid,
+			clusterName:         clusterName,
+			expectedErrContains: projectIDInvalid,
+		},
+		"invalid cluster_name": {
+			projectID:           projectID,
+			clusterName:         clusterNameInvalid,
+			expectedErrContains: clusterNameInvalid,
+		},
+	}
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			id, err := conversion.IDWithProjectIDClusterName(tc.projectID, tc.clusterName)
+			assert.Equal(t, tc.expectedErrContains == "", err == nil)
+			if err == nil {
+				assert.Equal(t, tc.expectedID, id)
+			} else {
+				assert.Contains(t, err.Error(), tc.expectedErrContains)
+			}
+		})
+	}
+}
+
+func TestValidateProjectID(t *testing.T) {
+	require.NoError(t, conversion.ValidateProjectID(projectID))
+	require.Error(t, conversion.ValidateProjectID(projectIDInvalid))
+}
+
+func TestValidateClusterName(t *testing.T) {
+	require.NoError(t, conversion.ValidateClusterName(clusterName))
+	require.Error(t, conversion.ValidateClusterName(clusterNameInvalid))
+}
