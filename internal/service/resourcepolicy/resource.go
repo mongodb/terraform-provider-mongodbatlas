ecb8996e7b7f8ffb30dc7f60ac30c4ea3f303c0f
EspenAlbert <espen.albert@mongodb.com>
2024-09-11 16:00:16 +0100

    feat: initial resource implementation

---
+++ internal/service/resourcepolicy/resource.go
@@ -0,0 +1,175 @@
+package resourcepolicy
+
+import (
+	"context"
+	"errors"
+	"net/http"
+	"regexp"
+
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/mongodb/terraform-provider-mongodbatlas/internal/config"
+	"go.mongodb.org/atlas-sdk/v20240805003/admin"
+)
+
+const resourceName = "resource_policy"
+
+var _ resource.ResourceWithConfigure = &resourcePolicyRS{}
+var _ resource.ResourceWithImportState = &resourcePolicyRS{}
+
+var (
+	errorCreate = "error creating resource"
+	errorRead   = "error reading resource"
+	errorUpdate = "error updating resource"
+)
+
+func Resource() resource.Resource {
+	return &resourcePolicyRS{
+		RSCommon: config.RSCommon{
+			ResourceName: resourceName,
+		},
+	}
+}
+
+type resourcePolicyRS struct {
+	config.RSCommon
+}
+
+func (r *resourcePolicyRS) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
+	resp.Schema = ResourceSchema(ctx)
+}
+
+func (r *resourcePolicyRS) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+	var plan TFResourcePolicyModel
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &plan)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+	orgID := plan.OrgID.ValueString()
+	policies, diags := NewTFPoliciesModelToSDK(ctx, plan.Policies)
+	if diags.HasError() {
+		resp.Diagnostics.Append(diags...)
+		return
+	}
+
+	connV2 := r.Client.AtlasV2
+	resourcePolicyAPI := connV2.AtlasResourcePoliciesApi
+	policySDK, _, err := resourcePolicyAPI.CreateAtlasResourcePolicy(ctx, orgID, &admin.ApiAtlasResourcePolicyCreate{
+		Name:     plan.Name.ValueStringPointer(),
+		Policies: policies,
+	}).Execute()
+	if err != nil {
+		resp.Diagnostics.AddError(errorCreate, err.Error())
+		return
+	}
+	newResourcePolicyModel, diags := NewTFResourcePolicyModel(ctx, policySDK)
+	if diags.HasError() {
+		resp.Diagnostics.Append(diags...)
+		return
+	}
+	resp.Diagnostics.Append(resp.State.Set(ctx, newResourcePolicyModel)...)
+}
+
+func (r *resourcePolicyRS) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+	var state TFResourcePolicyModel
+	resp.Diagnostics.Append(req.State.Get(ctx, &state)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+	orgID := state.OrgID.ValueString()
+	resourcePolicyID := state.ID.ValueString()
+	connV2 := r.Client.AtlasV2
+	policySDK, apiResp, err := connV2.AtlasResourcePoliciesApi.GetAtlasResourcePolicy(ctx, orgID, resourcePolicyID).Execute()
+
+	if err != nil {
+		if apiResp != nil && apiResp.StatusCode == http.StatusNotFound {
+			resp.State.RemoveResource(ctx)
+			return
+		}
+		resp.Diagnostics.AddError(errorRead, err.Error())
+		return
+	}
+
+	newResourcePolicyModel, diags := NewTFResourcePolicyModel(ctx, policySDK)
+	if diags.HasError() {
+		resp.Diagnostics.Append(diags...)
+		return
+	}
+	resp.Diagnostics.Append(resp.State.Set(ctx, newResourcePolicyModel)...)
+}
+
+func (r *resourcePolicyRS) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+	var plan TFResourcePolicyModel
+	var state TFResourcePolicyModel
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &plan)...)
+	resp.Diagnostics.Append(req.State.Get(ctx, &state)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+	orgID := plan.OrgID.ValueString()
+	resourcePolicyID := state.ID.ValueString()
+	policies, diags := NewTFPoliciesModelToSDK(ctx, plan.Policies)
+	if diags.HasError() {
+		resp.Diagnostics.Append(diags...)
+		return
+	}
+
+	connV2 := r.Client.AtlasV2
+	resourcePolicyAPI := connV2.AtlasResourcePoliciesApi
+	policySDK, _, err := resourcePolicyAPI.UpdateAtlasResourcePolicy(ctx, orgID, resourcePolicyID, &admin.ApiAtlasResourcePolicyEdit{
+		Name:     plan.Name.ValueStringPointer(),
+		Policies: policies,
+	}).Execute()
+
+	if err != nil {
+		resp.Diagnostics.AddError(errorUpdate, err.Error())
+		return
+	}
+	newResourcePolicyModel, diags := NewTFResourcePolicyModel(ctx, policySDK)
+	if diags.HasError() {
+		resp.Diagnostics.Append(diags...)
+		return
+	}
+	resp.Diagnostics.Append(resp.State.Set(ctx, newResourcePolicyModel)...)
+}
+
+func (r *resourcePolicyRS) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+	var resourcePolicyState *TFResourcePolicyModel
+	resp.Diagnostics.Append(req.State.Get(ctx, &resourcePolicyState)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+	orgID := resourcePolicyState.OrgID.ValueString()
+	resourcePolicyID := resourcePolicyState.ID.ValueString()
+	connV2 := r.Client.AtlasV2
+	resourcePolicyAPI := connV2.AtlasResourcePoliciesApi
+	if _, _, err := resourcePolicyAPI.DeleteAtlasResourcePolicy(ctx, orgID, resourcePolicyID).Execute(); err != nil {
+		resp.Diagnostics.AddError("error deleting resource", err.Error())
+		return
+	}
+}
+
+func (r *resourcePolicyRS) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
+	orgID, resourcePolicyID, err := splitImportID(req.ID)
+	if err != nil {
+		resp.Diagnostics.AddError("error splitting search deployment import ID", err.Error())
+		return
+	}
+
+	resp.Diagnostics.Append(resp.State.SetAttribute(ctx, path.Root("org_id"), orgID)...)
+	resp.Diagnostics.Append(resp.State.SetAttribute(ctx, path.Root("id"), resourcePolicyID)...)
+}
+
+func splitImportID(id string) (orgID, resourcePolicyID string, err error) {
+	var re = regexp.MustCompile(`(?s)^([0-9a-fA-F]{24})-(.*)$`)
+	parts := re.FindStringSubmatch(id)
+
+	if len(parts) != 3 {
+		err = errors.New("use the format {org_id}-{resource_policy_id}")
+		return
+	}
+
+	orgID = parts[1]
+	resourcePolicyID = parts[2]
+	return
+}
---
+++ internal/service/resourcepolicy/resource_test.go
@@ -0,0 +1,141 @@
+package resourcepolicy_test
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
+	"github.com/mongodb/terraform-provider-mongodbatlas/internal/testutil/acc"
+)
+
+var (
+	resourceType = "mongodbatlas_resource_policy"
+	resourceID   = fmt.Sprintf("%s.test", resourceType)
+)
+
+func TestAccResourcePolicyRS_basic(t *testing.T) {
+	var (
+		orgID       = os.Getenv("MONGODB_ATLAS_ORG_ID")
+		policyName  = "test-policy"
+		updatedName = "updated-policy"
+	)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:                 func() { acc.PreCheckBasic(t) },
+		ProtoV6ProviderFactories: acc.TestAccProviderV6Factories,
+		CheckDestroy:             checkDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: configBasic(orgID, &policyName),
+				Check:  checksResourcePolicy(orgID, &policyName, 1),
+			},
+			{
+				Config: configBasic(orgID, &updatedName),
+				Check:  checksResourcePolicy(orgID, &updatedName, 1),
+			},
+			{
+				Config:            configBasic(orgID, &updatedName),
+				ResourceName:      resourceType,
+				ImportStateIdFunc: checkImportStateIDFunc(resourceID),
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	},
+	)
+}
+
+func checksResourcePolicy(orgID string, name *string, policyCount int) resource.TestCheckFunc {
+	attrMap := map[string]string{
+		"org_id":     orgID,
+		"policies.#": fmt.Sprintf("%d", policyCount),
+	}
+	attrSet := []string{
+		"created_by_user",
+		"created_date",
+		"last_updated_by_user",
+		"last_updated_date",
+		"policies",
+		"id",
+		"version",
+	}
+	if name != nil {
+		attrMap["name"] = *name
+	}
+	checks := []resource.TestCheckFunc{checkExists()}
+	checks = acc.AddAttrChecks(resourceID, checks, attrMap)
+	checks = acc.AddAttrSetChecks(resourceID, checks, attrSet...)
+	for i := 0; i < policyCount; i++ {
+		checks = acc.AddAttrSetChecks(resourceID, checks, fmt.Sprintf("policies.%d.body", i), fmt.Sprintf("policies.%d.id", i))
+	}
+	return resource.ComposeAggregateTestCheckFunc(checks...)
+}
+
+func configBasic(orgID string, policyName *string) string {
+	var name string
+	if policyName != nil {
+		name = fmt.Sprintf("  name = %q", *policyName)
+	}
+	return fmt.Sprintf(`
+resource "mongodbatlas_resource_policy" "test" {
+	org_id = %[1]q
+	%[2]s
+	policies = [
+	{
+		body = <<EOF
+	forbid (
+	principal,
+	action == cloud::Action::"cluster.createEdit",
+	resource
+	) when {
+	context.cluster.cloudProviders.containsAny([cloud::cloudProvider::"aws"])
+	};
+	EOF
+   }
+ ]
+}
+`, orgID, name)
+}
+
+func checkImportStateIDFunc(resourceID string) resource.ImportStateIdFunc {
+	return func(s *terraform.State) (string, error) {
+		rs, ok := s.RootModule().Resources[resourceID]
+		if !ok {
+			return "", fmt.Errorf("not found: %s", resourceID)
+		}
+		return fmt.Sprintf("%s-%s", rs.Primary.Attributes["org_id"], rs.Primary.Attributes["id"]), nil
+	}
+}
+
+func checkExists() resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		for _, rs := range s.RootModule().Resources {
+			if rs.Type == resourceType {
+				orgID := rs.Primary.Attributes["org_id"]
+				id := rs.Primary.Attributes["id"]
+				_, _, err := acc.ConnV2().AtlasResourcePoliciesApi.GetAtlasResourcePolicy(context.Background(), orgID, id).Execute()
+				if err != nil {
+					return fmt.Errorf("resource policy (%s:%s) not found", orgID, id)
+				}
+			}
+		}
+		return nil
+	}
+}
+
+func checkDestroy(state *terraform.State) error {
+	for _, rs := range state.RootModule().Resources {
+		if rs.Type == resourceType {
+			orgID := rs.Primary.Attributes["org_id"]
+			id := rs.Primary.Attributes["id"]
+			_, _, err := acc.ConnV2().AtlasResourcePoliciesApi.GetAtlasResourcePolicy(context.Background(), orgID, id).Execute()
+			if err == nil {
+				return fmt.Errorf("resource policy (%s:%s) still exists", orgID, id)
+			}
+		}
+	}
+	return nil
+}
