db489f1a22a172e56bc5d0ecbf3e379855f4e9df
Leo Antoli <430982+lantoli@users.noreply.github.com>
2024-09-11 17:03:16 +0200

    helper functions to import state

---
+++ internal/common/conversion/import_state.go
@@ -0,0 +1,57 @@
+package conversion
+
+import (
+	"context"
+	"fmt"
+	"regexp"
+	"strings"
+
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+)
+
+func IDWithProjectIDClusterName(projectID, clusterName string) (string, error) {
+	if err := ValidateProjectID(projectID); err != nil {
+		return "", err
+	}
+	if err := ValidateClusterName(clusterName); err != nil {
+		return "", err
+	}
+	return projectID + "-" + clusterName, nil
+}
+
+func ImportStateProjectIDClusterName(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
+	parts := strings.Split(req.ID, "-")
+	if len(parts) != 2 {
+		resp.Diagnostics.AddError("invalid import ID", "expected 2 parts with project_id and cluster_name: "+req.ID)
+		return
+	}
+	projectID, clusterName := parts[0], parts[1]
+	if err := ValidateProjectID(projectID); err != nil {
+		resp.Diagnostics.AddError("invalid project_id in import ID", err.Error())
+	}
+	if err := ValidateClusterName(clusterName); err != nil {
+		resp.Diagnostics.AddError("invalid cluster_name in import ID", err.Error())
+	}
+	if resp.Diagnostics.HasError() {
+		return
+	}
+	resp.Diagnostics.Append(resp.State.SetAttribute(ctx, path.Root("project_id"), projectID)...)
+	resp.Diagnostics.Append(resp.State.SetAttribute(ctx, path.Root("cluster_name"), clusterName)...)
+}
+
+func ValidateProjectID(projectID string) error {
+	re := regexp.MustCompile("^([a-f0-9]{24})$")
+	if !re.MatchString(projectID) {
+		return fmt.Errorf("project_id must be a 24 character hex string: %s", projectID)
+	}
+	return nil
+}
+
+func ValidateClusterName(clusterName string) error {
+	re := regexp.MustCompile("^([a-zA-Z0-9][a-zA-Z0-9-]*)?[a-zA-Z0-9]+$")
+	if !re.MatchString(clusterName) || len(clusterName) < 1 || len(clusterName) > 64 {
+		return fmt.Errorf("cluster_name must be a string with length between 1 and 64, starting and ending with an alphanumeric character, and containing only alphanumeric characters and hyphens: %s", clusterName)
+	}
+	return nil
+}
