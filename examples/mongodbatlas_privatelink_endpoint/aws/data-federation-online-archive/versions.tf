b09637ac4713243f227c25a9755eb235e54ebd9b
Nathanael Smith <nsmith78660@gmail.com>
2024-09-17 15:32:43 -0500

    Remove versions from AWS OA DF PE example
    Removing the versions from versions.tf to prevent possible customer errors

--- examples/mongodbatlas_privatelink_endpoint/aws/data-federation-online-archive/versions.tf
+++ examples/mongodbatlas_privatelink_endpoint/aws/data-federation-online-archive/versions.tf
@@ -2,11 +2,9 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
     }
     mongodbatlas = {
       source  = "mongodb/mongodbatlas"
-      version = "~> 1.10.0"
     }
   }
   required_version = ">= 1.0"
