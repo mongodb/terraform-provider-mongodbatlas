f3c0f66276253bc3e2bf4426d6d91555dc598be7
Aastha Mahendru <aastha.mahendru@mongodb.com>
2024-10-02 12:20:23 +0100

    lint

--- tools/codegen/openapi/parser.go
+++ tools/codegen/openapi/parser.go
@@ -29,37 +29,6 @@ func ParseAtlasAdminAPI(filePath string) (*libopenapi.DocumentModel[v3.Document]
 	return docModel, nil
 }
 
-func fetchOpenAPISpec(ctx context.Context, urlPath string) error {
-	req, err := http.NewRequestWithContext(ctx, "GET", urlPath, http.NoBody)
-	if err != nil {
-		return err
-	}
-
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("failed to download Atlas API spec: %s", resp.Status)
-	}
-
-	file, err := os.Create("open-api-spec.yml")
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	_, err = io.Copy(file, resp.Body)
-	if err != nil {
-		return err
-	}
-
-	println("Downloaded Atlas API spec")
-	return nil
-}
-
 func DownloadOpenAPISpec(url, specFilePath string) (err error) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
 	defer cancel()
