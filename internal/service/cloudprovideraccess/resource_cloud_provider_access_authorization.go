5d2b981546f085bb6ce2699b37e6bca506efbc0c
Oriol Arbusi <oriol.abadal@mongodb.com>
2024-10-07 10:33:45 +0200

    revert change

--- internal/service/cloudprovideraccess/resource_cloud_provider_access_authorization.go
+++ internal/service/cloudprovideraccess/resource_cloud_provider_access_authorization.go
@@ -281,9 +281,11 @@ func authorizeRole(ctx context.Context, client *admin.APIClient, d *schema.Resou
 		roleID = targetRole.GetId()
 	}
 
+	var role *admin.CloudProviderAccessRole
 	var err error
+
 	for i := 0; i < 3; i++ {
-		_, _, err = client.CloudProviderAccessApi.AuthorizeCloudProviderAccessRole(ctx, projectID, roleID, req).Execute()
+		role, _, err = client.CloudProviderAccessApi.AuthorizeCloudProviderAccessRole(ctx, projectID, roleID, req).Execute()
 		if err != nil && strings.Contains(err.Error(), "CANNOT_ASSUME_ROLE") { // aws takes time to update , in case of single path
 			log.Printf("warning issue performing authorize: %s \n", err.Error())
 			log.Println("retrying")
@@ -298,10 +300,6 @@ func authorizeRole(ctx context.Context, client *admin.APIClient, d *schema.Resou
 
 	if err != nil {
 		return diag.FromErr(fmt.Errorf("error cloud provider access authorization %s", err))
-	}
-	role, _, err := client.CloudProviderAccessApi.GetCloudProviderAccessRole(ctx, projectID, roleID).Execute()
-	if err != nil {
-		return diag.FromErr(fmt.Errorf("error cloud provider access authorization read after authorization %s", err))
 	}
 
 	authSchema := roleToSchemaAuthorization(role)
