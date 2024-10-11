1df40df74382977fcf21d689cfaa9273dafbbb31
Aastha Mahendru <aastha.mahendru@mongodb.com>
2024-10-11 08:11:46 +0100

    minor

--- tools/codegen/codespec/merge_attributes.go
+++ tools/codegen/codespec/merge_attributes.go
@@ -5,7 +5,7 @@ import "sort"
 func mergeAttributes(pathParams, createRequest, createResponse, readResponse Attributes) Attributes {
 	merged := make(map[string]*Attribute)
 
-	addOrUpdate := func(attr *Attribute, cor ComputedOptionalRequired) {
+	addOrUpdate := func(attr *Attribute, computability ComputedOptionalRequired) {
 		if existingAttr, exists := merged[attr.Name]; exists {
 			if existingAttr.Description == nil || *existingAttr.Description == "" {
 				existingAttr.Description = attr.Description
@@ -15,28 +15,28 @@ func mergeAttributes(pathParams, createRequest, createResponse, readResponse Att
 				return
 			}
 
-			existingAttr.ComputedOptionalRequired = cor
+			existingAttr.ComputedOptionalRequired = computability
 		} else {
 			newAttr := *attr
-			newAttr.ComputedOptionalRequired = cor
+			newAttr.ComputedOptionalRequired = computability
 			merged[attr.Name] = &newAttr
 		}
 	}
 
-	// 1. Path parameters: all attributes will be "required"
+	// Path parameters: all attributes will be "required"
 	for i := range pathParams {
 		addOrUpdate(&pathParams[i], Required)
 	}
 
-	// 2. POST request body: optional/required is as defined
+	// POST request body: optional/required is as defined
 	for i := range createRequest {
 		addOrUpdate(&createRequest[i], createRequest[i].ComputedOptionalRequired)
 	}
 
-	// 3. POST/GET response body: properties not in the request body are "computed" or "computed_optional"
+	// POST/GET response body: properties not in the request body are "computed" or "computed_optional" (if a default is present)
 	for i := range createResponse {
 		if _, exists := merged[createResponse[i].Name]; !exists {
-			if isAttributeOptional(&createResponse[i]) {
+			if isOptional(&createResponse[i]) {
 				addOrUpdate(&createResponse[i], ComputedOptional)
 			} else {
 				addOrUpdate(&createResponse[i], Computed)
@@ -46,7 +46,7 @@ func mergeAttributes(pathParams, createRequest, createResponse, readResponse Att
 
 	for i := range readResponse {
 		if _, exists := merged[readResponse[i].Name]; !exists {
-			if isAttributeOptional(&readResponse[i]) {
+			if isOptional(&readResponse[i]) {
 				addOrUpdate(&readResponse[i], ComputedOptional)
 			} else {
 				addOrUpdate(&readResponse[i], Computed)
@@ -54,21 +54,21 @@ func mergeAttributes(pathParams, createRequest, createResponse, readResponse Att
 		}
 	}
 
-	finalAttributes := make(Attributes, 0, len(merged))
+	resourceAttributes := make(Attributes, 0, len(merged))
 	for _, attr := range merged {
-		finalAttributes = append(finalAttributes, *attr)
+		resourceAttributes = append(resourceAttributes, *attr)
 	}
 
-	sortAttributes(finalAttributes)
+	sortAttributes(resourceAttributes)
 
-	return finalAttributes
+	return resourceAttributes
 }
 
-func isAttributeOptional(attr *Attribute) bool {
+func isOptional(attr *Attribute) bool {
 	return (attr.Bool != nil && attr.Bool.Default != nil) ||
-		(attr.Float64 != nil && attr.Float64.Default != nil) ||
 		(attr.Int64 != nil && attr.Int64.Default != nil) ||
 		(attr.String != nil && attr.String.Default != nil) ||
+		(attr.Float64 != nil && attr.Float64.Default != nil) ||
 		(attr.Number != nil && attr.Number.Default != nil)
 }
 
