702bcebabfe487b1a7ba29cbb578d85d83577574
Espen Albert <espen.albert@mongodb.com>
2024-10-15 08:38:11 +0100

    test: Add a test to confirm multi-region cluster can be used with search deployment

--- internal/service/searchdeployment/resource_search_deployment_test.go
+++ internal/service/searchdeployment/resource_search_deployment_test.go
@@ -11,24 +11,28 @@ import (
 	"github.com/mongodb/terraform-provider-mongodbatlas/internal/testutil/acc"
 )
 
+const (
+	resourceID   = "mongodbatlas_search_deployment.test"
+	dataSourceID = "data.mongodbatlas_search_deployment.test"
+)
+
 func TestAccSearchDeployment_basic(t *testing.T) {
 	var (
-		resourceName = "mongodbatlas_search_deployment.test"
-		orgID        = os.Getenv("MONGODB_ATLAS_ORG_ID")
-		projectName  = acc.RandomProjectName()
-		clusterName  = acc.RandomClusterName()
+		orgID       = os.Getenv("MONGODB_ATLAS_ORG_ID")
+		projectName = acc.RandomProjectName()
+		clusterName = acc.RandomClusterName()
 	)
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:                 func() { acc.PreCheckBasic(t) },
 		ProtoV6ProviderFactories: acc.TestAccProviderV6Factories,
 		CheckDestroy:             checkDestroy,
 		Steps: []resource.TestStep{
-			newSearchNodeTestStep(resourceName, orgID, projectName, clusterName, "S20_HIGHCPU_NVME", 3),
-			newSearchNodeTestStep(resourceName, orgID, projectName, clusterName, "S30_HIGHCPU_NVME", 4),
+			newSearchNodeTestStep(resourceID, orgID, projectName, clusterName, "S20_HIGHCPU_NVME", 3),
+			newSearchNodeTestStep(resourceID, orgID, projectName, clusterName, "S30_HIGHCPU_NVME", 4),
 			{
 				Config:            configBasic(orgID, projectName, clusterName, "S30_HIGHCPU_NVME", 4),
-				ResourceName:      resourceName,
-				ImportStateIdFunc: importStateIDFunc(resourceName),
+				ResourceName:      resourceID,
+				ImportStateIdFunc: importStateIDFunc(resourceID),
 				ImportState:       true,
 				ImportStateVerify: true,
 			},
@@ -36,9 +40,39 @@ func TestAccSearchDeployment_basic(t *testing.T) {
 	})
 }
 
+func TestAccSearchDeployment_multiRegion(t *testing.T) {
+	var (
+		clusterInfo = acc.GetClusterInfo(t, &acc.ClusterRequest{
+			ClusterName: "multi-region-cluster",
+			ReplicationSpecs: []acc.ReplicationSpecRequest{
+				{
+					Region: "US_EAST_1",
+					ExtraRegionConfigs: []acc.ReplicationSpecRequest{
+						{Region: "US_WEST_2", Priority: 6, InstanceSize: "M10", NodeCount: 2},
+					},
+				},
+			},
+		})
+	)
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:                 func() { acc.PreCheckBasic(t) },
+		ProtoV6ProviderFactories: acc.TestAccProviderV6Factories,
+		CheckDestroy:             checkDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: clusterInfo.TerraformStr + configSearchDeployment(clusterInfo.ProjectID, clusterInfo.TerraformNameRef, "M10", 2),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					checkExists(resourceID),
+					resource.TestCheckResourceAttr(resourceID, "specs.#", "1"),
+				),
+			},
+		},
+	})
+}
+
 func newSearchNodeTestStep(resourceName, orgID, projectName, clusterName, instanceSize string, searchNodeCount int) resource.TestStep {
 	resourceChecks := searchNodeChecks(resourceName, clusterName, instanceSize, searchNodeCount)
-	dataSourceChecks := searchNodeChecks(fmt.Sprintf("data.%s", resourceName), clusterName, instanceSize, searchNodeCount)
+	dataSourceChecks := searchNodeChecks(dataSourceID, clusterName, instanceSize, searchNodeCount)
 	return resource.TestStep{
 		Config: configBasic(orgID, projectName, clusterName, instanceSize, searchNodeCount),
 		Check:  resource.ComposeAggregateTestCheckFunc(append(resourceChecks, dataSourceChecks...)...),
@@ -78,6 +112,21 @@ func configBasic(orgID, projectName, clusterName, instanceSize string, searchNod
 			cluster_name = mongodbatlas_search_deployment.test.cluster_name
 		}
 	`, clusterConfig, instanceSize, searchNodeCount)
+}
+
+func configSearchDeployment(projectID, clusterNameRef, instanceSize string, searchNodeCount int) string {
+	return fmt.Sprintf(`
+	resource "mongodbatlas_search_deployment" "test" {
+		project_id = %[1]q
+		cluster_name = %[2]s
+		specs = [
+			{
+				instance_size = %[3]q
+				node_count = %[4]d
+			}
+		]
+	}
+	`, projectID, clusterNameRef, instanceSize, searchNodeCount)
 }
 
 func advancedClusterConfig(orgID, projectName, clusterName string) string {
