416ea50fbc7565d10600fd412fcce29887be76fa
Oriol Arbusi <oriol.abadal@mongodb.com>
2024-10-17 16:37:58 +0200

    wip: model tests

--- internal/service/flexcluster/model_test.go
+++ internal/service/flexcluster/model_test.go
@@ -5,27 +5,39 @@ import (
 	"context"
 	"testing"
 
+	"github.com/mongodb/terraform-provider-mongodbatlas/internal/common/conversion"
 	"github.com/mongodb/terraform-provider-mongodbatlas/internal/service/flexcluster"
 	"github.com/stretchr/testify/assert"
 	"go.mongodb.org/atlas-sdk/v20240805004/admin"
 )
 
 type sdkToTFModelTestCase struct {
-	SDKResp         *admin.FlexClusterDescription20250101
+	input           *admin.FlexClusterDescription20250101
 	expectedTFModel *flexcluster.TFModel
 }
 
 func TestFlexClusterSDKToTFModel(t *testing.T) {
+	time, _ := admin.StringToTime("2021-08-17T17:00:00Z")
 	testCases := map[string]sdkToTFModelTestCase{ // TODO: consider adding test cases to contemplate all possible API responses
 		"Complete SDK response": {
-			SDKResp:         &admin.FlexClusterDescription20250101{},
+			input: &admin.FlexClusterDescription20250101{
+				ClusterType:                  conversion.StringPtr("REPLICASET"),
+				CreateDate:                   conversion.Pointer(time),
+				GroupId:                      conversion.StringPtr("5f3b6d1b0b8e4f0e7b8f6b1b"),
+				Id:                           conversion.StringPtr("5f3b6d1b0b8e4f0e7b8f6b1c"),
+				MongoDBVersion:               conversion.StringPtr("8.0"),
+				Name:                         conversion.StringPtr("myCluster"),
+				StateName:                    conversion.StringPtr("IDLE"),
+				TerminationProtectionEnabled: conversion.Pointer(true),
+				VersionReleaseSystem:         conversion.StringPtr("LTS"),
+			},
 			expectedTFModel: &flexcluster.TFModel{},
 		},
 	}
 
 	for testName, tc := range testCases {
 		t.Run(testName, func(t *testing.T) {
-			resultModel, diags := flexcluster.NewTFModel(context.Background(), tc.SDKResp)
+			resultModel, diags := flexcluster.NewTFModel(context.Background(), tc.input)
 			if diags.HasError() {
 				t.Errorf("unexpected errors found: %s", diags.Errors()[0].Summary())
 			}
