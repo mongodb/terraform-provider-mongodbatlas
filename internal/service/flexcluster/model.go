6ce649c1b048ed8a1ef08bac115430e059a50759
Oriol Arbusi <oriol.abadal@mongodb.com>
2024-10-17 11:23:06 +0200

    read implementation

--- internal/service/flexcluster/model.go
+++ internal/service/flexcluster/model.go
@@ -2,7 +2,6 @@ package flexcluster
 
 import (
 	"context"
-<<<<<<< HEAD
 
 	"github.com/hashicorp/terraform-plugin-framework/attr"
 	"github.com/hashicorp/terraform-plugin-framework/diag"
@@ -139,29 +138,4 @@ func newResourceTags(ctx context.Context, tags types.Map) *[]admin.ResourceTag {
 		})
 	}
 	return &tagsAdmin
-=======
-
-	"github.com/hashicorp/terraform-plugin-framework/diag"
-	"go.mongodb.org/atlas-sdk/v20240805004/admin"
-)
-
-// TODO: `ctx` parameter and `diags` return value can be removed if tf schema has no complex data types (e.g., schema.ListAttribute, schema.SetAttribute)
-func NewTFModel(ctx context.Context, apiResp *admin.FlexClusterDescription20250101) (*TFModel, diag.Diagnostics) {
-	// complexAttr, diagnostics := types.ListValueFrom(ctx, InnerObjectType, newTFComplexAttrModel(apiResp.ComplexAttr))
-	// if diagnostics.HasError() {
-	// 	return nil, diagnostics
-	// }
-	return &TFModel{}, nil
-}
-
-// TODO: If SDK defined different models for create and update separate functions will need to be defined.
-// TODO: `ctx` parameter and `diags` in return value can be removed if tf schema has no complex data types (e.g., schema.ListAttribute, schema.SetAttribute)
-func NewAtlasReq(ctx context.Context, plan *TFModel) (*admin.FlexClusterDescriptionCreate20250101, diag.Diagnostics) {
-	// var tfList []complexArgumentData
-	// resp.Diagnostics.Append(plan.ComplexArgument.ElementsAs(ctx, &tfList, false)...)
-	// if resp.Diagnostics.HasError() {
-	// 	return nil, diagnostics
-	// }
-	return &admin.FlexClusterDescriptionCreate20250101{}, nil
->>>>>>> 8cce85db (create implementation)
 }
