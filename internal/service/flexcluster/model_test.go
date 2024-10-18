9f59e878e1e4404194ff26fcae1d2e5e301b0c75
Oriol Arbusi <oriol.abadal@mongodb.com>
2024-10-18 16:51:18 +0200

    fix model test

--- internal/service/flexcluster/model_test.go
+++ internal/service/flexcluster/model_test.go
@@ -123,7 +123,7 @@ func TestNewTFModel(t *testing.T) {
 			expectedTFModel: &flexcluster.TFModel{
 				ProjectId:                    types.StringNull(),
 				Id:                           types.StringNull(),
-				Tags:                         types.Map{},
+				Tags:                         types.MapNull(types.StringType),
 				ProviderSettings:             nilProviderSettingsObject,
 				ConnectionStrings:            types.ObjectNull(flexcluster.ConnectionStringsType.AttrTypes),
 				CreateDate:                   types.StringNull(),
