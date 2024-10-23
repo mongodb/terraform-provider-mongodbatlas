11e7ba934bd7bda307617873e84f4ae1e802acec
cveticm <119604954+cveticm@users.noreply.github.com>
2024-10-23 09:48:34 +0100

    chore: Generate Flex Cluster data source file and schemas (#2725)
    * generate data source file and schema for singular Flex Cluster
    
    * Generate plural data source schema
    
    * Changing type of tags in schema
    
    * Fix to data source schema

---
+++ internal/service/flexcluster/plural_data_source_schema.go
@@ -0,0 +1,31 @@
+package flexcluster
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+func DataSourcePluralSchema(ctx context.Context) schema.Schema {
+	return schema.Schema{
+		Attributes: map[string]schema.Attribute{
+			"project_id": schema.StringAttribute{
+				Required:            true,
+				MarkdownDescription: "Unique 24-hexadecimal digit string that identifies your project. Use the [/groups](#tag/Projects/operation/listProjects) endpoint to retrieve all projects to which the authenticated user has access.\n\n**NOTE**: Groups and projects are synonymous terms. Your group id is the same as your project id. For existing groups, your group/project id remains the same. The resource and corresponding endpoints use the term groups.",
+			},
+			"flex_clusters": schema.ListNestedAttribute{
+				Computed: true,
+				NestedObject: schema.NestedAttributeObject{
+					Attributes: dataSourceSchema(true),
+				},
+				MarkdownDescription: "List of returned documents that MongoDB Cloud provides when completing this request.",
+			},
+		},
+	}
+}
+
+type TFModelDSP struct {
+	ProjectId    types.String `tfsdk:"project_id"`
+	FlexClusters []TFModel    `tfsdk:"flex_clusters"`
+}
