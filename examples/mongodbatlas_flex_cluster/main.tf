0f1dbc753d3d580ca23fee2aaa9955e6a7b88f01
Oriol Arbusi <oriol.abadal@mongodb.com>
2024-10-28 18:34:31 +0100

    fmt

--- examples/mongodbatlas_flex_cluster/main.tf
+++ examples/mongodbatlas_flex_cluster/main.tf
@@ -1,11 +1,9 @@
 resource "mongodbatlas_flex_cluster" "flex_cluster" {
-	project_id = var.project_id
-	name       = "clusterName"
-	provider_settings = {
-		backing_provider_name = "AWS"
-		region_name           = "US_EAST_1"
-	}
-	termination_protection_enabled = true
-}
-
-# TODO: add plural and singular data source example
+  project_id = var.project_id
+  name       = "clusterName"
+  provider_settings = {
+    backing_provider_name = "AWS"
+    region_name           = "US_EAST_1"
+  }
+  termination_protection_enabled = true
+} # TODO: add plural and singular data source example
