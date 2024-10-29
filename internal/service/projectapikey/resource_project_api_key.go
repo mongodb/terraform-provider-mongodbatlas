641090be8146edc96806abfc6443eafeef4f042a
Leo Antoli <430982+lantoli@users.noreply.github.com>
2024-10-29 10:37:41 +0100

    fix: Fixes change assigments in `mongodbatlas_project_api_key` (#2737)
    * fix changed assigments
    
    * fix TestAccProjectAPIKey_updateRole
    
    * changelog
    
    * join configMultiple to configBasic
    
    * don't allow duplicate projectIDs
    
    * fix TestAccProjectAPIKey_dataSources
    
    * refactor Create
    
    * refactor getAPIProjectAssignments and flattenProjectAssignments
    
    * refactor Read
    
    * refactor Delete
    
    * fix TestAccProjectAPIKey_recreateWhenDeletedExternally
    
    * refactor Import
    
    * remove flattenProjectAssignments, flattenProjectAPIKeyRoles and getAPIProjectAssignments
    
    * fix plural ds
    
    * fix basicTestCase
    
    * refactor Update description
    
    * refactor Update
    
    * refactor expandProjectAssignments
    
    * update changelog
    
    * initial checkAggr
    
    * checkExists
    
    * delete redundant TestAccProjectAPIKey_dataSources
    
    * refactor configDuplicatedProject
    
    * refactor configChangingProject
    
    * model file
    
    * more detailed changelog
    
    * revert import
    
    * doc
    
    * refactor checks

--- internal/service/projectapikey/resource_project_api_key.go
+++ internal/service/projectapikey/resource_project_api_key.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"net/http"
 	"strings"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
@@ -12,10 +11,6 @@ import (
 	"github.com/mongodb/terraform-provider-mongodbatlas/internal/common/conversion"
 	"github.com/mongodb/terraform-provider-mongodbatlas/internal/config"
 	"go.mongodb.org/atlas-sdk/v20240805005/admin"
-)
-
-const (
-	ErrorProjectSetting = "error setting `%s` for project (%s): %s"
 )
 
 func Resource() *schema.Resource {
@@ -69,64 +64,47 @@ func Resource() *schema.Resource {
 	}
 }
 
-type APIProjectAssignmentKeyInput struct {
-	ProjectID string   `json:"desc,omitempty"`
-	RoleNames []string `json:"roles,omitempty"`
-}
-
-const errorNoProjectAssignmentDefined = "could not obtain a project id as no assignments are defined"
-
 func resourceCreate(ctx context.Context, d *schema.ResourceData, meta any) diag.Diagnostics {
+	if err := validateUniqueProjectIDs(d); err != nil {
+		return diag.FromErr(err)
+	}
 	connV2 := meta.(*config.MongoDBClient).AtlasV2
-
-	var apiKey *admin.ApiKeyUserDetails
-	var resp *http.Response
-	var err error
-
-	createRequest := &admin.CreateAtlasProjectApiKey{
-		Desc: d.Get("description").(string),
+	val, ok := d.GetOk("project_assignment")
+	if !ok {
+		return diag.FromErr(errors.New("project_assignment not found"))
+	}
+	assignments := expandProjectAssignments(val.(*schema.Set))
+	projectIDs := make([]string, 0, len(assignments))
+	for projectID := range assignments {
+		projectIDs = append(projectIDs, projectID)
 	}
 
-	if projectAssignments, ok := d.GetOk("project_assignment"); ok {
-		projectAssignmentList := ExpandProjectAssignmentSet(projectAssignments.(*schema.Set))
-
-		// creates api key using project id of first defined project assignment
-		firstAssignment := projectAssignmentList[0]
-		createRequest.Roles = firstAssignment.RoleNames
-		apiKey, resp, err = connV2.ProgrammaticAPIKeysApi.CreateProjectApiKey(ctx, firstAssignment.ProjectID, createRequest).Execute()
-		if err != nil {
-			if resp != nil && resp.StatusCode == http.StatusNotFound {
-				d.SetId("")
-				return nil
-			}
-			return diag.FromErr(err)
-		}
-
-		// assign created api key to remaining project assignments
-		for _, apiKeyList := range projectAssignmentList[1:] {
-			assignment := []admin.UserAccessRoleAssignment{{Roles: &apiKeyList.RoleNames}}
-			_, _, err := connV2.ProgrammaticAPIKeysApi.AddProjectApiKey(ctx, apiKeyList.ProjectID, apiKey.GetId(), &assignment).Execute()
-			if err != nil {
-				if resp != nil && resp.StatusCode == http.StatusNotFound {
-					d.SetId("")
-					return nil
-				}
-			}
-		}
+	req := &admin.CreateAtlasProjectApiKey{
+		Desc:  d.Get("description").(string),
+		Roles: assignments[projectIDs[0]],
 	}
-
-	if err := d.Set("public_key", apiKey.GetPublicKey()); err != nil {
+	ret, _, err := connV2.ProgrammaticAPIKeysApi.CreateProjectApiKey(ctx, projectIDs[0], req).Execute()
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	apiKeyID := ret.GetId()
+	d.SetId(conversion.EncodeStateID(map[string]string{
+		"api_key_id": apiKeyID,
+	}))
+	if err := d.Set("public_key", ret.GetPublicKey()); err != nil {
 		return diag.FromErr(fmt.Errorf("error setting `public_key`: %s", err))
 	}
-
-	if err := d.Set("private_key", apiKey.GetPrivateKey()); err != nil {
+	if err := d.Set("private_key", ret.GetPrivateKey()); err != nil {
 		return diag.FromErr(fmt.Errorf("error setting `private_key`: %s", err))
 	}
 
-	d.SetId(conversion.EncodeStateID(map[string]string{
-		"api_key_id": apiKey.GetId(),
-	}))
-
+	for _, projectID := range projectIDs[1:] {
+		roles := assignments[projectID]
+		req := &[]admin.UserAccessRoleAssignment{{Roles: &roles}}
+		if _, _, err := connV2.ProgrammaticAPIKeysApi.AddProjectApiKey(ctx, projectID, apiKeyID, req).Execute(); err != nil {
+			return diag.FromErr(err)
+		}
+	}
 	return resourceRead(ctx, d, meta)
 }
 
@@ -135,106 +113,83 @@ func resourceRead(ctx context.Context, d *schema.ResourceData, meta any) diag.Di
 	ids := conversion.DecodeStateID(d.Id())
 	apiKeyID := ids["api_key_id"]
 
-	firstProjectID, err := getFirstProjectIDFromAssignments(d)
+	details, _, err := getKeyDetails(ctx, connV2, apiKeyID)
 	if err != nil {
-		return diag.FromErr(fmt.Errorf("could not obtain a project id from state: %s", err))
+		return diag.FromErr(err)
 	}
-
-	projectAPIKeys, _, err := connV2.ProgrammaticAPIKeysApi.ListProjectApiKeys(ctx, *firstProjectID).Execute()
-	if err != nil {
-		return diag.FromErr(fmt.Errorf("error getting api key information: %s", err))
-	}
-	apiKeyIsPresent := false
-	for _, val := range projectAPIKeys.GetResults() {
-		if val.GetId() != apiKeyID {
-			continue
-		}
-
-		apiKeyIsPresent = true
-		if err := d.Set("api_key_id", val.GetId()); err != nil {
-			return diag.FromErr(fmt.Errorf("error setting `api_key_id`: %s", err))
-		}
-
-		if err := d.Set("description", val.GetDesc()); err != nil {
-			return diag.FromErr(fmt.Errorf("error setting `description`: %s", err))
-		}
-
-		if err := d.Set("public_key", val.GetPublicKey()); err != nil {
-			return diag.FromErr(fmt.Errorf("error setting `public_key`: %s", err))
-		}
-
-		if projectAssignments, err := newProjectAssignment(ctx, connV2, apiKeyID); err == nil {
-			if err := d.Set("project_assignment", projectAssignments); err != nil {
-				return diag.Errorf("error setting `project_assignment` : %s", err)
-			}
-		}
-	}
-	if !apiKeyIsPresent {
-		// api key has been deleted, marking resource as destroyed
+	if details == nil {
 		d.SetId("")
 		return nil
 	}
 
+	if err := d.Set("api_key_id", details.GetId()); err != nil {
+		return diag.FromErr(fmt.Errorf("error setting `api_key_id`: %s", err))
+	}
+
+	if err := d.Set("description", details.GetDesc()); err != nil {
+		return diag.FromErr(fmt.Errorf("error setting `description`: %s", err))
+	}
+
+	if err := d.Set("public_key", details.GetPublicKey()); err != nil {
+		return diag.FromErr(fmt.Errorf("error setting `public_key`: %s", err))
+	}
+
+	if err := d.Set("project_assignment", flattenProjectAssignments(details.GetRoles())); err != nil {
+		return diag.Errorf("error setting `project_assignment` : %s", err)
+	}
 	return nil
 }
 
 func resourceUpdate(ctx context.Context, d *schema.ResourceData, meta any) diag.Diagnostics {
-	connV2 := meta.(*config.MongoDBClient).AtlasV2
+	if err := validateUniqueProjectIDs(d); err != nil {
+		return diag.FromErr(err)
+	}
 
+	connV2 := meta.(*config.MongoDBClient).AtlasV2
 	ids := conversion.DecodeStateID(d.Id())
 	apiKeyID := ids["api_key_id"]
 
-	if d.HasChange("project_assignment") {
-		// Getting the changes to api key project assignments
-		newAssignments, changedAssignments, removedAssignments := getStateProjectAssignmentAPIKeys(d)
-
-		// Adding new projects assignments
-		if len(newAssignments) > 0 {
-			for _, apiKey := range newAssignments {
-				projectID := apiKey.(map[string]any)["project_id"].(string)
-				roles := conversion.ExpandStringList(apiKey.(map[string]any)["role_names"].(*schema.Set).List())
-				assignment := []admin.UserAccessRoleAssignment{{Roles: &roles}}
-				_, _, err := connV2.ProgrammaticAPIKeysApi.AddProjectApiKey(ctx, projectID, apiKeyID, &assignment).Execute()
-				if err != nil {
-					return diag.Errorf("error assigning api_keys into the project(%s): %s", projectID, err)
-				}
-			}
-		}
-
-		// Removing projects assignments
-		for _, apiKey := range removedAssignments {
-			projectID := apiKey.(map[string]any)["project_id"].(string)
-			_, _, err := connV2.ProgrammaticAPIKeysApi.RemoveProjectApiKey(ctx, projectID, apiKeyID).Execute()
-			if err != nil && strings.Contains(err.Error(), "GROUP_NOT_FOUND") {
-				continue // allows removing assignment for a project that has been deleted
-			}
-			if err != nil {
-				return diag.Errorf("error removing api_key(%s) from the project(%s): %s", apiKeyID, projectID, err)
-			}
-		}
-
-		// Updating the role names for the project assignments
-		for _, apiKey := range changedAssignments {
-			projectID := apiKey.(map[string]any)["project_id"].(string)
-			roles := conversion.ExpandStringList(apiKey.(map[string]any)["role_names"].(*schema.Set).List())
-			assignment := []admin.UserAccessRoleAssignment{{Roles: &roles}}
-			_, _, err := connV2.ProgrammaticAPIKeysApi.AddProjectApiKey(ctx, projectID, apiKeyID, &assignment).Execute()
-			if err != nil {
-				return diag.Errorf("error updating role names for the api_key(%s): %s", apiKey, err)
-			}
-		}
+	details, orgID, err := getKeyDetails(ctx, connV2, apiKeyID)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	if details == nil {
+		return diag.Errorf("error updating project api_key (%s): not found", apiKeyID)
 	}
 
-	firstProjectID, err := getFirstProjectIDFromAssignments(d)
-	if err != nil {
-		return diag.FromErr(fmt.Errorf("could not obtain a project id from state: %s", err))
+	if d.HasChange("project_assignment") {
+		add, remove, update := getAssignmentChanges(d)
+
+		for projectID := range remove {
+			_, _, err := connV2.ProgrammaticAPIKeysApi.RemoveProjectApiKey(ctx, projectID, apiKeyID).Execute()
+			if err != nil {
+				if admin.IsErrorCode(err, "GROUP_NOT_FOUND") {
+					continue // allows removing assignment for a project that has been deleted
+				}
+				return diag.Errorf("error removing project_api_key(%s) from project(%s): %s", apiKeyID, projectID, err)
+			}
+		}
+
+		for projectID, roles := range add {
+			req := &[]admin.UserAccessRoleAssignment{{Roles: &roles}}
+			_, _, err := connV2.ProgrammaticAPIKeysApi.AddProjectApiKey(ctx, projectID, apiKeyID, req).Execute()
+			if err != nil {
+				return diag.Errorf("error adding project_api_key(%s) to project(%s): %s", apiKeyID, projectID, err)
+			}
+		}
+
+		for projectID, roles := range update {
+			req := &admin.UpdateAtlasProjectApiKey{Roles: &roles}
+			_, _, err := connV2.ProgrammaticAPIKeysApi.UpdateApiKeyRoles(ctx, projectID, apiKeyID, req).Execute()
+			if err != nil {
+				return diag.Errorf("error changing project_api_key(%s) in project(%s): %s", apiKeyID, projectID, err)
+			}
+		}
 	}
 
 	if d.HasChange("description") {
-		newDescription := d.Get("description").(string)
-		if _, _, err := connV2.ProgrammaticAPIKeysApi.UpdateApiKeyRoles(ctx, *firstProjectID, apiKeyID, &admin.UpdateAtlasProjectApiKey{
-			Desc: &newDescription,
-		}).Execute(); err != nil {
+		req := &admin.UpdateAtlasOrganizationApiKey{Desc: conversion.StringPtr(d.Get("description").(string))}
+		if _, _, err := connV2.ProgrammaticAPIKeysApi.UpdateApiKey(ctx, orgID, apiKeyID, req).Execute(); err != nil {
 			return diag.Errorf("error updating description in api key(%s): %s", apiKeyID, err)
 		}
 	}
@@ -246,223 +201,30 @@ func resourceDelete(ctx context.Context, d *schema.ResourceData, meta any) diag.
 	connV2 := meta.(*config.MongoDBClient).AtlasV2
 	ids := conversion.DecodeStateID(d.Id())
 	apiKeyID := ids["api_key_id"]
-	var orgID string
-
-	firstProjectID, err := getFirstProjectIDFromAssignments(d)
+	details, orgID, err := getKeyDetails(ctx, connV2, apiKeyID)
 	if err != nil {
-		return diag.FromErr(fmt.Errorf("could not obtain a project id from state: %s", err))
+		return diag.FromErr(err)
 	}
-
-	projectAPIKeys, _, err := connV2.ProgrammaticAPIKeysApi.ListProjectApiKeys(ctx, *firstProjectID).Execute()
-	if err != nil {
-		return diag.FromErr(fmt.Errorf("error getting api key information: %s", err))
-	}
-
-	for _, val := range projectAPIKeys.GetResults() {
-		if val.GetId() == apiKeyID {
-			for i, role := range val.GetRoles() {
-				if strings.HasPrefix(role.GetRoleName(), "ORG_") {
-					orgID = val.GetRoles()[i].GetOrgId()
-				}
-			}
-		}
-	}
-
-	apiKeyOrgList, _, err := connV2.RootApi.GetSystemStatus(ctx).Execute()
-	if err != nil {
-		return diag.FromErr(fmt.Errorf("error getting api key information: %s", err))
-	}
-
-	projectAssignments, err := getAPIProjectAssignments(ctx, connV2, apiKeyOrgList, apiKeyID)
-	if err != nil {
-		return diag.FromErr(fmt.Errorf("error getting api key information: %s", err))
-	}
-
-	for _, apiKey := range projectAssignments {
-		_, _, err = connV2.ProgrammaticAPIKeysApi.RemoveProjectApiKey(ctx, apiKey.ProjectID, apiKeyID).Execute()
-		if err != nil {
-			return diag.FromErr(fmt.Errorf("error deleting project api key: %s", err))
-		}
-	}
-
-	if orgID != "" {
+	if details != nil && orgID != "" {
 		if _, _, err = connV2.ProgrammaticAPIKeysApi.DeleteApiKey(ctx, orgID, apiKeyID).Execute(); err != nil {
-			return diag.FromErr(fmt.Errorf("error unable to delete Key (%s): %s", apiKeyID, err))
+			return diag.FromErr(fmt.Errorf("error deleting project key (%s): %s", apiKeyID, err))
 		}
 	}
-
 	d.SetId("")
 	return nil
 }
 
 func resourceImportState(ctx context.Context, d *schema.ResourceData, meta any) ([]*schema.ResourceData, error) {
-	connV2 := meta.(*config.MongoDBClient).AtlasV2
-
 	parts := strings.SplitN(d.Id(), "-", 2)
 	if len(parts) != 2 {
 		return nil, errors.New("import format error: to import a api key use the format {project_id}-{api_key_id}")
 	}
 
-	projectID := parts[0]
+	// projectID is not needed for import any more, but kept to maintain import format and avoid breaking changes
 	apiKeyID := parts[1]
 
-	projectAPIKeys, _, err := connV2.ProgrammaticAPIKeysApi.ListProjectApiKeys(ctx, projectID).Execute()
-	if err != nil {
-		return nil, fmt.Errorf("couldn't import api key %s in project %s, error: %s", projectID, apiKeyID, err)
-	}
-	for _, val := range projectAPIKeys.GetResults() {
-		if val.GetId() == apiKeyID {
-			if err := d.Set("description", val.GetDesc()); err != nil {
-				return nil, fmt.Errorf("error setting `description`: %s", err)
-			}
-
-			if err := d.Set("public_key", val.GetPublicKey()); err != nil {
-				return nil, fmt.Errorf("error setting `public_key`: %s", err)
-			}
-
-			if projectAssignments, err := newProjectAssignment(ctx, connV2, apiKeyID); err == nil {
-				if err := d.Set("project_assignment", projectAssignments); err != nil {
-					return nil, fmt.Errorf("error setting  `project_assignment`: %s", err)
-				}
-			}
-
-			d.SetId(conversion.EncodeStateID(map[string]string{
-				"api_key_id": val.GetId(),
-			}))
-		}
-	}
+	d.SetId(conversion.EncodeStateID(map[string]string{
+		"api_key_id": apiKeyID,
+	}))
 	return []*schema.ResourceData{d}, nil
-}
-
-func getFirstProjectIDFromAssignments(d *schema.ResourceData) (*string, error) {
-	if projectAssignments, ok := d.GetOk("project_assignment"); ok {
-		projectAssignmentList := ExpandProjectAssignmentSet(projectAssignments.(*schema.Set))
-		if len(projectAssignmentList) < 1 {
-			return nil, errors.New(errorNoProjectAssignmentDefined)
-		}
-		return admin.PtrString(projectAssignmentList[0].ProjectID), nil // can safely assume at least one assigment is defined because of schema definition
-	}
-	return nil, errors.New(errorNoProjectAssignmentDefined)
-}
-
-func flattenProjectAPIKeyRoles(projectID string, apiKeyRoles []admin.CloudAccessRoleAssignment) []string {
-	if len(apiKeyRoles) == 0 {
-		return nil
-	}
-
-	flattenedOrgRoles := []string{}
-
-	for _, role := range apiKeyRoles {
-		if strings.HasPrefix(role.GetRoleName(), "GROUP_") && role.GetGroupId() == projectID {
-			flattenedOrgRoles = append(flattenedOrgRoles, role.GetRoleName())
-		}
-	}
-
-	return flattenedOrgRoles
-}
-
-func ExpandProjectAssignmentSet(projectAssignments *schema.Set) []*APIProjectAssignmentKeyInput {
-	res := make([]*APIProjectAssignmentKeyInput, projectAssignments.Len())
-
-	for i, value := range projectAssignments.List() {
-		v := value.(map[string]any)
-		res[i] = &APIProjectAssignmentKeyInput{
-			ProjectID: v["project_id"].(string),
-			RoleNames: conversion.ExpandStringList(v["role_names"].(*schema.Set).List()),
-		}
-	}
-
-	return res
-}
-
-func newProjectAssignment(ctx context.Context, connV2 *admin.APIClient, apiKeyID string) ([]map[string]any, error) {
-	apiKeyOrgList, _, err := connV2.RootApi.GetSystemStatus(ctx).Execute()
-	if err != nil {
-		return nil, fmt.Errorf("error getting api key information: %s", err)
-	}
-
-	projectAssignments, err := getAPIProjectAssignments(ctx, connV2, apiKeyOrgList, apiKeyID)
-	if err != nil {
-		return nil, fmt.Errorf("error getting api key information: %s", err)
-	}
-
-	var results []map[string]any
-	var atlasRoles []admin.CloudAccessRoleAssignment
-	if len(projectAssignments) > 0 {
-		results = make([]map[string]any, len(projectAssignments))
-		for k, apiKey := range projectAssignments {
-			for _, roleName := range apiKey.RoleNames {
-				atlasRole := admin.CloudAccessRoleAssignment{
-					GroupId:  &apiKey.ProjectID,
-					RoleName: &roleName,
-				}
-
-				atlasRoles = append(atlasRoles, atlasRole)
-			}
-			results[k] = map[string]any{
-				"project_id": apiKey.ProjectID,
-				"role_names": flattenProjectAPIKeyRoles(apiKey.ProjectID, atlasRoles),
-			}
-		}
-	}
-	return results, nil
-}
-
-func getStateProjectAssignmentAPIKeys(d *schema.ResourceData) (newAssignments, changedAssignments, removedAssignments []any) {
-	prevAssignments, currAssignments := d.GetChange("project_assignment")
-
-	rAssignments := prevAssignments.(*schema.Set).Difference(currAssignments.(*schema.Set))
-	nAssignments := currAssignments.(*schema.Set).Difference(prevAssignments.(*schema.Set))
-	changedAssignments = make([]any, 0)
-
-	for _, changed := range nAssignments.List() {
-		for _, removed := range rAssignments.List() {
-			if changed.(map[string]any)["project_id"] == removed.(map[string]any)["project_id"] {
-				rAssignments.Remove(removed)
-			}
-		}
-
-		for _, current := range prevAssignments.(*schema.Set).List() {
-			if changed.(map[string]any)["project_id"] == current.(map[string]any)["project_id"] {
-				changedAssignments = append(changedAssignments, changed.(map[string]any))
-				nAssignments.Remove(changed)
-			}
-		}
-	}
-
-	newAssignments = nAssignments.List()
-	removedAssignments = rAssignments.List()
-
-	return
-}
-
-func getAPIProjectAssignments(ctx context.Context, connV2 *admin.APIClient, apiKeyOrgList *admin.SystemStatus, apiKeyID string) ([]APIProjectAssignmentKeyInput, error) {
-	projectAssignments := []APIProjectAssignmentKeyInput{}
-	for idx, role := range apiKeyOrgList.ApiKey.GetRoles() {
-		if !strings.HasPrefix(*role.RoleName, "ORG_") {
-			continue
-		}
-		roles := apiKeyOrgList.ApiKey.GetRoles()
-		orgKeys, _, err := connV2.ProgrammaticAPIKeysApi.ListApiKeys(ctx, *roles[idx].OrgId).Execute()
-		if err != nil {
-			return nil, fmt.Errorf("error getting api key information: %s", err)
-		}
-		for _, val := range orgKeys.GetResults() {
-			if val.GetId() == apiKeyID {
-				for _, r := range val.GetRoles() {
-					temp := new(APIProjectAssignmentKeyInput)
-					if strings.HasPrefix(r.GetRoleName(), "GROUP_") {
-						temp.ProjectID = r.GetGroupId()
-						for _, l := range val.GetRoles() {
-							if l.GetGroupId() == temp.ProjectID {
-								temp.RoleNames = append(temp.RoleNames, l.GetRoleName())
-							}
-						}
-						projectAssignments = append(projectAssignments, *temp)
-					}
-				}
-			}
-		}
-	}
-	return projectAssignments, nil
 }
