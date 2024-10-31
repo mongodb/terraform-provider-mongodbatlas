cae0f37ac9285c7c5e4b88e4c87fd7f2330a70fe
Leo Antoli <430982+lantoli@users.noreply.github.com>
2024-10-31 16:38:18 +0100

    chore: Creates base resource schema for `advancedclustertpf` (#2691)
    * add data source tests
    
    * call conversion.UpdateSchemaDescription
    
    * remove Description using regexp in VsCode:  ^(?!.*MarkdownDescription:).*Description:.*\n
    
    * required attributes in data source test
    
    * use resource model
    
    * PoC model
    
    * codegen config for advanced_cluster
    
    * apply latest codegen
    
    * make codegen the main schema file
    
    * change HC model names
    
    * use schemas as in current resource
    
    * simplify data sources
    
    * mongo db version error comment
    
    * fix build issues
    
    * delete CustomType generated by HC tool
    
    * fix schema
    
    * adjust test
    
    * advanced_configuration

--- internal/service/advancedclustertpf/model.go
+++ internal/service/advancedclustertpf/model.go
@@ -7,8 +7,8 @@ import (
 	"go.mongodb.org/atlas-sdk/v20241023001/admin"
 )
 
-func NewTFModel(ctx context.Context, apiResp *admin.ClusterDescription20240805) (*TFModel, diag.Diagnostics) {
-	return &TFModel{}, nil
+func NewTFModel(ctx context.Context, apiResp *admin.ClusterDescription20240805, diags *diag.Diagnostics) *TFModel {
+	return &TFModel{}
 }
 
 func NewAtlasReq(ctx context.Context, plan *TFModel) (*admin.ClusterDescription20240805, diag.Diagnostics) {
