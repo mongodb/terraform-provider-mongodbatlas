8b599c15b2ee011316d810bc5fe41801614ddadf
Marco Suma <marco.suma@mongodb.com>
2024-11-07 18:10:16 +0100

    skips migration test due to newly added field

--- internal/service/cloudbackupsnapshotrestorejob/resource_cloud_backup_snapshot_restore_job_migration_test.go
+++ internal/service/cloudbackupsnapshotrestorejob/resource_cloud_backup_snapshot_restore_job_migration_test.go
@@ -7,6 +7,6 @@ import (
 )
 
 func TestMigCloudBackupSnapshotRestoreJob_basic(t *testing.T) {
-	mig.SkipIfVersionBelow(t, "1.16.1")
+	mig.SkipIfVersionBelow(t, "1.21.4") // this is when the new `failed` field was added
 	mig.CreateAndRunTest(t, basicTestCase(t))
 }
