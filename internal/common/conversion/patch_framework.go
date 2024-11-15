e78304b262bed887402b9c98d5c8e9317cd79e08
Espen Albert <espen.albert@mongodb.com>
2024-11-15 08:43:28 +0000

    feat: introduce PatchPayloadHasChangesTpf

---
+++ internal/common/conversion/patch_framework.go
@@ -0,0 +1,25 @@
+package conversion
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+)
+
+func PatchPayloadHasChangesTpf[T any, R any](ctx context.Context, diags *diag.Diagnostics, state, plan *T, converter func(ctx context.Context, input *T, diags *diag.Diagnostics) *R, reqPatch *R) bool {
+	stateReq := converter(ctx, state, diags)
+	planReq := converter(ctx, plan, diags)
+	if diags.HasError() {
+		return false
+	}
+	noChanges, err := PatchPayloadNoChanges(stateReq, planReq, reqPatch)
+	if err != nil {
+		diags.AddError(fmt.Sprintf("error creating patch payload %T", reqPatch), err.Error())
+		return false
+	}
+	if noChanges {
+		return false
+	}
+	return true
+}
