75ebc169c3f78a9deeb0c61633dfa77e5d19e132
Espen Albert <espen.albert@mongodb.com>
2024-11-20 21:35:33 +0000

    feat: support state transitions and pause logic

--- internal/service/advancedclustertpf/resource.go
+++ internal/service/advancedclustertpf/resource.go
@@ -3,6 +3,8 @@ package advancedclustertpf
 import (
 	"context"
 	"fmt"
+	"reflect"
+	"time"
 
 	"github.com/hashicorp/terraform-plugin-framework-timeouts/resource/timeouts"
 	"github.com/hashicorp/terraform-plugin-framework/diag"
@@ -22,22 +24,27 @@ var _ resource.ResourceWithImportState = &rs{}
 var _ resource.ResourceWithMoveState = &rs{}
 
 const (
-	errorCreate                    = "error creating advanced cluster: %s"
-	errorRead                      = "error reading  advanced cluster (%s): %s"
-	errorDelete                    = "error deleting advanced cluster (%s): %s"
-	errorUpdate                    = "error updating advanced cluster (%s): %s"
-	errorConfigUpdate              = "error updating advanced cluster configuration options (%s): %s"
-	errorConfigRead                = "error reading advanced cluster configuration options (%s): %s"
-	ErrorClusterSetting            = "error setting `%s` for MongoDB Cluster (%s): %s"
-	ErrorAdvancedConfRead          = "error reading Advanced Configuration Option form MongoDB Cluster (%s): %s"
-	ErrorClusterAdvancedSetting    = "error setting `%s` for MongoDB ClusterAdvanced (%s): %s"
-	ErrorAdvancedClusterListStatus = "error awaiting MongoDB ClusterAdvanced List IDLE: %s"
-	ErrorOperationNotPermitted     = "error operation not permitted"
-	ignoreLabel                    = "Infrastructure Tool"
-	DeprecationOldSchemaAction     = "Please refer to our examples, documentation, and 1.18.0 migration guide for more details at https://registry.terraform.io/providers/mongodb/mongodbatlas/latest/docs/guides/1.18.0-upgrade-guide.html.markdown"
+	errorCreate                                  = "error creating advanced cluster: %s"
+	errorRead                                    = "error reading  advanced cluster (%s): %s"
+	errorDelete                                  = "error deleting advanced cluster (%s): %s"
+	errorUpdate                                  = "error updating advanced cluster (%s): %s"
+	errorConfigUpdate                            = "error updating advanced cluster configuration options (%s): %s"
+	errorConfigRead                              = "error reading advanced cluster configuration options (%s): %s"
+	ErrorClusterSetting                          = "error setting `%s` for MongoDB Cluster (%s): %s"
+	ErrorAdvancedConfRead                        = "error reading Advanced Configuration Option form MongoDB Cluster (%s): %s"
+	ErrorClusterAdvancedSetting                  = "error setting `%s` for MongoDB ClusterAdvanced (%s): %s"
+	ErrorAdvancedClusterListStatus               = "error awaiting MongoDB ClusterAdvanced List IDLE: %s"
+	ErrorOperationNotPermitted                   = "error operation not permitted"
+	ignoreLabel                                  = "Infrastructure Tool"
+	DeprecationOldSchemaAction                   = "Please refer to our examples, documentation, and 1.18.0 migration guide for more details at https://registry.terraform.io/providers/mongodb/mongodbatlas/latest/docs/guides/1.18.0-upgrade-guide.html.markdown"
+	defaultTimeout                 time.Duration = 30 * time.Second // TODO: Update
 )
 
-var DeprecationMsgOldSchema = fmt.Sprintf("%s %s", constant.DeprecationParam, DeprecationOldSchemaAction)
+var (
+	DeprecationMsgOldSchema = fmt.Sprintf("%s %s", constant.DeprecationParam, DeprecationOldSchemaAction)
+	pauseRequest            = admin20240805.ClusterDescription20240805{Paused: conversion.Pointer(true)}
+	resumeRequest           = admin20240805.ClusterDescription20240805{Paused: conversion.Pointer(false)}
+)
 
 func Resource() resource.Resource {
 	return &rs{
@@ -78,10 +85,15 @@ func (r *rs) createCluster(ctx context.Context, plan *TFModel, diags *diag.Diagn
 	apiLegacy := r.Client.AtlasV220240530.ClustersApi
 	projectID := plan.ProjectID.ValueString()
 	clusterName := plan.Name.ValueString()
-	cluster, _, err := api.CreateCluster(ctx, projectID, sdkReq).Execute()
+	_, _, err := api.CreateCluster(ctx, projectID, sdkReq).Execute()
 	if err != nil {
 		return "errorCreate", fmt.Sprintf(errorCreate, err.Error())
 	}
+	cluster, summary, detail := r.awaitChanges(ctx, &plan.Timeouts, diags, projectID, clusterName, "create")
+	if summary != "" || detail != "" {
+		return summary, detail
+	}
+
 	var legacyAdvConfig *admin20240530.ClusterDescriptionProcessArgs
 	legacyAdvConfigUpdate := NewAtlasReqAdvancedConfigurationLegacy(ctx, &plan.AdvancedConfiguration, diags)
 	if legacyAdvConfigUpdate != nil {
@@ -102,6 +114,42 @@ func (r *rs) createCluster(ctx context.Context, plan *TFModel, diags *diag.Diagn
 		}
 	}
 	return r.convertClusterAddAdvConfig(ctx, legacyAdvConfig, advConfig, cluster, plan.Timeouts, diags, &resp.State)
+}
+
+func (r *rs) awaitChanges(ctx context.Context, t *timeouts.Value, diags *diag.Diagnostics, projectID, clusterName, changeReason string) (cluster *admin20240805.ClusterDescription20240805, summary, detail string) {
+	var timeoutDuration time.Duration
+	var localDiags diag.Diagnostics
+	var targetState = "IDLE"
+	switch changeReason {
+	case "create":
+		timeoutDuration, localDiags = t.Create(ctx, defaultTimeout)
+		diags.Append(localDiags...)
+	case "update":
+		timeoutDuration, localDiags = t.Update(ctx, defaultTimeout)
+		diags.Append(localDiags...)
+	case "delete":
+		timeoutDuration, localDiags = t.Delete(ctx, defaultTimeout)
+		diags.Append(localDiags...)
+		targetState = "DELETED"
+	default:
+		return nil, "errorReadingTimeout", "unknown change reason" + changeReason
+	}
+	if diags.HasError() {
+		return nil, "errorReadingTimeout", ""
+	}
+	stateConf := CreateStateChangeConfig(ctx, r.Client.AtlasV220240805, projectID, clusterName, targetState, timeoutDuration)
+	clusterAny, err := stateConf.WaitForStateContext(ctx)
+	if err != nil {
+		return nil, "errorAwaitingCluster", fmt.Sprintf(errorCreate, err)
+	}
+	if targetState == "DELETED" {
+		return nil, "", ""
+	}
+	cluster, ok := clusterAny.(*admin20240805.ClusterDescription20240805)
+	if !ok {
+		return nil, "errorAwaitingCluster", fmt.Sprintf(errorCreate, "unexpected type from WaitForStateContext")
+	}
+	return cluster, "", ""
 }
 
 func (r *rs) convertClusterAddAdvConfig(ctx context.Context, legacyAdvConfig *admin20240530.ClusterDescriptionProcessArgs, advConfig *admin20240805.ClusterDescriptionProcessArgs20240805, cluster *admin20240805.ClusterDescription20240805, resourceTimeouts timeouts.Value, diags *diag.Diagnostics, state *tfsdk.State) (summary, detail string) {
@@ -127,9 +175,10 @@ func (r *rs) convertClusterAddAdvConfig(ctx context.Context, legacyAdvConfig *ad
 		return "", ""
 	}
 	AddAdvancedConfig(ctx, model, advConfig, legacyAdvConfig, diags)
-	if !diags.HasError() {
-		diags.Append(state.Set(ctx, model)...)
+	if diags.HasError() {
+		return "", ""
 	}
+	diags.Append(state.Set(ctx, model)...)
 	return "", ""
 }
 
@@ -174,28 +223,72 @@ func (r *rs) Update(ctx context.Context, req resource.UpdateRequest, resp *resou
 	patchReq := update.PatchPayloadTpf(ctx, diags, &state, &plan, NewAtlasReq)
 	projectID := plan.ProjectID.ValueString()
 	clusterName := plan.Name.ValueString()
+	var cluster *admin20240805.ClusterDescription20240805
+	var advConfig *admin20240805.ClusterDescriptionProcessArgs20240805
+	var legacyAdvConfig *admin20240530.ClusterDescriptionProcessArgs
+	var err error
 	if patchReq != nil {
-		_, _, err := api.UpdateCluster(ctx, projectID, clusterName, patchReq).Execute()
-		if err != nil {
-			diags.AddError("errorUpdate", fmt.Sprintf(errorUpdate, clusterName, err.Error()))
+		pauseAfter := false
+		if patchReq.Paused != nil && patchReq.GetPaused() {
+			// More changes than pause, need to pause after
+			if !reflect.DeepEqual(pauseRequest, *patchReq) {
+				pauseAfter = true
+				patchReq.Paused = nil
+			}
+		} else if patchReq.Paused != nil && !patchReq.GetPaused() {
+			// More changes than pause, need to resume before applying changes
+			if !reflect.DeepEqual(resumeRequest, *patchReq) {
+				patchReq.Paused = nil
+				cluster = r.updateAndWait(ctx, &resumeRequest, diags, &plan)
+				if cluster == nil {
+					return
+				}
+			}
+		}
+		cluster = r.updateAndWait(ctx, patchReq, diags, &plan)
+		if cluster == nil {
 			return
+		}
+		if pauseAfter {
+			cluster = r.updateAndWait(ctx, &pauseRequest, diags, &plan)
 		}
 	}
 	patchReqProcessArgs := update.PatchPayloadTpf(ctx, diags, &state.AdvancedConfiguration, &plan.AdvancedConfiguration, NewAtlasReqAdvancedConfiguration)
 	if patchReqProcessArgs != nil {
-		_, _, err := api.UpdateClusterAdvancedConfiguration(ctx, projectID, clusterName, patchReqProcessArgs).Execute()
+		advConfig, _, err = api.UpdateClusterAdvancedConfiguration(ctx, projectID, clusterName, patchReqProcessArgs).Execute()
 		if err != nil {
 			diags.AddError("errorUpdateAdvancedConfig", fmt.Sprintf(errorConfigUpdate, clusterName, err.Error()))
 		}
 	}
 	patchReqProcessArgsLegacy := update.PatchPayloadTpf(ctx, diags, &state.AdvancedConfiguration, &plan.AdvancedConfiguration, NewAtlasReqAdvancedConfigurationLegacy)
 	if patchReqProcessArgsLegacy != nil {
-		_, _, err := r.Client.AtlasV220240530.ClustersApi.UpdateClusterAdvancedConfiguration(ctx, projectID, clusterName, patchReqProcessArgsLegacy).Execute()
+		legacyAdvConfig, _, err = r.Client.AtlasV220240530.ClustersApi.UpdateClusterAdvancedConfiguration(ctx, projectID, clusterName, patchReqProcessArgsLegacy).Execute()
 		if err != nil {
 			diags.AddError("errorUpdateAdvancedConfigLegacy", fmt.Sprintf(errorConfigUpdate, clusterName, err.Error()))
 		}
 	}
-	r.readCluster(ctx, &plan, &resp.State, diags)
+	if cluster != nil {
+		r.convertClusterAddAdvConfig(ctx, legacyAdvConfig, advConfig, cluster, plan.Timeouts, diags, &resp.State)
+	} else {
+		r.readCluster(ctx, &plan, &resp.State, diags)
+	}
+}
+
+func (r *rs) updateAndWait(ctx context.Context, patchReq *admin20240805.ClusterDescription20240805, diags *diag.Diagnostics, tfModel *TFModel) *admin20240805.ClusterDescription20240805 {
+	api := r.Client.AtlasV220240805.ClustersApi
+	projectID := tfModel.ProjectID.ValueString()
+	clusterName := tfModel.Name.ValueString()
+	_, _, err := api.UpdateCluster(ctx, projectID, clusterName, patchReq).Execute()
+	if err != nil {
+		diags.AddError("errorUpdate", fmt.Sprintf(errorUpdate, clusterName, err.Error()))
+		return nil
+	}
+	cluster, summary, detail := r.awaitChanges(ctx, &tfModel.Timeouts, diags, projectID, clusterName, "update")
+	if summary != "" || detail != "" {
+		diags.AddError(summary, detail)
+		return nil
+	}
+	return cluster
 }
 
 func (r *rs) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
@@ -205,16 +298,20 @@ func (r *rs) Delete(ctx context.Context, req resource.DeleteRequest, resp *resou
 	if diags.HasError() {
 		return
 	}
-	name := state.Name.ValueString()
 	clusterName := state.Name.ValueString()
+	projectID := state.ProjectID.ValueString()
 	if clusterName == "" {
-		resp.Diagnostics.AddError("errorDelete", fmt.Sprintf(errorDelete, name, "clusterName is empty"))
+		resp.Diagnostics.AddError("errorDelete", fmt.Sprintf(errorDelete, clusterName, "clusterName is empty"))
 		return
 	}
 	api := r.Client.AtlasV2.ClustersApi
-	_, err := api.DeleteCluster(ctx, state.ProjectID.ValueString(), name).Execute()
+	_, err := api.DeleteCluster(ctx, projectID, clusterName).Execute()
 	if err != nil {
-		diags.AddError("errorDelete", fmt.Sprintf(errorDelete, name, err.Error()))
+		diags.AddError("errorDelete", fmt.Sprintf(errorDelete, clusterName, err.Error()))
+	}
+	_, summary, detail := r.awaitChanges(ctx, &state.Timeouts, diags, projectID, clusterName, "delete")
+	if summary != "" || detail != "" {
+		diags.AddError(summary, detail)
 	}
 }
 
---
+++ internal/service/advancedclustertpf/state_transition.go
@@ -0,0 +1,54 @@
+package advancedclustertpf
+
+import (
+	"context"
+	"strings"
+	"time"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
+	admin20240805 "go.mongodb.org/atlas-sdk/v20240805005/admin"
+)
+
+var (
+	RetryMinTimeout   = 1 * time.Minute
+	RetryDelay        = 30 * time.Second
+	RetryPollInterval = 30 * time.Second
+)
+
+func CreateStateChangeConfig(ctx context.Context, connV2 *admin20240805.APIClient, projectID, name, targetState string, timeout time.Duration) retry.StateChangeConf {
+	return retry.StateChangeConf{
+		Pending:      []string{"CREATING", "UPDATING", "REPAIRING", "REPEATING", "PENDING", "DELETING"},
+		Target:       []string{targetState},
+		Refresh:      resourceRefreshFunc(ctx, name, projectID, connV2),
+		Timeout:      timeout,
+		MinTimeout:   RetryMinTimeout,
+		Delay:        RetryDelay,
+		PollInterval: RetryPollInterval,
+	}
+}
+
+func resourceRefreshFunc(ctx context.Context, name, projectID string, connV2 *admin20240805.APIClient) retry.StateRefreshFunc {
+	return func() (any, string, error) {
+		cluster, resp, err := connV2.ClustersApi.GetCluster(ctx, projectID, name).Execute()
+		if err != nil && strings.Contains(err.Error(), "reset by peer") {
+			return nil, "REPEATING", nil
+		}
+
+		if err != nil && cluster == nil && resp == nil {
+			return nil, "", err
+		}
+
+		if err != nil {
+			if resp.StatusCode == 404 {
+				return "", "DELETED", nil
+			}
+			if resp.StatusCode == 503 {
+				return "", "PENDING", nil
+			}
+			return nil, "", err
+		}
+
+		state := cluster.GetStateName()
+		return cluster, state, nil
+	}
+}
