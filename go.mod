7b6f6e85555ef70c424d698c04c6b94fd61a1222
Espen Albert <EspenAlbert@users.noreply.github.com>
2024-11-29 15:15:48 +0000

    test: Adds support for mocked-acceptance-tests (#2820)
    * initial create operation
    
    * initial resource implementation
    
    * support returning nil when advanced config fields are null
    
    * test: Add support for overriding http round tripper to allow mocking http request/responses
    
    * minor error renaming
    
    * remove mocked responses
    
    * test: initial TestAdvancedCluster_configSharded requests
    
    * chore: use same admin version as existing cluster
    
    * mock: allow re-reading GETs
    
    * test: support raising error when mock variables are not set
    
    * fix: don't dump GroupID in cluster payloads
    
    * test: update test case to match existing
    
    * refactor: use full request.id() to keep aligned with generator format (differentiate requests with the distinct payloads)
    
    * fix: add `computed` to adv_cluster attributes to avoid provider errors
    
    * test: minor mock fixes
    
    * feat: support state transitions and pause logic
    
    * chore: test data updates
    
    * test: Support overriding diff files by using `_manual` suffix
    
    * style: fmt
    
    * test: delete old mock files
    
    * test: rename basic to replicast
    
    * fix: Deleted should support IDLE --> DELETED
    
    * fix: cluster name update requires replace
    
    * test: Add tenant test case
    
    * chore: include provider changes
    
    * fix(move_state): Use ValueString() and do a proper conversion
    
    * test: Support mocking test case
    
    * refactor: use new `unit.MockTestCase` to simplify resource_test.go
    
    * test: Introduce "live" acceptance test for tpf
    
    * refactor: re-use existing retry strategy states
    
    * refactor: introduce constant.error_codes
    
    * refactor: minor cleanups
    
    * test: Refactor MockTestCase to also run and support setting TF_ACC dynamically
    
    * chore: increase unit test timeout to enable tpf unit tests
    
    * minor improvements
    
    * unit test timeout increase. Used 75s locally
    
    * ci: support running mocked acceptance tests as part of advanced_cluster_tpf job
    
    * ci: as of now, advancedclustertpf unit tests cannot run as part of normal unit test since the resource is not added in provider
    
    * test: Skip broken move_state_tests
    
    * minor fix
    
    * chore: fix imports to latest SDK
    
    * estetic change to workflow file
    
    * refactor: Use provider_mock instead of customizing existing provider (revert all changes to client.go and provider.go
    
    * address PR comments
    
    * use `diags` instead of returning summary, detail
    
    * only removeResource in read
    
    * address immediate PR comments
    
    * refactor: Use a higher abstraction level in update
    
    * refactor: Return TFModel to make purpose more explicit
    
    * refactor: Replace awaitChanges method with AwaitChanges function for improved clarity and consistency
    
    * apply suggestion
    
    * chore: Add step count assertion
    
    * refactor: move out all the unit tests and mocked unit test logic
    
    * remove: accidental provider changes
    
    * remove last changes for unit tests
    
    * test: Add support for mocked acceptance tests
    
    * test: Support maintaining response order with responseIndex
    
    * test: update test data for adv cluster unit tests
    
    * test: mocker use colored diffs and support empty payloads in diffs
    
    * update mocker test data
    
    * test: Add check for AllowReReading
    
    * test: Add labels and tags to mock cluster JSON test data

--- go.mod
+++ go.mod
@@ -20,6 +20,7 @@ require (
 	github.com/hashicorp/terraform-plugin-sdk/v2 v2.35.0
 	github.com/hashicorp/terraform-plugin-testing v1.11.0
 	github.com/huandu/xstrings v1.5.0
+	github.com/jarcoal/httpmock v1.3.1
 	github.com/mongodb-forks/digest v1.1.0
 	github.com/mongodb/atlas-sdk-go v1.0.1-0.20241125083853-b62d35214341
 	github.com/pb33f/libopenapi v0.18.7
---
+++ internal/testutil/unit/http_mocker_test.go
@@ -0,0 +1,161 @@
+package unit_test
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"strings"
+	"testing"
+
+	"github.com/mongodb/terraform-provider-mongodbatlas/internal/testutil/unit"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"go.mongodb.org/atlas-sdk/v20241113001/admin"
+)
+
+func TestExtractVersion(t *testing.T) {
+	version, err := unit.ExtractVersion("application/json; version=2022-06-01")
+	require.NoError(t, err)
+	require.Equal(t, "2022-06-01", version)
+}
+func TestRequestInfo_Match(t *testing.T) {
+	req := unit.RequestInfo{
+		Version: "2022-06-01",
+		Method:  "GET",
+		Path:    "/v1/cluster/{cluster_id}",
+	}
+	assert.True(t, req.Match("GET", "/v1/cluster/123", "2022-06-01", map[string]string{"cluster_id": "123"}))
+	assert.False(t, req.Match("GET", "/v1/cluster/123", "2022-06-01", map[string]string{"cluster_id": "456"}))
+}
+
+func request(method, path string) *http.Request {
+	return &http.Request{
+		Method: method,
+		URL:    &url.URL{Path: path},
+		Header: http.Header{
+			"Accept": []string{"application/json; version=2024-08-05"},
+		},
+	}
+}
+
+const reqPoliciesCreateBody = `{
+ "name": "test-policy",
+ "policies": [
+  {
+   "body": "\t\t\t\n\tforbid (\n\tprincipal,\n\taction == cloud::Action::\"cluster.createEdit\",\n\tresource\n\t) when {\n\tcontext.cluster.cloudProviders.containsAny([cloud::cloudProvider::\"aws\"])\n\t};\n"
+  }
+ ]
+}`
+const reqPoliciesUpdateBody = `{
+ "name": "updated-policy",
+ "policies": [
+  {
+   "body": "\t\t\t\n\tforbid (\n\tprincipal,\n\taction == cloud::Action::\"cluster.createEdit\",\n\tresource\n\t) when {\n\tcontext.cluster.cloudProviders.containsAny([cloud::cloudProvider::\"aws\"])\n\t};\n"
+  }
+ ]
+}`
+
+const reqPoliciesManualValidateDelete = `{}`
+
+func TestMockRoundTripper(t *testing.T) {
+	orgID := "123"
+	resourcePolicyID := "456"
+	vars := map[string]string{
+		"orgId":            orgID,
+		"resourcePolicyId": resourcePolicyID,
+	}
+	mockTransport, checkFunc := unit.MockRoundTripper(t, vars, &unit.MockHTTPDataConfig{AllowMissingRequests: true})
+	client := &http.Client{
+		Transport: mockTransport,
+	}
+	// Error check
+	unknownRequest := request("GET", "/v1/cluster/123")
+	resp, err := client.Do(unknownRequest)
+	require.ErrorContains(t, err, "no matching request found")
+	assert.Nil(t, resp)
+
+	// Step 1
+	createRequest := request("POST", fmt.Sprintf("/api/atlas/v2/orgs/%s/resourcePolicies", orgID))
+	createRequest.Body = io.NopCloser(strings.NewReader(reqPoliciesCreateBody))
+	resp, err = client.Do(createRequest)
+
+	require.NoError(t, err)
+	require.Equal(t, 201, resp.StatusCode)
+	err = checkFunc(nil)
+	require.NoError(t, err)
+	// Step 2
+	patchRequest := request("PATCH", fmt.Sprintf("/api/atlas/v2/orgs/%s/resourcePolicies/%s", orgID, resourcePolicyID))
+	patchRequest.Body = io.NopCloser(strings.NewReader(reqPoliciesUpdateBody))
+	resp, err = client.Do(patchRequest)
+	require.NoError(t, err)
+	err = checkFunc(nil)
+	require.NoError(t, err)
+	var policyResp admin.ApiAtlasResourcePolicy
+	err = json.NewDecoder(resp.Body).Decode(&policyResp)
+	require.NoError(t, err)
+	assert.Equal(t, resourcePolicyID, policyResp.GetId())
+
+	// Step 3
+	// First GET request OK
+	// Second GET request OK
+	getRequest := request("GET", fmt.Sprintf("/api/atlas/v2/orgs/%s/resourcePolicies/%s", orgID, resourcePolicyID))
+	_, err = client.Do(getRequest)
+	require.NoError(t, err)
+	_, err = client.Do(getRequest)
+	require.NoError(t, err)
+	// Third GET request FAIL with no match as there are no more responses until after DELETE
+	_, err = client.Do(getRequest)
+	require.ErrorContains(t, err, "no matching request found")
+
+	// Test _manual diff file (set to {} instead of '')
+	validateRequest := request("DELETE", fmt.Sprintf("/api/atlas/v2/orgs/%s/resourcePolicies/%s", orgID, resourcePolicyID))
+	validateRequest.Body = io.NopCloser(strings.NewReader(reqPoliciesManualValidateDelete))
+	_, err = client.Do(validateRequest)
+	require.NoError(t, err)
+	// Fourth GET request OK, since we have gotten the diff
+	notFoundResp, err := client.Do(getRequest)
+	require.NoError(t, err)
+	notFoundMap := parseMapStringAny(t, notFoundResp)
+	assert.Equal(t, "RESOURCE_POLICY_NOT_FOUND", notFoundMap["errorCode"])
+
+	err = checkFunc(nil)
+	require.NoError(t, err)
+}
+
+func parseMapStringAny(t *testing.T, resp *http.Response) map[string]any {
+	t.Helper()
+	stringMap := map[string]any{}
+	err := json.NewDecoder(resp.Body).Decode(&stringMap)
+	require.NoError(t, err)
+	return stringMap
+}
+
+func TestMockRoundTripperAllowReRead(t *testing.T) {
+	orgID := "123"
+	resourcePolicyID := "456"
+	vars := map[string]string{
+		"orgId":            orgID,
+		"resourcePolicyId": resourcePolicyID,
+	}
+	mockTransport, checkFunc := unit.MockRoundTripper(t, vars, &unit.MockHTTPDataConfig{AllowReReadGet: true, AllowMissingRequests: true})
+	client := &http.Client{
+		Transport: mockTransport,
+	}
+	for range []int{0, 1, 2} {
+		getRequest := request("GET", fmt.Sprintf("/api/atlas/v2/orgs/%s/resourcePolicies", orgID))
+		resp, err := client.Do(getRequest)
+		require.NoError(t, err)
+		assert.Equal(t, "returned again", parseMapStringAny(t, resp)["expect"])
+	}
+	createRequest := request("POST", fmt.Sprintf("/api/atlas/v2/orgs/%s/resourcePolicies", orgID))
+	createRequest.Body = io.NopCloser(strings.NewReader(reqPoliciesCreateBody))
+	resp, err := client.Do(createRequest)
+
+	require.NoError(t, err)
+	require.Equal(t, 201, resp.StatusCode)
+	err = checkFunc(nil)
+	require.NoError(t, err)
+}
