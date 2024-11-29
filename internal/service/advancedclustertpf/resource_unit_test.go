13d1b52ff79691b3d3169d1958582bffeea54b3a
Espen Albert <espen.albert@mongodb.com>
2024-11-29 15:20:57 +0000

    address some PR comments

--- internal/service/advancedclustertpf/resource_unit_test.go
+++ internal/service/advancedclustertpf/resource_unit_test.go
@@ -24,44 +24,44 @@ func TestMockAdvancedCluster_replicaset(t *testing.T) {
 	var (
 		oneNewVariable = "backup_enabled = false"
 		fullUpdate     = `
-		backup_enabled = true
-		bi_connector_config = {
-			enabled = true
-		}
-		# config_server_management_mode = "ATLAS_MANAGED" UNSTABLE: After applying this test step, the non-refresh plan was not empty
-		labels = [{
-			key   = "env"
-			value = "test"
-		}]
-		tags = [{
-			key   = "env"
-			value = "test"
-		}]
-		mongo_db_major_version = "8.0"
-		paused = true
-		pit_enabled = true
-		redact_client_log_data = true
-		replica_set_scaling_strategy = "NODE_TYPE"
-		# retain_backups_enabled = true # only set on delete
-		root_cert_type = "ISRGROOTX1"
-		# termination_protection_enabled = true # must be reset to false to enable delete
-		version_release_system = "CONTINUOUS"
+	backup_enabled = true
+	bi_connector_config = {
+		enabled = true
+	}
+	# config_server_management_mode = "ATLAS_MANAGED" UNSTABLE: After applying this test step, the non-refresh plan was not empty
+	labels = [{
+		key   = "env"
+		value = "test"
+	}]
+	tags = [{
+		key   = "env"
+		value = "test"
+	}]
+	mongo_db_major_version = "8.0"
+	paused = true
+	pit_enabled = true
+	redact_client_log_data = true
+	replica_set_scaling_strategy = "NODE_TYPE"
+	# retain_backups_enabled = true # only set on delete
+	root_cert_type = "ISRGROOTX1"
+	# termination_protection_enabled = true # must be reset to false to enable delete
+	version_release_system = "CONTINUOUS"
 		`
 		// # oplog_min_retention_hours                                      = 5.5
 		// # oplog_size_mb                                                  = 1000
 		// # fail_index_key_too_long 								        = true # only valid for MongoDB version 4.4 and earlier
 		advClusterConfig = `
-		advanced_configuration = {
-			change_stream_options_pre_and_post_images_expire_after_seconds = 100
-			default_read_concern                                           = "available"
-			default_write_concern                                          = "majority"
-			javascript_enabled                                             = false
-			minimum_enabled_tls_protocol                                   = "TLS1_0"
-			no_table_scan                                                  = true
-			sample_refresh_interval_bi_connector                           = 310
-			sample_size_bi_connector                                       = 110
-			transaction_lifetime_limit_seconds                             = 300
-		}
+	advanced_configuration = {
+		change_stream_options_pre_and_post_images_expire_after_seconds = 100
+		default_read_concern                                           = "available"
+		default_write_concern                                          = "majority"
+		javascript_enabled                                             = false
+		minimum_enabled_tls_protocol                                   = "TLS1_0"
+		no_table_scan                                                  = true
+		sample_refresh_interval_bi_connector                           = 310
+		sample_size_bi_connector                                       = 110
+		transaction_lifetime_limit_seconds                             = 300
+	}
 		`
 		fullUpdateResumed = strings.Replace(fullUpdate, "paused = true", "paused = false", 1)
 		vars              = map[string]string{
@@ -131,7 +131,6 @@ func shortenRetries() {
 
 func TestMockAdvancedCluster_configSharded(t *testing.T) {
 	var (
-		projectID   = "111111111111111111111111"
 		clusterName = "sharded-multi-replication"
 		vars        = map[string]string{
 			"groupId":     projectID,
@@ -183,45 +182,43 @@ func configSharded(projectID, clusterName string, withUpdate bool) string {
 	// The API raises no errors, but the response reflects this rule
 	analyticsSpecsForSpec2 := strings.ReplaceAll(analyticsSpecs, "2000", "1000")
 	return fmt.Sprintf(`
-		resource "mongodbatlas_advanced_cluster" "test" {
-			project_id   = %[1]q
-			name         = %[2]q
-			cluster_type = "SHARDED"
+	resource "mongodbatlas_advanced_cluster" "test" {
+		project_id   = %[1]q
+		name         = %[2]q
+		cluster_type = "SHARDED"
 
-			replication_specs = [
-				{ # shard 1
-				region_configs = [{
-					electable_specs = {
-						instance_size   = "M30"
-						disk_iops       = 2000
-						node_count      = 3
-						ebs_volume_type = "PROVISIONED"
-					}
-					%[3]s
-					%[4]s
-					provider_name = "AWS"
-					priority      = 7
-					region_name   = "EU_WEST_1"
-					}]
-					},
-					{ # shard 2
-				region_configs = [{
-					electable_specs = {
-						instance_size   = "M30"
-						ebs_volume_type = "PROVISIONED"
-						disk_iops       = 1000
-						node_count      = 3
-					}
-					%[3]s
-					%[5]s
-					provider_name = "AWS"
-					priority      = 7
-					region_name   = "EU_WEST_1"
+		replication_specs = [
+			{ # shard 1
+			region_configs = [{
+				electable_specs = {
+					instance_size   = "M30"
+					disk_iops       = 2000
+					node_count      = 3
+					ebs_volume_type = "PROVISIONED"
+				}
+				%[3]s
+				%[4]s
+				provider_name = "AWS"
+				priority      = 7
+				region_name   = "EU_WEST_1"
 				}]
+				},
+				{ # shard 2
+			region_configs = [{
+				electable_specs = {
+					instance_size   = "M30"
+					ebs_volume_type = "PROVISIONED"
+					disk_iops       = 1000
+					node_count      = 3
+				}
+				%[3]s
+				%[5]s
+				provider_name = "AWS"
+				priority      = 7
+				region_name   = "EU_WEST_1"
 			}]
-			}
-		
-
+		}]
+	}
 	`, projectID, clusterName, autoScaling, analyticsSpecs, analyticsSpecsForSpec2)
 }
 
