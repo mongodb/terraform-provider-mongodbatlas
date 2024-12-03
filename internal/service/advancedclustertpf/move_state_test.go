f4c1c729dbf15fa22c7d9d27157ab761fc1a925a
Leo Antoli <430982+lantoli@users.noreply.github.com>
2024-12-03 13:37:21 +0100

    fix TestAccAdvancedCluster_move_invalid

--- internal/service/advancedclustertpf/move_state_test.go
+++ internal/service/advancedclustertpf/move_state_test.go
@@ -42,14 +42,14 @@ func TestAccAdvancedCluster_move_invalid(t *testing.T) {
 		CheckDestroy:             acc.CheckDestroyCluster,
 		Steps: []resource.TestStep{
 			{
-				Config: configMoveFirst(projectID, clusterName),
+				Config: configMoveFirstInvalid(projectID, clusterName),
 			},
 			{
-				Config:      configMoveSecond(projectID, clusterName),
+				Config:      configMoveSecondInvalid(projectID, clusterName),
 				ExpectError: regexp.MustCompile("Unable to Move Resource State"),
 			},
 			{
-				Config: configMoveFirst(projectID, clusterName),
+				Config: configMoveFirstInvalid(projectID, clusterName),
 			},
 		},
 	})
@@ -76,10 +76,34 @@ func configMoveFirst(projectID, clusterName string) string {
 	`, projectID, clusterName)
 }
 
+func configMoveFirstInvalid(projectID, clusterName string) string {
+	return fmt.Sprintf(`
+		resource "mongodbatlas_database_user" "old" {
+			project_id         = %[1]q
+			username           = %[2]q # use cluster name as username
+			password           = "test-acc-password"
+			auth_database_name = "admin"
+			roles {
+				role_name     = "atlasAdmin"
+				database_name = "admin"
+			}
+		}
+	`, projectID, clusterName)
+}
+
 func configMoveSecond(projectID, clusterName string) string {
 	return `
 		moved {
 			from = mongodbatlas_cluster.old
+			to   = mongodbatlas_advanced_cluster.test
+		}
+	` + configBasic(projectID, clusterName, "")
+}
+
+func configMoveSecondInvalid(projectID, clusterName string) string {
+	return `
+		moved {
+			from = mongodbatlas_database_user.old
 			to   = mongodbatlas_advanced_cluster.test
 		}
 	` + configBasic(projectID, clusterName, "")
