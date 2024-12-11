ff66a8d96f10f44812684c81e43b81a4157fc55a
Espen Albert <espen.albert@mongodb.com>
2024-12-11 07:36:52 +0000

    test: Add SkipInReplayMode helper to skip tests when in replay mode

--- internal/service/advancedclustertpf/move_state_test.go
+++ internal/service/advancedclustertpf/move_state_test.go
@@ -8,9 +8,11 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-plugin-testing/tfversion"
 	"github.com/mongodb/terraform-provider-mongodbatlas/internal/testutil/acc"
+	"github.com/mongodb/terraform-provider-mongodbatlas/internal/testutil/unit"
 )
 
 func TestAccAdvancedCluster_moveBasic(t *testing.T) {
+	unit.SkipInReplayMode(t)
 	var (
 		projectID   = acc.ProjectIDExecution(t)
 		clusterName = acc.RandomClusterName()
@@ -37,6 +39,7 @@ func TestAccAdvancedCluster_moveBasic(t *testing.T) {
 }
 
 func TestAccAdvancedCluster_moveInvalid(t *testing.T) {
+	unit.SkipInReplayMode(t)
 	var (
 		projectID   = acc.ProjectIDExecution(t)
 		clusterName = acc.RandomClusterName()
