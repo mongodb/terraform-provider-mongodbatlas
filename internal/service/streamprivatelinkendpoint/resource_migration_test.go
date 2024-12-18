ac2b4851be9aa9d21840e1aeaee27471e45e7fd4
Oriol Arbusi <oriol.abadal@mongodb.com>
2024-12-18 12:18:26 +0100

    adjust migration test to use confluent external provider

--- internal/service/streamprivatelinkendpoint/resource_migration_test.go
+++ internal/service/streamprivatelinkendpoint/resource_migration_test.go
@@ -8,5 +8,5 @@ import (
 
 func TestMigStreamPrivatelinkEndpoint_basic(t *testing.T) {
 	mig.SkipIfVersionBelow(t, "1.25.0") // when resource 1st released
-	mig.CreateAndRunTest(t, basicTestCase(t))
+	mig.CreateTestAndRunUseExternalProviderNonParallel(t, basicTestCase(t), mig.ExternalProvidersWithConfluent(), nil)
 }
