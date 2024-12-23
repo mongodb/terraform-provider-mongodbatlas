36d3d13e005eec02df1ba1bd3d2cbbb0b258e4f6
Oriol Arbusi <oriol.abadal@mongodb.com>
2024-12-23 16:55:56 +0100

    version constraint

--- examples/mongodbatlas_stream_privatelink_endpoint/confluent_serverless/versions.tf
+++ examples/mongodbatlas_stream_privatelink_endpoint/confluent_serverless/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     mongodbatlas = {
       source  = "mongodb/mongodbatlas"
-      version = "1.24.0"
+      version = "~> 1.24"
     }
     confluent = {
       source  = "confluentinc/confluent"
