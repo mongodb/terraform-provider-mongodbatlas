5ad54f342584bc79ef0e6c7c9533a579d34efe2e
Aastha Mahendru <aastha.mahendru@mongodb.com>
2024-12-24 14:50:20 +0000

    min

--- internal/service/team/update_user_test.go
+++ internal/service/team/update_user_test.go
@@ -1,6 +1,7 @@
 package team_test
 
 import (
+	"errors"
 	"testing"
 
 	"go.mongodb.org/atlas-sdk/v20241113003/admin"
@@ -70,7 +71,7 @@ func TestGetChangesForTeamUsers(t *testing.T) {
 func TestUpdateTeamUsers(t *testing.T) {
 	validuser1 := "validuser1"
 	validuser2 := "validuser2"
-	// invaliduser1 := "invaliduser1"
+	invaliduser1 := "invaliduser1"
 
 	testCases := map[string]struct {
 		mockFuncExpectations func(*mockadmin.TeamsApi, *mockadmin.MongoDBCloudUsersApi)
@@ -79,28 +80,28 @@ func TestUpdateTeamUsers(t *testing.T) {
 		testName             string
 		usernames            []string
 	}{
-		// "succeeds but no changes are required": {
-		// 	mockFuncExpectations: func(mockTeamsApi *mockadmin.TeamsApi, mockUsersApi *mockadmin.MongoDBCloudUsersApi) {
-		// 		mockValidUser1 := mockadmin.NewMongoDBCloudUsersApi(t)
-		// 		mockValidUser2 := mockadmin.NewMongoDBCloudUsersApi(t)
-		// 		mockUsersApi.EXPECT().GetUserByUsername(mock.Anything, validuser1).Return(admin.GetUserByUsernameApiRequest{ApiService: mockValidUser1})
-		// 		mockUsersApi.EXPECT().GetUserByUsername(mock.Anything, validuser2).Return(admin.GetUserByUsernameApiRequest{ApiService: mockValidUser2})
-		// 		mockValidUser1.EXPECT().GetUserByUsernameExecute(mock.Anything).Return(&admin.CloudAppUser{Id: &validuser1}, nil, nil)
-		// 		mockValidUser2.EXPECT().GetUserByUsernameExecute(mock.Anything).Return(&admin.CloudAppUser{Id: &validuser2}, nil, nil)
-		// 	},
-		// 	existingTeamUsers: &admin.PaginatedAppUser{Results: &[]admin.CloudAppUser{{Id: &validuser1}, {Id: &validuser2}}},
-		// 	usernames:         []string{validuser1, validuser2},
-		// 	expectError:       require.NoError,
-		// },
-		// "fails because one user is invalid": {
-		// 	mockFuncExpectations: func(mockTeamsApi *mockadmin.TeamsApi, mockUsersApi *mockadmin.MongoDBCloudUsersApi) {
-		// 		mockUsersApi.EXPECT().GetUserByUsername(mock.Anything, invaliduser1).Return(admin.GetUserByUsernameApiRequest{ApiService: mockUsersApi})
-		// 		mockUsersApi.EXPECT().GetUserByUsernameExecute(mock.Anything).Return(nil, nil, errors.New("invalid username"))
-		// 	},
-		// 	existingTeamUsers: &admin.PaginatedAppUser{Results: nil},
-		// 	usernames:         []string{invaliduser1},
-		// 	expectError:       require.Error,
-		// },
+		"succeeds but no changes are required": {
+			mockFuncExpectations: func(mockTeamsApi *mockadmin.TeamsApi, mockUsersApi *mockadmin.MongoDBCloudUsersApi) {
+				mockValidUser1 := mockadmin.NewMongoDBCloudUsersApi(t)
+				mockValidUser2 := mockadmin.NewMongoDBCloudUsersApi(t)
+				mockUsersApi.EXPECT().GetUserByUsername(mock.Anything, validuser1).Return(admin.GetUserByUsernameApiRequest{ApiService: mockValidUser1})
+				mockUsersApi.EXPECT().GetUserByUsername(mock.Anything, validuser2).Return(admin.GetUserByUsernameApiRequest{ApiService: mockValidUser2})
+				mockValidUser1.EXPECT().GetUserByUsernameExecute(mock.Anything).Return(&admin.CloudAppUser{Id: &validuser1}, nil, nil)
+				mockValidUser2.EXPECT().GetUserByUsernameExecute(mock.Anything).Return(&admin.CloudAppUser{Id: &validuser2}, nil, nil)
+			},
+			existingTeamUsers: &admin.PaginatedAppUser{Results: &[]admin.CloudAppUser{{Id: &validuser1}, {Id: &validuser2}}},
+			usernames:         []string{validuser1, validuser2},
+			expectError:       require.NoError,
+		},
+		"fails because one user is invalid": {
+			mockFuncExpectations: func(mockTeamsApi *mockadmin.TeamsApi, mockUsersApi *mockadmin.MongoDBCloudUsersApi) {
+				mockUsersApi.EXPECT().GetUserByUsername(mock.Anything, invaliduser1).Return(admin.GetUserByUsernameApiRequest{ApiService: mockUsersApi})
+				mockUsersApi.EXPECT().GetUserByUsernameExecute(mock.Anything).Return(nil, nil, errors.New("invalid username"))
+			},
+			existingTeamUsers: &admin.PaginatedAppUser{Results: &[]admin.CloudAppUser{}},
+			usernames:         []string{invaliduser1},
+			expectError:       require.Error,
+		},
 		"succeeds with one user to be added": {
 			mockFuncExpectations: func(mockTeamsApi *mockadmin.TeamsApi, mockUsersApi *mockadmin.MongoDBCloudUsersApi) {
 				mockValidUser1 := mockadmin.NewMongoDBCloudUsersApi(t)
@@ -110,46 +111,43 @@ func TestUpdateTeamUsers(t *testing.T) {
 				mockValidUser1.EXPECT().GetUserByUsernameExecute(mock.Anything).Return(&admin.CloudAppUser{Id: &validuser1}, nil, nil)
 				mockValidUser2.EXPECT().GetUserByUsernameExecute(mock.Anything).Return(&admin.CloudAppUser{Id: &validuser2}, nil, nil)
 
-				mockTeamsApi.EXPECT().AddTeamUser(mock.Anything, mock.Anything, mock.Anything, &[]admin.AddUserToTeam{{Id: validuser1}}).Return(admin.AddTeamUserApiRequest{ApiService: mockTeamsApi})
-				mockTeamsApi.EXPECT().AddTeamUserExecute(mock.Anything).Return(nil, nil, nil)
 				mockTeamsApi.EXPECT().AddTeamUser(mock.Anything, mock.Anything, mock.Anything, &[]admin.AddUserToTeam{{Id: validuser2}}).Return(admin.AddTeamUserApiRequest{ApiService: mockTeamsApi})
 				mockTeamsApi.EXPECT().AddTeamUserExecute(mock.Anything).Return(nil, nil, nil)
 			},
-			// existingTeamUsers: &admin.PaginatedAppUser{Results: &[]admin.CloudAppUser{{Id: &validuser1}}},
-			existingTeamUsers: &admin.PaginatedAppUser{Results: nil},
+			existingTeamUsers: &admin.PaginatedAppUser{Results: &[]admin.CloudAppUser{{Id: &validuser1}}},
 			usernames:         []string{validuser1, validuser2},
 			expectError:       require.NoError,
 		},
-		// "succeeds with one user to be removed": {
-		// 	mockFuncExpectations: func(mockTeamsApi *mockadmin.TeamsApi, mockUsersApi *mockadmin.MongoDBCloudUsersApi) {
-		// 		mockValidUser2 := mockadmin.NewMongoDBCloudUsersApi(t)
-		// 		mockUsersApi.EXPECT().GetUserByUsername(mock.Anything, validuser2).Return(admin.GetUserByUsernameApiRequest{ApiService: mockValidUser2})
-		// 		mockValidUser2.EXPECT().GetUserByUsernameExecute(mock.Anything).Return(&admin.CloudAppUser{Id: &validuser2}, nil, nil)
+		"succeeds with one user to be removed": {
+			mockFuncExpectations: func(mockTeamsApi *mockadmin.TeamsApi, mockUsersApi *mockadmin.MongoDBCloudUsersApi) {
+				mockValidUser2 := mockadmin.NewMongoDBCloudUsersApi(t)
+				mockUsersApi.EXPECT().GetUserByUsername(mock.Anything, validuser2).Return(admin.GetUserByUsernameApiRequest{ApiService: mockValidUser2})
+				mockValidUser2.EXPECT().GetUserByUsernameExecute(mock.Anything).Return(&admin.CloudAppUser{Id: &validuser2}, nil, nil)
 
-		// 		mockTeamsApi.EXPECT().RemoveTeamUser(mock.Anything, mock.Anything, mock.Anything, validuser1).Return(admin.RemoveTeamUserApiRequest{ApiService: mockTeamsApi})
-		// 		mockTeamsApi.EXPECT().RemoveTeamUserExecute(mock.Anything).Return(nil, nil)
-		// 	},
-		// 	existingTeamUsers: &admin.PaginatedAppUser{Results: &[]admin.CloudAppUser{{Id: &validuser1}, {Id: &validuser2}}},
-		// 	usernames:         []string{validuser2},
-		// 	expectError:       require.NoError,
-		// },
-		// "succeeds with one user to be added and the other removed": {
-		// 	mockFuncExpectations: func(mockTeamsApi *mockadmin.TeamsApi, mockUsersApi *mockadmin.MongoDBCloudUsersApi) {
-		// 		mockValidUser2 := mockadmin.NewMongoDBCloudUsersApi(t)
-		// 		mockUsersApi.EXPECT().GetUserByUsername(mock.Anything, validuser1).Return(admin.GetUserByUsernameApiRequest{ApiService: mockValidUser2})
-		// 		mockValidUser2.EXPECT().GetUserByUsernameExecute(mock.Anything).Return(&admin.CloudAppUser{Id: &validuser1}, nil, nil)
+				mockTeamsApi.EXPECT().RemoveTeamUser(mock.Anything, mock.Anything, mock.Anything, validuser1).Return(admin.RemoveTeamUserApiRequest{ApiService: mockTeamsApi})
+				mockTeamsApi.EXPECT().RemoveTeamUserExecute(mock.Anything).Return(nil, nil)
+			},
+			existingTeamUsers: &admin.PaginatedAppUser{Results: &[]admin.CloudAppUser{{Id: &validuser1}, {Id: &validuser2}}},
+			usernames:         []string{validuser2},
+			expectError:       require.NoError,
+		},
+		"succeeds with one user to be added and the other removed": {
+			mockFuncExpectations: func(mockTeamsApi *mockadmin.TeamsApi, mockUsersApi *mockadmin.MongoDBCloudUsersApi) {
+				mockValidUser2 := mockadmin.NewMongoDBCloudUsersApi(t)
+				mockUsersApi.EXPECT().GetUserByUsername(mock.Anything, validuser1).Return(admin.GetUserByUsernameApiRequest{ApiService: mockValidUser2})
+				mockValidUser2.EXPECT().GetUserByUsernameExecute(mock.Anything).Return(&admin.CloudAppUser{Id: &validuser1}, nil, nil)
 
-		// 		addCall := mockTeamsApi.EXPECT().AddTeamUser(mock.Anything, mock.Anything, mock.Anything, &[]admin.AddUserToTeam{{Id: validuser1}}).Return(admin.AddTeamUserApiRequest{ApiService: mockTeamsApi})
-		// 		mockTeamsApi.EXPECT().AddTeamUserExecute(mock.Anything).Return(nil, nil, nil)
+				addCall := mockTeamsApi.EXPECT().AddTeamUser(mock.Anything, mock.Anything, mock.Anything, &[]admin.AddUserToTeam{{Id: validuser1}}).Return(admin.AddTeamUserApiRequest{ApiService: mockTeamsApi})
+				mockTeamsApi.EXPECT().AddTeamUserExecute(mock.Anything).Return(nil, nil, nil)
 
-		// 		removeCall := mockTeamsApi.EXPECT().RemoveTeamUser(mock.Anything, mock.Anything, mock.Anything, validuser2).Return(admin.RemoveTeamUserApiRequest{ApiService: mockTeamsApi})
-		// 		removeCall.NotBefore(addCall.Call) // Ensures new additions are made before removing
-		// 		mockTeamsApi.EXPECT().RemoveTeamUserExecute(mock.Anything).Return(nil, nil)
-		// 	},
-		// 	existingTeamUsers: &admin.PaginatedAppUser{Results: &[]admin.CloudAppUser{{Id: &validuser2}}},
-		// 	usernames:         []string{validuser1},
-		// 	expectError:       require.NoError,
-		// },
+				removeCall := mockTeamsApi.EXPECT().RemoveTeamUser(mock.Anything, mock.Anything, mock.Anything, validuser2).Return(admin.RemoveTeamUserApiRequest{ApiService: mockTeamsApi})
+				removeCall.NotBefore(addCall.Call) // Ensures new additions are made before removing
+				mockTeamsApi.EXPECT().RemoveTeamUserExecute(mock.Anything).Return(nil, nil)
+			},
+			existingTeamUsers: &admin.PaginatedAppUser{Results: &[]admin.CloudAppUser{{Id: &validuser2}}},
+			usernames:         []string{validuser1},
+			expectError:       require.NoError,
+		},
 	}
 
 	for _, testCase := range testCases {
