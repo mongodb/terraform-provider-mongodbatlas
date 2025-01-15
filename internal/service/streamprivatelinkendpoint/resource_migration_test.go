51afe036ad6eb8a1af1169a0f43e4f2c2c6ff6fa
Oriol Arbusi <oriol.abadal@mongodb.com>
2025-01-15 09:45:26 +0100

    skip mig test

--- internal/service/streamprivatelinkendpoint/resource_migration_test.go
+++ internal/service/streamprivatelinkendpoint/resource_migration_test.go
@@ -3,10 +3,12 @@ package streamprivatelinkendpoint_test
 import (
 	"testing"
 
+	"github.com/mongodb/terraform-provider-mongodbatlas/internal/testutil/acc"
 	"github.com/mongodb/terraform-provider-mongodbatlas/internal/testutil/mig"
 )
 
 func TestMigStreamPrivatelinkEndpoint_basic(t *testing.T) {
+	acc.SkipTestForCI(t)
 	mig.SkipIfVersionBelow(t, "1.25.0") // when resource 1st released
 	mig.CreateTestAndRunUseExternalProviderNonParallel(t, basicTestCase(t), mig.ExternalProvidersWithConfluent(), nil)
 }
