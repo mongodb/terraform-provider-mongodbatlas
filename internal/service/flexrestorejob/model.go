3d5921233f38af34ac2ca025ddf93432ab385e37
Oriol Arbusi <oriol.abadal@mongodb.com>
2025-02-07 09:23:47 +0100

    restore job data sources

---
+++ internal/service/flexrestorejob/model.go
@@ -0,0 +1,38 @@
+package flexrestorejob
+
+import (
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/mongodb/terraform-provider-mongodbatlas/internal/common/conversion"
+	"go.mongodb.org/atlas-sdk/v20241113004/admin"
+)
+
+func NewTFModel(apiResp *admin.FlexBackupRestoreJob20241113) *TFModel {
+	return &TFModel{
+		ProjectId:                types.StringPointerValue(apiResp.ProjectId),
+		Name:                     types.StringPointerValue(apiResp.InstanceName),
+		DeliveryType:             types.StringPointerValue(apiResp.DeliveryType),
+		ExpirationDate:           types.StringPointerValue(conversion.TimePtrToStringPtr(apiResp.ExpirationDate)),
+		RestoreJobId:             types.StringPointerValue(apiResp.Id),
+		RestoreFinishedDate:      types.StringPointerValue(conversion.TimePtrToStringPtr(apiResp.RestoreFinishedDate)),
+		RestoreScheduledDate:     types.StringPointerValue(conversion.TimePtrToStringPtr(apiResp.RestoreScheduledDate)),
+		SnapshotFinishedDate:     types.StringPointerValue(conversion.TimePtrToStringPtr(apiResp.SnapshotFinishedDate)),
+		SnapshotId:               types.StringPointerValue(apiResp.SnapshotId),
+		SnapshotUrl:              types.StringPointerValue(apiResp.SnapshotUrl),
+		Status:                   types.StringPointerValue(apiResp.Status),
+		TargetDeploymentItemName: types.StringPointerValue(apiResp.TargetDeploymentItemName),
+		TargetProjectId:          types.StringPointerValue(apiResp.TargetProjectId),
+	}
+}
+
+func NewTFModelPluralDS(apiResp *[]admin.FlexBackupRestoreJob20241113) *TFFlexRestoreJobsDSModel {
+	if apiResp == nil {
+		return nil
+	}
+	var results []TFModel
+	for _, job := range *apiResp {
+		results = append(results, *NewTFModel(&job))
+	}
+	return &TFFlexRestoreJobsDSModel{
+		Results: results,
+	}
+}
