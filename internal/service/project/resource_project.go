e7081151cb0d58d6daccbac968bd5838cd63a382
Espen Albert <espen.albert@mongodb.com>
2025-02-12 09:34:58 +0000

    refactor: Simplify ReadIsSlowMsThresholdingEnabled as SDK is now returning a bool

--- internal/service/project/resource_project.go
+++ internal/service/project/resource_project.go
@@ -2,7 +2,6 @@ package project
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"log"
 	"reflect"
@@ -418,14 +417,9 @@ func SetSlowOperationThresholding(ctx context.Context, performanceAdvisorAPI adm
 }
 
 func ReadIsSlowMsThresholdingEnabled(ctx context.Context, api admin.PerformanceAdvisorApi, projectID string, warnings *diag.Diagnostics) (bool, error) {
-	_, response, err := api.GetManagedSlowMs(ctx, projectID).Execute()
+	isEnabled, _, err := api.GetManagedSlowMs(ctx, projectID).Execute()
 	if err != nil {
 		return false, err
-	}
-	var isEnabled bool
-	err = json.NewDecoder(response.Body).Decode(&isEnabled)
-	if err != nil {
-		return false, fmt.Errorf("error reading is_slow_operation_thresholding_enabled body %w", err)
 	}
 	return isEnabled, nil
 }
