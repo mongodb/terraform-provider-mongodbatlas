c64b251ba596613208d7195a02a30c1e409087b4
Leo Antoli <430982+lantoli@users.noreply.github.com>
2025-02-12 08:27:57 +0100

    doc: Examples for advanced_cluster preview v2 (#3054)
    * root readme
    
    * fix example as used region doesn't exist in GCP
    
    * change examples
    
    * undo comment
    
    * doc for backing_provider_name

--- examples/mongodbatlas_advanced_cluster (preview v2)/auto-scaling-per-shard/main.tf
+++ examples/mongodbatlas_advanced_cluster (preview v2)/auto-scaling-per-shard/main.tf
@@ -7,60 +7,60 @@ resource "mongodbatlas_advanced_cluster" "test" {
   project_id   = mongodbatlas_project.project.id
   name         = "AutoScalingCluster"
   cluster_type = "SHARDED"
-  replication_specs { # first shard
-    region_configs {
-      auto_scaling {
+  replication_specs = [{ # first shard
+    region_configs = [{
+      auto_scaling = {
         compute_enabled           = true
         compute_max_instance_size = "M60"
       }
-      analytics_auto_scaling {
+      analytics_auto_scaling = {
         compute_enabled           = true
         compute_max_instance_size = "M60"
       }
-      electable_specs {
+      electable_specs = {
         instance_size = "M40"
         node_count    = 3
       }
-      analytics_specs {
+      analytics_specs = {
         instance_size = "M40"
         node_count    = 1
       }
       provider_name = "AWS"
       priority      = 7
       region_name   = "EU_WEST_1"
-    }
+    }]
     zone_name = "Zone 1"
-  }
-  replication_specs { # second shard
-    region_configs {
-      auto_scaling {
+    }, { # second shard
+    region_configs = [{
+      auto_scaling = {
         compute_enabled           = true
         compute_max_instance_size = "M60"
       }
-      analytics_auto_scaling {
+      analytics_auto_scaling = {
         compute_enabled           = true
         compute_max_instance_size = "M60"
       }
-      electable_specs {
+      electable_specs = {
         instance_size = "M40"
         node_count    = 3
       }
-      analytics_specs {
+      analytics_specs = {
         instance_size = "M40"
         node_count    = 1
       }
       provider_name = "AWS"
       priority      = 7
       region_name   = "EU_WEST_1"
-    }
+    }]
     zone_name = "Zone 1"
-  }
+  }]
+
   lifecycle { # avoids non-empty plans as instance size start to scale from initial values
     ignore_changes = [
-      replication_specs[0].region_configs[0].electable_specs[0].instance_size,
-      replication_specs[0].region_configs[0].analytics_specs[0].instance_size,
-      replication_specs[1].region_configs[0].electable_specs[0].instance_size,
-      replication_specs[1].region_configs[0].analytics_specs[0].instance_size
+      replication_specs[0].region_configs[0].electable_specs.instance_size,
+      replication_specs[0].region_configs[0].analytics_specs.instance_size,
+      replication_specs[1].region_configs[0].electable_specs.instance_size,
+      replication_specs[1].region_configs[0].analytics_specs.instance_size
     ]
   }
 }
