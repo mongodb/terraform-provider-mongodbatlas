dee594f4b85767ecb29e0b8918d96de28a4b4fac
Espen Albert <espen.albert@mongodb.com>
2025-02-12 12:20:29 +0000

    address PR comments

--- examples/migrate_cluster_to_advanced_cluster/module_maintainer/v2/main.tf
+++ examples/migrate_cluster_to_advanced_cluster/module_maintainer/v2/main.tf
@@ -54,9 +54,55 @@ resource "mongodbatlas_advanced_cluster" "this" {
   tags                   = var.tags
 }
 
-data "mongodbatlas_cluster" "this" {
+data "mongodbatlas_cluster" "this" { # note the usage of `cluster` not `advanced_cluster`, this is to have outputs stay compatible with the v1 module
   name       = mongodbatlas_advanced_cluster.this.name
   project_id = mongodbatlas_advanced_cluster.this.project_id
 
   depends_on = [mongodbatlas_advanced_cluster.this]
 }
+
+# OLD cluster configuration:
+# resource "mongodbatlas_cluster" "this" {
+#   lifecycle {
+#     precondition {
+#       condition     = !(var.auto_scaling_disk_gb_enabled && var.disk_size > 0)
+#       error_message = "Must use either auto_scaling_disk_gb_enabled or disk_size, not both."
+#     }
+#   }
+
+#   project_id = var.project_id
+#   name       = var.cluster_name
+
+#   auto_scaling_disk_gb_enabled = var.auto_scaling_disk_gb_enabled
+#   cluster_type                 = var.cluster_type
+#   disk_size_gb                 = var.disk_size
+#   mongo_db_major_version       = var.mongo_db_major_version
+#   provider_instance_size_name  = var.instance_size
+#   provider_name                = var.provider_name
+
+#   dynamic "tags" {
+#     for_each = var.tags
+#     content {
+#       key   = tags.key
+#       value = tags.value
+#     }
+#   }
+
+#   dynamic "replication_specs" {
+#     for_each = var.replication_specs
+#     content {
+#       num_shards = replication_specs.value.num_shards
+#       zone_name  = replication_specs.value.zone_name
+
+#       dynamic "regions_config" {
+#         for_each = replication_specs.value.regions_config
+#         content {
+#           electable_nodes = regions_config.value.electable_nodes
+#           priority        = regions_config.value.priority
+#           read_only_nodes = regions_config.value.read_only_nodes
+#           region_name     = regions_config.value.region_name
+#         }
+#       }
+#     }
+#   }
+# }
