8157f4bc4ab64da53f31a659aa119e25a451cf86
KeisukeYamashita <19yamashita15@gmail.com>
2025-02-17 20:30:22 +0100

    chore: use typeset for custom db role's actions block
    Signed-off-by: KeisukeYamashita <19yamashita15@gmail.com>

--- internal/service/customdbrole/resource_custom_db_role.go
+++ internal/service/customdbrole/resource_custom_db_role.go
@@ -51,7 +51,7 @@ func Resource() *schema.Resource {
 				),
 			},
 			"actions": {
-				Type:     schema.TypeList,
+				Type:     schema.TypeSet,
 				Optional: true,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
