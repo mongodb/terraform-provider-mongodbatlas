40eadf59bd6995eef591ffcb3d22fc3170e1c677
Espen Albert <espen.albert@mongodb.com>
2025-02-17 18:03:57 +0000

    feat: Update CopyUnknowns to respect keepUnknown for region configurations

--- internal/service/advancedclustertpf/plan_modifier.go
+++ internal/service/advancedclustertpf/plan_modifier.go
@@ -11,6 +11,8 @@ import (
 	"go.mongodb.org/atlas-sdk/v20241113005/admin"
 )
 
+var keepUnknownRegionConfigs = []string{"instance_size", "disk_size_gb", "disk_iops", "node_count", "id", "analytics_specs", "read_only_specs"}
+
 func useStateForUnknowns(ctx context.Context, diags *diag.Diagnostics, state, plan *TFModel) {
 	if !schemafunc.HasUnknowns(plan) {
 		return
@@ -65,11 +67,11 @@ func useStateForUnknownsReplicationSpecs(ctx context.Context, diags *diag.Diagno
 			}
 			if update.IsZeroValues(patchSpec) {
 				schemafunc.CopyUnknowns(ctx, &stateRepSpecsTF[i], &planRepSpecsTF[i], nil)
-				if useIss {
-					planRepSpecsTF[i].Id = types.StringValue("") // ISS receive ASYMMETRIC_SHARD_UNSUPPORTED error from older cluster API and therefore, the ID should be empty
-				}
 			} else {
-				useStateForUnknownsRegionConfigs(ctx, diags, &stateSpec, &planSpec, &stateRepSpecsTF[i], &planRepSpecsTF[i])
+				schemafunc.CopyUnknowns(ctx, &stateRepSpecsTF[i], &planRepSpecsTF[i], keepUnknownRegionConfigs)
+			}
+			if useIss {
+				planRepSpecsTF[i].Id = types.StringValue("") // ISS receive ASYMMETRIC_SHARD_UNSUPPORTED error from older cluster API and therefore, the ID should be empty
 			}
 		}
 		planWithUnknowns = append(planWithUnknowns, planRepSpecsTF[i])
@@ -89,44 +91,4 @@ func TFModelList[T any](ctx context.Context, diags *diag.Diagnostics, input type
 		return nil
 	}
 	return elements
-}
-
-func useStateForUnknownsRegionConfigs(ctx context.Context, diags *diag.Diagnostics, state, plan *admin.ReplicationSpec20240805, stateTF, planTF *TFReplicationSpecsModel) {
-	stateRegionConfigs := state.GetRegionConfigs()
-	stateRegionConfigsTF := TFModelList[TFRegionConfigsModel](ctx, diags, stateTF.RegionConfigs)
-	planRegionConfigs := plan.GetRegionConfigs()
-	planRegionConfigsTF := TFModelList[TFRegionConfigsModel](ctx, diags, planTF.RegionConfigs)
-	if diags.HasError() || stateRegionConfigs == nil || planRegionConfigs == nil {
-		return
-	}
-	for i := range planRegionConfigsTF {
-		if i >= len(stateRegionConfigs) {
-			continue
-		}
-		stateConfig := stateRegionConfigs[i]
-		planConfig := planRegionConfigs[i]
-		patchConfig, err := update.PatchPayload(&stateConfig, &planConfig)
-		if err != nil {
-			diags.AddError("error find diff useStateForUnknownsRegionConfigs", err.Error())
-			return
-		}
-		if update.IsZeroValues(patchConfig) {
-			schemafunc.CopyUnknowns(ctx, &stateRegionConfigsTF[i], &planRegionConfigsTF[i], nil)
-		} else {
-			useStateForUnknownsRegionConfig(ctx, diags, patchConfig, &stateRegionConfigsTF[i], &planRegionConfigsTF[i])
-		}
-	}
-	listType, diagsLocal := types.ListValueFrom(ctx, RegionConfigsObjType, planRegionConfigsTF)
-	diags.Append(diagsLocal...)
-	if diags.HasError() {
-		return
-	}
-	planTF.RegionConfigs = listType
-}
-
-func useStateForUnknownsRegionConfig(ctx context.Context, diags *diag.Diagnostics, patch *admin.CloudRegionConfig20240805, stateTF, planTF *TFRegionConfigsModel) {
-	// Based on what is changed this impacts the other reginon configs too
-	// In the same region config, the read_only_specs and electable_specs are the same, so changing instance_size in electable will also change it in read_only_specs
-	// In the sibling region configs, the electable_specs are the same, so changing instance_size in electable will also change it in the sibling region configs
-	planTF.AnalyticsSpecs = types.ObjectUnknown(SpecsObjType.AttrTypes)
 }
