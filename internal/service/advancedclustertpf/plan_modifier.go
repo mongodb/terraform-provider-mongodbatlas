078358eb0de70398af9797e0709e14fe7af9610b
Espen Albert <espen.albert@mongodb.com>
2025-02-18 11:25:37 +0000

    refactor: simplified safer plan_minimize_replication_specs

--- internal/service/advancedclustertpf/plan_modifier.go
+++ internal/service/advancedclustertpf/plan_modifier.go
@@ -2,7 +2,6 @@ package advancedclustertpf
 
 import (
 	"context"
-	"reflect"
 	"slices"
 
 	"github.com/hashicorp/terraform-plugin-framework/diag"
@@ -22,20 +21,10 @@ func useStateForUnknowns(ctx context.Context, diags *diag.Diagnostics, state, pl
 	}
 	keepUnknown := determineKeepUnknowns(upgradeRequest, patchReq)
 	schemafunc.CopyUnknowns(ctx, state, plan, keepUnknown)
-	// `replication_specs` is handled by index to allow:
-	// 1. Using full state for "unchanged" specs
-	// 2. Using partial state for "changed" specs
+	// `replication_specs` is handled by index to allow using partial state for "changed" specs
 	if slices.Contains(keepUnknown, "replication_specs") {
-		// These fields must be kept unknown in the replication_specs[index_of_changes]
-		// *_specs are kept unknown as not having them in the config means that changes in "sibling" region_configs can impact the "computed" spec
-		// read_only_specs also reacts to changes in the electable_specs
-		// disk_size_gb can be change at any level/spec
-		// disk_iops can change based on instance_size changes
-		var keepUnknownReplicationSpecs = []string{"disk_size_gb", "disk_iops", "read_only_specs", "analytics_specs", "electable_specs"}
-		if upgradeRequest != nil {
-			// TenantUpgrade changes many extra fields that are normally ok to use state values for
-			keepUnknownReplicationSpecs = append(keepUnknownReplicationSpecs, "zone_id", "id", "container_id", "external_id", "auto_scaling")
-		}
+		// Always avoid copying `id` fields even when there are no changes to the spec
+		var keepUnknownReplicationSpecs = []string{"zone_id", "id", "external_id"}
 		useStateForUnknownsReplicationSpecs(ctx, diags, state, plan, keepUnknownReplicationSpecs)
 	}
 }
@@ -67,7 +56,6 @@ func useStateForUnknownsReplicationSpecs(ctx context.Context, diags *diag.Diagno
 		return
 	}
 	planWithUnknowns := []TFReplicationSpecsModel{}
-	useIss := clusterUseISS(planRepSpecs)
 	for i := range planRepSpecsTF {
 		if i < len(*stateRepSpecs) {
 			stateSpec := (*stateRepSpecs)[i]
@@ -78,19 +66,7 @@ func useStateForUnknownsReplicationSpecs(ctx context.Context, diags *diag.Diagno
 				return
 			}
 			if update.IsZeroValues(patchSpec) {
-				schemafunc.CopyUnknowns(ctx, &stateRepSpecsTF[i], &planRepSpecsTF[i], nil)
-			} else {
-				keepUnknownsSpec := slices.Clone(keepUnknowns)
-				if !regionsMatch(&stateSpec, &planSpec) { // If regions are different, we need to keep the container_id unknown
-					keepUnknownsSpec = append(keepUnknownsSpec, "container_id")
-				}
-				if !providersMatch(&stateSpec, &planSpec) { // If providers are different, we need to keep the ebs_volume_type unknown
-					keepUnknownsSpec = append(keepUnknownsSpec, "ebs_volume_type")
-				}
-				schemafunc.CopyUnknowns(ctx, &stateRepSpecsTF[i], &planRepSpecsTF[i], keepUnknownsSpec)
-			}
-			if useIss {
-				planRepSpecsTF[i].Id = types.StringValue("") // ISS receive ASYMMETRIC_SHARD_UNSUPPORTED error from older cluster API and therefore, the ID should be empty
+				schemafunc.CopyUnknowns(ctx, &stateRepSpecsTF[i], &planRepSpecsTF[i], keepUnknowns)
 			}
 		}
 		planWithUnknowns = append(planWithUnknowns, planRepSpecsTF[i])
@@ -110,71 +86,4 @@ func TFModelList[T any](ctx context.Context, diags *diag.Diagnostics, input type
 		return nil
 	}
 	return elements
-}
-
-// clusterUseISS checks if the cluster is using the ISS (Independent Shard Scaling) feature
-func clusterUseISS(specs *[]admin.ReplicationSpec20240805) bool {
-	if specs == nil {
-		return false
-	}
-	specInstancesSizes := map[string]string{}
-	keyElectable := "electable"
-	keyAnalytics := "analytics"
-	keyReadOnly := "readonly"
-	useIss := func(key, instanceSize string) bool {
-		if instanceSize == "" {
-			return false
-		}
-		oldInstanceSize, ok := specInstancesSizes[key]
-		if ok && oldInstanceSize != instanceSize {
-			return true
-		}
-		specInstancesSizes[key] = instanceSize
-		return false
-	}
-	for _, spec := range *specs {
-		for _, regionConfig := range spec.GetRegionConfigs() {
-			electable := regionConfig.GetElectableSpecs()
-			if useIss(keyElectable, electable.GetInstanceSize()) {
-				return true
-			}
-			readOnly := regionConfig.GetReadOnlySpecs()
-			if useIss(keyReadOnly, readOnly.GetInstanceSize()) {
-				return true
-			}
-			analytics := regionConfig.GetAnalyticsSpecs()
-			if useIss(keyAnalytics, analytics.GetInstanceSize()) {
-				return true
-			}
-		}
-	}
-	return false
-}
-
-func regionsMatch(state, plan *admin.ReplicationSpec20240805) bool {
-	regionsState := getRegions(state)
-	regionsPlan := getRegions(plan)
-	return reflect.DeepEqual(regionsState, regionsPlan)
-}
-
-func getRegions(spec *admin.ReplicationSpec20240805) []string {
-	regions := []string{}
-	for _, region := range spec.GetRegionConfigs() {
-		regions = append(regions, region.GetRegionName())
-	}
-	return regions
-}
-
-func providersMatch(state, plan *admin.ReplicationSpec20240805) bool {
-	providersState := getProviders(state)
-	providersPlan := getProviders(plan)
-	return reflect.DeepEqual(providersState, providersPlan)
-}
-
-func getProviders(spec *admin.ReplicationSpec20240805) []string {
-	providers := []string{}
-	for _, region := range spec.GetRegionConfigs() {
-		providers = append(providers, region.GetProviderName())
-	}
-	return providers
 }
