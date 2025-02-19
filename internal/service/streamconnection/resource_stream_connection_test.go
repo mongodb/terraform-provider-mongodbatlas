d64031f70bee616fc44e1c87769d6c78411d8a55
LijieZhang1998 <lijie.zhang@mongodb.com>
2025-02-19 14:13:14 -0600

    feat: minor change

--- internal/service/streamconnection/resource_stream_connection_test.go
+++ internal/service/streamconnection/resource_stream_connection_test.go
@@ -236,8 +236,6 @@ func TestAccStreamPrivatelinkEndpoint_streamConnection(t *testing.T) {
 }
 
 func TestAccStreamRSStreamConnection_AWSLambda(t *testing.T) {
-	acc.SkipTestForCI(t)
-
 	var (
 		resourceName   = "mongodbatlas_stream_connection.test"
 		projectID      = os.Getenv("MONGODB_ATLAS_ASP_PROJECT_EAR_PE_ID") //test-acc-tf-p-keep-ear-AWS-private-endpoint project has aws integration
