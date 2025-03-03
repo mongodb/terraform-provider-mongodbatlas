7934ad03207607cd19bf0b0809c009f38469d32e
Espen Albert <espen.albert@mongodb.com>
2025-03-03 16:08:28 +0000

    refactor: Introduce Modifier interface and enhance non-updatable attribute handling to support multiple planmodifiers

--- internal/common/customplanmodifier/non_updatable.go
+++ internal/common/customplanmodifier/non_updatable.go
@@ -4,33 +4,49 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/path"
 	planmodifier "github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 )
 
-func NonUpdatableStringAttributePlanModifier() planmodifier.String {
-	return &nonUpdatableStringAttributePlanModifier{}
+type Modifier interface {
+	planmodifier.String
+	planmodifier.Bool
 }
 
-type nonUpdatableStringAttributePlanModifier struct {
+func NonUpdatableAttributePlanModifier() Modifier {
+	return &nonUpdatableAttributePlanModifier{}
 }
 
-func (d *nonUpdatableStringAttributePlanModifier) Description(ctx context.Context) string {
+type nonUpdatableAttributePlanModifier struct {
+}
+
+func (d *nonUpdatableAttributePlanModifier) Description(ctx context.Context) string {
 	return d.MarkdownDescription(ctx)
 }
 
-func (d *nonUpdatableStringAttributePlanModifier) MarkdownDescription(ctx context.Context) string {
+func (d *nonUpdatableAttributePlanModifier) MarkdownDescription(ctx context.Context) string {
 	return "Ensures that update operations fails when updating an attribute."
 }
 
-func (d *nonUpdatableStringAttributePlanModifier) PlanModifyString(ctx context.Context, req planmodifier.StringRequest, resp *planmodifier.StringResponse) {
-	planAttributeValue := req.PlanValue
-	stateAttributeValue := req.StateValue
-
-	if !stateAttributeValue.IsNull() && stateAttributeValue.ValueString() != planAttributeValue.ValueString() {
-		resp.Diagnostics.AddError(
-			fmt.Sprintf("%s cannot be updated", req.Path),
-			fmt.Sprintf("%s cannot be updated", req.Path),
-		)
-		return
+func (d *nonUpdatableAttributePlanModifier) PlanModifyBool(ctx context.Context, req planmodifier.BoolRequest, resp *planmodifier.BoolResponse) {
+	if d.isUpdated(req.PlanValue, req.StateValue) {
+		d.addDiags(&resp.Diagnostics, req.Path)
 	}
+}
+
+func (d *nonUpdatableAttributePlanModifier) PlanModifyString(ctx context.Context, req planmodifier.StringRequest, resp *planmodifier.StringResponse) {
+	if d.isUpdated(req.PlanValue, req.StateValue) {
+		d.addDiags(&resp.Diagnostics, req.Path)
+	}
+}
+
+func (d *nonUpdatableAttributePlanModifier) isUpdated(planValue, stateValue attr.Value) bool {
+	return !stateValue.IsNull() && !planValue.Equal(stateValue)
+}
+
+func (d *nonUpdatableAttributePlanModifier) addDiags(diags *diag.Diagnostics, attrPath path.Path) {
+	message := fmt.Sprintf("%s cannot be updated", attrPath)
+	diags.AddError(message, message)
 }
