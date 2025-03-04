e09a266fe5ae33d0ecb286b0f0b3ffb3b813f063
Espen Albert <espen.albert@mongodb.com>
2025-03-04 20:32:05 +0000

    refactor: Add additional test steps for advanced_cluster to demonstrate No Plan Changes when removing a block & Unexpected Plan Changes when removing `advanced_configuration` block

--- internal/service/advancedcluster/resource_advanced_cluster_test.go
+++ internal/service/advancedcluster/resource_advanced_cluster_test.go
@@ -1270,6 +1270,10 @@ func TestAccMockableAdvancedCluster_replicasetAdvConfigUpdate(t *testing.T) {
 				Config: configBasicReplicaset(t, projectID, clusterName, fullUpdate),
 				Check:  checksUpdate,
 			},
+			{
+				Config: configBasicReplicaset(t, projectID, clusterName, ""),
+				Check:  checks,
+			},
 			acc.TestStepImportCluster(resourceName),
 		},
 	})
@@ -1312,6 +1316,10 @@ func TestAccMockableAdvancedCluster_shardedAddAnalyticsAndAutoScaling(t *testing
 			{
 				Config: configSharded(t, projectID, clusterName, true),
 				Check:  checksUpdated,
+			},
+			{
+				Config: configSharded(t, projectID, clusterName, false),
+				Check:  checks,
 			},
 			acc.TestStepImportCluster(resourceName),
 		},
