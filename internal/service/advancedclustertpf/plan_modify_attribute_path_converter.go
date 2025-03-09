09705112de954ece22b4b3399f2a8de94478a8fe
Espen Albert <espen.albert@mongodb.com>
2025-03-09 08:36:49 +0000

    refactor: Remove unused AttributeSimplified interface methods

--- internal/service/advancedclustertpf/plan_modify_attribute_path_converter.go
+++ internal/service/advancedclustertpf/plan_modify_attribute_path_converter.go
@@ -11,65 +11,8 @@ import (
 	"github.com/hashicorp/terraform-plugin-go/tftypes"
 )
 
-type AttributeSimplified interface {
-	// Implementations should include the tftypes.AttributePathStepper
-	// interface methods for proper path and data handling.
-	tftypes.AttributePathStepper
-
-	// Equal should return true if the other attribute is exactly equivalent.
-	Equal(o AttributeSimplified) bool
-
-	// GetDeprecationMessage should return a non-empty string if an attribute
-	// is deprecated. This is named differently than DeprecationMessage to
-	// prevent a conflict with the tfsdk.Attribute field name.
-	GetDeprecationMessage() string
-
-	// GetDescription should return a non-empty string if an attribute
-	// has a plaintext description. This is named differently than Description
-	// to prevent a conflict with the tfsdk.Attribute field name.
-	GetDescription() string
-
-	// GetMarkdownDescription should return a non-empty string if an attribute
-	// has a Markdown description. This is named differently than
-	// MarkdownDescription to prevent a conflict with the tfsdk.Attribute field
-	// name.
-	GetMarkdownDescription() string
-
-	// GetType should return the framework type of an attribute. This is named
-	// differently than Type to prevent a conflict with the tfsdk.Attribute
-	// field name.
-	GetType() attr.Type
-
-	// IsComputed should return true if the attribute configuration value is
-	// computed. This is named differently than Computed to prevent a conflict
-	// with the tfsdk.Attribute field name.
-	IsComputed() bool
-
-	// IsOptional should return true if the attribute configuration value is
-	// optional. This is named differently than Optional to prevent a conflict
-	// with the tfsdk.Attribute field name.
-	IsOptional() bool
-
-	// IsRequired should return true if the attribute configuration value is
-	// required. This is named differently than Required to prevent a conflict
-	// with the tfsdk.Attribute field name.
-	IsRequired() bool
-
-	// IsSensitive should return true if the attribute configuration value is
-	// sensitive. This is named differently than Sensitive to prevent a
-	// conflict with the tfsdk.Attribute field name.
-	IsSensitive() bool
-
-	// IsWriteOnly should return true if the attribute configuration value is
-	// write-only. This is named differently than WriteOnly to prevent a
-	// conflict with the tfsdk.Attribute field name.
-	//
-	// Write-only attributes are a managed-resource schema concept only.
-	IsWriteOnly() bool
-}
 type SimplifiedSchema interface {
 	TypeAtTerraformPath(context.Context, *tftypes.AttributePath) (attr.Type, error)
-	// AttributeAtTerraformPath(context.Context, *tftypes.AttributePath) (AttributeSimplified, error)
 }
 
 func AttributePathValue(ctx context.Context, diags *diag.Diagnostics, attributePath *tftypes.AttributePath, src tfsdk.State, schema SimplifiedSchema) (attr.Value, path.Path) {
