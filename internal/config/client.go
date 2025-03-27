07ab30f5f971d2308b3a76407940492905aedac3
Agustin Bettati <bettatiagustin@gmail.com>
2025-03-27 17:12:20 +0100

    chore: Defining helper function for making untyped API calls (#3215)
    * adopting adjusted branch and doing successful GET
    
    * remove project example calls
    
    * formatting
    
    * linting
    
    * include struct for passing parameters
    
    * inline api call

--- internal/config/client.go
+++ internal/config/client.go
@@ -236,6 +236,38 @@ func (c *MongoDBClient) GetRealmClient(ctx context.Context) (*realm.Client, erro
 	return realmClient, nil
 }
 
+type APICallParams struct {
+	VersionHeader string
+	RelativePath  string
+	PathParams    map[string]string
+	Method        string
+	Body          []byte
+}
+
+func (c *MongoDBClient) UntypedAPICall(ctx context.Context, params *APICallParams) (*http.Response, error) {
+	localBasePath, _ := c.AtlasV2.GetConfig().ServerURLWithContext(ctx, "")
+	localVarPath := localBasePath + params.RelativePath
+
+	for key, value := range params.PathParams {
+		localVarPath = strings.ReplaceAll(localVarPath, "{"+key+"}", url.PathEscape(value))
+	}
+
+	headerParams := make(map[string]string)
+	headerParams["Content-Type"] = params.VersionHeader
+	headerParams["Accept"] = params.VersionHeader
+
+	var reqBody any
+	if params.Body != nil { // if nil slice is sent with application/json content type SDK method returns an error
+		reqBody = params.Body
+	}
+	apiReq, err := c.AtlasV2.PrepareRequest(ctx, localVarPath, params.Method, reqBody, headerParams, nil, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return c.AtlasV2.CallAPI(apiReq)
+}
+
 func userAgent(c *Config) string {
 	platformVersions := []PlatformVersion{
 		{toolName, version.ProviderVersion},
