f4794a0637d534f590c66208bb9af932ffe04a9a
Leo Antoli <430982+lantoli@users.noreply.github.com>
2025-03-28 07:05:58 +0100

    Revert "TEMPORARY: simulate new TF version to test changes"
    This reverts commit 00831bb83ae98d1ce2e3faba124df26ab0bbb171.

--- scripts/update-tf-version-in-repository.sh
+++ scripts/update-tf-version-in-repository.sh
@@ -16,8 +16,7 @@
 set -euo pipefail
 
 # example value of TF_VERSIONS_ARR='["1.9.x", "1.8.x", "1.7.x"]'
-# TF_VERSIONS_ARR=$(./scripts/get-terraform-supported-versions.sh "false")
-TF_VERSIONS_ARR='["1.12.x", "1.11.x","1.10.x","1.9.x","1.8.x","1.7.x","1.6.x","1.5.x","1.4.x"]'
+TF_VERSIONS_ARR=$(./scripts/get-terraform-supported-versions.sh "false")
 
 TEST_SUITE_YAML_FILE=".github/workflows/test-suite.yml"
 ACCEPTANCE_TESTS_YAML_FILE=".github/workflows/acceptance-tests.yml"
@@ -39,8 +38,7 @@ sed -i.bak -E "s|terraform_version: '.*'|terraform_version: '$LATEST_TF_VERSION'
 sed -i.bak -E "s~terraform_version \|\| '[0-9]+\.[0-9]+\.x'~terraform_version \|\| '$LATEST_TF_VERSION'~" "$ACCEPTANCE_TESTS_YAML_FILE"
 
 # Update patch version occurrences
-# LATEST_TF_PATCH_VERSION=$(./scripts/get-terraform-supported-versions.sh "latest")
-LATEST_TF_PATCH_VERSION=1.12.5
+LATEST_TF_PATCH_VERSION=$(./scripts/get-terraform-supported-versions.sh "latest")
 
 # Update Terraform versions in .tool-versions
 sed -i.bak -E "s|^(terraform) [0-9]+\.[0-9]+\.[0-9]+|\1 $LATEST_TF_PATCH_VERSION|" "$TOOL_VERSIONS_FILE"
