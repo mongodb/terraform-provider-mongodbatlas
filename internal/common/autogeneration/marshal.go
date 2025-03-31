f29447a1e068ea9a4384f8e81de9baa841517854
Leo Antoli <430982+lantoli@users.noreply.github.com>
2025-03-31 19:00:32 +0200

    bools

--- internal/common/autogeneration/marshal.go
+++ internal/common/autogeneration/marshal.go
@@ -5,20 +5,21 @@ import (
 	"fmt"
 	"reflect"
 
+	"github.com/hashicorp/terraform-plugin-framework/attr"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/huandu/xstrings"
 )
 
 // Unmarshal gets a JSON (e.g. from an Atlas response) and unmarshals it into a Terraform model.
 func Unmarshal(raw []byte, dest any) error {
-	var fields map[string]any
-	if err := json.Unmarshal(raw, &fields); err != nil {
+	var src map[string]any
+	if err := json.Unmarshal(raw, &src); err != nil {
 		return err
 	}
-	return mapFields(fields, dest)
+	return mapFields(src, dest)
 }
 
-func mapFields(fields map[string]any, dest any) error {
+func mapFields(src map[string]any, dest any) error {
 	valDest := reflect.ValueOf(dest)
 	if valDest.Kind() != reflect.Ptr {
 		panic("dest must be pointer")
@@ -27,25 +28,39 @@ func mapFields(fields map[string]any, dest any) error {
 	if valDest.Kind() != reflect.Struct {
 		panic("dest must be pointer to struct")
 	}
-	for nameSrc, valueSrc := range fields {
-		nameDest := xstrings.ToPascalCase(nameSrc)
-		fieldDest := valDest.FieldByName(nameDest)
-		if !fieldDest.CanSet() {
-			continue // skip fields that cannot be set, are invalid or not found
-		}
-		switch v := valueSrc.(type) {
-		case bool:
-			return fmt.Errorf("not supported yet type %T for field %s", v, nameSrc)
-		case float64:
-			return fmt.Errorf("not supported yet type %T for field %s", v, nameSrc)
-		case string:
-			valObj := reflect.ValueOf(types.StringValue(v))
-			fieldDest.Set(valObj)
-		case nil:
-			return fmt.Errorf("not supported yet type %T for field %s", v, nameSrc)
-		default:
-			return fmt.Errorf("not supported yet type %T for field %s", v, nameSrc)
+	for nameAttrSrc, valueAttrSrc := range src {
+		if err := mapField(nameAttrSrc, valueAttrSrc, valDest); err != nil {
+			return err
 		}
 	}
+	return nil
+}
+
+func mapField(nameAttrSrc string, valueAttrSrc any, valDest reflect.Value) error {
+	nameDest := xstrings.ToPascalCase(nameAttrSrc)
+	fieldDest := valDest.FieldByName(nameDest)
+	if !fieldDest.CanSet() {
+		return nil // skip fields that cannot be set, are invalid or not found
+	}
+	switch v := valueAttrSrc.(type) {
+	case string:
+		return assignField(fieldDest, types.StringValue(v))
+	case bool:
+		return assignField(fieldDest, types.BoolValue(v))
+	case float64:
+		return fmt.Errorf("not supported yet type %T for field %s", v, nameAttrSrc)
+	case nil:
+		return nil // skip nil values, no need to set anything
+	default:
+		return fmt.Errorf("not supported yet type %T for field %s", v, nameAttrSrc)
+	}
+}
+
+func assignField(fieldDest reflect.Value, valueDest attr.Value) error {
+	valObj := reflect.ValueOf(valueDest)
+	if !fieldDest.Type().AssignableTo(valObj.Type()) {
+		return fmt.Errorf("can't assign value to model field %s", fieldDest.Type().Name())
+	}
+	fieldDest.Set(valObj)
 	return nil
 }
--- internal/common/autogeneration/marshal_test.go
+++ internal/common/autogeneration/marshal_test.go
@@ -5,22 +5,29 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/mongodb/terraform-provider-mongodbatlas/internal/common/autogeneration"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 func TestUnmarshal(t *testing.T) {
 	var model tfModelTest
 	require.NoError(t, autogeneration.Unmarshal([]byte(tfResponseJSON), &model))
-	require.Equal(t, "value_string", model.AttributeString.ValueString())
+	assert.Equal(t, "value_string", model.AttributeString.ValueString())
+	assert.True(t, model.AttributeTrue.ValueBool())
+	assert.False(t, model.AttributeFalse.ValueBool())
 }
 
 type tfModelTest struct {
 	AttributeString types.String `tfsdk:"attribute_string"`
+	AttributeTrue   types.Bool   `tfsdk:"attribute_true"`
+	AttributeFalse  types.Bool   `tfsdk:"attribute_false"`
 }
 
 const tfResponseJSON = `
 {
 	"attribute_string": "value_string",
+	"attribute_true": true,
+	"attribute_false": false,
 	"attribute_not_in_model": "val"
 }
 `
