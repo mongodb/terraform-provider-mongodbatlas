4f1783856ea2aa45c8957a041ee0a8e856b23fcb
Oriol <oriol.abadal@mongodb.com>
2025-03-31 14:48:00 +0200

    chore: Uses latest SDK in `mongodbatlas_alert_configuration` resource and data sources (#3229)
    * use latest sdk for alertconfiguration
    
    * rename
    
    * move ds tests to resource_test
    
    * combine DS and RS tests as suggested in PR review

--- internal/service/alertconfiguration/resource_test.go
+++ internal/service/alertconfiguration/resource_test.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"os"
 	"regexp"
+	"strconv"
+	"strings"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
@@ -32,19 +34,35 @@ func TestAccConfigRSAlertConfiguration_basic(t *testing.T) {
 		CheckDestroy:             checkDestroy(),
 		Steps: []resource.TestStep{
 			{
-				Config: configBasicRS(projectID, true),
+				Config: configBasic(projectID, true),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					checkExists(resourceName),
 					resource.TestCheckResourceAttr(resourceName, "project_id", projectID),
 					resource.TestCheckResourceAttr(resourceName, "notification.#", "2"),
+					// Data source checks
+					checkExists(dataSourceName),
+					resource.TestCheckResourceAttr(dataSourceName, "project_id", projectID),
+					resource.TestCheckResourceAttr(dataSourceName, "notification.#", "2"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "notification.0.notifier_id"),
+					resource.TestCheckResourceAttr(dataSourceName, "matcher.#", "1"),
+					resource.TestCheckResourceAttr(dataSourceName, "metric_threshold_config.#", "1"),
+					resource.TestCheckResourceAttr(dataSourceName, "threshold_config.#", "0"),
 				),
 			},
 			{
-				Config: configBasicRS(projectID, false),
+				Config: configBasic(projectID, false),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					checkExists(resourceName),
 					resource.TestCheckResourceAttr(resourceName, "project_id", projectID),
 					resource.TestCheckResourceAttr(resourceName, "notification.#", "2"),
+					// Data source checks
+					checkExists(dataSourceName),
+					resource.TestCheckResourceAttr(dataSourceName, "project_id", projectID),
+					resource.TestCheckResourceAttr(dataSourceName, "notification.#", "2"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "notification.0.notifier_id"),
+					resource.TestCheckResourceAttr(dataSourceName, "matcher.#", "1"),
+					resource.TestCheckResourceAttr(dataSourceName, "metric_threshold_config.#", "1"),
+					resource.TestCheckResourceAttr(dataSourceName, "threshold_config.#", "0"),
 				),
 			},
 			{
@@ -207,7 +225,7 @@ func TestAccConfigRSAlertConfiguration_withMetricUpdated(t *testing.T) {
 	})
 }
 
-func TestAccConfigRSAlertConfiguration_withThresholdUpdated(t *testing.T) {
+func TestAccConfigRSAlertConfiguration_withThreshold(t *testing.T) {
 	var (
 		projectID = acc.ProjectIDExecution(t)
 	)
@@ -218,17 +236,22 @@ func TestAccConfigRSAlertConfiguration_withThresholdUpdated(t *testing.T) {
 		CheckDestroy:             checkDestroy(),
 		Steps: []resource.TestStep{
 			{
-				Config: configWithThresholdUpdated(projectID, true, 1),
+				Config: configWithThreshold(projectID, true, 1),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					checkExists(resourceName),
 					resource.TestCheckResourceAttr(resourceName, "project_id", projectID),
 				),
 			},
 			{
-				Config: configWithThresholdUpdated(projectID, false, 3),
+				Config: configWithThreshold(projectID, false, 3),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					checkExists(resourceName),
 					resource.TestCheckResourceAttr(resourceName, "project_id", projectID),
+					resource.TestCheckResourceAttr(dataSourceName, "project_id", projectID),
+					resource.TestCheckResourceAttr(dataSourceName, "notification.#", "1"),
+					resource.TestCheckResourceAttr(dataSourceName, "matcher.#", "1"),
+					resource.TestCheckResourceAttr(dataSourceName, "threshold_config.#", "1"),
+					resource.TestCheckResourceAttr(dataSourceName, "metric_threshold_config.#", "0"),
 				),
 			},
 			{
@@ -295,7 +318,7 @@ func TestAccConfigRSAlertConfiguration_importIncorrectId(t *testing.T) {
 		CheckDestroy:             checkDestroy(),
 		Steps: []resource.TestStep{
 			{
-				Config: configBasicRS(projectID, true),
+				Config: configBasic(projectID, true),
 			},
 			{
 				ResourceName:  resourceName,
@@ -587,7 +610,7 @@ func importStateProjectIDFunc(resourceName string) resource.ImportStateIdFunc {
 	}
 }
 
-func configBasicRS(projectID string, enabled bool) string {
+func configBasic(projectID string, enabled bool) string {
 	return fmt.Sprintf(`
 	resource "mongodbatlas_alert_configuration" "test" {
 		project_id = %[1]q
@@ -624,6 +647,11 @@ func configBasicRS(projectID string, enabled bool) string {
 			units       = "RAW"
 			mode        = "AVERAGE"
 		}
+	}
+
+	data "mongodbatlas_alert_configuration" "test" {
+		project_id             = mongodbatlas_alert_configuration.test.project_id
+		alert_configuration_id = mongodbatlas_alert_configuration.test.id
 	}
 	`, projectID, enabled)
 }
@@ -748,37 +776,6 @@ func configWithoutRoles(projectID string, enabled bool, threshold float64) strin
 				threshold   = %[3]f
 				units       = "RAW"
 				mode        = "AVERAGE"
-			}
-		}
-	`, projectID, enabled, threshold)
-}
-
-func configWithThresholdUpdated(projectID string, enabled bool, threshold float64) string {
-	return fmt.Sprintf(`
-		resource "mongodbatlas_alert_configuration" "test" {
-			project_id = %[1]q
-			enabled    = %[2]t
-			event_type = "REPLICATION_OPLOG_WINDOW_RUNNING_OUT"
-
-			notification {
-				type_name     = "GROUP"
-				interval_min  = 5
-				delay_min     = 0
-				sms_enabled   = false
-				email_enabled = true
-				roles = ["GROUP_DATA_ACCESS_READ_ONLY", "GROUP_CLUSTER_MANAGER"]
-			}
-
-			matcher {
-				field_name = "REPLICA_SET_NAME"
-				operator   = "EQUALS"
-				value      = "SECONDARY"
-			}
-
-			threshold_config {
-				operator    = "LESS_THAN"
-				units       = "HOURS"
-				threshold   = %[3]f
 			}
 		}
 	`, projectID, enabled, threshold)
@@ -1040,4 +1037,303 @@ func configWithEmptyOptionalBlocks(projectID string) string {
 			}
 		}
 	`, projectID)
+}
+
+func TestAccConfigDSAlertConfiguration_withOutput(t *testing.T) {
+	var (
+		projectID   = acc.ProjectIDExecution(t)
+		outputLabel = "resource_import"
+	)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:                 func() { acc.PreCheckBasic(t) },
+		ProtoV6ProviderFactories: acc.TestAccProviderV6Factories,
+		CheckDestroy:             checkDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: configWithOutputs(projectID, outputLabel),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					checkExists(dataSourceName),
+					resource.TestCheckResourceAttr(dataSourceName, "project_id", projectID),
+					resource.TestCheckResourceAttr(dataSourceName, "notification.#", "1"),
+					resource.TestCheckResourceAttr(dataSourceName, "output.0.label", outputLabel),
+					resource.TestCheckResourceAttr(dataSourceName, "output.0.type", "resource_import"),
+					resource.TestCheckResourceAttrWith(dataSourceName, "output.0.value", acc.MatchesExpression("terraform import mongodbatlas_alert_configuration.*")),
+					resource.TestCheckResourceAttr(dataSourceName, "output.1.label", outputLabel),
+					resource.TestCheckResourceAttr(dataSourceName, "output.1.type", "resource_hcl"),
+					resource.TestCheckResourceAttrWith(dataSourceName, "output.1.value", acc.MatchesExpression("resource \"mongodbatlas_alert_configuration\".*")),
+				),
+			},
+		},
+	})
+}
+
+func TestAccConfigDSAlertConfiguration_withPagerDuty(t *testing.T) {
+	var (
+		projectID  = acc.ProjectIDExecution(t)
+		serviceKey = dummy32CharKey
+	)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:                 func() { acc.PreCheckBasic(t) },
+		ProtoV6ProviderFactories: acc.TestAccProviderV6Factories,
+		CheckDestroy:             checkDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: configWithPagerDutyDS(projectID, serviceKey, true),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					checkExists(dataSourceName),
+					resource.TestCheckResourceAttr(dataSourceName, "project_id", projectID),
+				),
+			},
+		},
+	})
+}
+
+// Plural Data Source Tests
+func TestAccConfigDSAlertConfigurations_basic(t *testing.T) {
+	var (
+		projectID = acc.ProjectIDExecution(t)
+	)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { acc.PreCheckBasic(t) },
+		ProtoV6ProviderFactories: acc.TestAccProviderV6Factories,
+		Steps: []resource.TestStep{
+			{
+				Config: configBasicPluralDS(projectID),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					checkCount(dataSourcePluralName),
+					resource.TestCheckResourceAttr(dataSourcePluralName, "project_id", projectID),
+					resource.TestCheckNoResourceAttr(dataSourcePluralName, "total_count"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccConfigDSAlertConfigurations_withOutputTypes(t *testing.T) {
+	var (
+		projectID   = acc.ProjectIDExecution(t)
+		outputTypes = []string{"resource_hcl", "resource_import"}
+	)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { acc.PreCheckBasic(t) },
+		ProtoV6ProviderFactories: acc.TestAccProviderV6Factories,
+		Steps: []resource.TestStep{
+			{
+				Config: configOutputType(projectID, outputTypes),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					checkCount(dataSourcePluralName),
+					resource.TestCheckResourceAttr(dataSourcePluralName, "project_id", projectID),
+					resource.TestCheckResourceAttr(dataSourcePluralName, "results.0.output.#", "2"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccConfigDSAlertConfigurations_invalidOutputTypeValue(t *testing.T) {
+	var (
+		projectID = acc.ProjectIDExecution(t)
+	)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { acc.PreCheckBasic(t) },
+		ProtoV6ProviderFactories: acc.TestAccProviderV6Factories,
+		Steps: []resource.TestStep{
+			{
+				Config:      configOutputType(projectID, []string{"resource_hcl", "invalid_type"}),
+				ExpectError: regexp.MustCompile("value must be one of:"),
+			},
+		},
+	})
+}
+
+func TestAccConfigDSAlertConfigurations_totalCount(t *testing.T) {
+	var (
+		projectID = acc.ProjectIDExecution(t)
+	)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { acc.PreCheckBasic(t) },
+		ProtoV6ProviderFactories: acc.TestAccProviderV6Factories,
+		Steps: []resource.TestStep{
+			{
+				Config: configTotalCount(projectID),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					checkCount(dataSourcePluralName),
+					resource.TestCheckResourceAttr(dataSourcePluralName, "project_id", projectID),
+					resource.TestCheckResourceAttrSet(dataSourcePluralName, "total_count"),
+				),
+			},
+		},
+	})
+}
+
+func configWithThreshold(projectID string, enabled bool, threshold float64) string {
+	return fmt.Sprintf(`
+		resource "mongodbatlas_alert_configuration" "test" {
+			project_id = %[1]q
+			enabled    = %[2]t
+			event_type = "REPLICATION_OPLOG_WINDOW_RUNNING_OUT"
+
+			notification {
+				type_name     = "GROUP"
+				interval_min  = 5
+				delay_min     = 0
+				sms_enabled   = false
+				email_enabled = true
+				roles = ["GROUP_DATA_ACCESS_READ_ONLY", "GROUP_CLUSTER_MANAGER"]
+			}
+
+			matcher {
+				field_name = "REPLICA_SET_NAME"
+				operator   = "EQUALS"
+				value      = "SECONDARY"
+			}
+
+			threshold_config {
+				operator    = "LESS_THAN"
+				units       = "HOURS"
+				threshold   = %[3]f
+			}
+		}
+
+		data "mongodbatlas_alert_configuration" "test" {
+			project_id             = mongodbatlas_alert_configuration.test.project_id
+			alert_configuration_id = mongodbatlas_alert_configuration.test.id
+		}
+	`, projectID, enabled, threshold)
+}
+
+func configWithOutputs(projectID, outputLabel string) string {
+	return fmt.Sprintf(`
+		resource "mongodbatlas_alert_configuration" "test" {
+			project_id = %[1]q
+
+			event_type = "NO_PRIMARY"
+			enabled    = true
+
+			notification {
+				type_name     = "GROUP"
+				interval_min  = 5
+				delay_min     = 0
+				sms_enabled   = true
+				email_enabled = false
+				roles = ["GROUP_DATA_ACCESS_READ_ONLY"]
+			}
+		}
+
+		data "mongodbatlas_alert_configuration" "test" {
+			project_id             = mongodbatlas_alert_configuration.test.project_id
+			alert_configuration_id = mongodbatlas_alert_configuration.test.id
+
+			output {
+				type = "resource_import"
+				label = %[2]q
+			}
+			output {
+				type = "resource_hcl"
+				label = %[2]q
+			}
+		}
+	`, projectID, outputLabel)
+}
+
+func configWithPagerDutyDS(projectID, serviceKey string, enabled bool) string {
+	return fmt.Sprintf(`
+		resource "mongodbatlas_alert_configuration" "test" {
+			project_id = %[1]q
+			enabled    = "%[3]t"
+			event_type = "NO_PRIMARY"
+
+			notification {
+				type_name    = "PAGER_DUTY"
+				service_key  = %[2]q
+				delay_min    = 0
+			}
+		}
+
+		data "mongodbatlas_alert_configuration" "test" {
+			project_id             = mongodbatlas_alert_configuration.test.project_id
+			alert_configuration_id = mongodbatlas_alert_configuration.test.id
+		}
+	`, projectID, serviceKey, enabled)
+}
+
+func configBasicPluralDS(projectID string) string {
+	return fmt.Sprintf(`
+		data "mongodbatlas_alert_configurations" "test" {
+			project_id = %[1]q
+
+			list_options {
+				page_num = 0
+			}
+		}
+	`, projectID)
+}
+
+func configOutputType(projectID string, outputTypes []string) string {
+	return fmt.Sprintf(`
+		data "mongodbatlas_alert_configurations" "test" {
+			project_id = %[1]q
+			output_type = %[2]s
+		}
+	`, projectID, strings.ReplaceAll(fmt.Sprintf("%+q", outputTypes), " ", ","))
+}
+
+func configTotalCount(projectID string) string {
+	return fmt.Sprintf(`
+		data "mongodbatlas_alert_configurations" "test" {
+			project_id = %[1]q
+
+			list_options {
+				include_count = true
+			}
+		}
+	`, projectID)
+}
+
+func checkCount(resourceName string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[resourceName]
+		if !ok {
+			return fmt.Errorf("not found: %s", resourceName)
+		}
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("no ID is set")
+		}
+		ids := conversion.DecodeStateID(rs.Primary.ID)
+		projectID := ids["project_id"]
+
+		alertResp, _, err := acc.ConnV2().AlertConfigurationsApi.ListAlertConfigurations(context.Background(), projectID).Execute()
+
+		if err != nil {
+			return fmt.Errorf("the Alert Configurations List for project (%s) could not be read", projectID)
+		}
+
+		resultsCountAttr := rs.Primary.Attributes["results.#"]
+		var resultsCount int
+		if resultsCount, err = strconv.Atoi(resultsCountAttr); err != nil {
+			return fmt.Errorf("%s results count is somehow not a number %s", resourceName, resultsCountAttr)
+		}
+
+		if resultsCount != len(alertResp.GetResults()) {
+			return fmt.Errorf("%s results count (%d) did not match that of current Alert Configurations (%d)", resourceName, resultsCount, len(alertResp.GetResults()))
+		}
+
+		if totalCountAttr := rs.Primary.Attributes["total_count"]; totalCountAttr != "" {
+			var totalCount int
+			if totalCount, err = strconv.Atoi(totalCountAttr); err != nil {
+				return fmt.Errorf("%s total count is somehow not a number %s", resourceName, totalCountAttr)
+			}
+			if totalCount != resultsCount {
+				return fmt.Errorf("%s total count (%d) did not match that of results count (%d)", resourceName, totalCount, resultsCount)
+			}
+		}
+
+		return nil
+	}
 }
