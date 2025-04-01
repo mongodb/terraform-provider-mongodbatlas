aee94c57f4324973bf66e2d3ffa5785092d2996b
Leo Antoli <430982+lantoli@users.noreply.github.com>
2025-04-01 08:12:43 +0200

    fix linter

--- internal/common/autogeneration/marshal_test.go
+++ internal/common/autogeneration/marshal_test.go
@@ -9,37 +9,38 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestUnmarshal(t *testing.T) {
-	var model tfModelTest
+func TestUnmarshalBasic(t *testing.T) {
+	var model struct {
+		AttributeFloat        types.Float64 `tfsdk:"attribute_float"`
+		AttributeFloatWithInt types.Float64 `tfsdk:"attribute_float_with_int"`
+		AttributeString       types.String  `tfsdk:"attribute_string"`
+		AttributeInt          types.Int64   `tfsdk:"attribute_int"`
+		AttributeIntWithFloat types.Int64   `tfsdk:"attribute_int_with_float"`
+		AttributeTrue         types.Bool    `tfsdk:"attribute_true"`
+		AttributeFalse        types.Bool    `tfsdk:"attribute_false"`
+	}
+	const (
+		epsilon = 10e-15 // float tolerance
+		// attribute_not_in_model is ignored but not an error
+		tfResponseJSON = `
+			{
+				"attribute_string": "value_string",
+				"attribute_true": true,
+				"attribute_false": false,
+				"attribute_int": 123,
+				"attribute_int_with_float": 10.6,
+				"attribute_float": 456.1,
+				"attribute_float_with_int": 13,
+				"attribute_not_in_model": "val"
+			}
+		`
+	)
 	require.NoError(t, autogeneration.Unmarshal([]byte(tfResponseJSON), &model))
 	assert.Equal(t, "value_string", model.AttributeString.ValueString())
 	assert.True(t, model.AttributeTrue.ValueBool())
 	assert.False(t, model.AttributeFalse.ValueBool())
 	assert.Equal(t, int64(123), model.AttributeInt.ValueInt64())
 	assert.Equal(t, int64(10), model.AttributeIntWithFloat.ValueInt64())
-	assert.Equal(t, float64(456.1), model.AttributeFloat.ValueFloat64())
-	assert.Equal(t, float64(13), model.AttributeFloatWithInt.ValueFloat64())
+	assert.InEpsilon(t, float64(456.1), model.AttributeFloat.ValueFloat64(), epsilon)
+	assert.InEpsilon(t, float64(13), model.AttributeFloatWithInt.ValueFloat64(), epsilon)
 }
-
-type tfModelTest struct {
-	AttributeString       types.String  `tfsdk:"attribute_string"`
-	AttributeTrue         types.Bool    `tfsdk:"attribute_true"`
-	AttributeFalse        types.Bool    `tfsdk:"attribute_false"`
-	AttributeInt          types.Int64   `tfsdk:"attribute_int"`
-	AttributeIntWithFloat types.Int64   `tfsdk:"attribute_int_with_float"`
-	AttributeFloat        types.Float64 `tfsdk:"attribute_float"`
-	AttributeFloatWithInt types.Float64 `tfsdk:"attribute_float_with_int"`
-}
-
-const tfResponseJSON = `
-{
-	"attribute_string": "value_string",
-	"attribute_true": true,
-	"attribute_false": false,
-	"attribute_int": 123,
-	"attribute_int_with_float": 10.6,
-	"attribute_float": 456.1,
-	"attribute_float_with_int": 13,
-	"attribute_not_in_model": "val"
-}
-`
