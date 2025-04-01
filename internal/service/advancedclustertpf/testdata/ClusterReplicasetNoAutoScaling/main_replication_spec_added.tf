fe206fc47b7158aaedc29adc8b62f8ce9c6fb783
Espen Albert <espen.albert@mongodb.com>
2025-04-01 12:37:58 +0200

    refactor: Add test case for replication_spec added

---
+++ internal/service/advancedclustertpf/testdata/ClusterReplicasetNoAutoScaling/main_replication_spec_added.tf
@@ -0,0 +1,30 @@
+resource "mongodbatlas_advanced_cluster" "test" {
+  project_id   = "111111111111111111111111"
+  name         = "mocked-cluster"
+  cluster_type = "SHARDED"
+
+  replication_specs = [{
+    region_configs = [{
+      electable_specs = {
+        instance_size = "M10"
+        node_count    = 3
+      }
+      priority      = 7
+      provider_name = "AWS"
+      region_name   = "US_EAST_1"
+    }]
+    zone_name = "Zone 1"
+  },
+  {
+    region_configs = [{
+      electable_specs = {
+        instance_size = "M10"
+        node_count    = 3
+      }
+      priority      = 7
+      provider_name = "AWS"
+      region_name   = "US_EAST_1"
+    }]
+    zone_name = "Zone 1"
+  }]
+}
