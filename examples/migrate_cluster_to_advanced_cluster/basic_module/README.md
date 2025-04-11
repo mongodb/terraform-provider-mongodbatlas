8582cca6602ad95d26bd4be045f9133dcbca0acc
Espen Albert <espen.albert@mongodb.com>
2025-04-11 10:36:53 +0100

    feat: add call to basic module for mongodbatlas_advanced_cluster

---
+++ examples/migrate_cluster_to_advanced_cluster/basic_module/README.md
@@ -0,0 +1,1 @@
+# Basic module for mongodbatlas_advanced_cluster (Preview for MongoDB Atlas Provider 2.0.0)
---
+++ examples/migrate_cluster_to_advanced_cluster/basic_module/main.tf
@@ -0,0 +1,20 @@
+provider "mongodbatlas" {
+  public_key  = var.public_key
+  private_key = var.private_key
+}
+
+module "cluster" {
+  source = "./modules/cluster"
+
+  cluster_name           = var.cluster_name
+  cluster_type           = var.cluster_type
+  mongo_db_major_version = var.mongo_db_major_version
+  project_id             = var.project_id
+  replication_specs      = var.replication_specs
+  tags                   = var.tags
+}
+
+output "mongodb_connection_strings" {
+  description = "Collection of Uniform Resource Locators that point to the MongoDB database."
+  value       = module.cluster.mongodb_connection_strings
+}
