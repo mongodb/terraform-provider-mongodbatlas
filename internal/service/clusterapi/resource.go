f6f5b27440b44df6e69174a46d19ad2399f3e03a
Agustin Bettati <bettatiagustin@gmail.com>
2025-04-25 16:44:39 +0200

    adjust template to use state for path param values in patch

--- internal/service/clusterapi/resource.go
+++ internal/service/clusterapi/resource.go
@@ -74,13 +74,16 @@ func (r *rs) Read(ctx context.Context, req resource.ReadRequest, resp *resource.
 
 func (r *rs) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
 	var plan TFModel
+	var state TFModel
 	resp.Diagnostics.Append(req.Plan.Get(ctx, &plan)...)
+	resp.Diagnostics.Append(req.State.Get(ctx, &state)...)
 	if resp.Diagnostics.HasError() {
 		return
 	}
+	// Path params are grabbed from state as they may be computed-only and not present in the plan
 	pathParams := map[string]string{
-		"groupId": plan.GroupId.ValueString(),
-		"name":    plan.Name.ValueString(),
+		"groupId": state.GroupId.ValueString(),
+		"name":    state.Name.ValueString(),
 	}
 	callParams := config.APICallParams{
 		VersionHeader: apiVersionHeader,
