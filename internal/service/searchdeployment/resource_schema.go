35b847633b44e26cec907c8739d73374c6f590ad
jwongmongodb <116834447+jwongmongodb@users.noreply.github.com>
2025-04-29 09:13:50 -0400

    doc: The commit adds Azure Streams PrivateLink documentation and examples (#3299)

--- internal/service/searchdeployment/resource_schema.go
+++ internal/service/searchdeployment/resource_schema.go
@@ -76,14 +76,14 @@ func ResourceSchema(ctx context.Context) schema.Schema {
 }
 
 type TFSearchDeploymentRSModel struct {
+	Specs                    types.List     `tfsdk:"specs"`
 	ID                       types.String   `tfsdk:"id"`
 	ClusterName              types.String   `tfsdk:"cluster_name"`
 	ProjectID                types.String   `tfsdk:"project_id"`
-	Specs                    types.List     `tfsdk:"specs"`
 	StateName                types.String   `tfsdk:"state_name"`
 	Timeouts                 timeouts.Value `tfsdk:"timeouts"`
-	SkipWaitOnUpdate         types.Bool     `tfsdk:"skip_wait_on_update"`
 	EncryptionAtRestProvider types.String   `tfsdk:"encryption_at_rest_provider"`
+	SkipWaitOnUpdate         types.Bool     `tfsdk:"skip_wait_on_update"`
 }
 
 type TFSearchNodeSpecModel struct {
