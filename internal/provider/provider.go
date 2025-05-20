4f6a0d0ea881ef0035b856d48d7c76ed440d9ebf
Leo Antoli <430982+lantoli@users.noreply.github.com>
2025-05-20 16:31:12 +0200

    TEMPORARY check catching api resources

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -46,6 +46,7 @@ import (
 	"github.com/mongodb/terraform-provider-mongodbatlas/internal/service/streaminstance"
 	"github.com/mongodb/terraform-provider-mongodbatlas/internal/service/streamprivatelinkendpoint"
 	"github.com/mongodb/terraform-provider-mongodbatlas/internal/service/streamprocessor"
+	"github.com/mongodb/terraform-provider-mongodbatlas/internal/serviceapi/customdbroleapi"
 	"github.com/mongodb/terraform-provider-mongodbatlas/version"
 )
 
@@ -478,6 +479,7 @@ func (p *MongodbtlasProvider) Resources(context.Context) []func() resource.Resou
 		streamprivatelinkendpoint.Resource,
 		flexcluster.Resource,
 		resourcepolicy.Resource,
+		customdbroleapi.Resource,
 	}
 	if config.PreviewProviderV2AdvancedCluster() {
 		resources = append(resources, advancedclustertpf.Resource)
