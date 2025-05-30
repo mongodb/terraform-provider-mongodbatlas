cd358c3634ae63878a9922de9465667570ec590e
Kush Patel <kush.patel@mongodb.com>
2025-05-30 06:55:55 -0500

    test: setup stream processor to fix acc test

--- internal/service/streamaccountdetails/data_source_test.go
+++ internal/service/streamaccountdetails/data_source_test.go
@@ -10,15 +10,16 @@ import (
 
 func TestAccStreamAccountDetailsDS_basic(t *testing.T) {
 	var (
-		projectID      = acc.ProjectIDExecution(t)
-		dataSourceName = "data.mongodbatlas_stream_account_details.test"
+		projectID, clusterName = acc.ClusterNameExecution(t, false)
+		dataSourceName         = "data.mongodbatlas_stream_account_details.test_details"
 	)
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:                 func() { acc.PreCheckBasic(t) },
 		ProtoV6ProviderFactories: acc.TestAccProviderV6Factories,
+		CheckDestroy:             acc.CheckDestroyStreamInstance,
 		Steps: []resource.TestStep{
 			{
-				Config: StreamAccountDetailsConfig(projectID, "aws", "US_EAST_1"),
+				Config: StreamAccountDetailsConfig(projectID, "aws", "US_EAST_1", clusterName),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttrSet(dataSourceName, "project_id"),
 					resource.TestCheckResourceAttrSet(dataSourceName, "aws_account_id"),
@@ -26,30 +27,52 @@ func TestAccStreamAccountDetailsDS_basic(t *testing.T) {
 					resource.TestCheckResourceAttrSet(dataSourceName, "vpc_id"),
 					resource.TestCheckResourceAttrSet(dataSourceName, "azure_subscription_id"),
 					resource.TestCheckResourceAttrSet(dataSourceName, "virtual_network_name"),
-				),
-			},
-			{
-				Config: StreamAccountDetailsConfig(projectID, "azure", "EASTUS"),
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttrSet(dataSourceName, "project_id"),
-					resource.TestCheckResourceAttrSet(dataSourceName, "azure_subscription_id"),
-					resource.TestCheckResourceAttrSet(dataSourceName, "cidr_block"),
-					resource.TestCheckResourceAttrSet(dataSourceName, "virtual_network_name"),
-					resource.TestCheckResourceAttrSet(dataSourceName, "aws_account_id"),
-					resource.TestCheckResourceAttrSet(dataSourceName, "vpc_id"),
 				),
 			},
 		},
 	})
 }
 
-func StreamAccountDetailsConfig(projectID, cloudProvider, regionName string) string {
+func StreamAccountDetailsConfig(projectID, cloudProvider, regionName, clusterName string) string {
+	instanceName := acc.RandomName()
+	streamInstanceConfig := acc.StreamInstanceConfig(projectID, instanceName, "VIRGINIA_USA", "AWS")
+
 	return fmt.Sprintf(`
-	
-	data "mongodbatlas_stream_account_details" "test" {
-  		project_id 		= %[1]q
-  		cloud_provider	= %[2]q
-  		region_name 	= %[3]q
+	%[1]s
+
+	resource "mongodbatlas_stream_connection" "test_connection" {
+		    project_id 			= resource.mongodbatlas_stream_instance.test.project_id
+			instance_name 		= resource.mongodbatlas_stream_instance.test.instance_name
+		 	connection_name 	= %[2]q
+			type            	= "Cluster"
+			cluster_name    	= %[3]q
+			db_role_to_execute 	= {
+				role = "atlasAdmin"
+				type = "BUILT_IN"
+			}
+		}
+		
+		resource "mongodbatlas_stream_processor" "test_processor" {
+			project_id     	= resource.mongodbatlas_stream_instance.test.project_id
+			instance_name  	= resource.mongodbatlas_stream_instance.test.instance_name
+			processor_name 	= "testProcessor"
+			pipeline       	= jsonencode([
+				{ "$source" = { "connectionName" = resource.mongodbatlas_stream_connection.test_connection.connection_name } },
+				{ "$merge"	= {
+					"into" = {
+						"connectionName" = resource.mongodbatlas_stream_connection.test_connection.connection_name,
+						"db" = "randomDb",
+						"coll" = "randomColl"
+					}
+				} }
+			])
+			state          	= "STARTED"
+		}
+
+	data "mongodbatlas_stream_account_details" "test_details" {
+  		project_id 		= resource.mongodbatlas_stream_instance.test.project_id
+  		cloud_provider	= %[4]q
+  		region_name 	= %[5]q
 	}
-`, projectID, cloudProvider, regionName)
+`, streamInstanceConfig, acc.RandomName(), clusterName, cloudProvider, regionName)
 }
