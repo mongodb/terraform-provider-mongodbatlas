d68f832225f149144b85335544451b5b265279de
Kush Patel <kush.patel@mongodb.com>
2025-06-03 05:17:37 -0500

    test: use getClusterInfo to create cluster in us-east-1

--- internal/service/streamaccountdetails/data_source_test.go
+++ internal/service/streamaccountdetails/data_source_test.go
@@ -10,17 +10,22 @@ import (
 
 func TestAccStreamAccountDetailsDS_basic(t *testing.T) {
 	var (
-		projectID, clusterName = acc.ClusterNameExecution(t, false)
-		dataSourceName         = "data.mongodbatlas_stream_account_details.test_details"
+		dataSourceName = "data.mongodbatlas_stream_account_details.test_details"
+		clusterRequest = acc.ClusterRequest{
+			ReplicationSpecs: []acc.ReplicationSpecRequest{
+				{Region: "US_EAST_1"}, // A cluster in a region supported by Streams tenants is required
+			},
+		}
+		clusterInfo = acc.GetClusterInfo(t, &clusterRequest)
 	)
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:                 func() { acc.PreCheckBasic(t) },
+		PreCheck:                 acc.PreCheckBasicSleep(t, &clusterInfo, "", ""),
 		ProtoV6ProviderFactories: acc.TestAccProviderV6Factories,
 		Steps: []resource.TestStep{
 			{
-				Config: StreamAccountDetailsConfig(projectID, "aws", "US_EAST_1", clusterName),
+				Config: StreamAccountDetailsConfig(clusterInfo.TerraformStr, clusterInfo.ProjectID, "aws", "US_EAST_1", clusterInfo.ResourceName),
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr(dataSourceName, "project_id", projectID),
+					resource.TestCheckResourceAttr(dataSourceName, "project_id", clusterInfo.ProjectID),
 					resource.TestCheckResourceAttrSet(dataSourceName, "aws_account_id"),
 					resource.TestCheckResourceAttrSet(dataSourceName, "vpc_id"),
 					resource.TestCheckNoResourceAttr(dataSourceName, "azure_subscription_id"),
@@ -31,50 +36,17 @@ func TestAccStreamAccountDetailsDS_basic(t *testing.T) {
 	})
 }
 
-func StreamAccountDetailsConfig(projectID, cloudProvider, regionName, clusterName string) string {
-	instanceName := acc.RandomName()
-	streamInstanceConfig := acc.StreamInstanceConfig(projectID, instanceName, "VIRGINIA_USA", "AWS")
-
+func StreamAccountDetailsConfig(clusterInfoStr, projectID, cloudProvider, regionName, clusterInfoNameRef string) string {
 	return fmt.Sprintf(`
-	%[1]s
-
-	resource "mongodbatlas_stream_connection" "test_connection" {
-		    project_id 			= resource.mongodbatlas_stream_instance.test.project_id
-			instance_name 		= resource.mongodbatlas_stream_instance.test.instance_name
-		 	connection_name 	= %[2]q
-			type            	= "Cluster"
-			cluster_name    	= %[3]q
-			db_role_to_execute 	= {
-				role = "atlasAdmin"
-				type = "BUILT_IN"
-			}
-		}
-		
-		resource "mongodbatlas_stream_processor" "test_processor" {
-			project_id     	= resource.mongodbatlas_stream_instance.test.project_id
-			instance_name  	= resource.mongodbatlas_stream_instance.test.instance_name
-			processor_name 	= "testProcessor"
-			pipeline       	= jsonencode([
-				{ "$source" = { "connectionName" = resource.mongodbatlas_stream_connection.test_connection.connection_name } },
-				{ "$merge"	= {
-					"into" = {
-						"connectionName" = resource.mongodbatlas_stream_connection.test_connection.connection_name,
-						"db" = "randomDb",
-						"coll" = "randomColl"
-					}
-				} }
-			])
-			state          	= "STARTED"
-		}
+		%[1]s
 
 		data "mongodbatlas_stream_account_details" "test_details" {
-			project_id 		= resource.mongodbatlas_stream_processor.test_processor.project_id
-			cloud_provider	= %[4]q
-			region_name 	= %[5]q
-			# The getAccountDetails endpoint curerntly requires a processor in a STARTED state
+			project_id 		= %[2]q
+			cloud_provider	= %[3]q
+			region_name 	= %[4]q
 			depends_on = [
-				mongodbatlas_stream_processor.test_processor
+				%[5]q
 			]
 		}
-`, streamInstanceConfig, acc.RandomName(), clusterName, cloudProvider, regionName)
+`, clusterInfoStr, projectID, cloudProvider, regionName, clusterInfoNameRef)
 }
