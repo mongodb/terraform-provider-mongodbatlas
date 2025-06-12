8d43c60df527b59bd819219b3eca8bac06b6aaac
Espen Albert <espen.albert@mongodb.com>
2025-06-12 10:16:53 +0100

    main.tf fixes

--- examples/mongodbatlas_backup_compliance_policy/module/main.tf
+++ examples/mongodbatlas_backup_compliance_policy/module/main.tf
@@ -30,20 +30,20 @@ module "cluster_with_schedule" {
   project_id    = var.project_id
   instance_size = var.instance_size
   cluster_name  = var.cluster_name
-  add_schedule  = false # change to false in Step 2
+  add_schedule  = true # change to false in Step 2
 }
 
 # Step 2: For removing the `mongodbatlas_cloud_backup_schedule` resource
 # Rename the resource to avoid the `Removed Resource still exists error`
-moved {
-  from = module.cluster_with_schedule.mongodbatlas_cloud_backup_schedule.this[0] # must be deleted with the `add_schedule` variable set to false
-  to   = mongodbatlas_cloud_backup_schedule.deleted                              # any resource name that doesn't exist works!
-}
+# moved {
+#   from = module.cluster_with_schedule.mongodbatlas_cloud_backup_schedule.this[0] # must be deleted with the `add_schedule` variable set to false
+#   to   = mongodbatlas_cloud_backup_schedule.to_be_deleted                              # any resource name that doesn't exist works!
+# }
 
-removed {
-  from = mongodbatlas_cloud_backup_schedule.deleted # any resource name that doesn't exist works!
+# removed {
+#   from = mongodbatlas_cloud_backup_schedule.to_be_deleted # any resource name that doesn't exist works!
 
-  lifecycle {
-    destroy = false
-  }
-}
+#   lifecycle {
+#     destroy = false
+#   }
+# }
