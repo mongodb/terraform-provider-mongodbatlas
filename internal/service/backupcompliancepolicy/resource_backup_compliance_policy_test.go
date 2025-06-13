2575215103a66ec25600a8aace1b8a6e6cb15ba6
Espen Albert <EspenAlbert@users.noreply.github.com>
2025-06-13 09:43:06 +0100

    chore: Avoids flaky tests by sleeping 30s (#3403)
    * test: avoid flaky tests by sleeping 30s
    
    * Update internal/service/backupcompliancepolicy/resource_backup_compliance_policy_test.go
    
    Co-authored-by: Leo Antoli <430982+lantoli@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Leo Antoli <430982+lantoli@users.noreply.github.com>

--- internal/service/backupcompliancepolicy/resource_backup_compliance_policy_test.go
+++ internal/service/backupcompliancepolicy/resource_backup_compliance_policy_test.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"regexp"
 	"testing"
+	"time"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
@@ -198,6 +199,7 @@ func checkExists(resourceName string) resource.TestCheckFunc {
 		if err != nil || policy == nil {
 			return fmt.Errorf("backup compliance policy (%s) does not exist: %s", rs.Primary.ID, err)
 		}
+		time.Sleep(30 * time.Second) // Wait for the bcp to be fully applied, see more details in CLOUDP-324378.
 		return nil
 	}
 }
