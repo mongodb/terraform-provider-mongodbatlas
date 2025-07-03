3d668cef353d3e8c5436b06874e536bb11c88f1c
Oriol Arbusi Abadal <oriol.abadal@mongodb.com>
2025-07-03 09:59:55 +0200

    return all in plural data source

--- internal/service/apikeyprojectassignment/plural_data_source.go
+++ internal/service/apikeyprojectassignment/plural_data_source.go
@@ -3,10 +3,14 @@ package apikeyprojectassignment
 import (
 	"context"
 	"fmt"
+	"net/http"
+
+	"go.mongodb.org/atlas-sdk/v20250312005/admin"
 
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
 
 	"github.com/mongodb/terraform-provider-mongodbatlas/internal/common/conversion"
+	"github.com/mongodb/terraform-provider-mongodbatlas/internal/common/dsschema"
 	"github.com/mongodb/terraform-provider-mongodbatlas/internal/config"
 )
 
@@ -40,13 +44,22 @@ func (d *pluralDS) Read(ctx context.Context, req datasource.ReadRequest, resp *d
 
 	connV2 := d.Client.AtlasV2
 	projectID := tfModel.ProjectId.ValueString()
-	apiKeys, _, err := connV2.ProgrammaticAPIKeysApi.ListProjectApiKeys(ctx, projectID).Execute()
+
+	params := &admin.ListProjectApiKeysApiParams{
+		GroupId: tfModel.ProjectId.ValueString(),
+	}
+
+	apiKeys, err := dsschema.AllPages(ctx, func(ctx context.Context, pageNum int) (dsschema.PaginateResponse[admin.ApiKeyUserDetails], *http.Response, error) {
+		request := connV2.ProgrammaticAPIKeysApi.ListProjectApiKeysWithParams(ctx, params)
+		request = request.PageNum(pageNum)
+		return request.Execute()
+	})
 	if err != nil {
 		resp.Diagnostics.AddError("error reading plural data source", err.Error())
 		return
 	}
 
-	newAPIKeyProjectAssignmentsModel, diags := NewTFModelDSP(ctx, projectID, apiKeys.GetResults())
+	newAPIKeyProjectAssignmentsModel, diags := NewTFModelDSP(ctx, projectID, apiKeys)
 	if diags.HasError() {
 		resp.Diagnostics.Append(diags...)
 		return
