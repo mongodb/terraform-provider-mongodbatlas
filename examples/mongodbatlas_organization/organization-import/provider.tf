a0a58c6ba33535bb996d9cc0c00d1ef29f11ed9f
Leo Antoli <430982+lantoli@users.noreply.github.com>
2025-07-08 12:34:36 +0200

    fix example linter

--- examples/mongodbatlas_organization/organization-import/provider.tf
+++ examples/mongodbatlas_organization/organization-import/provider.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_providers {
-    mongodbatlas = {
-      source  = "mongodb/mongodbatlas"
-      version = "~> 1.10"
-    }
-  }
-}
-
-# Configure the MongoDB Atlas Provider
 provider "mongodbatlas" {
   public_key  = var.public_key
   private_key = var.private_key
