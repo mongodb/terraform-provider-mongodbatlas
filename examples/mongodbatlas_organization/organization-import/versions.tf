3c0959df4b1eee95c073d2249217548fd6054c5e
Leo Antoli <430982+lantoli@users.noreply.github.com>
2025-07-08 12:23:47 +0200

    fix version.tf

--- examples/mongodbatlas_organization/organization-import/versions.tf
+++ examples/mongodbatlas_organization/organization-import/versions.tf
@@ -1,3 +1,9 @@
 terraform {
-  required_version = ">= 0.13"
-} 
+  required_providers {
+    mongodbatlas = {
+      source  = "mongodb/mongodbatlas"
+      version = "~> 1.0"
+    }
+  }
+  required_version = ">= 1.0"
+}
