6ecb1991739e786d650f63e9a777745cacee0ad4
maastha <aastha.mahendru@mongodb.com>
2025-07-11 00:00:22 +0200

    min

--- internal/service/clouduserorgassignment/resource_migration_test.go
+++ internal/service/clouduserorgassignment/resource_migration_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/mongodb/terraform-provider-mongodbatlas/internal/testutil/mig"
 )
 
-func TestMigSearchIndex_withVector(t *testing.T) {
+func TestMigCloudUserOrgAssignmentRS_basic(t *testing.T) {
 	mig.SkipIfVersionBelow(t, "2.0.0") // when resource 1st released
 	mig.CreateAndRunTest(t, basicTestCase(t))
 }
