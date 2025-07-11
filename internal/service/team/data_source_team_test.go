1ebf07466b5bc07fa23b9464f24443a4eac624d8
Cristina Sánchez <c.sanchezprincipal@gmail.com>
2025-07-11 10:21:31 +0200

    feat: Adds `users` attribute to `mongodbatlas_team` singular data source. (#3483)
    * Extracted users attribute common schema to dsschema/users_schema.go
    
    * Added attribute users to team data source
    
    * Modified tests
    
    * Modified doc
    
    * Added changelog
    
    * Update docs/data-sources/team.md
    
    Co-authored-by: davidhou17 <55004296+davidhou17@users.noreply.github.com>
    
    * Update docs/data-sources/team.md
    
    Co-authored-by: davidhou17 <55004296+davidhou17@users.noreply.github.com>
    
    * Addressed comments
    
    * Fixed typos and added comment.
    
    * Refactor: extract regex matchers for tests, relocate and update function visibility
    
    ---------
    
    Co-authored-by: Cristina Sánchez Sánchez <cristina.sanchez@mongodb.com>
    Co-authored-by: davidhou17 <55004296+davidhou17@users.noreply.github.com>

--- internal/service/team/data_source_team_test.go
+++ internal/service/team/data_source_team_test.go
@@ -29,6 +29,11 @@ func TestAccConfigDSTeam_basic(t *testing.T) {
 					resource.TestCheckResourceAttrSet(dataSourceName, "team_id"),
 					resource.TestCheckResourceAttr(dataSourceName, "name", name),
 					resource.TestCheckResourceAttr(dataSourceName, "usernames.#", "1"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "users.0.team_ids.0"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "users.0.roles.0.project_roles_assignments.#"),
+					resource.TestCheckResourceAttrWith(dataSourceName, "users.0.username", acc.IsUsername()),
+					resource.TestCheckResourceAttrWith(dataSourceName, "users.0.last_auth", acc.IsTimestamp()),
+					resource.TestCheckResourceAttrWith(dataSourceName, "users.0.created_at", acc.IsTimestamp()),
 				),
 			},
 		},
@@ -61,12 +66,38 @@ func TestAccConfigDSTeamByName_basic(t *testing.T) {
 	})
 }
 
+func TestAccConfigDSTeam_NoUsers(t *testing.T) {
+	var (
+		dataSourceName = "data.mongodbatlas_team.test3"
+		orgID          = os.Getenv("MONGODB_ATLAS_ORG_ID")
+		name           = acc.RandomName()
+	)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:                 func() { acc.PreCheckAtlasUsername(t) },
+		ProtoV6ProviderFactories: acc.TestAccProviderV6Factories,
+		CheckDestroy:             acc.CheckDestroyTeam,
+		Steps: []resource.TestStep{
+			{
+				Config: dataSourceConfigNoUsers(orgID, name),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrSet(dataSourceName, "org_id"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "team_id"),
+					resource.TestCheckResourceAttr(dataSourceName, "name", name),
+					resource.TestCheckResourceAttr(dataSourceName, "usernames.#", "0"),
+					resource.TestCheckResourceAttr(dataSourceName, "users.#", "0"),
+				),
+			},
+		},
+	})
+}
+
 func dataSourceConfigBasic(orgID, name, username string) string {
 	return fmt.Sprintf(`
 		resource "mongodbatlas_team" "test" {
-			org_id     = "%s"
-			name       = "%s"
-			usernames  = ["%s"]
+			org_id     = %[1]q
+			name       = %[2]q
+			usernames  = [%[3]q]
 		}
 
 		data "mongodbatlas_team" "test" {
@@ -80,9 +111,9 @@ func dataSourceConfigBasic(orgID, name, username string) string {
 func dataSourceConfigBasicByName(orgID, name, username string) string {
 	return fmt.Sprintf(`
 		resource "mongodbatlas_team" "test" {
-			org_id     = "%s"
-			name       = "%s"
-			usernames  = ["%s"]
+			org_id     = %[1]q
+			name       = %[2]q
+			usernames  = [%[3]q]
 		}
 
 		data "mongodbatlas_team" "test2" {
@@ -90,4 +121,20 @@ func dataSourceConfigBasicByName(orgID, name, username string) string {
 			name    = mongodbatlas_team.test.name
 		}
 	`, orgID, name, username)
+}
+
+func dataSourceConfigNoUsers(orgID, name string) string {
+	return fmt.Sprintf(`
+		resource "mongodbatlas_team" "test" {
+			org_id     = %[1]q
+			name       = %[2]q
+			usernames  = []
+		}
+
+		data "mongodbatlas_team" "test3" {
+			org_id     = mongodbatlas_team.test.org_id
+			team_id    = mongodbatlas_team.test.team_id
+		}
+
+	`, orgID, name)
 }
