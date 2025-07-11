86d712ece3595756064bebec9d201c7918dc2811
Leo Antoli <430982+lantoli@users.noreply.github.com>
2025-07-11 08:43:23 +0200

    adjust provider version for organization examples (#3492)

--- examples/mongodbatlas_organization/organization-import/versions.tf
+++ examples/mongodbatlas_organization/organization-import/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     mongodbatlas = {
       source  = "mongodb/mongodbatlas"
-      version = "~> 1.0"
+      version = "~> 1.38"
     }
   }
   required_version = ">= 1.0"
