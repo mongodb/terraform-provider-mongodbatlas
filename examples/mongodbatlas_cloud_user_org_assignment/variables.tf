faaf64bc7969a17b982ab1c77ca10b3120f5bfb1
maastha <aastha.mahendru@mongodb.com>
2025-07-12 01:47:22 +0200

    min

--- examples/mongodbatlas_cloud_user_org_assignment/variables.tf
+++ examples/mongodbatlas_cloud_user_org_assignment/variables.tf
@@ -1,19 +1,24 @@
-variable "public_key" {
-  description = "MongoDB Atlas API public key."
-  type        = string
-}
-
-variable "private_key" {
-  description = "MongoDB Atlas API private key."
-  type        = string
-}
-
 variable "org_id" {
-  description = "The ID of the MongoDB Atlas organization."
+  description = "The MongoDB Atlas organization ID"
   type        = string
 }
 
 variable "user_email" {
-  description = "The email address of the user to assign."
+  description = "The email address of the user"
   type        = string
-} 
+}
+
+variable "user_id" {
+  description = "The user ID"
+  type        = string
+}
+
+variable "public_key" {
+  description = "Atlas API public key"
+  type        = string
+}
+
+variable "private_key" {
+  description = "Atlas API private key"
+  type        = string
+}
