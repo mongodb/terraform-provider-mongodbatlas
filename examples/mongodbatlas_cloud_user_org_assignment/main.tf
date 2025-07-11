5f01691970f7ec9d061dc0354dd2ad2fe1039439
maastha <aastha.mahendru@mongodb.com>
2025-07-12 01:31:07 +0200

    min

--- examples/mongodbatlas_cloud_user_org_assignment/main.tf
+++ examples/mongodbatlas_cloud_user_org_assignment/main.tf
@@ -6,12 +6,12 @@ resource "mongodbatlas_cloud_user_org_assignment" "example" {
   }
 } 
 
-data "mongodbatlas_cloud_user_org_assignment" "example" {
+data "mongodbatlas_cloud_user_org_assignment" "example_username" {
   org_id   = var.org_id
   username = var.user_email
 }
 
-data "mongodbatlas_cloud_user_org_assignment" "example" {
+data "mongodbatlas_cloud_user_org_assignment" "example_user_id" {
   org_id   = var.org_id
   user_id  = var.user_id
 }
