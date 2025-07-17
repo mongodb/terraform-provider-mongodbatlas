c0c7e4611464f635c28d7989d55b2fefdcb8c4dc
Espen Albert <EspenAlbert@users.noreply.github.com>
2025-07-17 14:31:34 +0100

    refactor: Updates replication_specs blocks to use latest schema version (#3504)
    * refactor: update replication_specs blocks to use latest schema version
    
    * fix: correct string comparison in tf validation script to use exact matches

--- examples/modules/direct-resource/single-region-sharded.tf
+++ examples/modules/direct-resource/single-region-sharded.tf
@@ -6,39 +6,44 @@ resource "mongodbatlas_advanced_cluster" "single_region_sharded" {
   name                   = "single-region-sharded"
   cluster_type           = "SHARDED"
   mongo_db_major_version = "8.0"
-  replication_specs { # shard 1 (single zone)
-    region_configs {
-      auto_scaling {
-        disk_gb_enabled           = true
-        compute_enabled           = true
-        compute_max_instance_size = "M60"
-        compute_min_instance_size = "M30"
-      }
-      electable_specs {
-        instance_size = "M40" # Independently scaled shard
-        node_count    = 3
-      }
-      priority      = 7
-      provider_name = "AWS"
-      region_name   = "US_EAST_1"
+  replication_specs = [
+    { # shard 1 (single zone)
+      region_configs = [
+        {
+          auto_scaling = {
+            disk_gb_enabled           = true
+            compute_enabled           = true
+            compute_max_instance_size = "M60"
+            compute_min_instance_size = "M30"
+          }
+          electable_specs = {
+            instance_size = "M40" # Independently scaled shard
+            node_count    = 3
+          }
+          priority      = 7
+          provider_name = "AWS"
+          region_name   = "US_EAST_1"
+        }
+      ]
+    },
+    { # shard 2 (single zone)
+      region_configs = [
+        {
+          auto_scaling = {
+            disk_gb_enabled           = true
+            compute_enabled           = true
+            compute_max_instance_size = "M60"
+            compute_min_instance_size = "M30"
+          }
+          electable_specs = {
+            instance_size = "M30"
+            node_count    = 3
+          }
+          priority      = 7
+          provider_name = "AWS"
+          region_name   = "US_EAST_1"
+        }
+      ]
     }
-  }
-
-  replication_specs { # shard 2 (single zone)
-    region_configs {
-      auto_scaling {
-        disk_gb_enabled           = true
-        compute_enabled           = true
-        compute_max_instance_size = "M60"
-        compute_min_instance_size = "M30"
-      }
-      electable_specs {
-        instance_size = "M30"
-        node_count    = 3
-      }
-      priority      = 7
-      provider_name = "AWS"
-      region_name   = "US_EAST_1"
-    }
-  }
+  ]
 }
--- scripts/tf-validate.sh
+++ scripts/tf-validate.sh
@@ -40,16 +40,16 @@ is_v2_dir() {
   local grand_parent_dir
   parent_dir=$(basename "$1")
   grand_parent_dir=$(basename "$(dirname "$1")")
-  local v2_parent_dirs=("cluster_with_schedule")
+  local v2_parent_dirs=("cluster_with_schedule" "direct-resource")
   local v2_grand_parent_dirs=("module_maintainer" "module_user" "migrate_cluster_to_advanced_cluster" "mongodbatlas_backup_compliance_policy") # module_maintainer and module_user uses {PARENT_DIR}/vX/main.tf
   
   for dir in "${v2_parent_dirs[@]}"; do
-    if [[ $parent_dir =~ $dir ]]; then
+    if [[ $parent_dir == "$dir" ]]; then
       return 0  # True
     fi
   done
   for dir in "${v2_grand_parent_dirs[@]}"; do
-    if [[ $grand_parent_dir =~ $dir ]]; then
+    if [[ $grand_parent_dir == "$dir" ]]; then
       return 0  # True
     fi
   done
