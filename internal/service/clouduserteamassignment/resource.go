adf5af54b0027d5d2dc2aea1e928cf8a4fc54fdf
Cristina Sánchez Sánchez <cristina.sanchez@mongodb.com>
2025-07-21 14:18:56 +0200

    Fix

--- internal/service/clouduserteamassignment/resource.go
+++ internal/service/clouduserteamassignment/resource.go
@@ -94,15 +94,17 @@ func (r *rs) Read(ctx context.Context, req resource.ReadRequest, resp *resource.
 			resp.Diagnostics.AddError("Error getting team users by username", err.Error())
 			return
 		}
-		if len(userListResp.GetResults()) == 0 {
-			resp.State.RemoveResource(ctx)
-			return
-		}
-		results := userListResp.GetResults()
-		for i := range results {
-			if results[i].GetId() == userID {
-				userResp = &results[i]
-				break
+		if userListResp != nil && userListResp.Results != nil {
+			if len(userListResp.GetResults()) == 0 {
+				resp.State.RemoveResource(ctx)
+				return
+			}
+			results := userListResp.GetResults()
+			for i := range results {
+				if results[i].GetId() == userID {
+					userResp = &results[i]
+					break
+				}
 			}
 		}
 	} else if !state.Username.IsNull() && state.Username.ValueString() != "" { // required for import
