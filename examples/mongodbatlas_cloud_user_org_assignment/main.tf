e212c81937826cc1eec9fa52c546ae0961bdbce2
maastha <122359335+maastha@users.noreply.github.com>
2025-07-21 23:45:56 +0200

    doc: Adds example & documentation for new `mongodbatlas_cloud_user_org_assignment` resource (#3493)

---
+++ examples/mongodbatlas_cloud_user_org_assignment/main.tf
@@ -0,0 +1,17 @@
+resource "mongodbatlas_cloud_user_org_assignment" "example" {
+  org_id   = var.org_id
+  username = var.user_email
+  roles = {
+    org_roles = ["ORG_MEMBER"]
+  }
+}
+
+data "mongodbatlas_cloud_user_org_assignment" "example_username" {
+  org_id   = var.org_id
+  username = var.user_email
+}
+
+data "mongodbatlas_cloud_user_org_assignment" "example_user_id" {
+  org_id  = var.org_id
+  user_id = var.user_id
+}
