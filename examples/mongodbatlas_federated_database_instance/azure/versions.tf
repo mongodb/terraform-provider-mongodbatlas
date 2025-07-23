880fe39d14efc91f1c92166f83b6f22fbf78668f
Mar Cabrera <mar.cabrera@mongodb.com>
2025-07-23 10:08:55 +0200

    Decrease mongodbatlas version provider for tests

--- examples/mongodbatlas_federated_database_instance/azure/versions.tf
+++ examples/mongodbatlas_federated_database_instance/azure/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     mongodbatlas = {
       source  = "mongodb/mongodbatlas"
-      version = "~> 1.39.0"
+      version = "~> 1.38.0"
     }
   }
   required_version = ">= 1.0"
