69f0c40a2e4d4dfc95d36613c1dee39ba1e84f75
Leo Antoli <430982+lantoli@users.noreply.github.com>
2025-07-24 16:09:53 +0200

    rename to create only

--- internal/service/flexcluster/resource_schema.go
+++ internal/service/flexcluster/resource_schema.go
@@ -22,14 +22,14 @@ func ResourceSchema(ctx context.Context) schema.Schema {
 			"project_id": schema.StringAttribute{
 				Required: true,
 				PlanModifiers: []planmodifier.String{
-					customplanmodifier.NonUpdatableStringAttributePlanModifier(),
+					customplanmodifier.CreateOnlyStringPlanModifier(),
 				},
 				MarkdownDescription: "Unique 24-hexadecimal character string that identifies the project.",
 			},
 			"name": schema.StringAttribute{
 				Required: true,
 				PlanModifiers: []planmodifier.String{
-					customplanmodifier.NonUpdatableStringAttributePlanModifier(),
+					customplanmodifier.CreateOnlyStringPlanModifier(),
 				},
 				MarkdownDescription: "Human-readable label that identifies the instance.",
 			},
@@ -38,7 +38,7 @@ func ResourceSchema(ctx context.Context) schema.Schema {
 					"backing_provider_name": schema.StringAttribute{
 						Required: true,
 						PlanModifiers: []planmodifier.String{
-							customplanmodifier.NonUpdatableStringAttributePlanModifier(),
+							customplanmodifier.CreateOnlyStringPlanModifier(),
 						},
 						MarkdownDescription: "Cloud service provider on which MongoDB Cloud provisioned the flex cluster.",
 					},
@@ -59,7 +59,7 @@ func ResourceSchema(ctx context.Context) schema.Schema {
 					"region_name": schema.StringAttribute{
 						Required: true,
 						PlanModifiers: []planmodifier.String{
-							customplanmodifier.NonUpdatableStringAttributePlanModifier(),
+							customplanmodifier.CreateOnlyStringPlanModifier(),
 						},
 						MarkdownDescription: "Human-readable label that identifies the geographic location of your MongoDB flex cluster. The region you choose can affect network latency for clients accessing your databases. For a complete list of region names, see [AWS](https://docs.atlas.mongodb.com/reference/amazon-aws/#std-label-amazon-aws), [GCP](https://docs.atlas.mongodb.com/reference/google-gcp/), and [Azure](https://docs.atlas.mongodb.com/reference/microsoft-azure/).",
 					},
@@ -149,7 +149,7 @@ func ResourceSchema(ctx context.Context) schema.Schema {
 			"delete_on_create_timeout": schema.BoolAttribute{
 				Optional: true,
 				PlanModifiers: []planmodifier.Bool{
-					customplanmodifier.NonUpdatableBoolAttributePlanModifier(),
+					customplanmodifier.CreateOnlyBoolPlanModifier(),
 				},
 
 				MarkdownDescription: "Indicates whether to delete the resource if creation times out. Default is `true`. When Terraform apply fails, it returns immediately without waiting for cleanup to complete. If you suspect a transient error, wait before retrying to allow resource deletion to finish.",
