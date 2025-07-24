44cdd5af89270d750df06f887f339155fa2c2016
Agustin Bettati <bettatiagustin@gmail.com>
2025-07-24 11:10:50 +0200

    adding coalescelist for option 2

--- examples/modules/option-2/auto-scaling/main.tf
+++ examples/modules/option-2/auto-scaling/main.tf
@@ -1,5 +1,5 @@
-# Validate that only one of replication_specs, shards, or region_configs is defined
 locals {
+
   # Count how many of the three options are defined (with length > 0)
   defined_count = (
     length(var.replication_specs) > 0 ? 1 : 0
@@ -9,20 +9,9 @@ locals {
     length(var.region_configs) > 0 ? 1 : 0
   )
 
-}
-
-check "validate_only_one_defined" {
-  assert {
-    condition     = local.defined_count <= 1
-    error_message = "Only one of replication_specs, shards, or region_configs can be defined"
-  }
-}
-
-locals {
-
-  # Build the specs from replication_specs (with autoscaling), shards (geo-sharded), or region_configs (replica set)
-  effective_replication_specs = (
-    length(var.replication_specs) > 0 ? tolist([
+  # Build the specs from replication_specs, shards, or region_configs
+  effective_replication_specs = coalescelist(
+    tolist([
       for spec in var.replication_specs : {
         zone_name = try(spec.zone_name, null)
         region_configs = [
@@ -70,92 +59,92 @@ locals {
             }
           )
         ]
+      }]), 
+    tolist([
+      for shard in var.shards : {
+        zone_name = shard.zone_name
+        region_configs = [for region in shard.region_configs : {
+          provider_name = region.provider_name
+          region_name   = region.region_name
+          priority      = region.priority
+          electable_specs = {
+            instance_size   = var.auto_scaling.compute_min_instance_size
+            node_count      = region.electable_node_count
+            ebs_volume_type = try(region.ebs_volume_type, null)
+            disk_size_gb    = null
+            disk_iops       = try(region.disk_iops, null)
+          }
+          read_only_specs = (
+            try(region.read_only_node_count, 0) > 0 ? {
+              instance_size   = var.auto_scaling.compute_min_instance_size
+              node_count      = region.read_only_node_count
+              ebs_volume_type = try(region.ebs_volume_type, null)
+              disk_size_gb    = null
+              disk_iops       = try(region.disk_iops, null)
+            } : null
+          )
+          auto_scaling = merge({
+            compute_enabled = true
+            disk_gb_enabled = true
+          }, var.auto_scaling) # all autoscaling configs are the same cluster wide, this how API currently works
+          analytics_auto_scaling = merge({
+            compute_enabled = true
+            disk_gb_enabled = true
+          }, var.analytics_auto_scaling) # all analytics autoscaling configs are the same cluster wide, this how API currently works
+          analytics_specs = (
+            region.analytics_specs != null ? {
+              instance_size   = var.analytics_auto_scaling.compute_min_instance_size
+              node_count      = region.analytics_specs.node_count
+              ebs_volume_type = try(region.analytics_specs.ebs_volume_type, null)
+              disk_size_gb    = null
+              disk_iops       = try(region.analytics_specs.disk_iops, null)
+            } : null
+          )
+        }]
       }
-      ]) : (length(var.shards) > 0 ? tolist([
-        for shard in var.shards : {
-          zone_name = shard.zone_name
-          region_configs = [for region in shard.region_configs : {
-            provider_name = region.provider_name
-            region_name   = region.region_name
-            priority      = region.priority
-            electable_specs = {
-              instance_size   = var.auto_scaling.compute_min_instance_size
-              node_count      = region.electable_node_count
-              ebs_volume_type = try(region.ebs_volume_type, null)
-              disk_size_gb    = null
-              disk_iops       = try(region.disk_iops, null)
-            }
-            read_only_specs = (
-              try(region.read_only_node_count, 0) > 0 ? {
-                instance_size   = var.auto_scaling.compute_min_instance_size
-                node_count      = region.read_only_node_count
-                ebs_volume_type = try(region.ebs_volume_type, null)
-                disk_size_gb    = null
-                disk_iops       = try(region.disk_iops, null)
-              } : null
-            )
-            auto_scaling = merge({
-              compute_enabled = true
-              disk_gb_enabled = true
-            }, var.auto_scaling) # all autoscaling configs are the same cluster wide, this how API currently works
-            analytics_auto_scaling = merge({
-              compute_enabled = true
-              disk_gb_enabled = true
-            }, var.analytics_auto_scaling) # all analytics autoscaling configs are the same cluster wide, this how API currently works
-            analytics_specs = (
-              region.analytics_specs != null ? {
-                instance_size   = var.analytics_auto_scaling.compute_min_instance_size
-                node_count      = region.analytics_specs.node_count
-                ebs_volume_type = try(region.analytics_specs.ebs_volume_type, null)
-                disk_size_gb    = null
-                disk_iops       = try(region.analytics_specs.disk_iops, null)
-              } : null
-            )
-          }]
+      ]),
+    tolist([{
+      zone_name = null
+      region_configs = [for region in var.region_configs : {
+        provider_name = region.provider_name
+        region_name   = region.region_name
+        priority      = region.priority
+        electable_specs = {
+          instance_size   = var.auto_scaling.compute_min_instance_size
+          node_count      = region.electable_node_count
+          ebs_volume_type = try(region.ebs_volume_type, null)
+          disk_size_gb    = null
+          disk_iops       = try(region.disk_iops, null)
         }
-        ]) : tolist([
-        {
-          zone_name = null
-          region_configs = [for region in var.region_configs : {
-            provider_name = region.provider_name
-            region_name   = region.region_name
-            priority      = region.priority
-            electable_specs = {
-              instance_size   = var.auto_scaling.compute_min_instance_size
-              node_count      = region.electable_node_count
-              ebs_volume_type = try(region.ebs_volume_type, null)
-              disk_size_gb    = null
-              disk_iops       = try(region.disk_iops, null)
-            }
-            read_only_specs = ( # read_only_specs uses same compute and storage configs as electable_specs, this is how API currently works
-              try(region.read_only_node_count, 0) > 0 ? {
-                instance_size   = var.auto_scaling.compute_min_instance_size
-                node_count      = region.read_only_node_count
-                ebs_volume_type = try(region.ebs_volume_type, null)
-                disk_size_gb    = null
-                disk_iops       = try(region.disk_iops, null)
-              } : null
-            )
-            auto_scaling = merge({
-              compute_enabled = true
-              disk_gb_enabled = true
-            }, var.auto_scaling) # all autoscaling configs are the same cluster wide, this how API currently works
-            analytics_auto_scaling = merge({
-              compute_enabled = true
-              disk_gb_enabled = true
-            }, var.analytics_auto_scaling) # all analytics autoscaling configs are the same cluster wide, this how API currently works
-            analytics_specs = (
-              region.analytics_specs != null ? {
-                instance_size   = var.analytics_auto_scaling.compute_min_instance_size
-                node_count      = region.analytics_specs.node_count
-                ebs_volume_type = try(region.analytics_specs.ebs_volume_type, null)
-                disk_size_gb    = null
-                disk_iops       = try(region.analytics_specs.disk_iops, null)
-              } : null
-            )
-          }]
-        }
-    ]))
+        read_only_specs = ( # read_only_specs uses same compute and storage configs as electable_specs, this is how API currently works
+          try(region.read_only_node_count, 0) > 0 ? {
+            instance_size   = var.auto_scaling.compute_min_instance_size
+            node_count      = region.read_only_node_count
+            ebs_volume_type = try(region.ebs_volume_type, null)
+            disk_size_gb    = null
+            disk_iops       = try(region.disk_iops, null)
+          } : null
+        )
+        auto_scaling = merge({
+          compute_enabled = true
+          disk_gb_enabled = true
+        }, var.auto_scaling) # all autoscaling configs are the same cluster wide, this how API currently works
+        analytics_auto_scaling = merge({
+          compute_enabled = true
+          disk_gb_enabled = true
+        }, var.analytics_auto_scaling) # all analytics autoscaling configs are the same cluster wide, this how API currently works
+        analytics_specs = (
+          region.analytics_specs != null ? {
+            instance_size   = var.analytics_auto_scaling.compute_min_instance_size
+            node_count      = region.analytics_specs.node_count
+            ebs_volume_type = try(region.analytics_specs.ebs_volume_type, null)
+            disk_size_gb    = null
+            disk_iops       = try(region.analytics_specs.disk_iops, null)
+          } : null
+        )
+      }]
+    }
+  ])
   )
 }
 
@@ -190,6 +179,13 @@ resource "mongodbatlas_advanced_cluster" "this" {
 
 
   lifecycle {
+
+    # Validate that only one of replication_specs, shards, or region_configs is defined
+    precondition {
+      condition     = local.defined_count <= 1
+      error_message = "Only one of replication_specs, shards, or region_configs can be defined"
+    }
+
     # Terraform cannot make the ignore_changes block fully dynamic based on input variables or locals. The list must be static and known at plan time.
     # This static list supports up to 3 shards (replication specs) with up to 3 regions
     ignore_changes = [
