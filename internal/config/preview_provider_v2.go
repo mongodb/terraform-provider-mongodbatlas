e5df57aa5c41826572a75abd69f74b19bdbc5bd5
maastha <aastha.mahendru@mongodb.com>
2025-08-02 03:58:27 +0200

    min required

---
+++ internal/config/preview_provider_v2.go
@@ -0,0 +1,10 @@
+package config
+
+// const PreviewProviderV2AdvancedClusterEnvVar = "MONGODB_ATLAS_PREVIEW_PROVIDER_V2_ADVANCED_CLUSTER"
+
+// Environment variable is read only once to avoid possible changes during runtime
+// var previewProviderV2AdvancedCluster, _ = strconv.ParseBool(os.Getenv(PreviewProviderV2AdvancedClusterEnvVar))
+
+func PreviewProviderV2AdvancedCluster() bool {
+	return true
+}
