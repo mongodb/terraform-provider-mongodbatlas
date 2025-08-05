ae733782f3d309cfb2f44941d58f238c30decd7b
maastha <aastha.mahendru@mongodb.com>
2025-08-05 02:32:53 +0200

    remove SDKv2 specific tests

--- internal/service/advancedclustertpf/resource_advanced_cluster_migration_test.go
+++ internal/service/advancedclustertpf/resource_advanced_cluster_migration_test.go
@@ -1,7 +1,6 @@
 package advancedclustertpf_test
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
@@ -34,70 +33,7 @@ func TestMigAdvancedCluster_asymmetricShardedNewSchema(t *testing.T) {
 	migTest(t, asymmetricShardedNewSchemaTestCase)
 }
 
-func TestMigAdvancedCluster_replicaSetAWSProviderUpdate(t *testing.T) {
-	acc.SkipIfAdvancedClusterV2Schema(t) // This test is specific to the legacy schema
-	var (
-		projectID   = acc.ProjectIDExecution(t)
-		clusterName = acc.RandomClusterName()
-	)
-
-	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:     mig.PreCheckBasicSleep(t),
-		CheckDestroy: acc.CheckDestroyCluster,
-		Steps: []resource.TestStep{
-			{
-				ExternalProviders: acc.ExternalProviders(versionBeforeISSRelease),
-				Config: configAWSProvider(t, false, ReplicaSetAWSConfig{
-					ProjectID:          projectID,
-					ClusterName:        clusterName,
-					ClusterType:        "REPLICASET",
-					DiskSizeGB:         60,
-					NodeCountElectable: 3,
-					WithAnalyticsSpecs: true,
-				}),
-				Check: checkReplicaSetAWSProvider(false, projectID, clusterName, 60, 3, false, false),
-			},
-			{
-				ProtoV6ProviderFactories: acc.TestAccProviderV6Factories,
-				Config: configAWSProvider(t, false, ReplicaSetAWSConfig{
-					ProjectID:          projectID,
-					ClusterName:        clusterName,
-					ClusterType:        "REPLICASET",
-					DiskSizeGB:         60,
-					NodeCountElectable: 5,
-					WithAnalyticsSpecs: true,
-				}),
-				Check: checkReplicaSetAWSProvider(false, projectID, clusterName, 60, 5, true, true),
-			},
-		},
-	})
-}
-
-func TestMigAdvancedCluster_geoShardedOldSchemaUpdate(t *testing.T) {
-	acc.SkipIfAdvancedClusterV2Schema(t) // This test is specific to the legacy schema
-	projectID, clusterName := acc.ProjectIDExecutionWithCluster(t, 12)
-
-	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:     func() { mig.PreCheckBasic(t) },
-		CheckDestroy: acc.CheckDestroyCluster,
-		Steps: []resource.TestStep{
-			{
-				ExternalProviders: acc.ExternalProviders(versionBeforeISSRelease),
-				Config:            configGeoShardedOldSchema(t, false, projectID, clusterName, 2, 2, false),
-				Check:             checkGeoShardedOldSchema(false, clusterName, 2, 2, false, false),
-			},
-			{
-				ProtoV6ProviderFactories: acc.TestAccProviderV6Factories,
-				Config:                   configGeoShardedOldSchema(t, false, projectID, clusterName, 2, 1, false),
-				Check:                    checkGeoShardedOldSchema(false, clusterName, 2, 1, true, false),
-			},
-		},
-	})
-}
-
 func TestMigAdvancedCluster_shardedMigrationFromOldToNewSchema(t *testing.T) {
-	// acc.SkipIfAdvancedClusterV2Schema(t) // This test is specific to the legacy schema
-
 	projectID, clusterName := acc.ProjectIDExecutionWithCluster(t, 8)
 
 	resource.ParallelTest(t, resource.TestCase{
@@ -124,7 +60,6 @@ func TestMigAdvancedCluster_shardedMigrationFromOldToNewSchema(t *testing.T) {
 }
 
 func TestMigAdvancedCluster_geoShardedMigrationFromOldToNewSchema(t *testing.T) {
-	// acc.SkipIfAdvancedClusterV2Schema(t) // This test is specific to the legacy schema
 	projectID, clusterName := acc.ProjectIDExecutionWithCluster(t, 8)
 
 	resource.ParallelTest(t, resource.TestCase{
@@ -149,156 +84,6 @@ func TestMigAdvancedCluster_geoShardedMigrationFromOldToNewSchema(t *testing.T) 
 		},
 	})
 }
-
-func TestMigAdvancedCluster_partialAdvancedConf(t *testing.T) {
-	acc.SkipIfAdvancedClusterV2Schema(t) // This test is specific to the legacy schema
-	mig.SkipIfVersionBelow(t, "1.24.0")  // version where tls_cipher_config_mode was introduced
-	var (
-		projectID   = acc.ProjectIDExecution(t)
-		clusterName = acc.RandomClusterName()
-		// necessary to test oplog_min_retention_hours
-		autoScalingConfigured = `
-			auto_scaling {
-				disk_gb_enabled = true
-			}`
-		extraArgs = `
-			advanced_configuration  {
-				fail_index_key_too_long              = false
-				javascript_enabled                   = true
-				minimum_enabled_tls_protocol         = "TLS1_2"
-				no_table_scan                        = false
-				oplog_min_retention_hours 		     = 4
-			}
-
-			bi_connector_config {
-				enabled = true
-			}`
-
-		extraArgsUpdated = `
-			advanced_configuration  {
-				fail_index_key_too_long              = false
-				javascript_enabled                   = true
-				minimum_enabled_tls_protocol         = "TLS1_2"
-				no_table_scan                        = false
-				default_read_concern                 = "available"
-				sample_size_bi_connector			 = 110
-				sample_refresh_interval_bi_connector = 310
-				default_max_time_ms = 65
-				tls_cipher_config_mode               = "CUSTOM"
-			    custom_openssl_cipher_config_tls12   = ["TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256"]
-				}
-				
-				bi_connector_config {
-					enabled = false
-					read_preference = "secondary"
-			}`
-		configInitial = configPartialAdvancedConfig(projectID, clusterName, extraArgs, autoScalingConfigured)
-		configUpdated = configPartialAdvancedConfig(projectID, clusterName, extraArgsUpdated, "")
-	)
-
-	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:     mig.PreCheckBasicSleep(t),
-		CheckDestroy: acc.CheckDestroyCluster,
-		Steps: []resource.TestStep{
-			{
-				ExternalProviders: mig.ExternalProviders(),
-				Config:            configInitial,
-				Check: resource.ComposeAggregateTestCheckFunc(
-					acc.CheckExistsCluster(resourceName),
-					resource.TestCheckResourceAttr(resourceName, "advanced_configuration.0.fail_index_key_too_long", "false"),
-					resource.TestCheckResourceAttr(resourceName, "advanced_configuration.0.javascript_enabled", "true"),
-					resource.TestCheckResourceAttr(resourceName, "advanced_configuration.0.minimum_enabled_tls_protocol", "TLS1_2"),
-					resource.TestCheckResourceAttr(resourceName, "advanced_configuration.0.no_table_scan", "false"),
-					resource.TestCheckResourceAttr(resourceName, "advanced_configuration.0.oplog_min_retention_hours", "4"),
-					resource.TestCheckResourceAttr(resourceName, "advanced_configuration.0.tls_cipher_config_mode", "DEFAULT"),
-					resource.TestCheckResourceAttr(resourceName, "bi_connector_config.0.enabled", "true"),
-				),
-			},
-			mig.TestStepCheckEmptyPlan(configInitial),
-			{
-				ProtoV6ProviderFactories: acc.TestAccProviderV6Factories,
-				Config:                   configUpdated,
-				Check: resource.ComposeAggregateTestCheckFunc(
-					acc.CheckExistsCluster(resourceName),
-					resource.TestCheckResourceAttr(resourceName, "advanced_configuration.0.fail_index_key_too_long", "false"),
-					resource.TestCheckResourceAttr(resourceName, "advanced_configuration.0.javascript_enabled", "true"),
-					resource.TestCheckResourceAttr(resourceName, "advanced_configuration.0.minimum_enabled_tls_protocol", "TLS1_2"),
-					resource.TestCheckResourceAttr(resourceName, "advanced_configuration.0.no_table_scan", "false"),
-					resource.TestCheckResourceAttr(resourceName, "advanced_configuration.0.sample_refresh_interval_bi_connector", "310"),
-					resource.TestCheckResourceAttr(resourceName, "advanced_configuration.0.sample_size_bi_connector", "110"),
-					resource.TestCheckResourceAttr(resourceName, "advanced_configuration.0.default_max_time_ms", "65"),
-					resource.TestCheckResourceAttr(resourceName, "advanced_configuration.0.tls_cipher_config_mode", "CUSTOM"),
-					resource.TestCheckResourceAttr(resourceName, "advanced_configuration.0.custom_openssl_cipher_config_tls12.#", "1"),
-					resource.TestCheckResourceAttr(resourceName, "bi_connector_config.0.enabled", "false"),
-					resource.TestCheckResourceAttr(resourceName, "bi_connector_config.0.read_preference", "secondary"),
-				),
-			},
-			mig.TestStepCheckEmptyPlan(configUpdated),
-		},
-	})
-}
-
-func TestMigAdvancedCluster_newSchemaFromAutoscalingDisabledToEnabled(t *testing.T) {
-	acc.SkipIfAdvancedClusterV2Schema(t) // This test is specific to the legacy schema
-	projectID, clusterName := acc.ProjectIDExecutionWithCluster(t, 8)
-
-	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:     acc.PreCheckBasicSleep(t, nil, projectID, clusterName),
-		CheckDestroy: acc.CheckDestroyCluster,
-		Steps: []resource.TestStep{
-			{
-				ExternalProviders: acc.ExternalProviders("1.22.0"), // last version before cluster tier auto-scaling per shard was introduced
-				Config:            configShardedTransitionOldToNewSchema(t, false, projectID, clusterName, true, false),
-				Check:             acc.CheckIndependentShardScalingMode(resourceName, clusterName, "CLUSTER"),
-			},
-			{
-				ProtoV6ProviderFactories: acc.TestAccProviderV6Factories,
-				Config:                   configShardedTransitionOldToNewSchema(t, false, projectID, clusterName, true, true),
-				Check:                    acc.CheckIndependentShardScalingMode(resourceName, clusterName, "SHARD"),
-			},
-		},
-	})
-}
-
-func configPartialAdvancedConfig(projectID, clusterName, extraArgs, autoScaling string) string {
-	return fmt.Sprintf(`
-		resource "mongodbatlas_advanced_cluster" "test" {
-			project_id             = %[1]q
-			name                   = %[2]q
-			cluster_type           = "REPLICASET"
-
-			replication_specs {
-				region_configs {
-					electable_specs {
-						instance_size = "M10"
-						node_count    = 3
-					}
-					analytics_specs {
-						instance_size = "M10"
-						node_count    = 1
-					}
-					provider_name = "AWS"
-					priority      = 7
-					region_name   = "US_WEST_2"
-					%[4]s
-				}
-			}
-			%[3]s
-		}
-	`, projectID, clusterName, extraArgs, autoScaling)
-}
-
-// // migTest is a helper function to run migration tests in normal case (SDKv2 -> SDKv2, TPF -> TPF), or in mixed case (SDKv2 -> TPF).
-// func migTest(t *testing.T, testCaseFunc func(t *testing.T, usePreviewProvider bool) resource.TestCase) {
-// 	t.Helper()
-// 	usePreviewProvider := config.PreviewProviderV2AdvancedCluster()
-// 	if acc.IsTestSDKv2ToTPF() {
-// 		usePreviewProvider = false
-// 		t.Log("Running test SDKv2 to TPF")
-// 	}
-// 	testCase := testCaseFunc(t, usePreviewProvider)
-// 	mig.CreateAndRunTest(t, &testCase)
-// }
 
 // migTest is a helper function to run migration tests:
 // - TPF -> TPF: for versions 2.0.0+ (tests same config with older TPF provider vs newer TPF provider)
