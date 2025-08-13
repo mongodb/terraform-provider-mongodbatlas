41e18fdb0d6f27d7c60304a7771b5617a8a7f84e
Cristina Sánchez Sánchez <cristina.sanchez@mongodb.com>
2025-08-13 10:39:01 +0200

    Fix indent

--- examples/mongodbatlas_cloud_user_team_assignment/main.tf
+++ examples/mongodbatlas_cloud_user_team_assignment/main.tf
@@ -5,13 +5,13 @@ resource "mongodbatlas_cloud_user_team_assignment" "example" {
 }
 
 data "mongodbatlas_cloud_user_team_assignment" "example_user_id" {
-  org_id     = var.org_id
-  team_id    = var.team_id
-  user_id    = mongodbatlas_cloud_user_team_assignment.example.user_id
+  org_id  = var.org_id
+  team_id = var.team_id
+  user_id = mongodbatlas_cloud_user_team_assignment.example.user_id
 }
 
 data "mongodbatlas_cloud_user_team_assignment" "example_username" {
-  org_id     = var.org_id
-  team_id    = var.team_id
-  username   = mongodbatlas_cloud_user_team_assignment.example.username
+  org_id   = var.org_id
+  team_id  = var.team_id
+  username = mongodbatlas_cloud_user_team_assignment.example.username
 }
