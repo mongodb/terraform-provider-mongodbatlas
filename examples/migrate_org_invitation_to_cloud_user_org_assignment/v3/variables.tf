ffc9755a83b5eef23e1b67f09e764f0b049bdf36
Oriol <oriol.abadal@mongodb.com>
2025-08-14 15:36:14 +0200

    doc: Example for migration of `mongodbatlas_org_invitation` resource and data source to the `mongodbatlas_cloud_user_org_assignment` resource (#3589)
    * examples
    
    * Update examples/migrate_org_invitation_to_cloud_user_org_assignment/v2/README.md
    
    * Update examples/migrate_org_invitation_to_cloud_user_org_assignment/v2/README.md
    
    Co-authored-by: Marco Suma <marco.suma@mongodb.com>
    
    * pr comments
    
    * fix from resource
    
    * reference example in migration guide
    
    ---------
    
    Co-authored-by: Marco Suma <marco.suma@mongodb.com>

---
+++ examples/migrate_org_invitation_to_cloud_user_org_assignment/v3/variables.tf
@@ -0,0 +1,24 @@
+variable "public_key" {
+  type    = string
+  default = ""
+}
+variable "private_key" {
+  type    = string
+  default = ""
+}
+
+variable "org_id" { type = string }
+
+# Pending invite user (now managed via cloud_user_org_assignment)
+variable "pending_username" { type = string }
+variable "roles" {
+  type    = set(string)
+  default = ["ORG_MEMBER"]
+}
+variable "pending_team_ids" {
+  type    = set(string)
+  default = []
+}
+
+# Active user already in org
+variable "active_username" { type = string }
