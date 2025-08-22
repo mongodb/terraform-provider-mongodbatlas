e56d7afffe56ebdbdd61e24f863fa3fe547d3f40
maastha <aastha.mahendru@mongodb.com>
2025-08-22 18:16:51 +0200

    add plan modifier

--- internal/service/advancedclustertpf/resource.go
+++ internal/service/advancedclustertpf/resource.go
@@ -22,8 +22,8 @@ import (
 var _ resource.ResourceWithConfigure = &rs{}
 var _ resource.ResourceWithImportState = &rs{}
 var _ resource.ResourceWithMoveState = &rs{}
-// var _ resource.ResourceWithUpgradeState = &rs{}
-// var _ resource.ResourceWithModifyPlan = &rs{}
+var _ resource.ResourceWithUpgradeState = &rs{}
+var _ resource.ResourceWithModifyPlan = &rs{}
 
 const (
 	resourceName                  = "advanced_cluster"
@@ -95,24 +95,24 @@ type rs struct {
 // Why do we need this? Why can't we use planmodifier.UseStateForUnknown in different fields?
 // 1. UseStateForUnknown always copies the state for unknown values. However, that leads to `Error: Provider produced inconsistent result after apply` in some cases (see implementation below).
 // 2. Adding the different UseStateForUnknown is very verbose.
-// func (r *rs) ModifyPlan(ctx context.Context, req resource.ModifyPlanRequest, resp *resource.ModifyPlanResponse) {
-// 	if req.State.Raw.IsNull() || req.Plan.Raw.IsNull() || req.Plan.Raw.IsFullyKnown() { // Return early unless it is an Update
-// 		return
-// 	}
-// 	var plan, state TFModel
-// 	diags := &resp.Diagnostics
-// 	diags.Append(req.Plan.Get(ctx, &plan)...)
-// 	diags.Append(req.State.Get(ctx, &state)...)
-// 	if diags.HasError() {
-// 		return
-// 	}
+func (r *rs) ModifyPlan(ctx context.Context, req resource.ModifyPlanRequest, resp *resource.ModifyPlanResponse) {
+	if req.State.Raw.IsNull() || req.Plan.Raw.IsNull() || req.Plan.Raw.IsFullyKnown() { // Return early unless it is an Update
+		return
+	}
+	var plan, state TFModel
+	diags := &resp.Diagnostics
+	diags.Append(req.Plan.Get(ctx, &plan)...)
+	diags.Append(req.State.Get(ctx, &state)...)
+	if diags.HasError() {
+		return
+	}
 
-// 	useStateForUnknowns(ctx, diags, &state, &plan)
-// 	if diags.HasError() {
-// 		return
-// 	}
-// 	diags.Append(resp.Plan.Set(ctx, plan)...)
-// }
+	useStateForUnknowns(ctx, diags, &state, &plan)
+	if diags.HasError() {
+		return
+	}
+	diags.Append(resp.Plan.Set(ctx, plan)...)
+}
 
 func (r *rs) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
 	resp.Schema = resourceSchema(ctx)
