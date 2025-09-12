1d2647e9c890db0e4c15a5d312e31d5154d9ca7c
Leo Antoli <430982+lantoli@users.noreply.github.com>
2025-09-12 15:33:15 +0200

    trigger tests

--- internal/service/clouduserteamassignment/resource_test.go
+++ internal/service/clouduserteamassignment/resource_test.go
@@ -22,7 +22,6 @@ func TestAccCloudUserTeamAssignment_basic(t *testing.T) {
 
 func basicTestCase(t *testing.T) *resource.TestCase {
 	t.Helper()
-
 	orgID := os.Getenv("MONGODB_ATLAS_ORG_ID")
 	userID := os.Getenv("MONGODB_ATLAS_CLOUD_USER_ID")
 	teamName := acc.RandomName()
