ae2c6ab44a9a58034f567506cb6aa75de5773e53
maastha <aastha.mahendru@mongodb.com>
2025-10-14 00:22:47 +0200

    nit

--- examples/mongodbatlas_advanced_cluster/upgrade_advanced_cluster_to_v2.0.0_in_modules/module_maintainer/v1/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "mongodbatlas_advanced_cluster" {
-  value       = mongodbatlas_advanced_cluster.this
-  description = "Full cluster configuration"
-}
--- examples/mongodbatlas_advanced_cluster/upgrade_advanced_cluster_to_v2.0.0_in_modules/module_maintainer/v2/variables.tf
+++
@@ -1,6 +0,0 @@
-variable "project_id" { type = string }
-variable "name" { type = string }
-variable "provider_name" { type = string }
-variable "region_name" { type = string }
-variable "instance_size" { type = string }
-variable "tags" { type = map(string) default = {} }
