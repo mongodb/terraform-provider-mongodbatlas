19286c81e539eac846abd35b347d1904a1141627
Kush Patel <kush.patel@mongodb.com>
2025-10-31 07:59:03 -0500

    feat: add stream_workspace resource + ds to replace stream_instance

---
+++ internal/service/streamworkspace/data_source_stream_workspace.go
@@ -0,0 +1,60 @@
+package streamworkspace
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/mongodb/terraform-provider-mongodbatlas/internal/common/conversion"
+	"github.com/mongodb/terraform-provider-mongodbatlas/internal/config"
+	"github.com/mongodb/terraform-provider-mongodbatlas/internal/service/streaminstance"
+)
+
+var _ datasource.DataSource = &streamsWorkspaceDS{}
+var _ datasource.DataSourceWithConfigure = &streamsWorkspaceDS{}
+
+func DataSource() datasource.DataSource {
+	return &streamsWorkspaceDS{
+		DSCommon: config.DSCommon{
+			DataSourceName: streamsWorkspaceName,
+		},
+	}
+}
+
+type streamsWorkspaceDS struct {
+	config.DSCommon
+}
+
+func (d *streamsWorkspaceDS) Schema(ctx context.Context, req datasource.SchemaRequest, resp *datasource.SchemaResponse) {
+	resp.Schema = conversion.DataSourceSchemaFromResource(ResourceSchema(ctx), &conversion.DataSourceSchemaRequest{
+		RequiredFields: []string{"project_id", "workspace_name"},
+	})
+}
+
+func (d *streamsWorkspaceDS) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+	var streamsWorkspaceConfig TFStreamsWorkspaceModel
+	resp.Diagnostics.Append(req.Config.Get(ctx, &streamsWorkspaceConfig)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	connV2 := d.Client.AtlasV2
+	projectID := streamsWorkspaceConfig.ProjectID.ValueString()
+	workspaceName := streamsWorkspaceConfig.WorkspaceName.ValueString()
+	apiResp, _, err := connV2.StreamsApi.GetStreamWorkspace(ctx, projectID, workspaceName).Execute()
+	if err != nil {
+		resp.Diagnostics.AddError("error fetching resource", err.Error())
+		return
+	}
+
+	newInstanceModel, diags := streaminstance.NewTFStreamInstance(ctx, apiResp)
+	if diags.HasError() {
+		resp.Diagnostics.Append(diags...)
+		return
+	}
+
+	// Convert instance model to workspace model
+	var newWorkspaceModel TFStreamsWorkspaceModel
+	newWorkspaceModel.FromInstanceModel(newInstanceModel)
+
+	resp.Diagnostics.Append(resp.State.Set(ctx, newWorkspaceModel)...)
+}
---
+++ internal/service/streamworkspace/data_source_stream_workspaces_test.go
@@ -0,0 +1,83 @@
+package streamworkspace_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/mongodb/terraform-provider-mongodbatlas/internal/testutil/acc"
+)
+
+func TestAccStreamsWorkspacesDS_basic(t *testing.T) {
+	var (
+		dataSourceName = "data.mongodbatlas_stream_workspaces.test"
+		projectID      = acc.ProjectIDExecution(t)
+		workspaceName  = acc.RandomName()
+	)
+
+	checks := []resource.TestCheckFunc{
+		resource.TestCheckResourceAttrSet(dataSourceName, "project_id"),
+		resource.TestCheckResourceAttrSet(dataSourceName, "results.#"),
+		resource.TestCheckResourceAttrSet(dataSourceName, "results.0.workspace_name"),
+		resource.TestCheckResourceAttrSet(dataSourceName, "results.0.data_process_region.region"),
+		resource.TestCheckResourceAttrSet(dataSourceName, "results.0.data_process_region.cloud_provider"),
+		resource.TestCheckResourceAttrSet(dataSourceName, "results.0.hostnames.#"),
+	}
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:                 func() { acc.PreCheckBasic(t) },
+		ProtoV6ProviderFactories: acc.TestAccProviderV6Factories,
+		CheckDestroy:             acc.CheckDestroyStreamInstance,
+		Steps: []resource.TestStep{
+			{
+				Config: streamsWorkspacesDataSourceConfig(projectID, workspaceName, region, cloudProvider),
+				Check:  resource.ComposeAggregateTestCheckFunc(checks...),
+			},
+		},
+	})
+}
+
+func TestAccStreamsWorkspacesDS_withPageConfig(t *testing.T) {
+	var (
+		dataSourceName = "data.mongodbatlas_stream_workspaces.test"
+		projectID      = acc.ProjectIDExecution(t)
+		workspaceName  = acc.RandomName()
+		pageNumber     = 1000 // high page number so no results are returned
+	)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:                 func() { acc.PreCheckBasic(t) },
+		ProtoV6ProviderFactories: acc.TestAccProviderV6Factories,
+		CheckDestroy:             acc.CheckDestroyStreamInstance,
+		Steps: []resource.TestStep{
+			{
+				Config: streamsWorkspacesWithPageAttrDataSourceConfig(projectID, workspaceName, region, cloudProvider, pageNumber),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(dataSourceName, "results.#", "0"),
+				),
+			},
+		},
+	})
+}
+
+func streamsWorkspacesDataSourceConfig(projectID, workspaceName, region, cloudProvider string) string {
+	return fmt.Sprintf(`
+		%s
+
+		data "mongodbatlas_stream_workspaces" "test" {
+			project_id = mongodbatlas_stream_workspace.test.project_id
+		}
+	`, streamsWorkspaceConfig(projectID, workspaceName, region, cloudProvider))
+}
+
+func streamsWorkspacesWithPageAttrDataSourceConfig(projectID, workspaceName, region, cloudProvider string, pageNum int) string {
+	return fmt.Sprintf(`
+		%s
+
+		data "mongodbatlas_stream_workspaces" "test" {
+			project_id = mongodbatlas_stream_workspace.test.project_id
+			page_num = %d
+			items_per_page = 1
+		}
+	`, streamsWorkspaceConfig(projectID, workspaceName, region, cloudProvider), pageNum)
+}
