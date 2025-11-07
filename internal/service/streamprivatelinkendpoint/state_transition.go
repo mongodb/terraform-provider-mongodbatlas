642c31f5b23f945e9bf01653ea8f149d9db4ef93
Leo Antoli <430982+lantoli@users.noreply.github.com>
2025-11-07 17:01:26 +0100

    chore: Change `mongodbatlas_stream_privatelink_endpoint` timeout to 60m (#3857)

--- internal/service/streamprivatelinkendpoint/state_transition.go
+++ internal/service/streamprivatelinkendpoint/state_transition.go
@@ -12,7 +12,7 @@ import (
 )
 
 const (
-	defaultTimeout    = 20 * time.Minute // The amount of time to wait before timeout
+	defaultTimeout    = 60 * time.Minute // The amount of time to wait before timeout
 	defaultMinTimeout = 30 * time.Second // Smallest time to wait before refreshes
 )
 
