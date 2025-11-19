329469fc8480a04df00ba7a6285ba2ad20440450
Leo Antoli <430982+lantoli@users.noreply.github.com>
2025-11-19 11:02:04 +0100

    versions.tf file

--- examples/mongodbatlas_advanced_cluster/effective-fields-module/module/main.tf
+++ examples/mongodbatlas_advanced_cluster/effective-fields-module/module/main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    mongodbatlas = {
-      source  = "mongodb/mongodbatlas"
-      version = "~> 2.0"
-    }
-  }
-}
-
 # Create Atlas Project
 resource "mongodbatlas_project" "this" {
   name   = var.project_name
