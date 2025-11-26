570277e9a11485e09cf2f380bd0d083a0d03c439
Mar Cabrera <mar.cabrera@mongodb.com>
2025-11-26 15:45:29 +0100

    Add example for TLS 1.2 Configuration

---
+++ examples/mongodbatlas_advanced_cluster/tls-cipher-configuration/tls-1_2/main.tf
@@ -0,0 +1,39 @@
+provider "mongodbatlas" {
+  client_id     = var.atlas_client_id
+  client_secret = var.atlas_client_secret
+}
+
+resource "mongodbatlas_advanced_cluster" "cluster" {
+  project_id   = mongodbatlas_project.project.id
+  name         = "TLS12Configuration"
+  cluster_type = "REPLICASET"
+
+  replication_specs = [
+    {
+      region_configs = [
+        {
+          electable_specs = {
+            instance_size = var.provider_instance_size_name
+            node_count    = 3
+          }
+          provider_name = var.provider_name
+          region_name   = "US_EAST_1"
+          priority      = 7
+        }
+      ]
+    }
+  ]
+  advanced_configuration = {
+    custom_openssl_cipher_config_tls12 = [
+      "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384"
+    ]
+    minimum_enabled_tls_protocol = "TLS1_2"
+    tls_cipher_config_mode       = "CUSTOM"
+  }
+
+}
+
+resource "mongodbatlas_project" "project" {
+  name   = "TLS12Configuration"
+  org_id = var.atlas_org_id
+}
