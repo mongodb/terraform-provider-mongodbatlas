c00d103c55543f4a9498407f34b7a6119cd02a6e
Agustin Bettati <bettatiagustin@gmail.com>
2025-12-14 20:25:37 +0100

    service account secret resource and data source working

--- internal/serviceapi/orgserviceaccountsecretapi/custom_hooks.go
+++ internal/serviceapi/orgserviceaccountsecretapi/custom_hooks.go
@@ -5,11 +5,14 @@ import (
 	"errors"
 
 	"github.com/mongodb/terraform-provider-mongodbatlas/internal/common/autogen"
+	"github.com/mongodb/terraform-provider-mongodbatlas/internal/config"
 )
 
 type response struct {
 	Secrets []map[string]any
 }
+
+// Resource hooks
 
 func (r *rs) PostReadAPICall(req autogen.HandleReadReq, result autogen.APICallResult) autogen.APICallResult {
 	var responseJSON response
@@ -18,6 +21,31 @@ func (r *rs) PostReadAPICall(req autogen.HandleReadReq, result autogen.APICallRe
 	}
 
 	id := req.State.(*TFModel).Id.ValueString()
+	for _, secret := range responseJSON.Secrets {
+		if secret["id"] == id {
+			marshaledSecret, err := json.Marshal(secret)
+			return autogen.APICallResult{Body: marshaledSecret, Err: err}
+		}
+	}
+
+	return autogen.APICallResult{Body: nil, Err: errors.New("secret not found in service account response")}
+}
+
+// Data source hooks
+
+func (d *ds) PreReadAPICall(callParams config.APICallParams) config.APICallParams {
+	callParams.Method = "GET"
+	callParams.RelativePath = "/api/atlas/v2/orgs/{orgId}/serviceAccounts/{clientId}"
+	return callParams
+}
+
+func (d *ds) PostReadAPICall(req autogen.HandleReadReq, result autogen.APICallResult) autogen.APICallResult {
+	var responseJSON response
+	if err := json.Unmarshal(result.Body, &responseJSON); err != nil {
+		return autogen.APICallResult{Body: nil, Err: err}
+	}
+
+	id := req.State.(*TFDSModel).Id.ValueString()
 	for _, secret := range responseJSON.Secrets {
 		if secret["id"] == id {
 			marshaledSecret, err := json.Marshal(secret)
