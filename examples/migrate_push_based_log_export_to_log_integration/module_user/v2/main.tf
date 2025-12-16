5f8d27871de4cd4fdbd7530104f3f6d8ef97582a
Oriol Arbusi Abadal <oriol.abadal@mongodb.com>
2025-12-16 15:19:07 +0100

    Revert "remove module example"
    This reverts commit 2c0bfbf4b1d35d770b11e9ad05df0fb5ac965d44.

---
+++ examples/migrate_push_based_log_export_to_log_integration/module_user/v2/main.tf
@@ -0,0 +1,28 @@
+# v2: Module user - Migration complete, old resource removed
+
+# Using the updated module (v2) with the feature flag set to true
+# This removes the old push_based_log_export resource
+module "log_export" {
+  source = "../../module_maintainer/v2"
+
+  project_id      = var.project_id
+  bucket_name     = var.bucket_name
+  iam_role_id     = var.iam_role_id
+  prefix_path     = "atlas-logs"     # No longer used (old resource removed)
+  new_prefix_path = "atlas-logs"     # Can use original path now
+  log_types       = ["MONGOD", "MONGOS", "MONGOD_AUDIT", "MONGOS_AUDIT"]
+
+  # Remove old resource - migration complete
+  skip_push_based_log_export = true
+}
+
+output "prefix_path" {
+  description = "Prefix path for the log integration"
+  value       = module.log_export.new_prefix_path
+}
+
+output "log_integration_id" {
+  description = "ID of the log integration"
+  value       = module.log_export.log_integration_id
+}
+
