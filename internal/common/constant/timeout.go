e1be7d0481d140d1aa1b628ec6586adbd096a923
filipcirtog <filip.cirtog@mongodb.com>
2025-12-16 17:52:01 +0100

    update documentation constant

--- internal/common/constant/timeout.go
+++ internal/common/constant/timeout.go
@@ -7,7 +7,7 @@ const (
 )
 
 const (
-	timeoutBaseDescription             = "A string that can be [parsed as a duration](https://pkg.go.dev/time#ParseDuration) consisting of numbers and unit suffixes, such as \"30s\" or \"2h45m\". Valid time units are \"s\" (seconds), \"m\" (minutes), \"h\" (hours)."
+	timeoutBaseDescription             = "A string that can be [parsed as a duration](https://pkg.go.dev/time#ParseDuration) consisting of numbers and unit suffixes, such as \"30s\" or \"2h45m\". Valid time units are \"s\" (seconds), \"m\" (minutes), and \"h\" (hours)."
 	timeoutDeleteAdditionalDescription = "Setting a timeout for a Delete operation is only applicable if changes are saved into state before the destroy operation occurs."
 )
 
