579f1a68dcb394eff2ab9164d9863969b2281622
Agustin Bettati <bettatiagustin@gmail.com>
2025-12-18 11:22:10 +0100

    adding clarifying comment

--- internal/serviceapi/orgserviceaccountsecretapi/data_source_custom_hooks.go
+++ internal/serviceapi/orgserviceaccountsecretapi/data_source_custom_hooks.go
@@ -11,6 +11,8 @@ import (
 var _ autogen.PreReadAPICallHook = (*ds)(nil)
 var _ autogen.PostReadAPICallHook = (*ds)(nil)
 
+// config.yml configures POST operation for data source read operation to have an accurate response schema
+// this hook adjusts the call params to use the correct GET operation
 func (d *ds) PreReadAPICall(callParams config.APICallParams) config.APICallParams {
 	callParams.Method = "GET"
 	callParams.RelativePath = "/api/atlas/v2/orgs/{orgId}/serviceAccounts/{clientId}"
