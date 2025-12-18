3df35bfb642293b77952cc114f721934a7fcf5f7
Agustin Bettati <bettatiagustin@gmail.com>
2025-12-18 11:15:19 +0100

    split into resource and data source custom hook files

--- internal/serviceapi/orgserviceaccountsecretapi/data_source_custom_hooks.go
+++ internal/serviceapi/orgserviceaccountsecretapi/data_source_custom_hooks.go
@@ -8,34 +8,6 @@ import (
 	"github.com/mongodb/terraform-provider-mongodbatlas/internal/config"
 )
 
-// Resource hooks
-var _ autogen.PostReadAPICallHook = (*rs)(nil)
-
-type response struct {
-	Secrets []map[string]any
-}
-
-func (r *rs) PostReadAPICall(req autogen.HandleReadReq, result autogen.APICallResult) autogen.APICallResult {
-	if result.Err != nil {
-		return result
-	}
-	var responseJSON response
-	if err := json.Unmarshal(result.Body, &responseJSON); err != nil {
-		return autogen.APICallResult{Body: nil, Err: err}
-	}
-
-	id := req.State.(*TFModel).Id.ValueString()
-	for _, secret := range responseJSON.Secrets {
-		if secret["id"] == id {
-			marshaledSecret, err := json.Marshal(secret)
-			return autogen.APICallResult{Body: marshaledSecret, Err: err}
-		}
-	}
-
-	return autogen.APICallResult{Body: nil, Err: errors.New("secret not found in service account response")}
-}
-
-// Data source hooks
 var _ autogen.PreReadAPICallHook = (*ds)(nil)
 var _ autogen.PostReadAPICallHook = (*ds)(nil)
 
