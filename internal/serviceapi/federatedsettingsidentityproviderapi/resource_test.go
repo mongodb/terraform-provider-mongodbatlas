662d34ddd7a33130c83a177cacd3e53e8b1af770
maastha <aastha.mahendru@mongodb.com>
2025-12-19 02:52:36 +0100

    fix

--- internal/serviceapi/federatedsettingsidentityproviderapi/resource_test.go
+++ internal/serviceapi/federatedsettingsidentityproviderapi/resource_test.go
@@ -12,15 +12,18 @@ import (
 	"github.com/mongodb/terraform-provider-mongodbatlas/internal/testutil/acc"
 )
 
-const resourceName = "mongodbatlas_federated_settings_identity_provider_api.test"
-
 var (
-	oidcProtocol = "OIDC"
-	samlProtocol = "SAML"
+	oidcProtocol         = "OIDC"
+	samlProtocol         = "SAML"
+	authTypeUser         = "USER"
+	authTypeGroup        = "GROUP"
+	resourceName         = "mongodbatlas_federated_settings_identity_provider_api.test"
+	dataSourceName       = "data.mongodbatlas_federated_settings_identity_provider_api.test"
+	dataSourcePluralName = "data.mongodbatlas_federated_settings_identity_providers_api.test"
 )
 
 func TestAccFederatedSettingsIdentityProviderAPI_OIDCWorkforce(t *testing.T) {
-	resource.ParallelTest(t, *basicOIDCWorkforceTestCase(t))
+	resource.Test(t, *basicOIDCWorkforceTestCase(t))
 }
 
 func basicOIDCWorkforceTestCase(tb testing.TB) *resource.TestCase {
@@ -81,6 +84,110 @@ func basicOIDCWorkforceTestCase(tb testing.TB) *resource.TestCase {
 	}
 }
 
+func TestAccFederatedSettingsIdentityProvider_OIDCWorkload(t *testing.T) {
+	resource.ParallelTest(t, *basicOIDCWorkloadTestCase(t))
+}
+
+func basicOIDCWorkloadTestCase(tb testing.TB) *resource.TestCase {
+	tb.Helper()
+
+	var (
+		federationSettingsID = os.Getenv("MONGODB_ATLAS_FEDERATION_SETTINGS_ID")
+		audience1            = "audience"
+		audience2            = "audience-updated"
+		description1         = "tf-acc-test"
+		description2         = "tf-acc-test-updated"
+		attrMapCheckGroup    = map[string]string{
+			"audience":               audience1,
+			"authorization_type":     authTypeGroup,
+			"description":            description1,
+			"federation_settings_id": federationSettingsID,
+			"groups_claim":           "groups",
+			"issuer_uri":             "https://token.actions.githubusercontent.com",
+			"idp_type":               federatedsettingsidentityprovider.WORKLOAD,
+			"protocol":               "OIDC",
+		}
+		attrMapCheckUser = map[string]string{
+			"audience":               audience2,
+			"authorization_type":     authTypeUser,
+			"description":            description2,
+			"federation_settings_id": federationSettingsID,
+			"issuer_uri":             "https://token.actions.githubusercontent.com",
+			"idp_type":               federatedsettingsidentityprovider.WORKLOAD,
+			"protocol":               "OIDC",
+			"user_claim":             "sub",
+		}
+	)
+	nameChecks := []resource.TestCheckFunc{
+		// checkExistsManaged(resourceName),
+		resource.TestCheckResourceAttr(resourceName, "display_name", "OIDC-workload-CRUD"),
+		resource.TestCheckResourceAttr(dataSourceName, "display_name", "OIDC-workload-CRUD"),
+		resource.TestCheckResourceAttr(dataSourcePluralName, "results.0.display_name", "OIDC-workload-CRUD"),
+		resource.TestCheckResourceAttr(dataSourcePluralName, "results.#", "1"),
+	}
+	checks := acc.AddAttrChecks(resourceName, nameChecks, attrMapCheckGroup)
+	checks = acc.AddAttrChecks(dataSourceName, checks, attrMapCheckGroup)
+	checks = acc.AddAttrChecksPrefix(dataSourcePluralName, checks, attrMapCheckGroup, "results.0", "federation_settings_id")
+
+	checks2 := acc.AddAttrChecks(resourceName, nameChecks, attrMapCheckUser)
+	checks2 = acc.AddAttrChecks(dataSourceName, checks2, attrMapCheckUser)
+	checks2 = acc.AddAttrChecksPrefix(dataSourcePluralName, checks2, attrMapCheckUser, "results.0", "federation_settings_id")
+
+	return &resource.TestCase{
+		PreCheck:                 func() { acc.PreCheckFederatedSettingsIdentityProvider(tb) },
+		ProtoV6ProviderFactories: acc.TestAccProviderV6Factories,
+		Steps: []resource.TestStep{
+			{
+				Config: configOIDCWorkloadBasic(federationSettingsID, description1, audience1, authTypeGroup),
+				Check:  resource.ComposeAggregateTestCheckFunc(checks...),
+			},
+			{
+				Config: configOIDCWorkloadBasic(federationSettingsID, description2, audience2, authTypeUser),
+				Check:  resource.ComposeAggregateTestCheckFunc(checks2...),
+			},
+			// {
+			// 	Config:            configOIDCWorkloadBasic(federationSettingsID, description2, audience2, authTypeUser),
+			// 	ResourceName:      resourceName,
+			// 	ImportStateIdFunc: importStateIDFuncManaged(resourceName),
+			// 	ImportState:       true,
+			// 	ImportStateVerify: true,
+			// },
+		},
+	}
+}
+
+func configOIDCWorkloadBasic(federationSettingsID, description, audience, authorizationType string) string {
+	groupsClaimRaw := `"groups"`
+	if authorizationType == authTypeUser {
+		groupsClaimRaw = `null`
+	}
+	return fmt.Sprintf(`
+	resource "mongodbatlas_federated_settings_identity_provider" "test" {
+        federation_settings_id 		= %[1]q
+		audience 					= %[2]q
+		authorization_type			= %[5]q
+		description 				= %[3]q
+		issuer_uri 					= "https://token.actions.githubusercontent.com"
+		idp_type 					= %[4]q
+		name 						= "OIDC-workload-CRUD"
+		protocol 					= "OIDC"
+		groups_claim				= %[6]s
+		user_claim 					= "sub"
+	  }
+	  
+	  data "mongodbatlas_federated_settings_identity_provider" "test" {
+		federation_settings_id = mongodbatlas_federated_settings_identity_provider.test.federation_settings_id
+		identity_provider_id   = mongodbatlas_federated_settings_identity_provider.test.idp_id
+	  }
+	  data "mongodbatlas_federated_settings_identity_providers" "test" {
+		federation_settings_id 	= mongodbatlas_federated_settings_identity_provider.test.federation_settings_id
+		idp_type 				= [%[4]q]
+		protocol 				= [%[7]q]
+		depends_on 				= [mongodbatlas_federated_settings_identity_provider.test]
+	  }
+	  `, federationSettingsID, audience, description, federatedsettingsidentityprovider.WORKLOAD, authorizationType, groupsClaimRaw, federatedsettingsidentityprovider.OIDC)
+}
+
 func configOIDCWorkforceBasic(federationSettingsID, associatedDomain, description, audience string) string {
 	return fmt.Sprintf(`
 	resource "mongodbatlas_federated_settings_identity_provider_api" "test" {
@@ -92,7 +199,7 @@ func configOIDCWorkforceBasic(federationSettingsID, associatedDomain, descriptio
 		description 				= %[4]q
 		groups_claim				= "groups"
 		issuer_uri 					= "https://token.actions.githubusercontent.com"
-		name 						= "OIDC-CRUD-test"
+		displayName 						= "OIDC-CRUD-test"
 		protocol 					= "OIDC"
 		requested_scopes 			= ["profiles"]
 		user_claim 					= "sub"
