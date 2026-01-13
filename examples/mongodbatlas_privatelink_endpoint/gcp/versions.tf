6484466920a4f4bc2479968265e926ab536ef57a
filipcirtog <filip.cirtog@mongodb.com>
2026-01-13 12:19:40 +0100

    remove unused azure dependency from examples

--- examples/mongodbatlas_privatelink_endpoint/gcp/versions.tf
+++ examples/mongodbatlas_privatelink_endpoint/gcp/versions.tf
@@ -1,9 +1,5 @@
 terraform {
   required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
     mongodbatlas = {
       source = "mongodb/mongodbatlas"
     }
