14b46f39684ff733eb5eed97f2fc8b9b9a9ce698
filipcirtog <filip.cirtog@mongodb.com>
2026-01-15 16:37:41 +0100

    fix ci

--- examples/mongodbatlas_service_account_access_list_entry/variables.tf
+++ examples/mongodbatlas_service_account_access_list_entry/variables.tf
@@ -6,10 +6,12 @@ variable "org_id" {
 variable "atlas_client_id" {
   description = "MongoDB Atlas Service Account Client ID (for provider authentication)"
   type        = string
+  default     = ""
 }
 
 variable "atlas_client_secret" {
   description = "MongoDB Atlas Service Account Client Secret (for provider authentication)"
   type        = string
   sensitive   = true
+  default     = ""
 }
