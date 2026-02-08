04d7a2deddd3990cf12900d65bee331c091c1f24
Leo Antoli <430982+lantoli@users.noreply.github.com>
2026-02-08 11:39:36 +0100

    simplify state upgrader

--- internal/service/advancedcluster/move_upgrade_state.go
+++ internal/service/advancedcluster/move_upgrade_state.go
@@ -3,7 +3,6 @@ package advancedcluster
 import (
 	"context"
 	"fmt"
-	"math/big"
 	"strings"
 
 	"go.mongodb.org/atlas-sdk/v20250312013/admin"
@@ -26,11 +25,11 @@ func (r *rs) MoveState(context.Context) []resource.StateMover {
 	return []resource.StateMover{{StateMover: stateMover}}
 }
 
-// UpgradeState is used to upgrade from adv_cluster schema v1 (SDKv2) to v2 (TPF) and v2 to v3
+// UpgradeState is used to upgrade from adv_cluster schema v1 (SDKv2) or v2 (TPF v2) to v3 (TPF v3).
 func (r *rs) UpgradeState(ctx context.Context) map[int64]resource.StateUpgrader {
 	return map[int64]resource.StateUpgrader{
-		1: {StateUpgrader: stateUpgraderFromV1},
-		2: {StateUpgrader: stateUpgraderFromV2},
+		1: {StateUpgrader: stateUpgrader},
+		2: {StateUpgrader: stateUpgrader},
 	}
 }
 
@@ -38,46 +37,31 @@ func stateMover(ctx context.Context, req resource.MoveStateRequest, resp *resour
 	if req.SourceTypeName != "mongodbatlas_cluster" || !strings.HasSuffix(req.SourceProviderAddress, "/mongodbatlas") {
 		return
 	}
-	// Use always new sharding config when moving from cluster to adv_cluster
-	setStateResponse(ctx, &resp.Diagnostics, req.SourceRawState, &resp.TargetState, false)
+	setStateResponse(ctx, &resp.Diagnostics, req.SourceRawState, &resp.TargetState)
 }
 
-func stateUpgraderFromV1(ctx context.Context, req resource.UpgradeStateRequest, resp *resource.UpgradeStateResponse) {
-	// Use same sharding config as in SDKv2 when upgrading to TPF
-	setStateResponse(ctx, &resp.Diagnostics, req.RawState, &resp.State, true)
-}
-
-func stateUpgraderFromV2(ctx context.Context, req resource.UpgradeStateRequest, resp *resource.UpgradeStateResponse) {
-	// V2 to V3 upgrade is straightforward - just copy state
-	// The main change is removing use_effective_fields which will be handled by schema
-	setStateResponse(ctx, &resp.Diagnostics, req.RawState, &resp.State, true)
+func stateUpgrader(ctx context.Context, req resource.UpgradeStateRequest, resp *resource.UpgradeStateResponse) {
+	setStateResponse(ctx, &resp.Diagnostics, req.RawState, &resp.State)
 }
 
 // stateAttrs has the attributes needed from source schema.
 // Filling these attributes in the destination will prevent plan changes when moving/upgrading state.
 // Read will fill in the rest.
 var stateAttrs = map[string]tftypes.Type{
-	"project_id":             tftypes.String, // project_id and name to identify the cluster
+	"project_id":             tftypes.String, // project_id and name to identify the cluster.
 	"name":                   tftypes.String,
-	"retain_backups_enabled": tftypes.Bool,   // TF specific so can't be got in Read
-	"mongo_db_major_version": tftypes.String, // Has special logic in overrideAttributesWithPrevStateValue that needs the previous state
-	"timeouts": tftypes.Object{ // TF specific so can't be got in Read
+	"retain_backups_enabled": tftypes.Bool,   // TF specific so can't be got in Read.
+	"mongo_db_major_version": tftypes.String, // Has special logic in overrideAttributesWithPrevStateValue that needs the previous state.
+	"timeouts": tftypes.Object{ // TF specific so can't be got in Read.
 		AttributeTypes: map[string]tftypes.Type{
 			"create": tftypes.String,
 			"update": tftypes.String,
 			"delete": tftypes.String,
 		},
 	},
-	"replication_specs": tftypes.List{ // Needed to send num_shards to Read so it can decide if it's using the legacy schema.
-		ElementType: tftypes.Object{
-			AttributeTypes: map[string]tftypes.Type{
-				"num_shards": tftypes.Number,
-			},
-		},
-	},
 }
 
-func setStateResponse(ctx context.Context, diags *diag.Diagnostics, stateIn *tfprotov6.RawState, stateOut *tfsdk.State, allowOldShardingConfig bool) {
+func setStateResponse(ctx context.Context, diags *diag.Diagnostics, stateIn *tfprotov6.RawState, stateOut *tfsdk.State) {
 	rawStateValue, err := stateIn.UnmarshalWithOpts(tftypes.Object{
 		AttributeTypes: stateAttrs,
 	}, tfprotov6.UnmarshalOpts{ValueFromJSONOpts: tftypes.ValueFromJSONOpts{IgnoreUndefinedAttributes: true}})
@@ -110,9 +94,6 @@ func setStateResponse(ctx context.Context, diags *diag.Diagnostics, stateIn *tfp
 		return
 	}
 	setOptionalModelAttrs(stateObj, model)
-	if allowOldShardingConfig {
-		setReplicationSpecNumShardsAttr(ctx, stateObj, model)
-	}
 	// Set tags and labels to null instead of empty so there is no plan change if there are no tags or labels when Read is called.
 	model.Tags = types.MapNull(types.StringType)
 	model.Labels = types.MapNull(types.StringType)
@@ -163,36 +144,5 @@ func setOptionalModelAttrs(stateObj map[string]tftypes.Value, model *TFModel) {
 	}
 	if mongoDBMajorVersion := schemafunc.GetAttrFromStateObj[string](stateObj, "mongo_db_major_version"); mongoDBMajorVersion != nil {
 		model.MongoDBMajorVersion = types.StringPointerValue(mongoDBMajorVersion)
-	}
-}
-
-func setReplicationSpecNumShardsAttr(ctx context.Context, stateObj map[string]tftypes.Value, model *TFModel) {
-	specsVal := schemafunc.GetAttrFromStateObj[[]tftypes.Value](stateObj, "replication_specs")
-	if specsVal == nil {
-		return
-	}
-	var specModels []TFReplicationSpecsModel
-	for _, specVal := range *specsVal {
-		var specObj map[string]tftypes.Value
-		if err := specVal.As(&specObj); err != nil {
-			continue
-		}
-		if specModel := replicationSpecModelWithNumShards(specObj["num_shards"]); specModel != nil {
-			specModels = append(specModels, *specModel)
-		}
-	}
-	if len(specModels) > 0 {
-		model.ReplicationSpecs, _ = types.ListValueFrom(ctx, replicationSpecsObjType, specModels)
-	}
-}
-
-func replicationSpecModelWithNumShards(numShardsVal tftypes.Value) *TFReplicationSpecsModel {
-	var numShardsFloat *big.Float
-	if err := numShardsVal.As(&numShardsFloat); err != nil || numShardsFloat == nil {
-		return nil
-	}
-	return &TFReplicationSpecsModel{
-		RegionConfigs: types.ListNull(regionConfigsObjType),
-		ZoneName:      types.StringNull(),
 	}
 }
