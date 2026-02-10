873f4ffa3f427b4dda2cdb1f05496e792100bc19
Manuel Pedrozo <19614462+manupedrozo@users.noreply.github.com>
2026-02-10 14:42:03 +0100

    Add api_spec_url param to autogen-update-api-spec (#4155)

--- scripts/generate-autogen-api-spec.sh
+++ scripts/generate-autogen-api-spec.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-SOURCE_SPEC_URL="${SOURCE_SPEC_URL:-https://raw.githubusercontent.com/mongodb/openapi/main/openapi/v2.yaml}"
+SOURCE_SPEC_URL="${1:-https://raw.githubusercontent.com/mongodb/openapi/main/openapi/v2.yaml}"
 SPEC_PATH="tools/codegen/atlasapispec/raw-multi-version-api-spec.yml"
 FLATTENED_SPEC_PATH="tools/codegen/atlasapispec/multi-version-api-spec.flattened.yml"
 
