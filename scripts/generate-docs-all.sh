5934d12cd121e5e6fb9d29614a0f2b2bdcf57070
maastha <122359335+maastha@users.noreply.github.com>
2026-02-26 07:24:42 +0000

    chore: Updates repository to use supported Terraform versions

--- scripts/generate-docs-all.sh
+++ scripts/generate-docs-all.sh
@@ -26,7 +26,7 @@
 
 set -euo pipefail
 
-TF_VERSION="${TF_VERSION:-"1.14.5"}" # TF version to use when running tfplugindocs. Default: 1.14.5
+TF_VERSION="${TF_VERSION:-"1.14.6"}" # TF version to use when running tfplugindocs. Default: 1.14.6
 TEMPLATE_FOLDER_PATH="${TEMPLATE_FOLDER_PATH:-"templates"}" # PATH to the templates folder. Default: templates
 
 trap 'rm -R docs-out/' EXIT # temp dir cleanup when script exits
