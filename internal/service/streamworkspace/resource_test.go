d1a62b18ce9206374c82d4abfafa0e5db10edce0
Oriol Arbusi Abadal <oriol.abadal@mongodb.com>
2026-03-10 13:37:07 +0100

    fixes stream_workspace max_tier_size

--- internal/service/streamworkspace/resource_test.go
+++ internal/service/streamworkspace/resource_test.go
@@ -102,7 +102,40 @@ func checkStreamsWorkspaceImportStateIDFunc(resourceName string) resource.Import
 	}
 }
 
+func TestAccStreamWorkspaceRS_maxTierSize(t *testing.T) {
+	var (
+		resourceName  = "mongodbatlas_stream_workspace.test"
+		projectID     = acc.ProjectIDExecution(t)
+		workspaceName = acc.RandomName()
+	)
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:                 func() { acc.PreCheckBasic(t) },
+		ProtoV6ProviderFactories: acc.TestAccProviderV6Factories,
+		CheckDestroy:             acc.CheckDestroyStreamInstance,
+		Steps: []resource.TestStep{
+			{
+				Config: streamsWorkspaceWithStreamConfigConfig(projectID, workspaceName, region, cloudProvider, "SP10", "SP30"),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					streamsWorkspaceAttributeChecks(resourceName, workspaceName, region, cloudProvider),
+					resource.TestCheckResourceAttr(resourceName, "stream_config.tier", "SP10"),
+					resource.TestCheckResourceAttr(resourceName, "stream_config.max_tier_size", "SP30"),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportStateIdFunc: checkStreamsWorkspaceImportStateIDFunc(resourceName),
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
 func streamsWorkspaceConfig(projectID, workspaceName, region, cloudProvider string) string {
+	return streamsWorkspaceWithStreamConfigConfig(projectID, workspaceName, region, cloudProvider, "SP10", "SP30")
+}
+
+func streamsWorkspaceWithStreamConfigConfig(projectID, workspaceName, region, cloudProvider, tier, maxTierSize string) string {
 	return fmt.Sprintf(`
 		resource "mongodbatlas_stream_workspace" "test" {
 			project_id = %[1]q
@@ -112,9 +145,9 @@ func streamsWorkspaceConfig(projectID, workspaceName, region, cloudProvider stri
 				cloud_provider = %[4]q
 			}
 			stream_config = {
-				max_tier_size = "SP30"
-				tier = "SP10"
+				tier = %[5]q
+				max_tier_size = %[6]q
 			}
 		}
-	`, projectID, workspaceName, region, cloudProvider)
+	`, projectID, workspaceName, region, cloudProvider, tier, maxTierSize)
 }
