9e91b1c1f72e504d57befc992634662d173a6652
Olivia Caulfield <olivia.caulfield@mongodb.com>
2026-03-27 16:06:04 -0400

    Fix: remove note from variables

--- examples/migrate_cluster_to_advanced_cluster/module_user/v1/variables.tf
+++ examples/migrate_cluster_to_advanced_cluster/module_user/v1/variables.tf
@@ -12,7 +12,7 @@ variable "atlas_client_secret" {
 
 # v1 & v2 variables
 variable "project_id" {
-  description = "Unique 24-hexadecimal digit string that identifies your project. Use the `/groups` at https://www.mongodb.com/docs/api/doc/atlas-admin-api-v2/operation/operation-listprojects endpoint to retrieve all projects to which the authenticated user has access.  **NOTE**: Groups and projects are synonymous terms. Your group id is the same as your project id. For existing groups, your group/project id remains the same. The resource and corresponding endpoints use the term groups"
+  description = "Unique 24-hexadecimal digit string that identifies your project. Use the `/groups` at https://www.mongodb.com/docs/api/doc/atlas-admin-api-v2/operation/operation-listprojects endpoint to retrieve all projects to which the authenticated user has access. It is also known as groupId in the official documentation."
   type        = string
 }
 
