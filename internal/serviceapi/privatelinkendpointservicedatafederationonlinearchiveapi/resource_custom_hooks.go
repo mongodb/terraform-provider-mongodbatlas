48c532b085e932574805308eb47d57817fad6de6
Mar Cabrera <mar.cabrera@mongodb.com>
2026-03-30 12:47:03 +0200

    Remove shared behaviour for custom hooks in order for it to leave on resource custom hooks file

--- internal/serviceapi/privatelinkendpointservicedatafederationonlinearchiveapi/resource_custom_hooks.go
+++ internal/serviceapi/privatelinkendpointservicedatafederationonlinearchiveapi/resource_custom_hooks.go
@@ -151,3 +151,15 @@ func prepareBody(bodyReq []byte) ([]byte, bool) {
 	}
 	return modifiedBody, true
 }
+
+func normalizeOptionalStringFields(obj map[string]any) {
+	setEmptyStringIfMissing(obj, "comment")
+	setEmptyStringIfMissing(obj, "region")
+	setEmptyStringIfMissing(obj, "customerEndpointDNSName")
+}
+
+func setEmptyStringIfMissing(obj map[string]any, responseKey string) {
+	if val, exists := obj[responseKey]; !exists || val == nil {
+		obj[responseKey] = ""
+	}
+}
