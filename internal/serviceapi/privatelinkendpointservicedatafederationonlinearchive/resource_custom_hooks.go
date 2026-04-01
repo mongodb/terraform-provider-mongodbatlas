3a1279f02eb7adf3091f42f68a5af9bfdd7d4d81
Mar Cabrera <mar.cabrera@mongodb.com>
2026-04-01 15:11:03 +0200

    Hardcode status field for CREATE request

--- internal/serviceapi/privatelinkendpointservicedatafederationonlinearchive/resource_custom_hooks.go
+++ internal/serviceapi/privatelinkendpointservicedatafederationonlinearchive/resource_custom_hooks.go
@@ -185,10 +185,13 @@ func prepareBody(bodyReq []byte) ([]byte, bool) {
 	}
 
 	body["type"] = endpointType
-	delete(body, "status") // strips status field being sent as null on CREATE, preventing API rejection. Remove once CLOUDP-391704 is released
 	if providerRaw, ok := body["provider"].(string); ok && providerRaw != "" {
 		providerUpper := strings.ToUpper(providerRaw)
 		body["provider"] = providerUpper
+		// Temporary workaround until CLOUDP-391704 is released.
+		if providerUpper == "AZURE" {
+			body["status"] = "PENDING"
+		}
 	}
 
 	modifiedBody, err := json.Marshal(body)
