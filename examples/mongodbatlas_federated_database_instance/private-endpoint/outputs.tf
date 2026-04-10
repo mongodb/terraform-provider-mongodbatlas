317a5d25f7ae521eae070e627e936bbff1573559
Mar Cabrera <mar.cabrera@mongodb.com>
2026-04-10 11:54:13 +0200

    Add more outputs

--- examples/mongodbatlas_federated_database_instance/private-endpoint/outputs.tf
+++ examples/mongodbatlas_federated_database_instance/private-endpoint/outputs.tf
@@ -2,3 +2,12 @@ output "private_endpoint_hostnames" {
   description = "Private endpoint hostnames assigned to the Federated Database Instance"
   value       = mongodbatlas_federated_database_instance.this.private_endpoint_hostnames
 }
+output "mongodbatlas_fdata_federation_instance_name" {
+  description = "Name of the MongoDB Atlas Federated Database Instance"
+  value       = mongodbatlas_federated_database_instance.this.name
+}
+
+output "vpc_endpoint_id" {
+  description = "ID of the AWS VPC endpoint"
+  value       = aws_vpc_endpoint.this.id
+}
