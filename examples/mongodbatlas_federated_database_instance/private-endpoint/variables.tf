0da4f9e6f0f80caf1147638376cd9a7ebf17e4a8
Mar Cabrera <mar.cabrera@mongodb.com>
2026-04-10 15:17:18 +0200

    Add default values for vars

--- examples/mongodbatlas_federated_database_instance/private-endpoint/variables.tf
+++ examples/mongodbatlas_federated_database_instance/private-endpoint/variables.tf
@@ -41,14 +41,17 @@ variable "vpce_service_name" {
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
+  default     = "10.0.0.0/16"
 }
 
 variable "subnet_cidr" {
   description = "CIDR block for the subnet"
   type        = string
+  default     = "10.0.1.0/24"
 }
 
 variable "availability_zone" {
   description = "Availability zone for the subnet"
   type        = string
+  default     = "us-east-1a"
 }
