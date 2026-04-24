0c3f53b48ab2a79badaaebbc5a399acd139e59d2
Leo Antoli <430982+lantoli@users.noreply.github.com>
2026-04-24 13:20:03 +0200

    clarify pending states

--- internal/service/searchindex/model_search_index.go
+++ internal/service/searchindex/model_search_index.go
@@ -155,6 +155,8 @@ func diffSuppressJSON(k, old, newStr string, d *schema.ResourceData) bool {
 	return schemafunc.EqualJSON(old, newStr, "vector search index")
 }
 
+// searchIndexPendingStates is shared across all operations to be inclusive,
+// e.g., a customer might destroy an index while IN_PROGRESS if wait_for_index_build_completion was not used.
 var searchIndexPendingStates = []string{"PENDING", "BUILDING", "IN_PROGRESS", "MIGRATING", "DELETING"}
 
 func resourceSearchIndexRefreshFunc(ctx context.Context, clusterName, projectID, indexID string, connV2 *admin.APIClient) retry.StateRefreshFunc {
