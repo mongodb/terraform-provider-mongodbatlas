4447634d32fd0a89f5ea3bc49fb3a8b7286720ee
Espen Albert <espen.albert@mongodb.com>
2026-04-30 09:25:54 +0100

    address PR comments

--- internal/service/advancedcluster/plan_modifier.go
+++ internal/service/advancedcluster/plan_modifier.go
@@ -22,6 +22,7 @@ var (
 		"custom_openssl_cipher_config_tls13": {"custom_openssl_cipher_config_tls12"},
 		"cluster_type":                       {"config_server_management_mode", "config_server_type"}, // computed values of config server change when REPLICA_SET changes to SHARDED
 		// Atlas may recompute oplog when storage or effective tier changes; do not copy stale oplog from state.
+		"disk_iops":     {"oplog_size_mb"},
 		"disk_size_gb":  {"oplog_size_mb"},
 		"instance_size": {"oplog_size_mb"},
 	}
