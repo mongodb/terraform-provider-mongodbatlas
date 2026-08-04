ea050f4b5e086915db46f0542b2c1242084ef520
Leo Antoli <430982+lantoli@users.noreply.github.com>
2026-08-04 11:49:48 +0200

    test: Disable database edition null PATCH steps

--- internal/service/advancedcluster/resource_test.go
+++ internal/service/advancedcluster/resource_test.go
@@ -90,10 +90,11 @@ func TestAccClusterAdvancedCluster_infinite(t *testing.T) {
 				Config:      configDatabaseEdition(projectID, clusterName, new("CORE"), 2),
 				ExpectError: regexp.MustCompile("databaseEdition cannot be changed"),
 			},
-			{
-				Config:      configDatabaseEdition(projectID, clusterName, nil, 2),
-				ExpectError: regexp.MustCompile("databaseEdition cannot be changed"),
-			},
+			// TODO: CLOUDP-430964, re-enable once Atlas correctly handles databaseEdition=null in PATCH.
+			// {
+			// 	Config:      configDatabaseEdition(projectID, clusterName, nil, 2),
+			// 	ExpectError: regexp.MustCompile("databaseEdition cannot be changed"),
+			// },
 		},
 	})
 }
@@ -115,11 +116,12 @@ func TestAccClusterAdvancedCluster_core(t *testing.T) {
 				Config:      configDatabaseEdition(projectID, clusterName, new("INFINITE"), 3),
 				ExpectError: regexp.MustCompile("databaseEdition cannot be changed"),
 			},
-			{
-				Config:            configDatabaseEdition(projectID, clusterName, nil, 3),
-				Check:             checkDatabaseEdition(nil, "CORE"),
-				ConfigStateChecks: pluralDatabaseEditionChecks(clusterName, nil, "CORE"),
-			},
+			// TODO: CLOUDP-430964, re-enable once Atlas correctly handles databaseEdition=null in PATCH.
+			// {
+			// 	Config:            configDatabaseEdition(projectID, clusterName, nil, 3),
+			// 	Check:             checkDatabaseEdition(nil, "CORE"),
+			// 	ConfigStateChecks: pluralDatabaseEditionChecks(clusterName, nil, "CORE"),
+			// },
 		},
 	})
 }
