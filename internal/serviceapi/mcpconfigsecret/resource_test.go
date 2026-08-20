2e99717dfefa311dded50e40a2da98f1b097c49d
Layla Abdelmaksoud <layla.abdelmaksoud@mongodb.com>
2026-08-21 00:25:13 +0100

    fix: address comments

--- internal/serviceapi/mcpconfigsecret/resource_test.go
+++ internal/serviceapi/mcpconfigsecret/resource_test.go
@@ -28,7 +28,7 @@ func TestAccMcpConfigSecret_basic(t *testing.T) {
 		CheckDestroy:             checkDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: configBasic(orgID, name, 720),
+				Config: configBasic(orgID, name, 720, false),
 				Check:  checkBasic(true),
 			},
 			{
@@ -38,6 +38,50 @@ func TestAccMcpConfigSecret_basic(t *testing.T) {
 				ImportState:                          true,
 				ImportStateVerify:                    true,
 				ImportStateVerifyIgnore:              []string{"secret_expires_after_hours", "secret", "masked_secret_value"},
+			},
+		},
+	})
+}
+
+func TestAccMcpConfigSecret_rotateWithTaint(t *testing.T) {
+	var (
+		orgID         = os.Getenv("MONGODB_ATLAS_ORG_ID")
+		name          = acc.RandomName()
+		firstSecretID string
+	)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:                 func() { acc.PreCheckBasic(t) },
+		ProtoV6ProviderFactories: acc.TestAccProviderV6Factories,
+		CheckDestroy:             checkDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: configBasic(orgID, name, 720, false),
+				Check: resource.ComposeTestCheckFunc(
+					checkBasic(true),
+					func(s *terraform.State) error {
+						return getSecretID(s, resourceName, &firstSecretID)
+					},
+				),
+			},
+			{
+				// The `taint` command is deprecated in favor of the `-replace` flag: https://developer.hashicorp.com/terraform/cli/commands/taint.
+				// The testing plugin does not facilitate testing with replace, but it does enable tainting so using taint here.
+				Taint:  []string{resourceName},
+				Config: configBasic(orgID, name, 720, false),
+				Check: resource.ComposeTestCheckFunc(
+					checkBasic(true),
+					func(s *terraform.State) error {
+						var secondSecretID string
+						if err := getSecretID(s, resourceName, &secondSecretID); err != nil {
+							return err
+						}
+						if secondSecretID == firstSecretID {
+							return fmt.Errorf("expected secret %s to be replaced but it still exists", firstSecretID)
+						}
+						return nil
+					},
+				),
 			},
 		},
 	})
@@ -55,9 +99,9 @@ func TestAccMcpConfigSecret_maxSecretsLimit(t *testing.T) {
 		CheckDestroy:             checkDestroy,
 		Steps: []resource.TestStep{
 			{
-				// A config's ingress SA allows a maximum of 2 concurrent secrets
-				// creating both here confirms rotation overlap is possible.
-				Config: configTwoSecrets(orgID, name, 720),
+				// A config's ingress SA allows a maximum of 2 concurrent secrets.
+				// Creating both here confirms rotation overlap is possible.
+				Config: configBasic(orgID, name, 720, true),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					checkExists(resourceName+"_1"),
 					checkExists(resourceName+"_2"),
@@ -67,7 +111,30 @@ func TestAccMcpConfigSecret_maxSecretsLimit(t *testing.T) {
 	})
 }
 
-func configBasic(orgID, name string, secretExpiresAfterHours int) string {
+func configBasic(orgID, name string, secretExpiresAfterHours int, twoSecrets bool) string {
+	if twoSecrets {
+		return fmt.Sprintf(`
+			resource "mongodbatlas_mcp_config" "test" {
+				org_id          = %[1]q
+				mcp_config_name = %[2]q
+				roles           = ["ORG_READ_ONLY"]
+			}
+
+			resource "mongodbatlas_mcp_config_secret" "test_1" {
+				org_id                     = %[1]q
+				mcp_config_id              = mongodbatlas_mcp_config.test.mcp_config_id
+				secret_expires_after_hours = %[3]d
+			}
+
+			resource "mongodbatlas_mcp_config_secret" "test_2" {
+				org_id                     = %[1]q
+				mcp_config_id              = mongodbatlas_mcp_config.test.mcp_config_id
+				secret_expires_after_hours = %[3]d
+
+				depends_on = [mongodbatlas_mcp_config_secret.test_1]
+			}
+		`, orgID, name, secretExpiresAfterHours)
+	}
 	return fmt.Sprintf(`
 		resource "mongodbatlas_mcp_config" "test" {
 			org_id          = %[1]q
@@ -95,30 +162,6 @@ func configBasic(orgID, name string, secretExpiresAfterHours int) string {
 	`, orgID, name, secretExpiresAfterHours)
 }
 
-func configTwoSecrets(orgID, name string, secretExpiresAfterHours int) string {
-	return fmt.Sprintf(`
-		resource "mongodbatlas_mcp_config" "test" {
-			org_id          = %[1]q
-			mcp_config_name = %[2]q
-			roles           = ["ORG_READ_ONLY"]
-		}
-
-		resource "mongodbatlas_mcp_config_secret" "test_1" {
-			org_id                     = %[1]q
-			mcp_config_id              = mongodbatlas_mcp_config.test.mcp_config_id
-			secret_expires_after_hours = %[3]d
-		}
-
-		resource "mongodbatlas_mcp_config_secret" "test_2" {
-			org_id                     = %[1]q
-			mcp_config_id              = mongodbatlas_mcp_config.test.mcp_config_id
-			secret_expires_after_hours = %[3]d
-
-			depends_on = [mongodbatlas_mcp_config_secret.test_1]
-		}
-	`, orgID, name, secretExpiresAfterHours)
-}
-
 func checkBasic(isCreate bool) resource.TestCheckFunc {
 	commonAttrsSet := []string{"secret_id", "created_at", "expires_at"}
 	checks := acc.CheckRSAndDS(resourceName, new(dataSourceName), nil, commonAttrsSet, nil, checkExists(resourceName))
@@ -131,6 +174,19 @@ func checkBasic(isCreate bool) resource.TestCheckFunc {
 	additionalChecks = acc.AddAttrSetChecksPrefix(dataSourcePluralName, additionalChecks, []string{"masked_secret_value"}, "results.0")
 
 	return resource.ComposeAggregateTestCheckFunc(checks, resource.ComposeAggregateTestCheckFunc(additionalChecks...))
+}
+
+func getSecretID(s *terraform.State, resourceName string, secretID *string) error {
+	rs, ok := s.RootModule().Resources[resourceName]
+	if !ok {
+		return fmt.Errorf("not found: %s", resourceName)
+	}
+	id := rs.Primary.Attributes["secret_id"]
+	if id == "" {
+		return fmt.Errorf("secret_id is empty")
+	}
+	*secretID = id
+	return nil
 }
 
 func checkExists(resourceName string) resource.TestCheckFunc {
