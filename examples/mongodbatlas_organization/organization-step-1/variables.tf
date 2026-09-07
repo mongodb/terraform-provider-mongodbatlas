807a5fe07bcb8703a63fd7c4e59e2e6559fa2240
Mar Cabrera <mar.cabrera@mongodb.com>
2026-09-07 19:42:54 +0200

    fix: Use block syntax for custom_session_timeouts in organization example

--- examples/mongodbatlas_organization/organization-step-1/variables.tf
+++ examples/mongodbatlas_organization/organization-step-1/variables.tf
@@ -22,12 +22,14 @@ variable "operations_contact" {
   description = "Email address, typically a distribution list, for the organization to receive proactive notifications about its infrastructure"
   default     = null
 }
-variable "custom_session_timeouts" {
-  type = list(object({
-    absolute_session_timeout_in_seconds = number
-    idle_session_timeout_in_seconds     = number
-  }))
-  description = "Custom session timeout settings for the organization"
+variable "absolute_session_timeout_in_seconds" {
+  type        = number
+  description = "Maximum session duration in seconds for the Atlas UI. Accepted values range between 3,600 (1 hour) and 43,200 (12 hours)"
+  default     = null
+}
+variable "idle_session_timeout_in_seconds" {
+  type        = number
+  description = "Maximum idle session duration in seconds for the Atlas UI. Accepted values start at 300 (5 minutes)"
   default     = null
 }
 
