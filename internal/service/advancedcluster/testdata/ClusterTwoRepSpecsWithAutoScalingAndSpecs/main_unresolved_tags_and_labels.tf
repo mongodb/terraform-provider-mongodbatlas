08f8c45c2f2014de3a9e1c492cfc9a8a76ca9799
Leo Antoli <430982+lantoli@users.noreply.github.com>
2026-09-09 21:47:16 +0200

    test: Trim the plan-check fixture to the attributes under test

--- internal/service/advancedcluster/testdata/ClusterTwoRepSpecsWithAutoScalingAndSpecs/main_unresolved_tags_and_labels.tf
+++ internal/service/advancedcluster/testdata/ClusterTwoRepSpecsWithAutoScalingAndSpecs/main_unresolved_tags_and_labels.tf
@@ -10,67 +10,26 @@ resource "mongodbatlas_advanced_cluster" "test" {
   tags   = terraform_data.unresolved.output
   labels = terraform_data.unresolved.output
 
-
   replication_specs = [{
     region_configs = [{
-      analytics_auto_scaling = {
-        compute_enabled            = true
-        compute_max_instance_size  = "M30"
-        compute_min_instance_size  = "M10"
-        compute_scale_down_enabled = true
-        disk_gb_enabled            = true
-      }
-      auto_scaling = {
-        compute_enabled            = true
-        compute_max_instance_size  = "M30"
-        compute_min_instance_size  = "M10"
-        compute_scale_down_enabled = true
-        disk_gb_enabled            = true
-      }
       electable_specs = {
         instance_size = "M10"
         node_count    = 5
       }
       priority      = 7
       provider_name = "AWS"
-      read_only_specs = {
-        instance_size = "M10"
-        node_count    = 2
-      }
-      region_name = "US_EAST_1"
+      region_name   = "US_EAST_1"
     }]
     zone_name = "Zone 1"
     }, {
     region_configs = [{
-      analytics_auto_scaling = {
-        compute_enabled            = true
-        compute_max_instance_size  = "M30"
-        compute_min_instance_size  = "M10"
-        compute_scale_down_enabled = true
-        disk_gb_enabled            = true
-      }
-      analytics_specs = {
-        instance_size = "M10"
-        node_count    = 4
-      }
-      auto_scaling = {
-        compute_enabled            = true
-        compute_max_instance_size  = "M30"
-        compute_min_instance_size  = "M10"
-        compute_scale_down_enabled = true
-        disk_gb_enabled            = true
-      }
       electable_specs = {
-        instance_size = "M10"
+        instance_size = "M20"
         node_count    = 3
       }
       priority      = 7
       provider_name = "AWS"
-      read_only_specs = {
-        instance_size = "M10"
-        node_count    = 1
-      }
-      region_name = "US_WEST_2"
+      region_name   = "US_WEST_2"
     }]
     zone_name = "Zone 2"
   }]
