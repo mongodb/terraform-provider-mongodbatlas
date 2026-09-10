15154a9118d78b3f0a7b46151539db4b7ec6d41e
Mar Cabrera <102970376+marcabreracast@users.noreply.github.com>
2026-09-10 19:06:37 +0200

    feat: Add custom session timeouts to mongodbatlas_organization (#4713)
    * Add field to resource schema
    
    * Add field to CRUD methods
    
    * Remove extra trailing space
    
    * Add acceptance tests
    
    * Add field to example
    
    * Add field to docs
    
    * Adapt variable in example
    
    * Add field to singular and plural data sources
    
    * Add docs for data sources
    
    * Rework tests
    
    * fix: Suppress perpetual plan diff for partial custom session timeouts
    
    * chore: Add changelog entry
    
    * fix: Use block syntax for custom_session_timeouts in organization example
    
    * test: Document why withOperationsContact avoids conversion.StringPtr
    
    * revert: Remove unneeded DiffSuppressFunc on custom_session_timeouts children
    
    * Add additional step to test only one attribute
    
    * Update documentation based on PR comments
    
    * Fix null values issue by introducing DiffSuppressFunc
    
    * refactor test for clarity
    
    * Pass schema config to test
    
    * Remove provider behaviour where setting block to [] sends null
    
    * Split Settings tests into different ones for readability
    
    * Re-org steps
    
    * Address PR comments

--- examples/mongodbatlas_organization/organization-step-1/variables.tf
+++ examples/mongodbatlas_organization/organization-step-1/variables.tf
@@ -22,6 +22,16 @@ variable "operations_contact" {
   description = "Email address, typically a distribution list, for the organization to receive proactive notifications about its infrastructure"
   default     = null
 }
+variable "absolute_session_timeout_in_seconds" {
+  type        = number
+  description = "Maximum session duration in seconds for the Atlas UI. Accepted values range between 3,600 (1 hour) and 43,200 (12 hours)"
+  default     = null
+}
+variable "idle_session_timeout_in_seconds" {
+  type        = number
+  description = "Maximum idle session duration in seconds for the Atlas UI. Accepted values start at 300 (5 minutes)"
+  default     = null
+}
 
 
 
